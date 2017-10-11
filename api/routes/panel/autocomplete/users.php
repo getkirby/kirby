e2b09aca16331b3d1d52cccbe229539addb92a65
Bastian Allgeier <mail@bastianallgeier.com>
2017-10-11 10:59:09 +0200

    Rough first implementation of the user autocomplete API

--- api/routes/panel/autocomplete/users.php
+++ api/routes/panel/autocomplete/users.php
@@ -3,6 +3,8 @@
 return [
     'pattern' => 'panel/autocomplete/users',
     'action'  => function () {
-
+        return array_values(array_map(function ($user) {
+            return (string)$user->email();
+        }, $this->users()->toArray()));
     }
 ];
