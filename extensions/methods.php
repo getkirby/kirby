f5b00ae566e79e80d0ccd5273dc42eaf2c74971b
Bastian Allgeier <mail@bastianallgeier.com>
2017-10-11 17:15:40 +0200

    More field methods

--- extensions/methods.php
+++ extensions/methods.php
@@ -8,22 +8,82 @@ use Kirby\Toolkit\Str;
  * Field method setup
  */
 Field::method([
-    'upper' => function () {
+
+    // states
+    'isEmpty' => function () {
+        return empty($this->value());
+    },
+    'isFalse' => function () {
+        return $this->toBool() === false;
+    },
+    'isNotEmpty' => function () {
+        return $this->isEmpty() === false;
+    },
+    'isTrue' => function () {
+        return $this->toBool() === true;
+    },
+
+    // converters
+    'toArray' => function ($separator) {
+        return $this->split($separator);
+    },
+    'toBool' => function ($default = false) {
+        $value = $this->isEmpty() ? $default : $this->value();
+        return filter_var($value, FILTER_VALIDATE_BOOLEAN);
+    },
+    'toExcerpt' => function () {
+
+    },
+    'toFile' => function () {
+
+    },
+    'toFloat' => function ($default = 0) {
+        $value = $this->isEmpty() ? $default : $this->value();
+        return floatval($value);
+    },
+    'toInt' => function ($default = 0) {
+        $value = $this->isEmpty() ? $default : $this->value();
+        return intval($value);
+    },
+    'toLink' => function () {
+
+    },
+    'toPage' => function () {
+
+    },
+    'toPages' => function () {
+
+    },
+    'toStructure' => function () {
+
+    },
+    'toUrl' => function () {
+
+    },
+
+    // inspectors
+    'length' => function () {
+        return Str::length($this->value());
+    },
+
+    // manipulators
+    'escape' => function () {
+
+    },
+    'html' => function (bool $keepTags = true) {
+        return $this;
+    },
+    'kirbytext' => function () {
         return $this->value(function($value) {
-            return Str::upper($value);
-        });
+            return App::instance()->kirbytext()->parse((string)$value);
+        })->markdown();
+    },
+    'kirbytags' => function () {
+
     },
     'lower' => function () {
         return $this->value(function($value) {
             return Str::lower($value);
-        });
-    },
-    'int' => function () {
-        return intval($this->value());
-    },
-    'smartypants' => function () {
-        return $this->value(function($value) {
-            return App::instance()->smartypants()->parse((string)$value);
         });
     },
     'markdown' => function () {
@@ -31,12 +91,44 @@ Field::method([
             return App::instance()->markdown()->parse((string)$value);
         });
     },
-    'kirbytext' => function () {
+    'or' => function ($fallback = null) {
+
+        if ($field->isNotEmpty()) {
+            return $this;
+        }
+
+        if (is_a($fallback, Field::class)) {
+            return $fallback;
+        }
+
+        return $this->value(function ($value) use ($fallback) {
+            return $fallback;
+        });
+
+    },
+    'short' => function () {
+
+    },
+    'smartypants' => function () {
         return $this->value(function($value) {
-            return App::instance()->kirbytext()->parse((string)$value);
-        })->markdown();
+            return App::instance()->smartypants()->parse((string)$value);
+        });
     },
     'split' => function ($separator = ',') {
         return Str::split((string)$this->value(), $separator);
+    },
+    'upper' => function () {
+        return $this->value(function($value) {
+            return Str::upper($value);
+        });
+    },
+    'widont' => function () {
+
+    },
+    'words' => function () {
+
+    },
+    'xml' => function () {
+
     }
 ]);
