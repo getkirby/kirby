1bd6b419c8f5a0a224b4718a34677233f161ce2a
Bastian Allgeier <mail@bastianallgeier.com>
2017-10-25 20:59:25 +0200

    Fix file delete api

--- api/routes/pages/files/delete.php
+++ api/routes/pages/files/delete.php
@@ -1,21 +1,11 @@
 <?php
 
-use Kirby\Cms\Assets\PageAssets;
-
 return [
     'pattern' => 'pages/(:all)/files/(:any)',
     'method'  => 'DELETE',
     'action'  => function ($path, $filename) {
-
         if ($file = $this->site()->file($path . '/' . $filename)) {
-
-            // delete the asset first
-            $assets = new PageAssets($this->app()->root('files'), $file->page());
-            $assets->delete($filename);
-
             return $file->delete();
-
         }
-
     }
 ];
