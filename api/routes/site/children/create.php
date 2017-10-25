231f62c7470ad13c69fd1b64d3d5ca89b18af9bd
Bastian Allgeier <mail@bastianallgeier.com>
2017-10-25 22:43:35 +0200

    Fix page creation for main pages

--- api/routes/site/children/create.php
+++ api/routes/site/children/create.php
@@ -1,18 +1,20 @@
 <?php
 
+use Kirby\Cms\Page;
+
 return [
     'pattern' => 'site/children',
     'method'  => 'POST',
     'action'  => function () {
 
-        $child = $this->site()->child([
-            'template' => $this->input('template'),
-            'slug'     => $this->input('slug'),
-            'content'  => $this->input('content')
-        ]);
+        $page = Page::create(
+            null,
+            $this->input('slug'),
+            $this->input('template'),
+            $this->input('content')
+        );
 
-        $child->save();
+        return $this->output('page', $page);
 
-        return $this->output('page', $child);
     }
 ];
