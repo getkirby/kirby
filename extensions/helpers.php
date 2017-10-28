e9f42769cd4476da35dd7fc6f8d69e7bb66b340b
Bastian Allgeier <mail@bastianallgeier.com>
2017-10-28 21:15:35 +0200

    Added go() helper function

--- extensions/helpers.php
+++ extensions/helpers.php
@@ -1,16 +1,42 @@
 <?php
 
 use Kirby\Cms\App;
+use Kirby\Http\Response\Redirect;
 use Kirby\Toolkit\View;
+
+
+function css($url)
+{
+    return '<link rel="stylesheet" href="' . url($url) . '">';
+}
+
+function go($url) {
+    die(new Redirect(url($url)));
+}
+
+function js($src)
+{
+    return '<script src="' . url($src) . '"></script>';
+}
 
 function kirby()
 {
     return App::instance();
 }
 
-function site()
+function kirbytag($input)
 {
-    return kirby()->site();
+    return kirby()->kirbytext()->tag($input);
+}
+
+function kirbytext($text)
+{
+    return kirby()->kirbytext()->parse($text);
+}
+
+function markdown($text)
+{
+    return kirby()->markdown()->parse($text);
 }
 
 function page(...$id)
@@ -23,9 +49,27 @@ function pages(...$id)
     return site()->find(...$id);
 }
 
-function url($path = null)
+/**
+ * Smart version of return with an if condition as first argument
+ *
+ * @param mixed $condition
+ * @param mixed $value The string to be returned if the condition is true
+ * @param mixed $alternative An alternative string which should be returned when the condition is false
+ * @return null
+ */
+function r($condition, $value, $alternative = null)
 {
-    return rtrim(kirby()->url() . '/' . $path, '/');
+    return $condition ? $value : $alternative;
+}
+
+function site()
+{
+    return kirby()->site();
+}
+
+function smartypants($text)
+{
+    return kirby()->smartypants()->parse($text);
 }
 
 /**
@@ -47,51 +91,13 @@ function snippet($name, $data = [], $return = false)
     echo $snippet;
 }
 
-function css($url)
-{
-    return '<link rel="stylesheet" href="' . url($url) . '">';
-}
-
-function js($src)
-{
-    return '<script src="' . url($src) . '"></script>';
-}
-
-/**
- * Smart version of return with an if condition as first argument
- *
- * @param mixed $condition
- * @param mixed $value The string to be returned if the condition is true
- * @param mixed $alternative An alternative string which should be returned when the condition is false
- * @return null
- */
-function r($condition, $value, $alternative = null)
-{
-    return $condition ? $value : $alternative;
-}
-
-function markdown($text)
-{
-    return kirby()->markdown()->parse($text);
-}
-
-function smartypants($text) {
-    return kirby()->smartypants()->parse($text);
-}
-
-function kirbytext($text)
-{
-    return kirby()->kirbytext()->parse($text);
-}
-
-function kirbytag($input)
-{
-    return kirby()->kirbytext()->tag($input);
-}
-
 function svg($root)
 {
     require kirby()->root() . '/' . $root;
 }
 
+function url($path = null)
+{
+    return rtrim(kirby()->url() . '/' . $path, '/');
+}
 
