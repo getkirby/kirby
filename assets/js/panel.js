0f25644103b6960d69fd25156e15b795409b0e7b
Bastian Allgeier <mail@bastianallgeier.com>
2017-11-24 13:49:51 +0100

    Custom section example

--- assets/js/panel.js
+++ assets/js/panel.js
@@ -42,3 +42,50 @@ panel.field('range', {
     </kirby-field>
   `,
 });
+
+
+// Custom Section
+panel.section('test', {
+  props: {
+    config: Object
+  },
+  data () {
+    return {
+      message: this.config.message
+    }
+  },
+  methods: {
+    submit () {
+      this.$refs.dialog.close();
+    }
+  },
+  computed: {
+    fields () {
+      return [
+        {
+          label: 'Message',
+          name: 'message',
+          type: 'text'
+        }
+      ];
+    }
+  },
+  template: `
+    <section>
+      <kirby-headline>
+        {{ config.headline }}
+
+        <kirby-button-group slot="options">
+          <kirby-button @click="$refs.dialog.open()" icon="cog"></kirby-button>
+        </kirby-button-group>
+      </kirby-headline>
+      <kirby-box @click="$refs.dialog.open()">
+        {{ message ? message : 'Please change me …' }}
+      </kirby-box>
+
+      <kirby-dialog ref="dialog" button="Ok" @submit="submit">
+        <kirby-form :fields="fields" :values="$data"></kirby-form>
+      <kirby-dialog>
+    </section>
+  `
+});
