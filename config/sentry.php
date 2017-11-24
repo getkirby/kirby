890f8228cc343c7d62c9a1518fc4ae430481430e
Nico Hoffmann <on-github@nhoffmann.com>
2017-11-24 16:04:24 +0100

    Add PHP version to Sentry

--- config/sentry.php
+++ config/sentry.php
@@ -7,6 +7,9 @@ if (true) {
      * Sentry Error Logging
      */
     $client = new Raven_Client('https://633c43e3b08940f087e618bd19e30722:5a9a7260a4164e56b2a8eacbb7a91e46@sentry.io/230268');
+    $client->tags_context([
+        'php_version' => phpversion()
+    ]);
 
     $errorHandler = new Raven_ErrorHandler($client);
     $errorHandler->registerExceptionHandler();
