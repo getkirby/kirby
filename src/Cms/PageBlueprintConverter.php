55c6189afffa54812fb59126ce8d93d9bea7fe3f
Bastian Allgeier <mail@bastianallgeier.com>
2017-11-25 14:34:46 +0100

    Fix missing title attribute in blueprint converter

--- src/Cms/PageBlueprintConverter.php
+++ src/Cms/PageBlueprintConverter.php
@@ -11,7 +11,7 @@ class PageBlueprintConverter
     {
 
         $this->data = [
-            'title'  => $blueprint['title'],
+            'title'  => $blueprint['title'] ?? $blueprint['name'],
             'name'   => $blueprint['name'],
             'layout' => [
                 [
