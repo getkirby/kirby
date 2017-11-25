2dc8b6746a3595f350e291b924c3b1e1699b3822
Bastian Allgeier <mail@bastianallgeier.com>
2017-11-25 14:42:44 +0100

    Fix #29

--- api/routes/panel/languages/read.php
+++ api/routes/panel/languages/read.php
@@ -6,10 +6,17 @@ return [
     'pattern' => 'panel/languages/(:any)',
     'action'  => function ($locale) {
 
-        $dir             = $this->app()->root('panel') . '/assets/languages/' . $locale;
-        $json            = Data::read($dir . '/package.json');
+        $dir     = $this->app()->root('panel') . '/assets/languages/' . $locale;
+        $package = $dir . '/package.json';
+        $core    = $dir . '/core.json';
+
+        if (is_dir($dir) === false || is_file($package) === false || is_file($core) === false) {
+            return null;
+        }
+
+        $json            = Data::read($package);
+        $json['strings'] = Data::read($core);
         $json['locale']  = $locale;
-        $json['strings'] = Data::read($dir . '/core.json');
 
         return $this->output('panel/language', $json);
 
