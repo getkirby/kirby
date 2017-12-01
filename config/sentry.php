029fc15df76f706311adb86b1bf66fd4badf86f5
Nico Hoffmann <on-github@nhoffmann.com>
2017-12-01 10:50:37 +0100

    Add version to Sentry

--- config/sentry.php
+++ config/sentry.php
@@ -8,7 +8,8 @@ if (true) {
      */
     $client = new Raven_Client('https://633c43e3b08940f087e618bd19e30722:5a9a7260a4164e56b2a8eacbb7a91e46@sentry.io/230268');
     $client->tags_context([
-        'php_version' => phpversion()
+        'php_version' => phpversion(),
+        'kirby' => json_decode(file_get_contents(__DIR__ . '/../composer.json'), true)['version']
     ]);
 
     $errorHandler = new Raven_ErrorHandler($client);
