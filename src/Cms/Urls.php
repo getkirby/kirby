18c93a99a1fbd59063122d497afdaa558a63143b
Bastian Allgeier <mail@bastianallgeier.com>
2017-12-05 19:25:42 +0100

    Add Url class test and docblocks

--- src/Cms/Urls.php
+++ src/Cms/Urls.php
@@ -2,12 +2,26 @@
 
 namespace Kirby\Cms;
 
+/**
+ * Registry for all system-relevant Urls
+ *
+ * @package   Kirby Cms
+ * @author    Bastian Allgeier <bastian@getkirby.com>
+ * @link      http://getkirby.com
+ * @copyright Bastian Allgeier
+ */
 class Urls extends Object
 {
 
+    /**
+     * Creates the Urls registry.
+     * Urls can be overwritten or added
+     * with the $props variable.
+     *
+     * @param array $props
+     */
     public function __construct(array $props = [])
     {
-
         parent::__construct($props, [
             'index' => [
                 'type'    => 'string',
@@ -34,9 +48,16 @@ class Urls extends Object
                 }
             ]
         ]);
-
     }
 
+    /**
+     * Get a specific Url by key
+     * If no key is specified the index Url
+     * will be returned
+     *
+     * @param  string $key
+     * @return string|null
+     */
     public function get(string $key = 'index')
     {
         if ($key === '/') {
@@ -46,6 +67,13 @@ class Urls extends Object
         return $this->prop($key);
     }
 
+    /**
+     * Magic caller to fetch Urls by method call
+     *
+     * @param  string $method
+     * @param  array $arguments
+     * @return string|null
+     */
     public function __call(string $method, array $arguments = [])
     {
         return $this->get($method);
---
+++ tests/Cms/UrlsTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Kirby\Cms;
+
+class UrlsTest extends TestCase
+{
+
+    public function defaultUrlProvider(): array
+    {
+        return [
+            ['/',      'index'],
+            ['/',      '/'],
+            ['/media', 'media'],
+            ['/panel', 'panel'],
+            ['/api',   'api'],
+        ];
+    }
+
+    /**
+     * @dataProvider defaultUrlProvider
+     */
+    public function testDefaulUrl($url, $method)
+    {
+        $urls = new Urls();
+
+        $this->assertEquals($url, $urls->$method());
+        $this->assertEquals($url, $urls->get($method));
+    }
+
+    public function customBaseUrlProvider(): array
+    {
+        return [
+            ['https://getkirby.com',       'index'],
+            ['https://getkirby.com',       '/'],
+            ['https://getkirby.com/media', 'media'],
+            ['https://getkirby.com/panel', 'panel'],
+            ['https://getkirby.com/api',   'api'],
+        ];
+    }
+
+    /**
+     * @dataProvider customBaseUrlProvider
+     */
+    public function testWithCustomBaseUrl($url, $method)
+    {
+        $urls = new Urls([
+            'index' => 'https://getkirby.com'
+        ]);
+
+        $this->assertEquals($url, $urls->$method());
+        $this->assertEquals($url, $urls->get($method));
+    }
+
+    public function customUrlProvider(): array
+    {
+        return [
+            ['https://getkirby.com',       'index'],
+            ['https://getkirby.com',       '/'],
+            ['https://cdn.getkirby.com',   'media'],
+            ['https://getkirby.com/admin', 'panel'],
+            ['https://getkirby.com/rest',  'api'],
+        ];
+    }
+
+    /**
+     * @dataProvider customUrlProvider
+     */
+    public function testWithCustomUrl($url, $method)
+    {
+
+        $urls = new Urls([
+            'index' => 'https://getkirby.com',
+            'media' => 'https://cdn.getkirby.com',
+            'panel' => 'https://getkirby.com/admin',
+            'api'   => 'https://getkirby.com/rest'
+        ]);
+
+        $this->assertEquals($url, $urls->$method());
+        $this->assertEquals($url, $urls->get($method));
+    }
+
+
+}
