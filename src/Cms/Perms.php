23cdc0900e4fe0484f9229e729dd93149ffd6eeb
Bastian Allgeier <mail@bastianallgeier.com>
2017-12-06 17:30:05 +0100

    Perms class tests and docblocks

--- src/Cms/Perms.php
+++ src/Cms/Perms.php
@@ -2,7 +2,14 @@
 
 namespace Kirby\Cms;
 
-class Perms extends Rules
-{
-
-}
+/**
+ * The permissions class registeres
+ * permission rules which are checked
+ * before a store action is being executed
+ *
+ * @package   Kirby Cms
+ * @author    Bastian Allgeier <bastian@getkirby.com>
+ * @link      http://getkirby.com
+ * @copyright Bastian Allgeier
+ */
+class Perms extends Rules {}
