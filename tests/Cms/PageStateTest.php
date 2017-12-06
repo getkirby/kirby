effd8effbc48554b1422dab3626227222dd5c66a
Bastian Allgeier <mail@bastianallgeier.com>
2017-12-06 12:43:24 +0100

    Complete Page State Tests

--- tests/Cms/PageStateTest.php
+++ tests/Cms/PageStateTest.php
@@ -41,4 +41,51 @@ class PageStateTest extends TestCase
         $this->assertFalse($pageA->$method());
     }
 
+    public function testIsOpen()
+    {
+        $parent = new Page(['id' => 'test']);
+        $child  = new Page(['id' => 'test/child', 'parent' => $parent]);
+        $site   = new Site(['page' => $child]);
+
+        Page::use('site', $site);
+
+        $this->assertTrue($parent->isOpen());
+        $this->assertTrue($child->isOpen());
+    }
+
+    public function testIsNotOpen()
+    {
+        $parent = new Page(['id' => 'test']);
+        $child  = new Page(['id' => 'test/child', 'parent' => $parent]);
+        $active = new Page(['id' => 'active']);
+        $site   = new Site(['page' => $active]);
+
+        Page::use('site', $site);
+
+        $this->assertFalse($parent->isOpen());
+        $this->assertFalse($child->isOpen());
+    }
+
+    public function testIsVisible()
+    {
+        $page = new Page(['id' => 'test']);
+
+        $this->assertFalse($page->isVisible());
+
+        $page = new Page(['id' => 'test', 'num' => 1]);
+
+        $this->assertTrue($page->isVisible());
+    }
+
+    public function testIsInvisible()
+    {
+        $page = new Page(['id' => 'test']);
+
+        $this->assertTrue($page->isInvisible());
+
+        $page = new Page(['id' => 'test', 'num' => 1]);
+
+        $this->assertFalse($page->isInvisible());
+    }
+
 }
