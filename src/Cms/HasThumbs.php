381eba2d0e806c4952a6a99daf85f48a7341b852
Bastian Allgeier <mail@bastianallgeier.com>
2017-12-08 10:58:43 +0100

    Crop position

--- src/Cms/HasThumbs.php
+++ src/Cms/HasThumbs.php
@@ -5,13 +5,25 @@ namespace Kirby\Cms;
 trait HasThumbs
 {
 
-    public function crop(int $width, int $height = null, int $quality = null)
+    public function crop(int $width, int $height = null, $options = null)
     {
+        $quality = null;
+        $crop    = 'center';
+
+        if (is_int($options) === true) {
+            $quality = $options;
+        } elseif (is_string($options)) {
+            $crop = $options;
+        } elseif (is_array($options)) {
+            $quality = $options['quality'] ?? $quality;
+            $crop    = $options['crop']    ?? $crop;
+        }
+
         return $this->thumb([
             'width'   => $width,
             'height'  => $height,
             'quality' => $quality,
-            'crop'    => 'center'
+            'crop'    => $crop
         ]);
     }
 
