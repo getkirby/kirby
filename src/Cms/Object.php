3f140af1c514044bab7759df5ed9aeb68b41f4c5
Bastian Allgeier <mail@bastianallgeier.com>
2017-12-08 10:58:19 +0100

    Fix caller priorities in Object

--- src/Cms/Object.php
+++ src/Cms/Object.php
@@ -218,12 +218,12 @@ class Object
      */
     public function __call(string $method, array $arguments = [])
     {
-        if ($this->hasPlugin($method)) {
-            return $this->plugin($method, $arguments);
-        }
-
         if ($this->hasProp($method)) {
             return $this->prop($method, $arguments);
+        }
+
+        if ($this->hasPlugin($method)) {
+            return $this->plugin($method, $arguments);
         }
 
         return null;
