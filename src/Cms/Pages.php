061d6b534b51c0c9a304990dc8805dacbeaed930
Bastian Allgeier <mail@bastianallgeier.com>
2017-12-11 23:50:50 +0100

    Pages collection class tests and docblocks

--- src/Cms/Pages.php
+++ src/Cms/Pages.php
@@ -4,24 +4,71 @@ namespace Kirby\Cms;
 
 use Exception;
 
+/**
+ * The Pages collection contains
+ * any number and mixture of page objects
+ * They don't necessarily have to belong
+ * to the same parent in comparison to
+ * the Children collection. The Children
+ * collection is based on the Pages
+ * collection though.
+ *
+ * Pages collection can be constructed very
+ * easily:
+ *
+ * ```php
+ * $collection = new Pages([
+ *   new Page(['id' => 'project-a']),
+ *   new Page(['id' => 'project-b']),
+ *   new Page(['id' => 'project-c']),
+ * ]);
+ * ```
+ *
+ * @package   Kirby Cms
+ * @author    Bastian Allgeier <bastian@getkirby.com>
+ * @link      http://getkirby.com
+ * @copyright Bastian Allgeier
+ */
 class Pages extends Collection
 {
 
+    /**
+     * Only accepts Page objects
+     *
+     * @var string
+     */
     protected static $accept = Page::class;
 
+    /**
+     * Initialize the PagesFinder class,
+     * which is handling findBy and find
+     * methods
+     *
+     * @return PagesFinder
+     */
     protected function finder()
     {
         return new PagesFinder($this);
     }
 
-    public function visible(): self
-    {
-        return $this->filterBy('isVisible', '==', true);
-    }
-
+    /**
+     * Returns all invisible pages in the collection
+     *
+     * @return self
+     */
     public function invisible(): self
     {
         return $this->filterBy('isVisible', '==', false);
+    }
+
+    /**
+     * Returns all visible pages in the collection
+     *
+     * @return self
+     */
+    public function visible(): self
+    {
+        return $this->filterBy('isVisible', '==', true);
     }
 
 }
---
+++ tests/Cms/PagesTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Kirby\Cms;
+
+class PagesTest extends TestCase
+{
+
+    protected function pages()
+    {
+        return new Pages([
+            new Page(['id' => 'a', 'num' => 1]),
+            new Page(['id' => 'b', 'num' => 2]),
+            new Page(['id' => 'c'])
+        ]);
+    }
+
+    public function testFind()
+    {
+        $this->assertIsPage($this->pages()->find('a'), 'a');
+        $this->assertIsPage($this->pages()->find('b'), 'b');
+        $this->assertIsPage($this->pages()->find('c'), 'c');
+    }
+
+    public function testInvisible()
+    {
+        $this->assertCount(1, $this->pages()->invisible());
+    }
+
+    public function testVisible()
+    {
+        $this->assertCount(2, $this->pages()->visible());
+    }
+
+}
