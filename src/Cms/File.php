5a2f0d461e6883d6a072584449915144438550a8
Bastian Allgeier <mail@bastianallgeier.com>
2017-12-11 17:21:21 +0100

    File class tests and docblocks

--- src/Cms/File.php
+++ src/Cms/File.php
@@ -5,15 +5,32 @@ namespace Kirby\Cms;
 use Exception;
 use Kirby\Image\Image;
 
+/**
+ * The File class is a wrapper around
+ * the Kirby\Image\Image class, which
+ * is used to handle all file methods.
+ * In addition the File class handles
+ * File meta data via Kirby\Cms\Content.
+ *
+ * @package   Kirby Cms
+ * @author    Bastian Allgeier <bastian@getkirby.com>
+ * @link      http://getkirby.com
+ * @copyright Bastian Allgeier
+ */
 class File extends Object
 {
 
     use HasSiblings;
     use HasThumbs;
 
+    /**
+     * Creates a new File object
+     * Required props are `id`, `url` and `root`
+     *
+     * @param array $props
+     */
     public function __construct(array $props = [])
     {
-
         parent::__construct($props, [
             'asset' => [
                 'type' => Image::class,
@@ -46,6 +63,12 @@ class File extends Object
                 'required' => true,
                 'type'     => 'string',
             ],
+            'store' => [
+                'type'    => Store::class,
+                'default' => function () {
+                    return $this->plugin('store');
+                }
+            ],
             'url' => [
                 'required' => true,
                 'type'     => 'string'
@@ -54,9 +77,18 @@ class File extends Object
                 'type' => File::class,
             ]
         ]);
-
     }
 
+    /**
+     * Clones a File object and throws out
+     * all stuff that is too specific to provide
+     * a clean slate. You can pass additional
+     * props that will be merged with the existing
+     * props with the `$props` argument.
+     *
+     * @param array $props
+     * @return self
+     */
     public function clone(array $props = []): self
     {
         return new static(array_merge([
@@ -67,11 +99,29 @@ class File extends Object
         ], $props));
     }
 
+    /**
+     * Creates a new file on disk and returns the
+     * File object. The store is used to handle file
+     * writing, so it can be replaced by any other
+     * way of generating files.
+     *
+     * @param Page $parent
+     * @param string $source
+     * @param string $filename
+     * @param array $content
+     * @return self
+     */
     public static function create(Page $parent = null, string $source, string $filename, array $content = []): self
     {
         return static::store()->commit('file.create', $parent, $source, $filename, $content);
     }
 
+    /**
+     * Deletes the file. The store is used to
+     * manipulate the filesystem or whatever you prefer.
+     *
+     * @return bool
+     */
     public function delete(): bool
     {
         $this->rules()->check('file.delete', $this);
@@ -80,16 +130,37 @@ class File extends Object
         return $this->store()->commit('file.delete', $this);
     }
 
+    /**
+     * Alias for the old way of fetching File
+     * content. Nowadays `File::content()` should
+     * be used instead.
+     *
+     * @return Content
+     */
     public function meta()
     {
         return $this->content();
     }
 
+    /**
+     * Returns the parent model.
+     * This is normally the parent page
+     * or the site object.
+     *
+     * @return Site|Page
+     */
     public function model()
     {
         return is_a($this->page(), Page::class) ? $this->page() : $this->site();
     }
 
+    /**
+     * Renames the file without touching the extension
+     * The store is used to actually execute this.
+     *
+     * @param string $name
+     * @return self
+     */
     public function rename(string $name): self
     {
         $this->rules()->check('file.rename', $this, $name);
@@ -98,11 +169,30 @@ class File extends Object
         return $this->store()->commit('file.rename', $this, $name);
     }
 
+    /**
+     * Replaces the file. The source must
+     * be an absolute path to a file or a Url.
+     * The store handles the replacement so it
+     * finally decides what it will support as
+     * source.
+     *
+     * @param string $source
+     * @return self
+     */
     public function replace(string $source): self
     {
         return $this->store()->commit('file.replace', $this, $source);
     }
 
+    /**
+     * Updates the file content.
+     * The store writes content into text
+     * files or any other place you determin in
+     * custom store implementations.
+     *
+     * @param array $content
+     * @return self
+     */
     public function update(array $content = []): self
     {
         $this->rules()->check('file.update', $this, $content);
@@ -111,11 +201,28 @@ class File extends Object
         return $this->store()->commit('file.update', $this, $content);
     }
 
+    /**
+     * Returns the parent Site object if available
+     *
+     * @return Site|null
+     */
     public function site()
     {
         return $this->plugin('site');
     }
 
+    /**
+     * Creates a modified version of images
+     * The media manager takes care of generating
+     * those modified versions and putting them
+     * in the right place. This is normally the
+     * /media folder of your installation, but
+     * could potentially also be a CDN or any other
+     * place.
+     *
+     * @param array $options
+     * @return self
+     */
     public function thumb(array $options = []): self
     {
         try {
@@ -129,14 +236,22 @@ class File extends Object
         }
     }
 
+    /**
+     * Magic caller for props, plugins, file methods
+     * and content fields. (in this order)
+     *
+     * @param string $method
+     * @param array $arguments
+     * @return mixed
+     */
     public function __call(string $method, array $arguments = [])
     {
-        if ($this->hasPlugin($method)) {
-            return $this->plugin($method, $arguments);
-        }
-
         if ($this->hasProp($method)) {
             return $this->prop($method, $arguments);
+        }
+
+        if ($this->hasPlugin($method)) {
+            return $this->plugin($method, $arguments);
         }
 
         if (method_exists($this->asset(), $method)) {
@@ -145,6 +260,5 @@ class File extends Object
 
         return $this->content()->get($method, $arguments);
     }
-
 
 }
---
+++ tests/Cms/FilePropsTest.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace Kirby\Cms;
+
+use Kirby\Image\Image;
+
+class FilePropsTest extends TestCase
+{
+
+    protected function defaults(): array
+    {
+        return [
+            'id'   => 'projects/project-a/cover.jpg',
+            'root' => '/var/www/content/projects/project-a/cover.jpg',
+            'url'  => 'https://getkirby.com/projects/project-a/cover.jpg'
+        ];
+    }
+
+    protected function file(array $props = [])
+    {
+        return new File(array_merge($this->defaults(), $props));
+    }
+
+    public function testAsset()
+    {
+        $file = $this->file([
+            'asset' => $asset = new Image('/test/cover.jpg', 'https://cdn.getkirby.com/test/cover.jpg')
+        ]);
+
+        $this->assertEquals($asset, $file->asset());
+        $this->assertEquals($asset->root(), $file->asset()->root());
+        $this->assertEquals($asset->url(), $file->asset()->url());
+    }
+
+    public function testDefaultAsset()
+    {
+        $file = $this->file();
+
+        $this->assertInstanceOf(Image::class, $file->asset());
+        $this->assertEquals($file->root(), $file->asset()->root());
+        $this->assertEquals($file->url(), $file->asset()->url());
+    }
+
+    public function testCollection()
+    {
+        $file = $this->file([
+            'collection' => $files = new Files()
+        ]);
+
+        $this->assertEquals($files, $file->collection());
+    }
+
+    public function testDefaultCollection()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testContent()
+    {
+        $file = $this->file([
+            'content' => $content = new Content()
+        ]);
+
+        $this->assertEquals($content, $file->content());
+    }
+
+    public function testDefaultContent()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testId()
+    {
+        $this->assertEquals($this->defaults()['id'], $this->file()->id());
+    }
+
+    public function testMissingId()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testPage()
+    {
+        $file = $this->file([
+            'page' => $page = new Page(['id' => 'test'])
+        ]);
+
+        $this->assertEquals($page, $file->page());
+    }
+
+    public function testDefaultPage()
+    {
+        return $this->assertNull($this->file()->page());
+    }
+
+    public function testRoot()
+    {
+        $this->assertEquals($this->defaults()['root'], $this->file()->root());
+    }
+
+    public function testMissingRoot()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testUrl()
+    {
+        $this->assertEquals($this->defaults()['url'], $this->file()->url());
+    }
+
+    public function testMissingUrl()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testOriginal()
+    {
+        $file = $this->file([
+            'original' => $original = new File([
+                'id'   => 'test',
+                'root' => 'test.jpg',
+                'url'  => 'test.jpg'
+            ])
+        ]);
+
+        $this->assertEquals($original, $file->original());
+    }
+
+    public function testDefaultOriginal()
+    {
+        return $this->assertNull($this->file()->original());
+    }
+
+}
