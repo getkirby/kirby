58aa3d15e4a01eb40578289dcd1321626d54e341
Bastian Allgeier <mail@bastianallgeier.com>
2018-01-04 11:15:04 +0100

    New Schema utility class and tests

---
+++ src/Util/Schema.php
@@ -0,0 +1,274 @@
+<?php
+
+namespace Kirby\Util;
+
+use Closure;
+use Exception;
+
+/**
+ * The Schema class can be used
+ * to create reliable prop/attribute
+ * schemas including type checks and validation
+ * for any kind of purpose.
+ *
+ * @package   Kirby Util
+ * @author    Bastian Allgeier <bastian@getkirby.com>
+ * @link      http://getkirby.com
+ * @copyright Bastian Allgeier
+ * @license   MIT
+ */
+class Schema
+{
+
+    /**
+     * Holds the Schema definition
+     *
+     * @var array
+     */
+    protected $schema;
+
+    /**
+     * Creates a new Schema object
+     *
+     * @param array $schema
+     */
+    public function __construct(array $schema)
+    {
+        $this->schema = $schema;
+    }
+
+    /**
+     * Validates the value and returns true
+     * or false. It's basically the same as
+     * Schema::validate, but doesn't throw
+     * Exceptions on errors.
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return bool
+     */
+    public function accepts(string $key, $value): bool
+    {
+        try {
+            return $this->validate($key, $value);
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+
+    /**
+     * Plucks all fields defined in the schema
+     * from the given $data array and returns
+     * the result. Every field that is not included
+     * in the $data array, but defined in the schema
+     * will be set to null.
+     *
+     * @param array $data
+     * @return array
+     */
+    public function pluck(array $data): array
+    {
+        $result = [];
+
+        foreach ($this->schema as $key => $definition) {
+            $result[$key] = $data[$key] ?? null;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Returns the Schema definition
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return $this->schema;
+    }
+
+    /**
+     * Throws an Exception with info about the
+     * failed prop type validation and the current class
+     *
+     * @param string $key
+     * @param string $value
+     * @return Exception
+     */
+    protected function typeError(string $key, $value): Exception
+    {
+        if (isset($this->schema[$key]) === false) {
+            return true;
+        }
+
+        $expectedType = $this->schema[$key]['type'] ?? null;
+        $valueType    = gettype($value);
+
+        if ($expectedType === null) {
+            return true;
+        }
+
+        if ($valueType === 'object') {
+            $valueType = get_class($value);
+        }
+
+        return new Exception(sprintf('"%s" must be of type "%s" not "%s"', $key, $expectedType, $valueType));
+    }
+
+    /**
+     * Validates a given value by the rules
+     * set in the schema. It validates by
+     * the given type and an optional custom
+     * validate rule. This will throw Exceptions
+     * on errors. Use the Schema::accepts method
+     * to get true or false as results.
+     *
+     * @param string|array $key
+     * @param mixed $value
+     * @return bool
+     */
+    public function validate($key, $value = null): bool
+    {
+        if (is_array($key) === true) {
+
+            // pluck an array with all fields
+            // in the schema, to also make sure
+            // we validate missing required fields
+            // correctly.
+            $data = $this->pluck($key);
+
+            foreach ($data as $key => $value) {
+                $this->validate($key, $value);
+            }
+
+            return true;
+        }
+
+        // get the schema definition for this field
+        $schema = $this->schema[$key] ?? [];
+
+        // skip fields without schema
+        if (empty($schema) === true) {
+            return true;
+        }
+
+        // check required fields
+        if ($this->validateRequired($value, $schema['required'] ?? false) === false) {
+            throw new Exception(sprintf('"%s" is required and must not be null', $key));
+        }
+
+        // skip validation if the field is empty
+        if ($value === null) {
+            return true;
+        }
+
+        if ($this->validateType($value, $schema['type'] ?? null) === false) {
+            throw $this->typeError($key, $value);
+        }
+
+        if ($this->validateCustom($value, $schema['validate'] ?? null) === false) {
+            throw new Exception(sprintf('Validation for "%s" failed', $key));
+        }
+
+        return true;
+    }
+
+    /**
+     * Custom function validation used in Schema::validate
+     *
+     * @param mixed $value
+     * @param Closure $function
+     * @return bool
+     */
+    protected function validateCustom($value, Closure $function = null): bool
+    {
+        if (is_a($function, Closure::class) === false) {
+            return true;
+        }
+
+        return $function($value);
+    }
+
+    /**
+     * Used in Schema::validate to validate numeric values
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    protected function validateNumber($value): bool
+    {
+        return is_string($value) === false && is_numeric($value) === true;
+    }
+
+    /**
+     * Used in Schema::validate to validate objects
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    protected function validateObject($value): bool
+    {
+        return is_object($value) === true;
+    }
+
+    /**
+     * Validates required values
+     *
+     * @param mixed $value
+     * @param bool $required
+     * @return bool
+     */
+    protected function validateRequired($value, bool $required): bool
+    {
+        return $required === true && $value === null ? false : true;
+    }
+
+    /**
+     * Used in Schema::validate to validate scalar values
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    protected function validateScalar($value): bool
+    {
+        return is_scalar($value) === true;
+    }
+
+    /**
+     * General type validation used in Schema::validate
+     *
+     * @param mixed $value
+     * @param string $expectedType
+     * @return bool
+     */
+    protected function validateType($value, string $expectedType = null): bool
+    {
+        // validation passes if no type is set
+        if ($expectedType === null) {
+            return true;
+        }
+
+        // get the type of the given value
+        $valueType = gettype($value);
+
+        // further validation is needed
+        if ($valueType === $expectedType) {
+            return true;
+        }
+
+        switch ($expectedType) {
+            case 'number':
+            case 'object':
+            case 'scalar':
+                return $this->{'validate' . $expectedType}($value);
+            default:
+                if ($valueType === 'object' && is_a($value, $expectedType)) {
+                    return true;
+                }
+        }
+
+        return false;
+    }
+
+}
---
+++ tests/Util/SchemaTest.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace Kirby\Util;
+
+use PHPUnit\Framework\TestCase;
+
+class SchemaTest extends TestCase
+{
+
+    public function schemaData(): array
+    {
+        return [
+            'array' => [
+                'type' => 'array'
+            ],
+            'boolean' => [
+                'type' => 'boolean'
+            ],
+            'class' => [
+                'type' => Schema::class
+            ],
+            'double' => [
+                'type' => 'double'
+            ],
+            'integer' => [
+                'type' => 'integer'
+            ],
+            'number' => [
+                'type' => 'number'
+            ],
+            'scalar' => [
+                'type' => 'scalar'
+            ],
+            'string' => [
+                'type' => 'string',
+            ],
+        ];
+    }
+
+    public function schema()
+    {
+        return new Schema($this->schemaData());
+    }
+
+    public function validDataProvider()
+    {
+        return [
+            ['array', []],
+            ['boolean', true],
+            ['boolean', false],
+            ['class', new Schema([])],
+            ['double', 1.1],
+            ['integer', 1],
+            ['number', 0],
+            ['number', 1],
+            ['number', 1.1],
+            ['scalar', 1],
+            ['scalar', 'string'],
+            ['scalar', true],
+            ['string', 'string'],
+        ];
+    }
+
+    public function invalidDataProvider()
+    {
+        return [
+            ['array', 'array'],
+            ['boolean', 'true'],
+            ['class', false],
+            ['double', 1],
+            ['integer', 1.1],
+            ['number', 'a'],
+            ['number', '1'],
+            ['number', '1.1'],
+            ['scalar', new Schema([])],
+            ['scalar', []],
+            ['string', 1],
+        ];
+    }
+
+    /**
+     * @dataProvider validDataProvider
+     */
+    public function testAccepts($key, $value)
+    {
+        $this->assertTrue($this->schema()->accepts($key, $value));
+    }
+
+    /**
+     * @dataProvider invalidDataProvider
+     */
+    public function testDoesNotAccept($key, $value)
+    {
+        $this->assertFalse($this->schema()->accepts($key, $value));
+    }
+
+    /**
+     * @dataProvider validDataProvider
+     */
+    public function testValidates($key, $value)
+    {
+        $this->assertTrue($this->schema()->validate($key, $value));
+    }
+
+    /**
+     * @dataProvider invalidDataProvider
+     * @expectedException Exception
+     */
+    public function testDoesNotValidate($key, $value)
+    {
+        $this->schema()->validate($key, $value);
+    }
+
+    public function testValidatesMultiple()
+    {
+        $schema = new Schema([
+            'a' => ['type' => 'string'],
+            'b' => ['type' => 'boolean']
+        ]);
+
+        $input = [
+            'a' => 'a',
+            'b' => true
+        ];
+
+        $this->assertTrue($schema->validate($input));
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage "c" is required and must not be null
+     */
+    public function testDoesNotValidateMultiple()
+    {
+        $schema = new Schema([
+            'a' => ['type' => 'string'],
+            'b' => ['type' => 'boolean'],
+            'c' => ['type' => 'string', 'required' => true]
+        ]);
+
+        $input = [
+            'a' => 'a',
+            'b' => true
+        ];
+
+        $this->assertTrue($schema->validate($input));
+    }
+
+    public function testPluck()
+    {
+        $schema = new Schema([
+            'a' => ['type' => 'string'],
+            'b' => ['type' => 'string'],
+            'c' => ['type' => 'string']
+        ]);
+
+        $input = [
+            'a' => 'a',
+            'b' => 'b',
+        ];
+
+        $expected = [
+            'a' => 'a',
+            'b' => 'b',
+            'c' => null,
+        ];
+
+        $this->assertEquals($expected, $schema->pluck($input));
+    }
+
+    public function testToArray()
+    {
+        $this->assertEquals($this->schemaData(), $this->schema()->toArray());
+    }
+
+}
