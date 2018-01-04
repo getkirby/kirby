cb534bfb311f94a6851a8e55d2fdf8fe1a16e277
Bastian Allgeier <mail@bastianallgeier.com>
2018-01-04 11:14:16 +0100

    New V::time validation

--- src/Toolkit/V.php
+++ src/Toolkit/V.php
@@ -279,6 +279,9 @@ V::$validators = [
     'startsWith' => function (string $value, string $start): bool {
         return Str::startsWith($value, $start);
     },
+    'time' => function ($value): bool {
+        return V::date($value);
+    },
     'url' => function ($value): bool {
         // In search for the perfect regular expression: https://mathiasbynens.be/demo/url-regex
         $regex = '_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iu';
