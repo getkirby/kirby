a9788c30da546f07634caf8af1461e9f592e0c4a
Bastian Allgeier <mail@bastianallgeier.com>
2018-01-27 14:00:18 +0100

    Better component setup

--- config/components.php
+++ config/components.php
@@ -1,136 +1,134 @@
 <?php
 
-use Kirby\Cms\File;
-use Kirby\Cms\FileStore;
-use Kirby\Cms\Media;
-use Kirby\Cms\Page;
-use Kirby\Cms\PageStore;
-use Kirby\Cms\Pagination;
-use Kirby\Cms\Site;
-use Kirby\Cms\SiteStore;
-use Kirby\Cms\User;
-use Kirby\Cms\UserStore;
-use Kirby\Http\Request;
-use Kirby\Http\Router;
-use Kirby\Http\Server;
-use Kirby\Image\Darkroom;
-use Kirby\Image\Darkroom\GdLib;
-use Kirby\Text\Tags as Kirbytext;
-use Kirby\Text\Markdown;
-use Kirby\Text\Smartypants;
-use Kirby\Toolkit\Url;
-use Kirby\Toolkit\Url\Query;
+return function (Kirby\Cms\App $app) {
 
-return [
-    'Darkroom' => [
-        'singleton' => true,
-        'type'      => Darkroom::class,
-        'instance'  => function () {
-            return new GdLib([
-                'quality' => 80
-            ]);
-        }
-    ],
-    'FileStore' => [
-        'singleton' => false,
-        'type'      => FileStore::class,
-        'instance'  => function (File $file) {
-            return new FileStore($file);
-        }
-    ],
-    'Kirbytext' => [
-        'singleton' => true,
-        'type'      => Kirbytext::class,
-        'instance'  => function () {
-            return new Kirbytext([
-                'breaks' => true
-            ]);
-        }
-    ],
-    'Markdown' => [
-        'singleton' => true,
-        'type'      => Markdown::class,
-        'instance'  => function () {
-            return new Markdown([
-                'breaks' => true
-            ]);
-        }
-    ],
-    'Media' => [
-        'singleton' => true,
-        'type'      => Media::class,
-        'instance'  => function (Darkroom $darkroom, string $root, string $url) {
-            return new Media([
-                'darkroom' => $darkroom,
-                'root'     => $root,
-                'url'      => $url
-            ]);
-        }
-    ],
-    'PageStore' => [
-        'singleton' => false,
-        'type'      => PageStore::class,
-        'instance'  => function (Page $page) {
-            return new PageStore($page);
-        }
-    ],
-    'Pagination' => [
-        'singleton' => false,
-        'type'      => Pagination::class,
-        'instance'  => function (array $options) {
+    return [
+        'Darkroom' => [
+            'singleton' => true,
+            'type'      => Kirby\Image\Darkroom::class,
+            'instance'  => function () {
+                return new Kirby\Image\Darkroom\GdLib([
+                    'quality' => 80
+                ]);
+            }
+        ],
+        'FileStore' => [
+            'singleton' => false,
+            'type'      => Kirby\Cms\FileStore::class,
+            'instance'  => function (Kirby\Cms\File $file) {
+                return new Kirby\Cms\FileStore($file);
+            }
+        ],
+        'Kirbytext' => [
+            'singleton' => true,
+            'type'      => Kirby\Text\Tags::class,
+            'instance'  => function () {
+                return new Kirby\Text\Tags([
+                    'breaks' => true
+                ]);
+            }
+        ],
+        'Markdown' => [
+            'singleton' => true,
+            'type'      => Kirby\Text\Markdown::class,
+            'instance'  => function () {
+                return new Kirby\Text\Markdown([
+                    'breaks' => true
+                ]);
+            }
+        ],
+        'Media' => [
+            'singleton' => true,
+            'type'      => Kirby\Cms\Media::class,
+            'instance'  => function (array $props) {
+                return new Kirby\Cms\Media($props);
+            }
+        ],
+        'PageStore' => [
+            'singleton' => false,
+            'type'      => Kirby\Cms\PageStore::class,
+            'instance'  => function (Kirby\Cms\Page $page) {
+                return new Kirby\Cms\PageStore($page);
+            }
+        ],
+        'Pagination' => [
+            'singleton' => false,
+            'type'      => Kirby\Cms\Pagination::class,
+            'instance'  => function (array $options = []) use ($app) {
 
-            // TODO: make this nicer!
-            $options = array_merge([
-                'limit' => 20,
-                'page'  => App::instance()->request()->query()->get('page'),
-                'url'   => Query::strip(Url::current())
-            ], $options);
+                // TODO: make this nicer!
+                $options = array_merge([
+                    'limit' => 20,
+                    'page'  => $app->request()->query()->get('page'),
+                    'url'   => Kirby\Toolkit\Url\Query::strip(Kirby\Toolkit\Url::current())
+                ], $options);
 
-            return new Pagination($options);
+                return new Kirby\Cms\Pagination($options);
+            }
+        ],
+        'Request' => [
+            'singleton' => true,
+            'type'      => Kirby\Http\Request::class,
+            'instance'  => function () {
+                return new Kirby\Http\Request();
+            }
+        ],
+        'Response' => [
+            'singleton' => false,
+            'type'      => Kirby\Http\Response::class,
+            'instance'  => function ($input) {
+                return Kirby\Cms\Response::for($input);
+            }
+        ],
+        'Router' => [
+            'singleton' => true,
+            'type'      => Kirby\Http\Router::class,
+            'instance'  => function (array $routes = []) {
+                return new Kirby\Http\Router($routes);
+            }
+        ],
+        'Server' => [
+            'singleton' => true,
+            'type'      => Kirby\Http\Server::class,
+            'instance'  => function () {
+                return new Kirby\Http\Server;
+            }
+        ],
+        'SiteStore' => [
+            'singleton' => false,
+            'type'      => Kirby\Cms\SiteStore::class,
+            'instance'  => function (Kirby\Cms\Site $site) {
+                return new Kirby\Cms\SiteStore($site);
+            }
+        ],
+        'SmartyPants' => [
+            'singleton' => true,
+            'type'      => Kirby\Text\SmartyPants::class,
+            'instance'  => function () {
+                return new Kirby\Text\SmartyPants();
+            }
+        ],
+        'Snippet' => [
+            'singleton' => false,
+            'type'      => Kirby\Cms\Snippet::class,
+            'instance'  => function (string $name, array $data = []) {
+                return new Kirby\Cms\Snippet($name, $data);
+            }
+        ],
+        'Template' => [
+            'singleton' => false,
+            'type'      => Kirby\Cms\Template::class,
+            'instance'  => function (string $name, array $data = [], string $appendix = null) {
+                return new Kirby\Cms\Template($name, $data, $appendix);
+            }
+        ],
+        'UserStore' => [
+            'singleton' => false,
+            'type'      => Kirby\Cms\UserStore::class,
+            'instance'  => function (Kirby\Cms\User $user) {
+                return new Kirby\Cms\UserStore($user);
+            }
+        ],
+    ];
 
-        }
-    ],
-    'Request' => [
-        'singleton' => true,
-        'type'      => Request::class,
-        'instance'  => function () {
-            return new Request();
-        }
-    ],
-    'Router' => [
-        'singleton' => true,
-        'type'      => Router::class,
-        'instance'  => function (array $routes) {
-            return new Router($array);
-        }
-    ],
-    'Server' => [
-        'singleton' => true,
-        'type'      => Server::class,
-        'instance'  => function () {
-            return new Server;
-        }
-    ],
-    'SiteStore' => [
-        'singleton' => false,
-        'type'      => SiteStore::class,
-        'instance'  => function (Site $site) {
-            return new SiteStore($site);
-        }
-    ],
-    'SmartyPants' => [
-        'singleton' => true,
-        'type'      => SmartyPants::class,
-        'instance'  => function () {
-            return new SmartyPants();
-        }
-    ],
-    'UserStore' => [
-        'singleton' => false,
-        'type'      => UserStore::class,
-        'instance'  => function (User $user) {
-            return new UserStore($user);
-        }
-    ],
-];
-
+};
