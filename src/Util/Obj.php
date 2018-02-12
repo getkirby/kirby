1ad6fbac6aeea1c5a0a1e96725b2e632b074178c
Bastian Allgeier <mail@bastianallgeier.com>
2018-02-12 17:06:41 +0100

    Ported Obj utility

---
+++ src/Util/Obj.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Kirby\Util;
+
+use stdClass;
+
+class Obj extends stdClass
+{
+
+    /**
+     * Constructor
+     *
+     * @param array $data
+     */
+    public function __construct(array $data = [])
+    {
+        foreach ($data as $key => $val) {
+            if (is_string($key) === false || Str::length($key) === 0) {
+                continue;
+            }
+
+            $this->{strtolower($key)} = $val;
+        }
+    }
+
+    /**
+     * Magic getter
+     *
+     * @param string $method
+     * @param array $arguments
+     * @return mixed
+     */
+    public function __call(string $method, array $arguments)
+    {
+        return $this->{strtolower($method)} ?? null;
+    }
+
+    /**
+     * Converts the object to an array
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        $result = [];
+
+        foreach ((array)$this as $key => $value) {
+            if (is_object($value) === true && method_exists($value, 'toArray')) {
+                $result[$key] = $value->toArray();
+            } else {
+                $result[$key] = $value;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Improved var_dump() output
+     *
+     * @return array
+     */
+    public function __debuginfo(): array
+    {
+        return $this->toArray();
+    }
+
+}
