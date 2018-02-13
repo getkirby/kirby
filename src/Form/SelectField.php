abd69c23f3d10e0e4787abf7b7a055c60efb88f5
Bastian Allgeier <mail@bastianallgeier.com>
2018-02-13 18:05:30 +0100

    The select field no longer uses the Columns mixin

--- src/Form/SelectField.php
+++ src/Form/SelectField.php
@@ -2,7 +2,21 @@
 
 namespace Kirby\Form;
 
-class SelectField extends RadioField
+class SelectField extends Field
 {
+
+    use Mixins\Autofocus;
+    use Mixins\Label;
+    use Mixins\Options;
+    use Mixins\Required;
+    use Mixins\Value;
+
+    protected function validate($value)
+    {
+        $this->validateRequired($value);
+        $this->validateSingleOption($value);
+
+        return true;
+    }
 
 }
