89b1e877286868209f3a7a4a860712b7a417b064
Nico Hoffmann <on-github@nhoffmann.com>
2018-02-18 21:48:55 +0100

    Add Range field

---
+++ src/Form/RangeField.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Kirby\Form;
+
+class RangeField extends NumberField
+{
+
+    protected $append;
+    protected $prepend;
+
+    public function append()
+    {
+        return $this->append;
+    }
+
+    protected function defaultMin()
+    {
+        return 0;
+    }
+
+    protected function defaultMax()
+    {
+        return 100;
+    }
+
+    protected function defaultStep()
+    {
+        return 1;
+    }
+
+    public function prepend()
+    {
+        return $this->prepend;
+    }
+
+    protected function setAppend(string $append = null)
+    {
+        $this->append = $append;
+        return $this;
+    }
+
+    protected function setPrepend(string $prepend = null)
+    {
+        $this->prepend = $prepend;
+        return $this;
+    }
+
+}
