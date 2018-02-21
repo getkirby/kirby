60eceb9c08d02593ce8f2524114fd6f38c3612aa
Bastian Allgeier <mail@bastianallgeier.com>
2018-02-21 17:03:28 +0100

    Installation API endpoint

--- config/api/routes/system.php
+++ config/api/routes/system.php
@@ -8,6 +8,7 @@ return [
     [
         'pattern' => 'system',
         'method'  => 'GET',
+        'auth'    => false,
         'action'  => function () {
 
             $system = $this->kirby()->system();
@@ -18,6 +19,25 @@ return [
                 'details'     => $system->toArray()
             ];
 
+        }
+    ],
+    [
+        'pattern' => 'system/install',
+        'method'  => 'POST',
+        'auth'    => false,
+        'action'  => function () {
+            $system = $this->kirby()->system();
+
+            if ($system->isOk() === false) {
+                throw new Exception('The server is not setup correctly');
+            }
+
+            if ($system->isInstalled() === true) {
+                throw new Exception('The panel is already installed');
+            }
+
+            // create the first user
+            return $this->users()->create($this->requestBody());
         }
     ]
 
