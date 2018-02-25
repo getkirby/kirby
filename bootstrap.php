f7e64c194eb0493de9e5cbe6f40a41d92da85fe2
Bastian Allgeier <mail@bastianallgeier.com>
2018-02-25 21:31:48 +0100

    Better bootstrapper

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,11 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+if (is_dir(__DIR__ . '/vendor')) {
+    require __DIR__ . '/vendor/autoload.php';
+} else {
+    require __DIR__ . '/../../autoload.php';
+}
+
 require __DIR__ . '/config/sentry.php';
 require __DIR__ . '/extensions/helpers.php';
 require __DIR__ . '/config/aliases.php';
