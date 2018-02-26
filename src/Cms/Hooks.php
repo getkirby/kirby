458152e3bf19e8dfa6153a51c5416dcd3ea15508
Bastian Allgeier <mail@bastianallgeier.com>
2018-02-26 22:10:47 +0100

    DocBlocks for the Hooks class

--- src/Cms/Hooks.php
+++ src/Cms/Hooks.php
@@ -4,18 +4,46 @@ namespace Kirby\Cms;
 
 use Closure;
 
+/**
+ * The Hooks registry is implanted in the App
+ * class and used to create and trigger hooks
+ *
+ * @package   Kirby Cms
+ * @author    Bastian Allgeier <bastian@getkirby.com>
+ * @link      http://getkirby.com
+ * @copyright Bastian Allgeier
+ */
 class Hooks
 {
 
+    /**
+     * @var App
+     */
     protected $bind;
+
+    /**
+     * @var array
+     */
     protected $hooks = [];
 
+    /**
+     * Creates a new Hooks registry
+     *
+     * @param App $bind
+     */
     public function __construct($bind)
     {
         $this->bind = $bind;
     }
 
-    public function register($name, Closure $function)
+    /**
+     * Registers a new hook
+     *
+     * @param string $name
+     * @param Closure $function
+     * @return self
+     */
+    public function register(string $name, Closure $function)
     {
         if (isset($this->hooks[$name]) === false) {
             $this->hooks[$name] = [];
@@ -25,6 +53,14 @@ class Hooks
         return $this;
     }
 
+    /**
+     * Triggers all registered hooks for a given
+     * name if they exist
+     *
+     * @param string $name
+     * @param mixed ...$arguments
+     * @return void
+     */
     public function trigger(string $name, ...$arguments)
     {
         if (isset($this->hooks[$name]) === false) {
