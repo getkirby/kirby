90a4f6fa17cc4fb394655789061bfe0155e38842
Bastian Allgeier <mail@bastianallgeier.com>
2018-03-01 13:59:22 +0100

    Remove non-functioning config

--- site/config/config.php
+++ site/config/config.php
@@ -2,44 +2,4 @@
 
 return [
     'license' => 'K3-alpha',
-    'urls' => [
-        'index' => 'https://getkirby.com',
-        'media' => 'https://cdn.getkirby.com',
-        'panel' => 'https://panel.getkirby.com',
-        'api' => 'https://getkirby.com/api'
-    ],
-    'debug' => true,
-    'pages' => [
-        'home' => 'home',
-        'error' => 'error'
-    ],
-    'content' => [
-        'extension' => 'txt',
-        'ignore' => function ($file) {
-
-        }
-    ],
-    'cache' => [
-        'enabled' => true,
-        'driver' => 'memcached',
-        'options' => [
-            'host' => 'localhost:11211'
-        ],
-        'autoupdate' => false,
-        'ignore' => function ($page) {
-
-        }
-    ],
-    'date' => [
-        'handler' => 'strftime'
-    ],
-    'headers' => [
-
-    ],
-    'kirbytext' => [
-
-    ],
-    'routes' => [
-
-    ]
 ];
