2befca18ba388e1984cecd85f972696942258eb3
Bastian Allgeier <mail@bastianallgeier.com>
2018-03-08 11:00:33 +0100

    New Plugin class with tests

---
+++ src/Cms/Plugin.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Kirby\Cms;
+
+use Exception;
+use Kirby\Data\Data;
+
+class Plugin extends Model
+{
+
+    protected $extends;
+    protected $info;
+    protected $name;
+    protected $root;
+
+    public function __call(string $key, array $arguments = null)
+    {
+        return $this->info()[$key] ?? null;
+    }
+
+    public function __construct(array $props = [])
+    {
+        $this->setProperties($props);
+    }
+
+    public function extends(): array
+    {
+        return $this->extends;
+    }
+
+    public function info(): array
+    {
+        if (is_array($this->info) === true) {
+            return $this->info;
+        }
+
+        return $this->info = Data::read($this->manifest());
+    }
+
+    public function manifest(): string
+    {
+        return $this->root() . '/composer.json';
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function option(string $key)
+    {
+        return $this->kirby()->option($this->prefix() . '.' . $key);
+    }
+
+    public function prefix(): string
+    {
+        return str_replace('/', '.', $this->name());
+    }
+
+    public function resource(string $path)
+    {
+        return Resource::forPlugin($this, $path);
+    }
+
+    public function root(): string
+    {
+        return $this->root;
+    }
+
+    protected function setExtends(array $extends = null)
+    {
+        $this->extends = $extends;
+        return $this;
+    }
+
+    protected function setName(string $name)
+    {
+        if (preg_match('!^[a-z-]+\/[a-z-]+$!', $name) == false) {
+            throw new Exception('The plugin name must follow the format "abc/def"');
+        }
+
+        $this->name = $name;
+        return $this;
+    }
+
+    protected function setRoot(string $root)
+    {
+        $this->root = $root;
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        return $this->propertiesToArray();
+    }
+
+}
---
+++ tests/Cms/PluginTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Kirby\Cms;
+
+class PluginTest extends TestCase
+{
+
+    public function testProps()
+    {
+        $plugin = new Plugin([
+            'name'    => 'getkirby/test-plugin',
+            'root'    => __DIR__,
+            'extends' => [
+
+            ]
+        ]);
+
+        $this->assertEquals('getkirby/test-plugin', $plugin->name());
+        $this->assertEquals(__DIR__, $plugin->root());
+        $this->assertEquals([], $plugin->extends());
+    }
+
+    public function testManifest()
+    {
+        $plugin = new Plugin([
+            'name' => 'getkirby/test-plugin',
+            'root' => __DIR__
+        ]);
+
+        $this->assertEquals(__DIR__ . '/composer.json', $plugin->manifest());
+    }
+
+    public function testEmptyInfo()
+    {
+        $plugin = new Plugin([
+            'name' => 'getkirby/test-plugin',
+            'root' => __DIR__
+        ]);
+
+        $this->assertEquals([], $plugin->info());
+    }
+
+    public function testInfo()
+    {
+        $plugin = new Plugin([
+            'name' => 'getkirby/test-plugin',
+            'root' => __DIR__ . '/fixtures/plugin'
+        ]);
+
+        $authors = [
+            [
+                'name'  => 'Bastian Allgeier',
+                'email' => 'bastian@getkirby.com'
+            ]
+        ];
+
+        $this->assertEquals('getkirby/test-plugin', $plugin->info()['name']);
+        $this->assertEquals('MIT', $plugin->info()['license']);
+        $this->assertEquals('1.0.0', $plugin->info()['version']);
+        $this->assertEquals('plugin', $plugin->info()['type']);
+        $this->assertEquals('Some really nice description', $plugin->info()['description']);
+        $this->assertEquals($authors, $plugin->info()['authors']);
+    }
+
+    public function testMagicCaller()
+    {
+        $plugin = new Plugin([
+            'name' => 'getkirby/test-plugin',
+            'root' => __DIR__ . '/fixtures/plugin'
+        ]);
+
+        $this->assertEquals('1.0.0', $plugin->version());
+        $this->assertEquals('MIT', $plugin->license());
+    }
+
+    public function testPrefix()
+    {
+        $plugin = new Plugin([
+            'name' => 'getkirby/test-plugin',
+            'root' => __DIR__
+        ]);
+
+        $this->assertEquals('getkirby.test-plugin', $plugin->prefix());
+    }
+
+}
