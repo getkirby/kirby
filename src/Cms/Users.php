9a2d97bb85014a6cc6d01ad810e3f2a93473a14b
Bastian Allgeier <mail@bastianallgeier.com>
2018-03-20 14:06:58 +0100

    Add Users::factory to load users from disk

--- src/Cms/Users.php
+++ src/Cms/Users.php
@@ -2,6 +2,8 @@
 
 namespace Kirby\Cms;
 
+use Kirby\Util\Dir;
+
 class Users extends Collection
 {
     protected static $accept = User::class;
@@ -12,6 +14,31 @@ class Users extends Collection
         $data['store'] = UserStore::class;
 
         return User::create($data);
+    }
+
+    public static function factory(App $app = null): self
+    {
+
+        $app   = $app ?? App::instance();
+        $root  = $app->root('accounts');
+        $users = new static;
+
+        foreach (Dir::read($root) as $userDirectory) {
+            if (is_dir($root . '/' . $userDirectory) === false) {
+                continue;
+            }
+
+            $user = new User([
+                'email' => $userDirectory,
+                'kirby' => $app,
+                'store' => UserStore::class
+            ]);
+
+            $users->set($user->id(), $user);
+        }
+
+        return $users;
+
     }
 
 }
