157079424030af6cf3ee36f3d3547259c42e9c6a
Nico Hoffmann <on-github@nhoffmann.com>
2018-03-29 20:22:30 +0200

    SelectField: add icon

--- src/Form/SelectField.php
+++ src/Form/SelectField.php
@@ -5,10 +5,16 @@ namespace Kirby\Form;
 class SelectField extends Field
 {
 
+    use Mixins\Icon;
     use Mixins\Label;
     use Mixins\Options;
     use Mixins\Required;
     use Mixins\Value;
+
+    protected function defaultIcon()
+    {
+        return 'angle-down';
+    }
 
     protected function validate($value)
     {
