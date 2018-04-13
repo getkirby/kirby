a0fcb430f1f6abcd5a16cd20efb4dae7fce3aff8
Nico Hoffmann <on-github@nhoffmann.com>
2018-04-13 11:58:41 +0200

    Add Exceptions to Cms\Page

--- src/Cms/Page.php
+++ src/Cms/Page.php
@@ -738,9 +738,13 @@ class Page extends Model
         // react if even the default template does not exist
         if ($template->exists() === false) {
             if ($this->isErrorPage() === true) {
-                throw new Exception('The error template does not exist');
+                throw new NotFoundException([
+                    'key' => 'template.error.notFound'
+                ]);
             } else {
-                throw new Exception('The default template does not exist');
+                throw new NotFoundException([
+                    'key' => 'template.default.notFound'
+                ]);
             }
         }
 
