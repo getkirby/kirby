17c00efc398ee1cf78a097e86cea94bec6bd8d15
Nico Hoffmann <on-github@nhoffmann.com>
2018-04-14 13:53:10 +0200

    Data\Handler\Yaml: Fix comment after linting

--- src/Data/Handler/Yaml.php
+++ src/Data/Handler/Yaml.php
@@ -26,7 +26,7 @@ class Yaml extends Handler
      */
     public static function encode(array $data): string
     {
-        //                           $indent $wordwrap $no_opening_dashes
+        // $data, $indent, $wordwrap, $no_opening_dashes
         return Spyc::YAMLDump($data, false, false, true);
     }
 
