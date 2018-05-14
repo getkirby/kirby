c0ed8779d21793cbbe7004285505b83e17d46f7b
Bastian Allgeier <mail@bastianallgeier.com>
2018-05-14 15:16:34 +0200

    Add social links

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -5,8 +5,8 @@
       <a href="">&copy; 2018 / Kirby Starterkit</a>
 
       <nav class="social">
-        <a href="">Twitter</a>
-        <a href="">Instagram</a>
+        <a href="https://twitter.com/<?= $site->twitter() ?>">Twitter</a>
+        <a href="https://instagram.com/<?= $site->instagram() ?>">Instagram</a>
       </nav>
     </div>
   </footer>
