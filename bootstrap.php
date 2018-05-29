27db6838bddeaf264ca322f45163ce00b4698f44
Lukas Bestle <mail@lukasbestle.com>
2018-05-29 20:55:32 +0200

    Support for installation with Composer

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,15 @@
 <?php
 
-if (is_dir(__DIR__ . '/vendor')) {
+if (is_file(dirname(__DIR__) . '/vendor/autoload.php')) {
+    // always prefer a site-wide Composer autoloader
+    // if it exists, it means that the user has probably installed additional packages
+    require dirname(__DIR__) . '/vendor/autoload.php';
+} elseif (is_file(__DIR__ . '/vendor/autoload.php')) {
+    // fall back to the local autoloader if that exists
     require __DIR__ . '/vendor/autoload.php';
 } else {
-    require __DIR__ . '/../../autoload.php';
+    // if neither one exists, don't bother searching
+    // it's a custom directory setup and the users need to load the autoloader themselves
 }
 
 define('DS', '/');
