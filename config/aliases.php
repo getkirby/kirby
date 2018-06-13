11820c7808228adbfe2057f4810f83e6b437d23c
Bastian Allgeier <mail@bastianallgeier.com>
2018-06-13 10:16:57 +0200

    Add class aliases

--- config/aliases.php
+++ config/aliases.php
@@ -3,20 +3,29 @@
 $aliases = [
 
     // cms classes
-    'dir'       => 'Kirby\Cms\Dir',
-    'file'      => 'Kirby\Cms\File',
-    'html'      => 'Kirby\Cms\Html',
-    'kirby'     => 'Kirby\Cms\App',
-    'page'      => 'Kirby\Cms\Page',
-    'response'  => 'Kirby\Cms\Response',
-    'site'      => 'Kirby\Cms\Site',
-    'structure' => 'Kirby\Cms\Structure',
-    'url'       => 'Kirby\Cms\Url',
+    'dir'        => 'Kirby\Cms\Dir',
+    'field'      => 'Kirby\Cms\Field',
+    'file'       => 'Kirby\Cms\File',
+    'files'      => 'Kirby\Cms\Files',
+    'html'       => 'Kirby\Cms\Html',
+    'kirby'      => 'Kirby\Cms\App',
+    'page'       => 'Kirby\Cms\Page',
+    'pages'      => 'Kirby\Cms\Pages',
+    'pagination' => 'Kirby\Cms\Pagination',
+    'response'   => 'Kirby\Cms\Response',
+    'site'       => 'Kirby\Cms\Site',
+    'structure'  => 'Kirby\Cms\Structure',
+    'url'        => 'Kirby\Cms\Url',
+    'user'       => 'Kirby\Cms\User',
+    'users'      => 'Kirby\Cms\Users',
 
     // data handler
     'data'      => 'Kirby\Data\Data',
     'json'      => 'Kirby\Data\Json',
     'yaml'      => 'Kirby\Data\Yaml',
+
+    // image classes
+    'dimensions' => 'Kirby\Image\Dimensions',
 
     // toolkit classes
     'a'          => 'Kirby\Toolkit\A',
