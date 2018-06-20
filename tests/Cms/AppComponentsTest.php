1b68db62654a138da19057a36e2074fae6bfa7f8
Bastian Allgeier <mail@bastianallgeier.com>
2018-06-20 22:53:17 +0200

    Basic components tests

---
+++ tests/Cms/AppComponentsTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Kirby\Cms;
+
+class AppComponentsTest extends TestCase
+{
+
+    public function testMarkdown()
+    {
+        $app      = new App();
+        $text     = 'Test';
+        $expected = '<p>Test</p>';
+
+        $this->assertEquals($expected, $app->markdown($text));
+    }
+
+    public function testResponse()
+    {
+        $app      = new App();
+        $response = $app->response('anything');
+
+        $this->assertInstanceOf(Response::class, $response);
+    }
+
+    public function testSmartypants()
+    {
+        $app      = new App();
+        $text     = '"Test"';
+        $expected = '&#8220;Test&#8221;';
+
+        $this->assertEquals($expected, $app->smartypants($text));
+    }
+
+    public function testSnippet()
+    {
+        $app     = new App();
+        $snippet = $app->snippet('default');
+
+        $this->assertInstanceOf(Snippet::class, $snippet);
+    }
+
+    public function testTemplate()
+    {
+        $app      = new App();
+        $template = $app->template('default');
+
+        $this->assertInstanceOf(Template::class, $template);
+    }
+
+}
