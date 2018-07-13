deaa7d9b652770aac3536dd998393b215fa70cd5
Bastian Allgeier <mail@bastianallgeier.com>
2018-07-13 10:23:12 +0200

    Fix line field

--- panel/src/ui/components/Forms/Field/LineField.vue
+++ panel/src/ui/components/Forms/Field/LineField.vue
@@ -7,6 +7,7 @@
   position: relative;
   border: 0;
   height: 3rem;
+  width: auto;
 
   &::after {
     position: absolute;
@@ -15,7 +16,7 @@
     margin-top: -1px;
     left: 0;
     right: 0;
-    height: 2px;
+    height: 1px;
     background: $color-border;
   }
 }
