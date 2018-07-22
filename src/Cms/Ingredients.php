0dd3c2f22b8a4376e8e48eff7507b9ddb28b9570
Bastian Allgeier <mail@bastianallgeier.com>
2018-07-22 21:30:17 +0200

    Improve docs and __debuginfo for the Ingredients class

--- src/Cms/Ingredients.php
+++ src/Cms/Ingredients.php
@@ -12,23 +12,62 @@ use Closure;
  */
 class Ingredients
 {
-    protected $ingredients;
 
+    /**
+     * @var array
+     */
+    protected $ingredients = [];
+
+    /**
+     * Creates a new ingredient collection
+     *
+     * @param array $ingredients
+     */
     public function __construct(array $ingredients)
     {
         $this->ingredients = $ingredients;
     }
 
+    /**
+     * Magic getter for single ingredients
+     *
+     * @param string $method
+     * @param array $args
+     * @return mixed
+     */
     public function __call(string $method, array $args = null)
     {
         return $this->ingredients[$method] ?? null;
     }
 
+    /**
+     * Improved var_dump output
+     *
+     * @return array
+     */
+    public function __debuginfo(): array
+    {
+        return $this->ingredients;
+    }
+
+    /**
+     * Get a single ingredient by key
+     *
+     * @param string $key
+     * @return mixed
+     */
     public function __get(string $key)
     {
         return $this->ingredients[$key] ?? null;
     }
 
+    /**
+     * Resolves all ingredient callbacks
+     * and creates a plain array
+     *
+     * @param array $ingredients
+     * @return self
+     */
     public static function bake(array $ingredients): self
     {
         foreach ($ingredients as $name => $ingredient) {
@@ -40,6 +79,11 @@ class Ingredients
         return new static($ingredients);
     }
 
+    /**
+     * Returns all ingredients as plain array
+     *
+     * @return array
+     */
     public function toArray(): array
     {
         return $this->ingredients;
