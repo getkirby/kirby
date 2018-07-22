89027b800539c462cf6a912b401c1686337a3c07
Bastian Allgeier <mail@bastianallgeier.com>
2018-07-22 21:42:49 +0200

    Improve the docs and var_dump output of the Translation class

--- src/Cms/Translation.php
+++ src/Cms/Translation.php
@@ -9,47 +9,112 @@ use Kirby\Data\Data;
  * Wrapper around Kirby's localization files,
  * which are store in `kirby/translations`.
  */
-class Translation extends Component
+class Translation
 {
+    /**
+     * @var string
+     */
     protected $code;
-    protected $data;
 
+    /**
+     * @var array
+     */
+    protected $data = [];
+
+    /**
+     * @param string $code
+     * @param array $data
+     */
     public function __construct(string $code, array $data)
     {
         $this->code = $code;
         $this->data = $data;
     }
 
+    /**
+     * Improved var_dump output
+     *
+     * @return array
+     */
+    public function __debuginfo(): array
+    {
+        return $this->toArray();
+    }
+
+    /**
+     * Returns the translation author
+     *
+     * @return string
+     */
     public function author(): string
     {
         return $this->get('translation.author', 'Kirby');
     }
 
+    /**
+     * Returns the official translation code
+     *
+     * @return string
+     */
     public function code(): string
     {
         return $this->code;
     }
 
+    /**
+     * Returns an array with all
+     * translation strings
+     *
+     * @return array
+     */
     public function data(): array
     {
         return $this->data;
     }
 
+    /**
+     * Returns the writing direction
+     * (ltr or rtl)
+     *
+     * @return string
+     */
     public function direction(): string
     {
         return $this->get('translation.direction', 'ltr');
     }
 
-    public function get(string $key, $default = null)
+    /**
+     * Returns a single translation
+     * string by key
+     *
+     * @param string $key
+     * @param string $default
+     * @return void
+     */
+    public function get(string $key, string $default = null): ?string
     {
         return $this->data[$key] ?? $default;
     }
 
+    /**
+     * Returns the translation id,
+     * which is also the code
+     *
+     * @return string
+     */
     public function id(): string
     {
         return $this->code;
     }
 
+    /**
+     * Loads the translation from the
+     * json file in Kirby's translations folder
+     *
+     * @param string $code
+     * @param string $root
+     * @return self
+     */
     public static function load(string $code, string $root)
     {
         try {
@@ -60,11 +125,22 @@ class Translation extends Component
         }
     }
 
+    /**
+     * Returns the human-readable translation name.
+     *
+     * @return string
+     */
     public function name(): string
     {
         return $this->get('translation.name', $this->code);
     }
 
+    /**
+     * Converts the most important
+     * properties to an array
+     *
+     * @return array
+     */
     public function toArray(): array
     {
         return [
