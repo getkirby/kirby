72ea18b2f5ec0148a388dddcecc8690201fbd8af
Bastian Allgeier <mail@bastianallgeier.com>
2018-07-25 18:03:22 +0200

    fix the go helper

--- config/helpers.php
+++ config/helpers.php
@@ -127,7 +127,7 @@ function gist(string $url, string $file = null): string
  */
 function go(string $url = null, int $code = 301)
 {
-    // die(Response::redirect($url, $code));
+    die(Response::redirect($url, $code));
 }
 
 /**
