ddd5b8fd4a76a0b134898a150232bdebd42e38ff
Bastian Allgeier <mail@bastianallgeier.com>
2018-08-01 11:16:11 +0200

    Add Mime class tests

--- tests/Toolkit/MimeTest.php
+++ tests/Toolkit/MimeTest.php
@@ -7,16 +7,39 @@ class MimeTest extends TestCase
 
     const FIXTURES = __DIR__ . '/fixtures/mime';
 
-    public function testOptimizedSvg()
+    public function testFromExtension()
+    {
+        $mime = Mime::fromExtension('jpg');
+        $this->assertEquals('image/jpeg', $mime);
+    }
+
+    public function testFromMimeContentType()
+    {
+        $mime = Mime::fromMimeContentType(__FILE__);
+        $this->assertEquals('text/x-php', $mime);
+    }
+
+    public function testToExtension()
+    {
+        $extension = Mime::toExtension('image/jpeg');
+        $this->assertEquals('jpg', $extension);
+    }
+
+    public function testTypeWithOptimizedSvg()
     {
         $mime = Mime::type(static::FIXTURES . '/optimized.svg');
         $this->assertEquals('image/svg+xml', $mime);
     }
 
-    public function testUnoptimizedSvg()
+    public function testTypeWithUnoptimizedSvg()
     {
         $mime = Mime::type(static::FIXTURES . '/unoptimized.svg');
         $this->assertEquals('image/svg+xml', $mime);
+    }
+
+    public function testTypes()
+    {
+        $this->assertEquals(Mime::$types, Mime::types());
     }
 
 }
