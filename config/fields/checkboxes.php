e08978cc35db09ee1dbbb27c865aa70cf8b447ee
Bastian Allgeier <mail@bastianallgeier.com>
2018-10-30 13:21:24 +0100

    Add docs to the checkboxes field

--- config/fields/checkboxes.php
+++ config/fields/checkboxes.php
@@ -6,15 +6,27 @@ use Kirby\Toolkit\Str;
 return [
     'mixins' => ['options'],
     'props' => [
+        /**
+         * Arranges the checkboxes in the given number of columns
+         */
         'columns' => function (int $columns = 1) {
             return $columns;
         },
+        /**
+         * Default value for the field, which will be used when a Page/File/User is created
+         */
         'default' => function ($default = null) {
             return Str::split($default, ',');
         },
+        /**
+         * Maximum number of checked boxes
+         */
         'max' => function (int $max = null) {
             return $max;
         },
+        /**
+         * Minimum number of checked boxes
+         */
         'min' => function (int $min = null) {
             return $min;
         },
