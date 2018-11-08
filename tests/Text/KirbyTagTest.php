91f166eb434146e347ebb56a0160236476417669
Lukas Bestle <mail@lukasbestle.com>
2018-11-08 22:15:03 +0100

    Refactor and unit test KirbyTag::parse()

--- tests/Text/KirbyTagTest.php
+++ tests/Text/KirbyTagTest.php
@@ -12,8 +12,8 @@ class KirbyTagTest extends TestCase
         KirbyTag::$types = [
             'test' => [
                 'attr' => ['a', 'b'],
-                'html' => function () {
-                    return 'test';
+                'html' => function ($tag) {
+                    return 'test: ' . $tag->value . '-' . $tag->a . '-' . $tag->b;
                 }
             ],
             'noHtml' => [
@@ -55,6 +55,96 @@ class KirbyTagTest extends TestCase
 
         $this->assertNull($tag->b);
         $this->assertEquals('fallback', $tag->attr('b', 'fallback'));
+    }
+
+    public function testParse()
+    {
+        $tag = KirbyTag::parse('(test: test value)', ['some' => 'data'], ['some' => 'options']);
+        $this->assertEquals('test', $tag->type);
+        $this->assertEquals('test value', $tag->value);
+        $this->assertEquals(['some' => 'data'], $tag->data);
+        $this->assertEquals(['some' => 'options'], $tag->options);
+        $this->assertEquals([], $tag->attrs);
+
+        $tag = KirbyTag::parse('test: test value');
+        $this->assertEquals('test', $tag->type);
+        $this->assertEquals('test value', $tag->value);
+        $this->assertEquals([], $tag->attrs);
+
+        $tag = KirbyTag::parse('test:');
+        $this->assertEquals('test', $tag->type);
+        $this->assertEquals('', $tag->value);
+        $this->assertEquals([], $tag->attrs);
+
+        $tag = KirbyTag::parse('test: ');
+        $this->assertEquals('test', $tag->type);
+        $this->assertEquals('', $tag->value);
+        $this->assertEquals([], $tag->attrs);
+
+        $tag = KirbyTag::parse('test: test value a: attrA b: attrB');
+        $this->assertEquals('test', $tag->type);
+        $this->assertEquals('test value', $tag->value);
+        $this->assertEquals([
+            'a' => 'attrA',
+            'b' => 'attrB'
+        ], $tag->attrs);
+
+        $tag = KirbyTag::parse('test:test value a:attrA b:attrB');
+        $this->assertEquals('test', $tag->type);
+        $this->assertEquals('test value', $tag->value);
+        $this->assertEquals([
+            'a' => 'attrA',
+            'b' => 'attrB'
+        ], $tag->attrs);
+
+        $tag = KirbyTag::parse('test: test value a: attrA b:');
+        $this->assertEquals('test', $tag->type);
+        $this->assertEquals('test value', $tag->value);
+        $this->assertEquals([
+            'a' => 'attrA',
+            'b' => ''
+        ], $tag->attrs);
+
+        $tag = KirbyTag::parse('test: test value a: attrA b: ');
+        $this->assertEquals('test', $tag->type);
+        $this->assertEquals('test value', $tag->value);
+        $this->assertEquals([
+            'a' => 'attrA',
+            'b' => ''
+        ], $tag->attrs);
+
+        $tag = KirbyTag::parse('test: test value a: attrA b: attrB ');
+        $this->assertEquals('test', $tag->type);
+        $this->assertEquals('test value', $tag->value);
+        $this->assertEquals([
+            'a' => 'attrA',
+            'b' => 'attrB'
+        ], $tag->attrs);
+
+        $tag = KirbyTag::parse('test: test value a: attrA c: attrC b: attrB');
+        $this->assertEquals('test', $tag->type);
+        $this->assertEquals('test value', $tag->value);
+        $this->assertEquals([
+            'a' => 'attrA c: attrC',
+            'b' => 'attrB'
+        ], $tag->attrs);
+
+        $tag = KirbyTag::parse('test: test value a: attrA b: attrB c: attrC');
+        $this->assertEquals('test', $tag->type);
+        $this->assertEquals('test value', $tag->value);
+        $this->assertEquals([
+            'a' => 'attrA',
+            'b' => 'attrB c: attrC'
+        ], $tag->attrs);
+    }
+
+    /**
+     * @expectedException        Kirby\Exception\InvalidArgumentException
+     * @expectedExceptionMessage Undefined tag type: invalid
+     */
+    public function testParseInvalid()
+    {
+        KirbyTag::parse('invalid: test value a: attrA b: attrB');
     }
 
     public function testRender()
