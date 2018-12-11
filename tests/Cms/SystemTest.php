87e22c3a79181234a1243df0287fc12ad835ed3d
Lukas Bestle <mail@lukasbestle.com>
2018-12-11 21:35:31 +0100

    Include index URL in license validation

--- tests/Cms/SystemTest.php
+++ tests/Cms/SystemTest.php
@@ -3,6 +3,7 @@
 namespace Kirby\Cms;
 
 use Exception;
+use ReflectionClass;
 
 class SystemTest extends TestCase
 {
@@ -93,6 +94,75 @@ class SystemTest extends TestCase
 
         // reset SERVER_ADDR
         $_SERVER['SERVER_ADDR'] = null;
+    }
+
+    public function licenseUrlProvider()
+    {
+        return [
+            ['http://getkirby.com', 'getkirby.com'],
+            ['https://getkirby.com', 'getkirby.com'],
+            ['https://getkirby.com/test', 'getkirby.com/test'],
+            ['/', 'example.com'],
+            ['/test', 'example.com/test'],
+            ['getkirby.com/test', 'example.com/getkirby.com/test'],
+        ];
+    }
+
+    /**
+     * @dataProvider licenseUrlProvider
+     */
+    public function testLicenseUrl($indexUrl, $expected)
+    {
+        $reflector = new ReflectionClass(System::class);
+        $licenseUrl = $reflector->getMethod('licenseUrl');
+        $licenseUrl->setAccessible(true);
+
+        $_SERVER['SERVER_ADDR'] = 'example.com';
+
+        $system = new System(new App([
+            'options' => [
+                'url' => $indexUrl
+            ]
+        ]));
+        $this->assertEquals($expected, $licenseUrl->invoke($system));
+
+        // reset SERVER_ADDR
+        $_SERVER['SERVER_ADDR'] = null;
+    }
+
+    public function licenseUrlNormalizedProvider()
+    {
+        return [
+            [null, 'getkirby.com'],
+            ['example.com', 'example.com'],
+            ['www.example.com', 'example.com'],
+            ['dev.example.com', 'example.com'],
+            ['test.example.com', 'example.com'],
+            ['staging.example.com', 'example.com'],
+            ['sub.example.com', 'sub.example.com'],
+            ['www.example.com/test', 'www.example.com/test'],
+            ['dev.example.com/test', 'dev.example.com/test'],
+            ['test.example.com/test', 'test.example.com/test'],
+            ['staging.example.com/test', 'staging.example.com/test'],
+            ['sub.example.com/test', 'sub.example.com/test'],
+        ];
+    }
+
+    /**
+     * @dataProvider licenseUrlNormalizedProvider
+     */
+    public function testLicenseUrlNormalized($url, $expected)
+    {
+        $reflector = new ReflectionClass(System::class);
+        $licenseUrlNormalized = $reflector->getMethod('licenseUrlNormalized');
+        $licenseUrlNormalized->setAccessible(true);
+
+        $system = new System(new App([
+            'options' => [
+                'url' => 'https://getkirby.com'
+            ]
+        ]));
+        $this->assertEquals($expected, $licenseUrlNormalized->invoke($system, $url));
     }
 
 }
