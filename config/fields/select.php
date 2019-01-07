c1d00ead565b5bd812038bbeb3ec928fd7f28014
Nico Hoffmann <on-github@nhoffmann.com>
2019-01-07 19:06:33 +0100

    Select field: unset inherited `columns` prop

--- config/fields/select.php
+++ config/fields/select.php
@@ -4,6 +4,11 @@ return [
     'extends' => 'radio',
     'props' => [
         /**
+         * Unset inherited props
+         */
+        'columns' => null,
+
+        /**
          * Custom icon to replace the arrow down.
          */
         'icon' => function (string $icon = null) {
