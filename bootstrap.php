8001e9752a7acd558dca0f2f86aba46206925ccc
Bastian Allgeier <mail@bastianallgeier.com>
2019-01-08 12:36:39 +0100

    Nicer format for the bootstrapper

--- bootstrap.php
+++ bootstrap.php
@@ -1,19 +1,37 @@
 <?php
 
+/**
+ * Validate the PHP version to already
+ * stop at older versions
+ */
 if (version_compare(phpversion(), '7.1.0', '>') === false) {
     die(include __DIR__ . '/views/php.php');
 }
 
-if (is_file(dirname(__DIR__) . '/vendor/autoload.php')) {
-    // always prefer a site-wide Composer autoloader
-    // if it exists, it means that the user has probably installed additional packages
-    require dirname(__DIR__) . '/vendor/autoload.php';
-} elseif (is_file(__DIR__ . '/vendor/autoload.php')) {
-    // fall back to the local autoloader if that exists
-    require __DIR__ . '/vendor/autoload.php';
+if (is_file($autoloader = dirname(__DIR__) . '/vendor/autoload.php')) {
+
+    /**
+     * Always prefer a site-wide Composer autoloader
+     * if it exists, it means that the user has probably
+     * installed additional packages
+     */
+    include $autoloader;
+
+} elseif (is_file($autoloader = __DIR__ . '/vendor/autoload.php')) {
+
+    /**
+     * Fall back to the local autoloader if that exists
+     */
+    include $autoloader;
+
 } else {
-    // if neither one exists, don't bother searching
-    // it's a custom directory setup and the users need to load the autoloader themselves
+
+    /**
+     * If neither one exists, don't bother searching
+     * it's a custom directory setup and the users need to
+     * load the autoloader themselves
+     */
+
 }
 
 define('DS', '/');
