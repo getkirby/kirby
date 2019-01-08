9548029a8d4256ad25fe2314f523fe14d4c19409
Bastian Allgeier <mail@bastianallgeier.com>
2019-01-08 13:39:29 +0100

    Add latest dist files

--- config/tests.php
+++ config/tests.php
@@ -3,9 +3,7 @@
 $testDir = dirname(__DIR__) . '/tests';
 
 if (is_dir($testDir) === true) {
-
     spl_autoload_register(function ($className) use ($testDir) {
-
         $path = str_replace('Kirby\\', '', $className);
         $path = str_replace('\\', '/', $path);
         $file = $testDir . '/' . $path . '.php';
@@ -13,8 +11,5 @@ if (is_dir($testDir) === true) {
         if (file_exists($file)) {
             include $file;
         }
-
     });
-
 }
-
