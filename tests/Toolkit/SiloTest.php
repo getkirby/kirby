fe290401a66dde0d306b994dd92be73f87b53e3c
Bastian Allgeier <mail@bastianallgeier.com>
2019-01-29 13:25:15 +0100

    Add Silo tests

---
+++ tests/Toolkit/SiloTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Kirby\Toolkit;
+
+use PHPUnit\Framework\TestCase;
+
+class SiloTest extends TestCase
+{
+    public function setUp()
+    {
+        Silo::$data = [];
+    }
+
+    public function testSetAndGet()
+    {
+        Silo::set('foo', 'bar');
+        $this->assertEquals('bar', Silo::get('foo'));
+    }
+
+    public function testSetArray()
+    {
+        Silo::set([
+            'a' => 'A',
+            'b' => 'B'
+        ]);
+
+        $this->assertEquals(['a' => 'A', 'b' => 'B'], Silo::get());
+    }
+
+    public function testGetArray()
+    {
+        Silo::set('a', 'A');
+        Silo::set('b', 'B');
+
+        $this->assertEquals(['a' => 'A', 'b' => 'B'], Silo::get());
+    }
+
+    public function testRemoveByKey()
+    {
+        Silo::set('a', 'A');
+        $this->assertEquals('A', Silo::get('a'));
+        Silo::remove('a');
+        $this->assertNull(Silo::get('a'));
+    }
+
+    public function testRemoveAll()
+    {
+        Silo::set('a', 'A');
+        Silo::set('b', 'B');
+        $this->assertEquals('A', Silo::get('a'));
+        $this->assertEquals('B', Silo::get('b'));
+        Silo::remove();
+        $this->assertNull(Silo::get('a'));
+        $this->assertNull(Silo::get('b'));
+    }
+}
