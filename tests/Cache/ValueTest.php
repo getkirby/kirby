ac2c94b5673b78768139cadc5a4a4ac5291c3972
Bastian Allgeier <mail@bastianallgeier.com>
2019-04-18 11:38:48 +0200

    More Cache Value tests

--- tests/Cache/ValueTest.php
+++ tests/Cache/ValueTest.php
@@ -19,4 +19,36 @@ class ValueTest extends TestCase
         $value = new Value('foo', 100);
         $this->assertEquals(time() + (100 * 60), $value->expires());
     }
+
+    public function testFromArray()
+    {
+        $value = Value::fromArray([
+            'value'   => 'foo',
+            'minutes' => 1,
+            'created' => 0
+        ]);
+
+        $this->assertEquals('foo', $value->value());
+        $this->assertEquals(0, $value->created());
+        $this->assertEquals(60, $value->expires());
+    }
+
+    public function testFromJson()
+    {
+        $value = Value::fromJson(json_encode([
+            'value'   => 'foo',
+            'minutes' => 1,
+            'created' => 0
+        ]));
+
+        $this->assertEquals('foo', $value->value());
+        $this->assertEquals(0, $value->created());
+        $this->assertEquals(60, $value->expires());
+    }
+
+    public function testFromInvalidJson()
+    {
+        $value = Value::fromJson('foo');
+        $this->assertNull($value->value());
+    }
 }
