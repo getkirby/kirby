5aa0c08668fa3f2fe1948ca3463cc421dd1d0bcd
Lukas Bestle <mail@lukasbestle.com>
2019-04-22 10:57:42 +0200

    Exception: Code comments and doc blocks

--- src/Exception/DuplicateException.php
+++ src/Exception/DuplicateException.php
@@ -2,6 +2,17 @@
 
 namespace Kirby\Exception;
 
+/**
+ * DuplicateException
+ * Thrown when an object could not be created
+ * because it already exists
+ *
+ * @package   Kirby Exception
+ * @author    Nico Hoffmann <nico@getkirby.com>
+ * @link      http://getkirby.com
+ * @copyright Bastian Allgeier
+ * @license   MIT
+ */
 class DuplicateException extends Exception
 {
     protected static $defaultKey = 'duplicate';
