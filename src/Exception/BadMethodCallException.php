b4650809217edd2ef0f9d8034c323b3da5b40dd0
Lukas Bestle <mail@lukasbestle.com>
2019-04-22 10:57:42 +0200

    Exception: Code comments and doc blocks

--- src/Exception/BadMethodCallException.php
+++ src/Exception/BadMethodCallException.php
@@ -2,6 +2,16 @@
 
 namespace Kirby\Exception;
 
+/**
+ * BadMethodCallException
+ * Thrown when a method was called that does not exist
+ *
+ * @package   Kirby Exception
+ * @author    Nico Hoffmann <nico@getkirby.com>
+ * @link      http://getkirby.com
+ * @copyright Bastian Allgeier
+ * @license   MIT
+ */
 class BadMethodCallException extends Exception
 {
     protected static $defaultKey = 'invalidMethod';
--- src/Exception/PermissionException.php
+++ src/Exception/PermissionException.php
@@ -2,6 +2,17 @@
 
 namespace Kirby\Exception;
 
+/**
+ * PermissionException
+ * Thrown when the current user has insufficient
+ * permissions for the action
+ *
+ * @package   Kirby Exception
+ * @author    Nico Hoffmann <nico@getkirby.com>
+ * @link      http://getkirby.com
+ * @copyright Bastian Allgeier
+ * @license   MIT
+ */
 class PermissionException extends Exception
 {
     protected static $defaultKey = 'permission';
