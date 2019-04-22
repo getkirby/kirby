e841a8b606648c952fa1bb1f52a7c4f3a342f06b
Lukas Bestle <mail@lukasbestle.com>
2019-04-22 10:57:42 +0200

    Exception: Code comments and doc blocks

--- src/Exception/NotFoundException.php
+++ src/Exception/NotFoundException.php
@@ -2,6 +2,16 @@
 
 namespace Kirby\Exception;
 
+/**
+ * NotFoundException
+ * Thrown when something was not found
+ *
+ * @package   Kirby Exception
+ * @author    Nico Hoffmann <nico@getkirby.com>
+ * @link      http://getkirby.com
+ * @copyright Bastian Allgeier
+ * @license   MIT
+ */
 class NotFoundException extends Exception
 {
     protected static $defaultKey = 'notFound';
