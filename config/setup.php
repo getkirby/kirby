4a87ad35b8ad5954aba6f01cb37699b3d236fb3a
Lukas Bestle <mail@lukasbestle.com>
2019-04-23 19:37:45 +0200

    Reduce number of autoloaded config files

--- config/setup.php
+++ config/setup.php
@@ -1,7 +1,19 @@
 <?php
 
-$aliases = [
+/**
+ * Constants
+ */
+define('DS', '/');
 
+/**
+ * Load files that can't be autoloaded
+ */
+require_once __DIR__ . '/helpers.php';
+
+/**
+ * Class aliases
+ */
+$aliases = [
     // cms classes
     'asset'      => 'Kirby\Cms\Asset',
     'collection' => 'Kirby\Cms\Collection',
@@ -64,3 +76,20 @@ spl_autoload_register(function ($class) use ($aliases) {
         class_alias($aliases[$class], $class);
     }
 });
+
+/**
+ * Tests
+ */
+$testDir = dirname(__DIR__) . '/tests';
+
+if (is_dir($testDir) === true) {
+    spl_autoload_register(function ($className) use ($testDir) {
+        $path = str_replace('Kirby\\', '', $className);
+        $path = str_replace('\\', '/', $path);
+        $file = $testDir . '/' . $path . '.php';
+
+        if (file_exists($file)) {
+            include $file;
+        }
+    });
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,6 +6,5 @@ ini_set('display_errors', 'on');
 ini_set('display_startup_errors', 'on');
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../config/aliases.php';
 
 define('KIRBY_TESTING', true);
