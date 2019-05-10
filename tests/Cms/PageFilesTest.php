d7cd044979866ee2c06515fc593bdf58d0f308aa
Nico Hoffmann <on-github@nhoffmann.com>
2019-05-10 16:40:20 +0200

    Add unit test for page files

--- tests/Cms/PageFilesTest.php
+++ tests/Cms/PageFilesTest.php
@@ -33,4 +33,33 @@ class PageFilesTest extends TestCase
         $this->assertInstanceOf(Files::class, $page->files());
         $this->assertCount(1, $page->files());
     }
+
+    public function testImages()
+    {
+        $page = new Page([
+            'slug'  => 'test',
+            'files' => [
+                ['filename' => 'test.ai'],
+                ['filename' => 'test.bmp'],
+                ['filename' => 'test.gif'],
+                ['filename' => 'test.eps'],
+                ['filename' => 'test.ico'],
+                ['filename' => 'test.jpeg'],
+                ['filename' => 'test.jpg'],
+                ['filename' => 'test.jpe'],
+                ['filename' => 'test.png'],
+                ['filename' => 'test.ps'],
+                ['filename' => 'test.psd'],
+                ['filename' => 'test.svg'],
+                ['filename' => 'test.tif'],
+                ['filename' => 'test.tiff'],
+                ['filename' => 'test.webp'],
+                ['filename' => 'test.txt'],
+                ['filename' => 'test.doc'],
+            ]
+        ]);
+
+        $this->assertInstanceOf(Files::class, $page->images());
+        $this->assertCount(15, $page->images());
+    }
 }
