94fdbe30b336adde24484b7ddc1cd0275ff344cc
Lukas Bestle <mail@lukasbestle.com>
2019-05-18 15:34:19 +0200

    File system locking for .lock files

--- src/Cms/ContentLocks.php
+++ src/Cms/ContentLocks.php
@@ -2,16 +2,16 @@
 
 namespace Kirby\Cms;
 
-use Kirby\Data\Data;
+use Kirby\Data\Yaml;
+use Kirby\Exception\Exception;
 use Kirby\Toolkit\F;
-
-use Throwable;
 
 /**
  * Manages all content lock files
  *
  * @package   Kirby Cms
- * @author    Nico Hoffmann <nico@getkirby.com>
+ * @author    Nico Hoffmann <nico@getkirby.com>,
+ *            Lukas Bestle <lukas@getkirby.com>
  * @link      https://getkirby.com
  * @copyright Bastian Allgeier GmbH
  * @license   https://getkirby.com/license
@@ -27,6 +27,47 @@ class ContentLocks
      * @var array
      */
     protected $data = [];
+
+    /**
+     * PHP file handles for all currently
+     * open `.lock` files
+     *
+     * @var array
+     */
+    protected $handles = [];
+
+    /**
+     * Closes the open file handles
+     *
+     * @codeCoverageIgnore
+     */
+    public function __destruct()
+    {
+        foreach ($this->handles as $file => $handle) {
+            $this->closeHandle($file);
+        }
+    }
+
+    /**
+     * Removes the file lock and closes the file handle
+     *
+     * @param string $file
+     * @return void
+     */
+    protected function closeHandle(string $file)
+    {
+        if (!isset($this->handles[$file])) {
+            return;
+        }
+        $handle = $this->handles[$file];
+
+        $result = flock($handle, LOCK_UN) && fclose($handle);
+        if ($result !== true) {
+            throw new Exception('Unexpected file system error.'); // @codeCoverageIgnore
+        }
+
+        unset($this->handles[$file]);
+    }
 
     /**
      * Returns the path to a model's lock file
@@ -55,15 +96,64 @@ class ContentLocks
             return $this->data[$file][$id] ?? [];
         }
 
-        try {
-            $data = Data::read($file, 'yaml');
-        } catch (Throwable $th) {
+        // first get a handle to ensure a file system lock
+        $handle = $this->handle($file);
+
+        if (is_resource($handle)) {
+            // read data from file
+            clearstatcache();
+            $filesize = filesize($file);
+            if ($filesize > 0) {
+                // always read the whole file
+                rewind($handle);
+                $string = fread($handle, $filesize);
+
+                $data = Yaml::decode($string);
+            } else {
+                // we don't need to read empty files
+                $data = [];
+            }
+        } else {
+            // file does not already exist
             $data = [];
         }
 
         $this->data[$file] = $data;
 
         return $this->data[$file][$id] ?? [];
+    }
+
+    /**
+     * Returns the file handle to a `.lock` file
+     *
+     * @param string $file
+     * @param boolean $create Whether to create the file if it does not exist
+     * @return resource|null File handle
+     */
+    protected function handle(string $file, bool $create = false)
+    {
+        // check for an already open handle
+        if (isset($this->handles[$file])) {
+            return $this->handles[$file];
+        }
+
+        // don't create a file if not requested
+        if (is_file($file) !== true && $create !== true) {
+            return null;
+        }
+
+        $handle = @fopen($file, 'c+b');
+        if (!is_resource($handle)) {
+            throw new Exception('Lock file ' . $file . ' could not be opened.'); // @codeCoverageIgnore
+        }
+
+        // lock the lock file exclusively to prevent changes by other threads
+        $result = flock($handle, LOCK_EX);
+        if ($result !== true) {
+            throw new Exception('Unexpected file system error.'); // @codeCoverageIgnore
+        }
+
+        return $this->handles[$file] = $handle;
     }
 
     /**
@@ -87,8 +177,9 @@ class ContentLocks
      */
     public function set(ModelWithContent $model, array $data): bool
     {
-        $file = static::file($model);
-        $id   = static::id($model);
+        $file   = static::file($model);
+        $id     = static::id($model);
+        $handle = $this->handle($file, true);
 
         $this->data[$file][$id] = $data;
 
@@ -113,9 +204,27 @@ class ContentLocks
 
         // there is no data left in the file whatsoever, delete the file
         if (count($this->data[$file]) === 0) {
+            unset($this->data[$file]);
+
+            // close the file handle, otherwise we can't delete it on Windows
+            $this->closeHandle($file);
+
             return F::remove($file);
         }
 
-        return Data::write($file, $this->data[$file], 'yaml');
+        $yaml = Yaml::encode($this->data[$file]);
+
+        // delete all file contents first
+        if (rewind($handle) !== true || ftruncate($handle, 0) !== true) {
+            throw new Exception('Could not write lock file ' . $file . '.'); // @codeCoverageIgnore
+        }
+
+        // write the new contents
+        $result = fwrite($handle, $yaml);
+        if (!is_int($result) || $result === 0) {
+            throw new Exception('Could not write lock file ' . $file . '.'); // @codeCoverageIgnore
+        }
+
+        return true;
     }
 }
