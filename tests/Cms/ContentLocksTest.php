6ac04d870d628a84ee0f9b5e4ed5dc4a869ad5a8
Nico Hoffmann <on-github@nhoffmann.com>
2019-05-19 12:27:31 +0200

    Add tests for content locking

---
+++ tests/Cms/ContentLocksTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Kirby\Cms;
+
+use PHPUnit\Framework\TestCase;
+use Kirby\Toolkit\F;
+use Kirby\Toolkit\Str;
+
+class ContentLocksTest extends TestCase
+{
+    protected $app;
+    protected $fixtures;
+
+    public function app()
+    {
+        return new App([
+            'roots' => [
+               'index' => $this->fixtures = __DIR__ . '/fixtures/ContentLocksTest'
+            ],
+            'site' => [
+                'children' => [
+                    [
+                        'slug'  => 'test'
+                    ]
+                ]
+            ]
+        ]);
+    }
+
+    public function setUp(): void
+    {
+        $this->app = $this->app();
+        Dir::make($this->fixtures);
+    }
+
+    public function tearDown(): void
+    {
+        Dir::remove($this->fixtures);
+    }
+
+    public function testFile()
+    {
+        $app = $this->app;
+        $page = $app->page('test');
+        $this->assertTrue(Str::endsWith($app->locks()->file($page), 'content/test/.lock'));
+    }
+
+    public function testId()
+    {
+        $app = $this->app;
+        $page = $app->page('test');
+        $this->assertEquals('/test', $app->locks()->id($page));
+    }
+
+    public function testGetSet()
+    {
+        $app = $this->app;
+        $page = $app->page('test');
+        $root = $this->fixtures . '/content/test';
+
+        // create fixtures directory
+        $this->assertEquals($root . '/.lock', $app->locks()->file($page));
+        Dir::make($root);
+
+        // check if empty
+        $this->assertEquals([], $app->locks()->get($page));
+        $this->assertFalse(F::exists($app->locks()->file($page)));
+
+        // set data
+        $this->assertTrue($app->locks()->set($page, [
+            'lock'   => ['user' => 'homer'],
+            'unlock' => []
+        ]));
+
+        // check if exists
+        $this->assertTrue(F::exists($app->locks()->file($page)));
+        $this->assertEquals([
+            'lock' => ['user' => 'homer']
+        ], $app->locks()->get($page));
+
+        // set null data
+        $this->assertTrue($app->locks()->set($page, []));
+
+        // check if empty
+        $this->assertEquals([], $app->locks()->get($page));
+        $this->assertFalse(F::exists($app->locks()->file($page)));
+    }
+}
