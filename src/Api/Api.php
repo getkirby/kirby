7a410b78f657ebaa9c7b976de0510c14007be61f
Bastian Allgeier <mail@bastianallgeier.com>
2019-09-13 11:56:29 +0200

    Check for exceeded post_max_size limit on uploads #1812

--- src/Api/Api.php
+++ src/Api/Api.php
@@ -690,7 +690,14 @@ class Api
         ];
 
         if (empty($files) === true) {
-            throw new Exception(t('upload.error.noFiles'));
+            $postMaxSize       = 1024 * 1024 * (int)ini_get('post_max_size');
+            $uploadMaxFileSize = 1024 * 1024 * (int)ini_get('upload_max_filesize');
+
+            if ($postMaxSize < $uploadMaxFileSize) {
+                throw new Exception(t('upload.error.iniPostSize'));
+            } else {
+                throw new Exception(t('upload.error.noFiles'));
+            }
         }
 
         foreach ($files as $upload) {
