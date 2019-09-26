f8a6866c7e1bfafba7b0aec3329164a49afc42eb
Bastian Allgeier <mail@bastianallgeier.com>
2019-09-26 16:03:40 +0200

    Fix vimeo and youtube helper options #2118

--- src/Toolkit/Html.php
+++ src/Toolkit/Html.php
@@ -465,7 +465,7 @@ class Html
         }
 
         // build the options query
-        if (!empty($options)) {
+        if (empty($options) === false) {
             $query = '?' . http_build_query($options);
         } else {
             $query = '';
