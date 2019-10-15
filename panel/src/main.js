7600a710267f43c3d62a7db1bd1a776cbbc8082f
Bastian Allgeier <mail@bastianallgeier.com>
2019-10-15 08:44:06 +0200

    Remove outdated tab directive #566

--- panel/src/main.js
+++ panel/src/main.js
@@ -1,5 +1,4 @@
 import App from "./App.vue";
-import Directives from "./config/directives.js";
 import Filters from "./config/filters.js";
 import Events from "./config/events.js";
 import Vue from "vue";
@@ -16,7 +15,6 @@ import "./config/i18n.js";
 import "./config/libraries.js";
 import "./config/plugins.js";
 
-Vue.use(Directives);
 Vue.use(Events);
 Vue.use(Filters);
 Vue.use(Vuelidate);
