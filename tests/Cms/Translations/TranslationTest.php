b1034fcee000514de8b1e9b70ceaa91d07fef39c
Lukas Bestle <mail@lukasbestle.com>
2019-11-10 20:06:53 +0100

    Translation: New locale field #2233

--- tests/Cms/Translations/TranslationTest.php
+++ tests/Cms/Translations/TranslationTest.php
@@ -6,16 +6,18 @@ class TranslationTest extends TestCase
 {
     public function testProps()
     {
-        $translation = new Translation('de', [
+        $translation = new Translation('en', [
             'translation.author'    => 'Kirby',
             'translation.name'      => 'English',
             'translation.direction' => 'ltr',
+            'translation.locale'    => 'en_GB',
             'test'                  => 'Test'
         ]);
 
         $this->assertEquals('Kirby', $translation->author());
         $this->assertEquals('English', $translation->name());
         $this->assertEquals('ltr', $translation->direction());
+        $this->assertEquals('en_GB', $translation->locale());
         $this->assertEquals('Test', $translation->get('test'));
     }
 
