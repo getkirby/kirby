7a931d3e48c12b2f2936f644136a9c59677376f3
Bastian Allgeier <mail@bastianallgeier.com>
2020-04-14 14:34:58 +0200

    Remove Dialog

---
+++ panel/src/ui/components/RemoveDialog.stories.js
@@ -0,0 +1,107 @@
+import RemoveDialog from "./RemoveDialog.vue";
+import { action } from "@storybook/addon-actions";
+
+export default {
+  title: "Dialog / Remove Dialog",
+  component: RemoveDialog
+};
+
+export const regular = () => ({
+  methods: {
+    cancel: action("cancel"),
+    close: action("close"),
+    open: action("open"),
+    submit: action("submit")
+  },
+  template: `
+    <div>
+      <k-button icon="open" @click="$refs.dialog.open()">Open Dialog</k-button>
+
+      <k-remove-dialog
+        ref="dialog"
+        text="Do you really want to delete this item?"
+        @cancel="cancel"
+        @close="close"
+        @open="open"
+        @submit="submit"
+      />
+    </div>
+  `,
+});
+
+export const open = () => ({
+  methods: {
+    cancel: action("cancel"),
+    close: action("close"),
+    open: action("open"),
+    submit: action("submit")
+  },
+  template: `
+    <div>
+      <k-button icon="open" @click="$refs.dialog.open()">Open Dialog</k-button>
+
+      <k-remove-dialog
+        ref="dialog"
+        :visible="true"
+        text="Do you really want to delete this item?"
+        @cancel="cancel"
+        @close="close"
+        @open="open"
+        @submit="submit"
+      />
+    </div>
+  `,
+});
+
+export const errorNotification = () => ({
+  methods: {
+    cancel: action("cancel"),
+    close: action("close"),
+    open: action("open"),
+    submit() {
+      action("submit");
+      this.$refs.dialog.error("Something went wrong");
+    }
+  },
+  template: `
+    <div>
+      <k-button icon="open" @click="$refs.dialog.open()">Open Dialog</k-button>
+      <k-remove-dialog
+        ref="dialog"
+        text="Click confirm to raise an error"
+        @cancel="cancel"
+        @close="close"
+        @open="open"
+        @submit="submit"
+      />
+    </div>
+  `,
+});
+
+export const successNotification = () => ({
+  methods: {
+    cancel: action("cancel"),
+    close: action("close"),
+    open: action("open"),
+    submit() {
+      action("submit");
+      this.$refs.dialog.success("Yayayay!");
+    }
+  },
+  template: `
+    <div>
+      <k-button icon="open" @click="$refs.dialog.open()">Open Dialog</k-button>
+      <k-remove-dialog
+        ref="dialog"
+        text="Click confirm to raise a success message"
+        @cancel="cancel"
+        @close="close"
+        @open="open"
+        @submit="submit"
+      />
+    </div>
+  `,
+});
+
+
+
---
+++ panel/src/ui/components/RemoveDialog.vue
@@ -0,0 +1,23 @@
+<script>
+import Dialog from "./Dialog.vue";
+
+export default {
+  extends: Dialog,
+  props: {
+    icon: {
+      type: String,
+      default: "trash"
+    },
+    submitButton: {
+      type: [String, Boolean],
+      default() {
+        return this.$t("delete");
+      }
+    },
+    theme: {
+      type: String,
+      default: "negative"
+    }
+  }
+}
+</script>
