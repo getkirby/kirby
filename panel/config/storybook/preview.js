96de469472894c960b187aa554155225bc54cbfb
Nico Hoffmann <on-github@nhoffmann.com>
2020-04-18 21:51:38 +0200

    Storybook: sort stories

--- panel/config/storybook/preview.js
+++ panel/config/storybook/preview.js
@@ -15,6 +15,10 @@ addParameters({
   docs: {
     inlineStories: true,
   },
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
 });
 
 addDecorator(() => {
