20a56ec4646114ace89e1d3ee566c6bfd44250c3
Bastian Allgeier <mail@bastianallgeier.com>
2020-04-30 16:52:29 +0200

    New page dropdown

---
+++ panel/src/ui/components/PageDropdown.stories.js
@@ -0,0 +1,33 @@
+import PageDropdown from "./PageDropdown.vue";
+import Padding from "../storybook/Padding.js";
+import { action } from "@storybook/addon-actions";
+
+export default {
+  title: "UI | Dropdown / Page Dropdown",
+  decorators: [Padding],
+  component: PageDropdown
+};
+
+export const regular = () => ({
+  data() {
+    return {
+      page: 1,
+      pages: 50,
+    };
+  },
+  methods: {
+    onChange(page) {
+      action("change")(page);
+      this.page = page;
+    }
+  },
+  template: `
+    <k-page-dropdown
+      :page="page"
+      :pages="pages"
+      :text="'Page: ' + page + ' / ' + pages"
+      @change="onChange"
+    />
+  `
+});
+
