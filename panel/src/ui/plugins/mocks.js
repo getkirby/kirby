c37463a1a2214d3e8fefadd20efd29d2127a244b
Bastian Allgeier <mail@bastianallgeier.com>
2020-05-06 17:41:32 +0200

    Simple store dispatch and commit mocks

--- panel/src/ui/plugins/mocks.js
+++ panel/src/ui/plugins/mocks.js
@@ -13,11 +13,11 @@ export default {
       state: {
 
       },
-      dispatch() {
-
+      dispatch(action, ...args) {
+        console.log(`store.dispatch(${action}, ${args[0]})`);
       },
       commit() {
-
+        console.log(`store.commit(${action}, ${args[0]})`);
       }
     };
 
