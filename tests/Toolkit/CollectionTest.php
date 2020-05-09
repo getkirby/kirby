a01afe3c6c75cd6355ca49f0da00212f574a56dc
Lukas Bestle <mail@lukasbestle.com>
2020-05-09 15:30:08 +0200

    Collection: Add case-sensitive mode

--- tests/Toolkit/CollectionTest.php
+++ tests/Toolkit/CollectionTest.php
@@ -75,6 +75,53 @@ class CollectionTest extends TestCase
         $this->assertEquals(['A', 'B', 'C'], $collection->values());
     }
 
+    public function testCaseSensitive()
+    {
+        $normalCollection = new Collection([
+            'lowercase' => 'test1',
+            'UPPERCASE' => 'test2',
+            'MiXeD'     => 'test3'
+        ]);
+        $normalCollection->set('AnOtHeR', 'test4');
+
+        $this->assertSame([
+            'lowercase' => 'test1',
+            'uppercase' => 'test2',
+            'mixed'     => 'test3',
+            'another'   => 'test4'
+        ], $normalCollection->data());
+        $this->assertSame('test1', $normalCollection->get('lowercase'));
+        $this->assertSame('test2', $normalCollection->get('UPPERCASE'));
+        $this->assertSame('test3', $normalCollection->get('MiXeD'));
+        $this->assertSame('test4', $normalCollection->get('AnOtHeR'));
+        $this->assertSame('test1', $normalCollection->get('LowerCase'));
+        $this->assertSame('test2', $normalCollection->get('uppercase'));
+        $this->assertSame('test3', $normalCollection->get('mIxEd'));
+        $this->assertSame('test4', $normalCollection->get('another'));
+
+        $sensitiveCollection = new Collection([
+            'lowercase' => 'test1',
+            'UPPERCASE' => 'test2',
+            'MiXeD'     => 'test3'
+        ], true);
+        $sensitiveCollection->set('AnOtHeR', 'test4');
+
+        $this->assertSame([
+            'lowercase' => 'test1',
+            'UPPERCASE' => 'test2',
+            'MiXeD'     => 'test3',
+            'AnOtHeR'   => 'test4'
+        ], $sensitiveCollection->data());
+        $this->assertSame('test1', $sensitiveCollection->get('lowercase'));
+        $this->assertSame('test2', $sensitiveCollection->get('UPPERCASE'));
+        $this->assertSame('test3', $sensitiveCollection->get('MiXeD'));
+        $this->assertSame('test4', $sensitiveCollection->get('AnOtHeR'));
+        $this->assertNull($sensitiveCollection->get('Lowercase'));
+        $this->assertNull($sensitiveCollection->get('uppercase'));
+        $this->assertNull($sensitiveCollection->get('mixed'));
+        $this->assertNull($sensitiveCollection->get('another'));
+    }
+
     public function testCount()
     {
         $this->assertEquals(3, $this->collection->count());
