297b3681d51d9412177aa8683d5c5edab01f4724
Bastian Allgeier <mail@bastianallgeier.com>
2020-05-25 17:13:46 +0200

    The registration dialog has its own stories now

---
+++ panel/src/app/components/RegistrationDialog.stories.js
@@ -0,0 +1,22 @@
+import { action } from "@storybook/addon-actions";
+import Padding from "../../../storybook/theme/Padding.js";
+import RegistrationDialog from "./RegistrationDialog.vue";
+
+export default {
+  title: "App | Dialogs / Registration Dialog",
+  component: RegistrationDialog,
+  decorators: [Padding],
+};
+
+export const regular = () => ({
+  template: `
+    <div>
+      <k-button @click="$refs.dialog.open()" class="mb-8">Open</k-button>
+
+      <k-text theme="help">Use: <b>K3-test</b> for a successful registration.</k-text>
+
+
+      <k-registration-dialog ref="dialog" />
+    </div>
+  `
+});
