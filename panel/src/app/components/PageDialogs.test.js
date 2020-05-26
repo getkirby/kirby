6aa78e2988a0122f6d3d9471f129f5e712bc6f61
Nico Hoffmann <on-github@nhoffmann.com>
2020-05-26 23:02:51 +0200

    Cypress: a few more PageDialogs tests

--- panel/src/app/components/PageDialogs.test.js
+++ panel/src/app/components/PageDialogs.test.js
@@ -5,6 +5,8 @@ const prepare = () => {
   cy.get("[data-cy=pages]").children().as("pages");
   cy.get("@pages").first().as("page");
   cy.get("@page").find(".k-options-dropdown").as("options");
+  cy.get(".k-code-block").as("code");
+
 }
 
 describe("PageDialogs", () => {
@@ -21,21 +23,56 @@ describe("PageRenameDialog", () => {
   beforeEach(prepare);
 
   it("cancel", () => {
+    cy.get("@code").should("contains.text", '"title": "Animals"');
     cy.get("@page").should("contains.text", "Animals");
     cy.get("@options").click();
     cy.get("@options").contains("Rename").click();
     cy.get("input[name=title]").type("foo");
     cy.get(".k-dialog-cancel-button").click();
+    cy.get("@code").should("contains.text", '"title": "Animals"');
     cy.get("@page").should("contains.text", "Animals");
   });
 
   it("submit", () => {
+    cy.get("@code").should("contains.text", '"title": "Animals"');
     cy.get("@page").should("contains.text", "Animals");
     cy.get("@options").click();
     cy.get("@options").contains("Rename").click();
     cy.get("input[name=title]").type("foo");
     cy.get(".k-dialog-submit-button").click();
+    cy.get("@code").should("contains.text", '"title": "foo"');
     cy.get("@page").should("contains.text", "foo");
+  });
+})
+
+describe("PageStatusDialog", () => {
+  beforeEach(prepare);
+
+  it("cancel", () => {
+    cy.get("@code").should("contains.text", '"status": "listed"');
+    cy.get("@options").click();
+    cy.get("@options").contains("Change status").click();
+    cy.get(".k-radio-input label").first().click();
+    cy.get(".k-dialog-cancel-button").click();
+    cy.get("@code").should("contains.text", '"status": "listed"');
+  });
+
+  it("submit as draft", () => {
+    cy.get("@code").should("contains.text", '"status": "listed"');
+    cy.get("@options").click();
+    cy.get("@options").contains("Change status").click();
+    cy.get(".k-radio-input label").first().click();
+    cy.get(".k-dialog-submit-button").click();
+    cy.get("@code").should("contains.text", '"status": "draft"');
+  });
+
+  it("submit as unlisted", () => {
+    cy.get("@code").should("contains.text", '"status": "listed"');
+    cy.get("@options").click();
+    cy.get("@options").contains("Change status").click();
+    cy.get(".k-radio-input label").eq(1).click();
+    cy.get(".k-dialog-submit-button").click();
+    cy.get("@code").should("contains.text", '"status": "unlisted"');
   });
 })
 
