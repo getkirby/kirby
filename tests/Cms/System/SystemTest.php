6b01f35a9728884d05b1f40b63aa0570455e516a
Lukas Bestle <mail@lukasbestle.com>
2020-06-08 21:25:51 +0200

    New System::updateStatus() method

--- tests/Cms/System/SystemTest.php
+++ tests/Cms/System/SystemTest.php
@@ -2,9 +2,14 @@
 
 namespace Kirby\Cms;
 
+use Kirby\Cms\System\RemoteMock;
 use Kirby\Toolkit\Dir;
 use ReflectionClass;
+use ReflectionProperty;
 
+/**
+ * @coversDefaultClass Kirby\Cms\System
+ */
 class SystemTest extends TestCase
 {
     protected $_SERVER = null;
@@ -27,6 +32,9 @@ class SystemTest extends TestCase
         $_SERVER = $this->_SERVER;
 
         Dir::remove($this->fixtures);
+
+        RemoteMock::$mockContent = '';
+        RemoteMock::$mockCode = 200;
     }
 
     public function serverProvider()
@@ -224,5 +232,198 @@ class SystemTest extends TestCase
         $this->expectExceptionMessage('Please enter a valid email address');
 
         $system->register('K3-PRO-abc', 'invalid');
+    }
+
+    /**
+     * @covers ::updateStatus
+     */
+    public function testUpdateStatus()
+    {
+        $versionProperty = new ReflectionProperty('Kirby\Cms\App', 'version');
+        $versionProperty->setAccessible(true);
+        $versionProperty->setValue(null, '3.4.5');
+
+        $data = [
+            'latest'    => '3.4.5',
+            'latestUrl' => 'https://kirby.test/release/3.4.5',
+            'incidents' => []
+        ];
+        RemoteMock::$mockContent = json_encode($data);
+
+        $app = $this->app->clone([
+            'options' => [
+                'cache.system' => false
+            ]
+        ]);
+        $app->impersonate('kirby');
+        $system = new System($app, 'Kirby\Cms\System\RemoteMock');
+
+        // latest version
+        $status = $system->updateStatus();
+        $this->assertSame('ok', $status['status']);
+        $this->assertNull($status['severity']);
+        $this->assertSame('3.4.5', $status['current']);
+        $this->assertSame('3.4.5', $status['latest']);
+        $this->assertSame('https://kirby.test/release/3.4.5', $status['latestUrl']);
+        $this->assertSame([], $status['incidents']);
+
+        // outdated version
+        $data['latest'] = '3.4.6';
+        RemoteMock::$mockContent = json_encode($data);
+        $status = $system->updateStatus();
+        $this->assertSame('outdated', $status['status']);
+        $this->assertNull($status['severity']);
+        $this->assertSame('3.4.5', $status['current']);
+        $this->assertSame('3.4.6', $status['latest']);
+        $this->assertSame([], $status['incidents']);
+
+        // incidents
+        $data['incidents'] = [
+            [
+                'affected'    => '>3.0.0 <=3.5.0',
+                'description' => 'Incident 1',
+                'fixed'       => '3.1.2',
+                'severity'    => 'notable'
+            ],
+            [
+                'affected'    => '<=3.1.0',
+                'description' => 'Should be filtered out',
+                'fixed'       => '3.1.2',
+                'severity'    => 'minor'
+            ],
+            [
+                'affected'    => '>3.0.0 invalid-constraint',
+                'description' => 'Incident 2',
+                'fixed'       => '3.1.2',
+                'severity'    => 'minor'
+            ],
+            [
+                'affected'    => '<3.5.0',
+                'description' => 'Incident 3',
+                'fixed'       => '3.5.0',
+                'severity'    => 'invalid'
+            ]
+        ];
+        RemoteMock::$mockContent = json_encode($data);
+        $status = $system->updateStatus();
+        $this->assertSame('at-risk', $status['status']);
+        $this->assertSame('notable', $status['severity']);
+        $this->assertSame('3.4.5', $status['current']);
+        $this->assertSame('3.4.6', $status['latest']);
+        $this->assertSame([
+            [
+                'affected'    => '>3.0.0 <=3.5.0',
+                'description' => 'Incident 1',
+                'fixed'       => '3.1.2',
+                'severity'    => 'notable'
+            ],
+            [
+                'affected'    => '>3.0.0 invalid-constraint',
+                'description' => 'Incident 2',
+                'fixed'       => '3.1.2',
+                'severity'    => 'minor'
+            ],
+            [
+                'affected'    => '<3.5.0',
+                'description' => 'Incident 3',
+                'fixed'       => '3.5.0',
+                'severity'    => 'invalid'
+            ]
+        ], $status['incidents']);
+
+        // disabled update option I
+        $app = $this->app->clone([
+            'options' => [
+                'cache.system' => false,
+                'update'       => 'manual'
+            ]
+        ]);
+        $app->impersonate('kirby');
+        $system = new System($app, 'Kirby\Cms\System\RemoteMock');
+        $this->assertNull($system->updateStatus());
+        $this->assertIsArray($system->updateStatus(true));
+
+        // disabled update option II
+        $app = $this->app->clone([
+            'options' => [
+                'cache.system' => false,
+                'update.kirby' => false
+            ]
+        ]);
+        $app->impersonate('kirby');
+        $system = new System($app, 'Kirby\Cms\System\RemoteMock');
+        $this->assertFalse($system->updateStatus());
+        $this->assertFalse($system->updateStatus(true));
+
+        // disabled update option III
+        $app = $this->app->clone([
+            'options' => [
+                'cache.system' => false,
+                'update.kirby' => 'manual'
+            ]
+        ]);
+        $app->impersonate('kirby');
+        $system = new System($app, 'Kirby\Cms\System\RemoteMock');
+        $this->assertNull($system->updateStatus());
+        $this->assertIsArray($system->updateStatus(true));
+
+        // cache
+        $app = $this->app->clone([
+            'options' => [
+                'cache.system' => [
+                    'type' => 'memory'
+                ]
+            ]
+        ]);
+        $app->impersonate('kirby');
+        $system = new System($app, 'Kirby\Cms\System\RemoteMock');
+        $app->cache('system')->set('updateStatus', $cached = ['current' => '3.4.5', 'latest' => '3.5.0']);
+
+        // use cached data if the version is the same
+        $this->assertSame($cached, $system->updateStatus());
+
+        // always get current data if requested
+        $this->assertSame('3.4.6', $system->updateStatus(true)['latest']);
+
+        // the new data should be cached
+        $status = $system->updateStatus();
+        $this->assertNotSame($cached, $status);
+        $this->assertSame('3.4.5', $status['current']);
+
+        // update data if the version has changed
+        $versionProperty->setValue(null, '3.4.6');
+        $this->assertSame('3.4.6', $system->updateStatus()['current']);
+    }
+
+    /**
+     * @covers ::updateStatus
+     */
+    public function testUpdateStatusNoUser()
+    {
+        $this->expectException('Kirby\Exception\PermissionException');
+        $this->expectExceptionMessage('You are not allowed to do this');
+
+        $system = new System($this->app, 'Kirby\Cms\System\RemoteMock');
+
+        $system->updateStatus();
+    }
+
+    /**
+     * @covers ::updateStatus
+     */
+    public function testUpdateStatusNoPermission()
+    {
+        $this->expectException('Kirby\Exception\PermissionException');
+        $this->expectExceptionMessage('You are not allowed to do this');
+
+        $app = $this->app->clone([
+            'user' => 'user@domain.com',
+            'users' => [
+                ['email' => 'user@domain.com', 'role' => 'editor'],
+            ]
+        ]);
+        $system = new System($app, 'Kirby\Cms\System\RemoteMock');
+
+        $system->updateStatus();
     }
 }
