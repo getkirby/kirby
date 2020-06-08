2c9ef152e73e24e22b87083ad13aa9feafadc6b5
Bastian Allgeier <mail@bastianallgeier.com>
2020-06-08 14:44:59 +0200

    Display system info in settings route

--- panel/src/app/components/SettingsRoute.vue
+++ panel/src/app/components/SettingsRoute.vue
@@ -1,11 +1,25 @@
 <template>
-  <k-settings-view />
+  <k-settings-view
+    :license="license"
+    :version="version"
+  />
 </template>
 
 <script>
 export default {
   created() {
     this.$model.system.title(this.$t("view.settings"));
+  },
+  computed: {
+    license() {
+      return this.$store.state.system.license;
+    },
+    multilang() {
+      return this.$store.state.system.multilang;
+    },
+    version() {
+      return this.$store.state.system.version;
+    }
   }
 };
 </script>
