159f7a9ca915b6eb9e930ad703050bcb544106aa
Nico Hoffmann <on-github@nhoffmann.com>
2020-06-09 22:39:27 +0200

    Fix icons from plugins

--- panel/src/ui/components/Icons.vue
+++ panel/src/ui/components/Icons.vue
@@ -981,7 +981,7 @@
         <path d="M15.8,4.8c-0.2-1.3-0.8-2.2-2.2-2.4C11.4,2,8,2,8,2S4.6,2,2.4,2.4C1,2.6,0.3,3.5,0.2,4.8C0,6.1,0,8,0,8 s0,1.9,0.2,3.2c0.2,1.3,0.8,2.2,2.2,2.4C4.6,14,8,14,8,14s3.4,0,5.6-0.4c1.4-0.3,2-1.1,2.2-2.4C16,9.9,16,8,16,8S16,6.1,15.8,4.8z M6,11V5l5,3L6,11z" />
       </symbol>
       <symbol
-        v-for="(icon, type) in $options.icons"
+        v-for="(icon, type) in icons"
         :id="'icon-' + type"
         :key="type"
         viewBox="0 0 16 16"
@@ -995,7 +995,7 @@
 export default {
   computed: {
     icons() {
-      if (window.panel && window.panel.plugins && window.plugins.icons) {
+      if (window.panel && window.panel.plugins && window.panel.plugins.icons) {
         return window.panel.plugins.icons
       }
 
