9a4dbb06a84b15c3d7b85e443de1b5b77b209515
Bastian Allgeier <mail@bastianallgeier.com>
2020-07-16 10:16:22 +0200

    New Vue.$helper.string.camelToKebab

--- panel/src/helpers/string.js
+++ panel/src/helpers/string.js
@@ -1,11 +1,13 @@
-
 export default {
-  ucfirst (string) {
-    const str = String(string);
-    return str.charAt(0).toUpperCase() + str.substr(1);
+  camelToKebab(string) {
+    return string.replace(/([a-z0-9])([A-Z])/g, "$1-$2").toLowerCase();
   },
-  lcfirst (string) {
+  lcfirst(string) {
     const str = String(string);
     return str.charAt(0).toLowerCase() + str.substr(1);
+  },
+  ucfirst(string) {
+    const str = String(string);
+    return str.charAt(0).toUpperCase() + str.substr(1);
   }
-} ;
+};
