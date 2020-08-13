aeb5e958b35aa39763eb183dbe756cd2ea978165
Lukas Bestle <lukas@getkirby.com>
2020-08-13 21:58:48 +0200

    Move mock TestCache class to mocks file

--- tests/Cache/mocks.php
+++ tests/Cache/mocks.php
@@ -17,3 +17,32 @@ function time(): int
 
     return 1337;
 }
+
+class TestCache extends Cache
+{
+    public $store = [];
+
+    public function set(string $key, $value, int $minutes = 0, int $created = null): bool
+    {
+        $value = new Value($value, $minutes, $created);
+        $this->store[$key] = $value;
+        return true;
+    }
+
+    public function retrieve(string $key): ?Value
+    {
+        return $this->store[$key] ?? null;
+    }
+
+    public function remove(string $key): bool
+    {
+        unset($this->store[$key]);
+        return true;
+    }
+
+    public function flush(): bool
+    {
+        $this->store = [];
+        return true;
+    }
+}
