7ac1e887d1242970ed34f5431291b7f7a2296ba7
Ahmet Bora <byybora@gmail.com>
2020-08-18 00:09:54 +0300

    Add missing search mixin for sections
    https://github.com/getkirby/ideas/issues/18

--- config/sections/mixins/search.php
+++ config/sections/mixins/search.php
@@ -2,6 +2,17 @@
 
 return [
     'props' => [
-
+        /**
+         * Enable/disable the search in the sections
+         */
+        'search' => function (bool $search = false) {
+            return $search;
+        },
+        /**
+         * Sets the default query for the sections
+         */
+        'query' => function (string $query = null) {
+            return get('query', $query);
+        }
     ]
 ];
