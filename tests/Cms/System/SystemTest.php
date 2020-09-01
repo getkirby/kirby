de2a5fb4d0ca7d054bcdcc975d9cfef22478b478
Ahmet Bora <byybora@gmail.com>
2020-09-02 00:24:26 +0300

    Add missing Cms unit tests to increase code coverage

--- tests/Cms/System/SystemTest.php
+++ tests/Cms/System/SystemTest.php
@@ -3,6 +3,7 @@
 namespace Kirby\Cms;
 
 use Kirby\Toolkit\Dir;
+use Kirby\Toolkit\F;
 use ReflectionClass;
 
 class SystemTest extends TestCase
@@ -10,6 +11,7 @@ class SystemTest extends TestCase
     protected $_SERVER = null;
     protected $app;
     protected $fixtures;
+    protected $subFixtures;
 
     public function setUp(): void
     {
@@ -25,6 +27,11 @@ class SystemTest extends TestCase
     public function tearDown(): void
     {
         $_SERVER = $this->_SERVER;
+
+        if ($this->subFixtures !== null) {
+            chmod($this->subFixtures, 0755);
+            Dir::remove($this->subFixtures);
+        }
 
         Dir::remove($this->fixtures);
     }
@@ -253,5 +260,96 @@ class SystemTest extends TestCase
         $this->expectExceptionMessage('Please enter a valid email address');
 
         $system->register('K3-PRO-abc', 'invalid');
+    }
+
+    public function testStatus()
+    {
+        $system = new System($this->app);
+
+        $expected = [
+            'accounts' => true,
+            'content' => true,
+            'curl' => true,
+            'sessions' => false,
+            'mbstring' => true,
+            'media' => true,
+            'php' => true,
+            'server' => false,
+        ];
+        $this->assertSame($expected, $system->status());
+        $this->assertSame($expected, $system->toArray());
+    }
+
+    public function testIsInstalled()
+    {
+        $system = new System($this->app);
+        $this->assertFalse($system->isInstalled());
+
+        $this->app->users()->create(['email' => 'test@getkirby.com']);
+        $this->assertTrue($system->isInstalled());
+    }
+
+    public function rootsProvider()
+    {
+        return [
+            ['accounts'],
+            ['content'],
+            ['media']
+        ];
+    }
+
+    /**
+     * @dataProvider rootsProvider
+     * @param $root
+     * @throws \Kirby\Exception\PermissionException
+     */
+    public function testInitPermission($root)
+    {
+        $this->subFixtures = $this->fixtures . '/' . ucfirst($root) . 'Test';
+
+        $app = $this->app->clone([
+            'roots' => [
+                'index' => $this->fixtures,
+                $root   => $this->subFixtures . '/' . $root,
+            ]
+        ]);
+
+        // create test roots
+        Dir::make($this->subFixtures);
+
+        // set no writable
+        chmod($this->subFixtures, 0444);
+
+        // /site/accounts
+        $this->expectException('Kirby\Exception\PermissionException');
+        $this->expectExceptionMessage('The ' . $root . ' directory could not be created');
+
+        new System($app);
+    }
+
+    public function testLicense()
+    {
+        $system = new System($this->app);
+
+        // no license file
+        $this->assertFalse($system->license());
+
+        // empty license file
+        F::write($this->fixtures . '/site/config/.license', '');
+        $this->assertFalse($system->license());
+
+        // invalid license file
+        $testLicense = [
+            'license'  => 'K3-PRO-test',
+            'order'    => 'ORDER-test',
+            'email'    => 'bastian@getkirby.com',
+            'domain'   => 'starterkit.test',
+            'date'     => '2020-08-15',
+            // invalid/random hexadecimal string
+            'signature' => '67dad6736aa92a844bdba78256da5074e2b61fc0e82c872424f67a46267f1781948bb48a4c7dcc34e843448ec6d612584f210aee30681d89f20f8b7b02a7e8efb1d4b21dd129628a02b355abe2267913f663f5b1cc603cd66a047935bf69061c0f28e6343da220b01a240b49186c7bf143eae2b0d612e08cad5e09741cc888f9551bcb86ceed555e753092af69e1b4d13fa3228b0b9f417ec4ed8b2b148d8c9c1bca54813e0fde5bbb9a33e6b3ea47ddb1d45ca49654e6027696143302515802eac174a7f41dd70b4772245497e69c94aeece9f6b85d6a16005fd3bbaccde766ea7071161ba645853f88678dd935e8a248d12ca013f28ef34aa2865002e57667'
+        ];
+
+        F::write($this->fixtures . '/site/config/.license', json_encode($testLicense));
+        $this->assertFalse($system->license());
     }
 }
