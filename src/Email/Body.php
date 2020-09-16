5234ced8c8c12724c6e9cd9612ed70db0a8a8310
Ahmet Bora <byybora@gmail.com>
2020-09-16 16:02:48 +0300

    Update doc blocks for Email namepace

--- src/Email/Body.php
+++ src/Email/Body.php
@@ -19,30 +19,64 @@ class Body
 {
     use Properties;
 
+    /**
+     * @var string|null
+     */
     protected $html;
+
+    /**
+     * @var string|null
+     */
     protected $text;
 
+    /**
+     * Email body constructor
+     *
+     * @param array $props
+     */
     public function __construct(array $props = [])
     {
         $this->setProperties($props);
     }
 
+    /**
+     * Returns body html
+     *
+     * @return string|null
+     */
     public function html()
     {
         return $this->html;
     }
 
+    /**
+     * Returns body text
+     *
+     * @return string|null
+     */
     public function text()
     {
         return $this->text;
     }
 
+    /**
+     * Sets body as HTML
+     *
+     * @param string|null $html
+     * @return self
+     */
     protected function setHtml(string $html = null)
     {
         $this->html = $html;
         return $this;
     }
 
+    /**
+     * Sets body as plain text
+     *
+     * @param string|null $text
+     * @return self
+     */
     protected function setText(string $text = null)
     {
         $this->text = $text;
