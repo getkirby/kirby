d14901babcac96023bc3f9afcdb62bbb1ea6c994
Nico Hoffmann <on-github@nhoffmann.com>
2020-09-26 15:19:04 +0300

    New k-loader component

---
+++ panel/src/components/Misc/Loader.vue
@@ -0,0 +1,12 @@
+<template>
+  <k-icon class="k-loader" type="loader" />
+</template>
+
+<style lang="scss">
+.k-loader {
+  z-index: 1;
+}
+.k-loader svg {
+  animation: Spin .9s linear infinite;
+}
+</style>
