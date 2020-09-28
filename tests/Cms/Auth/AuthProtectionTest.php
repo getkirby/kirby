966c19f60d2921bc910dbdef5006d4134de059a6
Lukas Bestle <lukas@getkirby.com>
2020-09-28 20:35:24 +0200

    AuthProtectionTest: Use assertSame()

--- tests/Cms/Auth/AuthProtectionTest.php
+++ tests/Cms/Auth/AuthProtectionTest.php
@@ -59,7 +59,7 @@ class AuthProtectionTest extends TestCase
      */
     public function testLogfile()
     {
-        $this->assertEquals($this->fixtures . '/site/accounts/.logins', $this->auth->logfile());
+        $this->assertSame($this->fixtures . '/site/accounts/.logins', $this->auth->logfile());
     }
 
     /**
@@ -70,10 +70,10 @@ class AuthProtectionTest extends TestCase
         copy(__DIR__ . '/fixtures/logins.cleanup.json', $this->fixtures . '/site/accounts/.logins');
 
         // should delete expired and old entries and add by-email array
-        $this->assertEquals([
+        $this->assertSame([
             'by-ip' => [
                 '38f0a08519792a17e18a251008f3a116977907f921b0b287c8' => [
-                    'time'   => '9999999999',
+                    'time'   => 9999999999,
                     'trials' => 5
                 ]
             ],
@@ -83,7 +83,7 @@ class AuthProtectionTest extends TestCase
 
         // should handle missing .logins file
         unlink($this->fixtures . '/site/accounts/.logins');
-        $this->assertEquals([
+        $this->assertSame([
             'by-ip'    => [],
             'by-email' => []
         ], $this->auth->log());
@@ -91,7 +91,7 @@ class AuthProtectionTest extends TestCase
 
         // should handle invalid .logins file
         file_put_contents($this->fixtures . '/site/accounts/.logins', 'some gibberish');
-        $this->assertEquals([
+        $this->assertSame([
             'by-ip'    => [],
             'by-email' => []
         ], $this->auth->log());
@@ -105,7 +105,7 @@ class AuthProtectionTest extends TestCase
     {
         $this->app->visitor()->ip('10.1.123.234');
 
-        $this->assertEquals('87084f11690867b977a611dd2c943a918c3197f4c02b25ab59', $this->auth->ipHash());
+        $this->assertSame('87084f11690867b977a611dd2c943a918c3197f4c02b25ab59', $this->auth->ipHash());
     }
 
     /**
@@ -178,8 +178,8 @@ class AuthProtectionTest extends TestCase
                 ]
             ]
         ];
-        $this->assertEquals($data, $this->auth->log());
-        $this->assertEquals(json_encode($data), file_get_contents($this->fixtures . '/site/accounts/.logins'));
+        $this->assertSame($data, $this->auth->log());
+        $this->assertSame(json_encode($data), file_get_contents($this->fixtures . '/site/accounts/.logins'));
     }
 
     /**
@@ -193,7 +193,7 @@ class AuthProtectionTest extends TestCase
         $user = $this->auth->validatePassword('marge@simpsons.com', 'springfield123');
 
         $this->assertInstanceOf(User::class, $user);
-        $this->assertEquals('marge@simpsons.com', $user->email());
+        $this->assertSame('marge@simpsons.com', $user->email());
         $this->assertNull($this->failedEmail);
     }
 
@@ -210,7 +210,7 @@ class AuthProtectionTest extends TestCase
         $this->app->visitor()->ip('10.3.123.234');
         $this->auth->validatePassword('lisa@simpsons.com', 'springfield123');
 
-        $this->assertEquals(1, $this->auth->log()['by-ip']['85a06e36d926cb901f05d1167913ebd7ec3d8f5bce4551f5da']['trials']);
+        $this->assertSame(1, $this->auth->log()['by-ip']['85a06e36d926cb901f05d1167913ebd7ec3d8f5bce4551f5da']['trials']);
         $this->assertSame('lisa@simpsons.com', $this->failedEmail);
     }
 
@@ -227,8 +227,8 @@ class AuthProtectionTest extends TestCase
         $this->app->visitor()->ip('10.3.123.234');
         $this->auth->validatePassword('marge@simpsons.com', 'invalid-password');
 
-        $this->assertEquals(1, $this->auth->log()['by-ip']['85a06e36d926cb901f05d1167913ebd7ec3d8f5bce4551f5da']['trials']);
-        $this->assertEquals(10, $this->auth->log()['by-email']['marge@simpsons.com']['trials']);
+        $this->assertSame(1, $this->auth->log()['by-ip']['85a06e36d926cb901f05d1167913ebd7ec3d8f5bce4551f5da']['trials']);
+        $this->assertSame(10, $this->auth->log()['by-email']['marge@simpsons.com']['trials']);
         $this->assertSame('marge@simpsons.com', $this->failedEmail);
     }
 
@@ -266,7 +266,7 @@ class AuthProtectionTest extends TestCase
         $this->app->visitor()->ip('10.3.123.234');
         $this->auth->validatePassword('lisa@simpsons.com', 'springfield123');
 
-        $this->assertEquals(1, $this->auth->log()['by-ip']['85a06e36d926cb901f05d1167913ebd7ec3d8f5bce4551f5da']['trials']);
+        $this->assertSame(1, $this->auth->log()['by-ip']['85a06e36d926cb901f05d1167913ebd7ec3d8f5bce4551f5da']['trials']);
         $this->assertSame('lisa@simpsons.com', $this->failedEmail);
     }
 
@@ -289,8 +289,8 @@ class AuthProtectionTest extends TestCase
         $this->app->visitor()->ip('10.3.123.234');
         $this->auth->validatePassword('marge@simpsons.com', 'invalid-password');
 
-        $this->assertEquals(1, $this->auth->log()['by-ip']['85a06e36d926cb901f05d1167913ebd7ec3d8f5bce4551f5da']['trials']);
-        $this->assertEquals(10, $this->auth->log()['by-email']['marge@simpsons.com']['trials']);
+        $this->assertSame(1, $this->auth->log()['by-ip']['85a06e36d926cb901f05d1167913ebd7ec3d8f5bce4551f5da']['trials']);
+        $this->assertSame(10, $this->auth->log()['by-email']['marge@simpsons.com']['trials']);
         $this->assertSame('marge@simpsons.com', $this->failedEmail);
     }
 
