d50e46a7fdfcc19f57ab60f3e291471fe480d04c
Nico Hoffmann <on-github@nhoffmann.com>
2020-10-09 09:12:41 +0200

    $helper.string.template unit tests

--- panel/src/helpers/string.js
+++ panel/src/helpers/string.js
@@ -32,12 +32,17 @@ export default {
 
     Object.keys(values).forEach(key => {
       // replace string template with value
-      string = string.replace(new RegExp(`${opening}${key}${closing}`, "gi"), values[key] || "…");
+      string = string.replace(
+        new RegExp(`${opening}${key}${closing}`, "gi"),
+        values[key] || "…"
+      );
 
       // for arrays, allow string templates for length/count
       if (Array.isArray(values[key]) === true) {
-        string = string.replace(new RegExp(`${opening}${key}.count${closing}`, "gi"), values[key].length || 0);
-        string = string.replace(new RegExp(`${opening}${key}.length${closing}`, "gi"), values[key].length || 0);
+        string = string.replace(
+          new RegExp(`${opening}${key}.count${closing}|${opening}${key}.length${closing}`, "gi"),
+          values[key].length || 0
+        );
       }
     })
 
