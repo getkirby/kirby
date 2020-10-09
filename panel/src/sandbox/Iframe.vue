c78b744e2be6390898eb81db6a16edec486dfb16
Nico Hoffmann <on-github@nhoffmann.com>
2020-10-09 14:21:10 +0200

    Sandbox: log $emit events

--- panel/src/sandbox/Iframe.vue
+++ panel/src/sandbox/Iframe.vue
@@ -1,8 +1,23 @@
 <template>
-  <component :is="'sandbox-' + $route.params.component" />
+  <component ref="el" :is="'sandbox-' + $route.params.component" />
 </template>
 
 <script>
+export default {
+  mounted() {
+    const emit = this.$refs.el.$emit;
+    this.$refs.el.$emit = (...args) => {
 
+      if (args[0] || null) {
+        console.log(`Event: ${args[0]}`);
+      }
 
+      if (args[1] || null) {
+        console.log(args[1]);
+      }
+
+      emit.apply(this, args);
+    }
+  }
+}
 </script>
