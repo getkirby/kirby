685dbf79bd24e6f25ac444c8995d28056f997dc7
Bastian Allgeier <mail@bastianallgeier.com>
2020-10-12 13:00:55 +0200

    Emit an event when the button gets clicked

--- panel/src/sandbox/components/Button.vue
+++ panel/src/sandbox/components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <k-view align="center">
     <k-button icon="open" @click="$refs.dialog.open()">Open Dialog</k-button>
-    <k-dialog ref="dialog">This is a nice dialog</k-dialog>
+    <k-dialog ref="dialog" @submit="$emit('submit', $event)">This is a nice dialog</k-dialog>
   </k-view>
 </template>
