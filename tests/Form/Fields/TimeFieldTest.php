a4f82b809ceb5c2393f443facaa04b7878a9f64b
Nico Hoffmann <on-github@nhoffmann.com>
2020-10-24 17:36:41 +0200

    Date + time field: add unit tests for min/max

--- tests/Form/Fields/TimeFieldTest.php
+++ tests/Form/Fields/TimeFieldTest.php
@@ -18,6 +18,93 @@ class TimeFieldTest extends TestCase
         $this->assertTrue($field->save());
     }
 
+    public function testMinMax()
+    {
+        // no limits
+        $field = $this->field('time', [
+            'value' => '10:00:00'
+        ]);
+
+        $field->validate();
+        $this->assertTrue($field->isValid());
+        $this->assertFalse($field->isInvalid());
+
+        // valid
+        $field = $this->field('time', [
+            'min'   => '09:00',
+            'value' => '10:00:00',
+            'max'   => '11:00'
+        ]);
+
+        $field->validate();
+        $this->assertTrue($field->isValid());
+        $this->assertFalse($field->isInvalid());
+
+        // same time valid
+        $field = $this->field('time', [
+            'min'   => '10:00',
+            'value' => '10:00:00',
+            'max'   => '10:00'
+        ]);
+
+        $field->validate();
+        $this->assertTrue($field->isValid());
+        $this->assertFalse($field->isInvalid());
+
+        // min failed
+        $field = $this->field('time', [
+            'min'   => '11:00',
+            'value' => '10:00:00'
+        ]);
+
+        $field->validate();
+        $this->assertFalse($field->isValid());
+        $this->assertTrue($field->isInvalid());
+
+        // max failed
+        $field = $this->field('time', [
+            'value' => '10:00:00',
+            'max'   => '09:00'
+        ]);
+
+        $field->validate();
+        $this->assertFalse($field->isValid());
+        $this->assertTrue($field->isInvalid());
+
+        // valid with seconds
+        $field = $this->field('time', [
+            'min'   => '09:00:00',
+            'value' => '10:00:00',
+            'max'   => '11:00:00',
+            'step'  => 'second'
+        ]);
+
+        $field->validate();
+        $this->assertTrue($field->isValid());
+        $this->assertFalse($field->isInvalid());
+
+        // invalid with seconds
+        $field = $this->field('time', [
+            'min'   => '10:00:05',
+            'value' => '10:00:00',
+            'step'  => 'second'
+        ]);
+
+        $field->validate();
+        $this->assertFalse($field->isValid());
+        $this->assertTrue($field->isInvalid());
+
+        $field = $this->field('time', [
+            'value' => '10:00:05',
+            'max'   => '10:00:00',
+            'step'  => 'second'
+        ]);
+
+        $field->validate();
+        $this->assertFalse($field->isValid());
+        $this->assertTrue($field->isInvalid());
+    }
+
     public function valueProvider()
     {
         return [
@@ -28,8 +115,8 @@ class TimeFieldTest extends TestCase
             ['22:33:00', date('Y-m-d ') . '22:35:00', 5],
             ['22:36:00', date('Y-m-d ') . '22:35:00', 5],
             ['22:39:00', date('Y-m-d ') . '22:45:00', 15],
-            ['22:35:15', date('Y-m-d ') . '22:35:30', ['size'=>30, 'unit'=>'second']],
-            ['22:35:15', date('Y-m-d ') . '22:00:00', ['size'=>1, 'unit'=>'hour']],
+            ['22:35:15', date('Y-m-d ') . '22:35:30', ['size' => 30, 'unit' => 'second']],
+            ['22:35:15', date('Y-m-d ') . '22:00:00', ['size' => 1, 'unit' => 'hour']],
             ['2012-12-12 22:33:00', '2012-12-12 22:33:00']
         ];
     }
