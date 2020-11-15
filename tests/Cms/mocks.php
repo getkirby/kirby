ef45ccc2228eed7f7bee942d49bb65a4907c30b2
Lukas Bestle <lukas@getkirby.com>
2020-11-15 11:45:31 +0100

    Mock usleep() function for faster PHPUnit tests

--- tests/Cms/mocks.php
+++ tests/Cms/mocks.php
@@ -22,3 +22,19 @@ class MockTime
 {
     public static $time = 1337000000;
 }
+
+/**
+ * Mock for the PHP usleep() function to skip over
+ * waiting times while testing
+ *
+ * @param int $microSeconds
+ * @return void
+ */
+function usleep(int $microSeconds): void
+{
+    if (defined('KIRBY_TESTING') !== true || KIRBY_TESTING !== true) {
+        throw new Exception('Mock usleep() function was loaded outside of the test environment. This should never happen.');
+    }
+
+    // do nothing
+}
