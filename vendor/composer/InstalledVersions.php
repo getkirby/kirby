0d2751af7cb478df14f30fac47fe9a2cd3b38114
Bastian Allgeier <mail@bastianallgeier.com>
2020-12-15 09:09:43 +0100

    Update composer stuff

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -14,8 +14,8 @@ class InstalledVersions
 private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => '3.5.0-rc.7',
-    'version' => '3.5.0.0-RC7',
+    'pretty_version' => '3.5.0',
+    'version' => '3.5.0.0',
     'aliases' => 
     array (
     ),
@@ -44,8 +44,8 @@ private static $installed = array (
     ),
     'getkirby/cms' => 
     array (
-      'pretty_version' => '3.5.0-rc.7',
-      'version' => '3.5.0.0-RC7',
+      'pretty_version' => '3.5.0',
+      'version' => '3.5.0.0',
       'aliases' => 
       array (
       ),
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array (
   'root' => 
   array (
-    'pretty_version' => '3.5.0-rc.7',
-    'version' => '3.5.0.0-RC7',
+    'pretty_version' => '3.5.0',
+    'version' => '3.5.0.0',
     'aliases' => 
     array (
     ),
@@ -31,8 +31,8 @@
     ),
     'getkirby/cms' => 
     array (
-      'pretty_version' => '3.5.0-rc.7',
-      'version' => '3.5.0.0-RC7',
+      'pretty_version' => '3.5.0',
+      'version' => '3.5.0.0',
       'aliases' => 
       array (
       ),
