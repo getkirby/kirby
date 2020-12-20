64ae740a1ee413ce91dff01e6482a59aacbc50e7
Lukas Bestle <lukas@getkirby.com>
2020-12-20 21:22:57 +0100

    New Cms\Auth\Status class

---
+++ src/Cms/Auth/Status.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace Kirby\Cms\Auth;
+
+use Kirby\Cms\App;
+use Kirby\Exception\InvalidArgumentException;
+use Kirby\Toolkit\Properties;
+
+/**
+ * Information container for the
+ * authentication status
+ *
+ * @package   Kirby Cms
+ * @author    Lukas Bestle <lukas@getkirby.com>
+ * @link      https://getkirby.com
+ * @copyright Bastian Allgeier GmbH
+ * @license   https://getkirby.com/license
+ */
+class Status
+{
+    use Properties;
+
+    /**
+     * Type of the active challenge
+     *
+     * @var string|null
+     */
+    protected $challenge = null;
+
+    /**
+     * Challenge type to use as a fallback
+     * when $challenge is `null`
+     *
+     * @var string|null
+     */
+    protected $challengeFallback = null;
+
+    /**
+     * Email address of the current/pending user
+     *
+     * @var string|null
+     */
+    protected $email = null;
+
+    /**
+     * Kirby instance for user lookup
+     *
+     * @var \Kirby\Cms\App
+     */
+    protected $kirby;
+
+    /**
+     * Authentication status:
+     * `active|impersonated|pending|inactive`
+     *
+     * @var string
+     */
+    protected $status;
+
+    /**
+     * Class constructor
+     *
+     * @param array $props
+     */
+    public function __construct(array $props)
+    {
+        $this->setProperties($props);
+    }
+
+    /**
+     * Returns the authentication status
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->status();
+    }
+
+    /**
+     * Returns the type of the active challenge
+     *
+     * @param bool $automaticFallback If set to `false`, no faked challenge is returned;
+     *                                WARNING: never send the resulting `null` value to the
+     *                                user to avoid leaking whether the pending user exists
+     * @return string|null
+     */
+    public function challenge(bool $automaticFallback = true): ?string
+    {
+        // never return a challenge type if the status doesn't match
+        if ($this->status() !== 'pending') {
+            return null;
+        }
+
+        if ($automaticFallback === false) {
+            return $this->challenge;
+        } else {
+            return $this->challenge ?? $this->challengeFallback;
+        }
+    }
+
+    /**
+     * Returns the email address of the current/pending user
+     *
+     * @return string|null
+     */
+    public function email(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * Returns the authentication status
+     *
+     * @return string `active|impersonated|pending|inactive`
+     */
+    public function status(): string
+    {
+        return $this->status;
+    }
+
+    /**
+     * Returns an array with all public status data
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return [
+            'challenge' => $this->challenge(),
+            'email'     => $this->email(),
+            'status'    => $this->status()
+        ];
+    }
+
+    /**
+     * Returns the currently logged in user
+     *
+     * @return \Kirby\Cms\User
+     */
+    public function user()
+    {
+        // for security, only return the user if they are
+        // already logged in
+        if (in_array($this->status(), ['active', 'impersonated']) !== true) {
+            return null;
+        }
+
+        return $this->kirby->user($this->email());
+    }
+
+    /**
+     * Sets the type of the active challenge
+     *
+     * @param string|null $challenge
+     * @return self
+     */
+    protected function setChallenge(?string $challenge = null)
+    {
+        $this->challenge = $challenge;
+        return $this;
+    }
+
+    /**
+     * Sets the challenge type to use as
+     * a fallback when $challenge is `null`
+     *
+     * @param string|null $challengeFallback
+     * @return self
+     */
+    protected function setChallengeFallback(?string $challengeFallback = null)
+    {
+        $this->challengeFallback = $challengeFallback;
+        return $this;
+    }
+
+    /**
+     * Sets the email address of the current/pending user
+     *
+     * @param string|null $email
+     * @return self
+     */
+    protected function setEmail(?string $email = null)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * Sets the Kirby instance for user lookup
+     *
+     * @param \Kirby\Cms\App $kirby
+     * @return self
+     */
+    protected function setKirby(App $kirby)
+    {
+        $this->kirby = $kirby;
+        return $this;
+    }
+
+    /**
+     * Sets the authentication status
+     *
+     * @param string $status `active|impersonated|pending|inactive`
+     * @return self
+     */
+    protected function setStatus(string $status)
+    {
+        if (in_array($status, ['active', 'impersonated', 'pending', 'inactive']) !== true) {
+            throw new InvalidArgumentException([
+                'data' => ['argument' => '$props[\'status\']', 'method' => 'Status::__construct']
+            ]);
+        }
+
+        $this->status = $status;
+        return $this;
+    }
+}
---
+++ tests/Cms/Auth/StatusTest.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace Kirby\Cms\Auth;
+
+use Kirby\Cms\App;
+use Kirby\Cms\TestCase;
+
+/**
+ * @coversDefaultClass Kirby\Cms\Auth\Status
+ * @covers ::__construct
+ * @covers ::setChallenge
+ * @covers ::setChallengeFallback
+ * @covers ::setEmail
+ * @covers ::setKirby
+ * @covers ::setStatus
+ */
+class StatusTest extends TestCase
+{
+    protected $app;
+
+    public function setUp(): void
+    {
+        $this->app = new App([
+            'roots' => [
+                'index' => '/dev/null'
+            ],
+            'users' => [
+                [
+                    'email' => 'homer@simpsons.com',
+                    'name'  => 'Homer Simpson'
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * @covers ::__toString
+     */
+    public function testToString()
+    {
+        $status = new Status([
+            'kirby'  => $this->app,
+            'status' => 'active'
+        ]);
+
+        $this->assertSame('active', (string)$status);
+    }
+
+    /**
+     * @covers ::challenge
+     */
+    public function testChallenge()
+    {
+        // no challenge when not in pending status
+        $status = new Status([
+            'kirby'             => $this->app,
+            'challenge'         => 'totp',
+            'challengeFallback' => 'email',
+            'status'            => 'active'
+        ]);
+        $this->assertNull($status->challenge());
+
+        // with pending challenge
+        $status = new Status([
+            'kirby'             => $this->app,
+            'challenge'         => 'totp',
+            'challengeFallback' => 'email',
+            'status'            => 'pending'
+        ]);
+        $this->assertSame('totp', $status->challenge());
+
+        // with faked challenge
+        $status = new Status([
+            'kirby'             => $this->app,
+            'challenge'         => null,
+            'challengeFallback' => 'email',
+            'status'            => 'pending'
+        ]);
+        $this->assertSame('email', $status->challenge());
+
+        // with faked challenge, but without automatic fallback
+        $status = new Status([
+            'kirby'             => $this->app,
+            'challenge'         => null,
+            'challengeFallback' => 'email',
+            'status'            => 'pending'
+        ]);
+        $this->assertNull($status->challenge(false));
+    }
+
+    /**
+     * @covers ::email
+     */
+    public function testEmail()
+    {
+        $status = new Status([
+            'kirby'  => $this->app,
+            'status' => 'inactive'
+        ]);
+        $this->assertNull($status->email());
+
+        $status = new Status([
+            'kirby'  => $this->app,
+            'email'  => 'homer@simpsons.com',
+            'status' => 'active'
+        ]);
+        $this->assertSame('homer@simpsons.com', $status->email());
+    }
+
+    /**
+     * @covers ::status
+     * @covers ::setStatus
+     */
+    public function testStatus()
+    {
+        $status = new Status([
+            'kirby'  => $this->app,
+            'status' => 'active'
+        ]);
+        $this->assertSame('active', $status->status());
+
+        $status = new Status([
+            'kirby'  => $this->app,
+            'status' => 'impersonated'
+        ]);
+        $this->assertSame('impersonated', $status->status());
+
+        $status = new Status([
+            'kirby'  => $this->app,
+            'status' => 'pending'
+        ]);
+        $this->assertSame('pending', $status->status());
+
+        $status = new Status([
+            'kirby'  => $this->app,
+            'status' => 'inactive'
+        ]);
+        $this->assertSame('inactive', $status->status());
+
+        $this->expectException('Kirby\Exception\InvalidArgumentException');
+        $this->expectExceptionMessage('Invalid argument "$props[\'status\']" in method "Status::__construct"');
+        $status = new Status([
+            'kirby'  => $this->app,
+            'status' => 'invalid'
+        ]);
+    }
+
+    /**
+     * @covers ::toArray
+     */
+    public function testToArray()
+    {
+        $status = new Status([
+            'kirby'             => $this->app,
+            'challenge'         => null,
+            'challengeFallback' => 'email',
+            'email'             => 'homer@simpsons.com',
+            'status'            => 'pending'
+        ]);
+
+        $this->assertSame([
+            'challenge' => 'email',
+            'email'     => 'homer@simpsons.com',
+            'status'    => 'pending'
+        ], $status->toArray());
+    }
+
+    /**
+     * @covers ::user
+     */
+    public function testUser()
+    {
+        // only return active users
+        $status = new Status([
+            'kirby'  => $this->app,
+            'email'  => 'homer@simpsons.com',
+            'status' => 'pending'
+        ]);
+        $this->assertNull($status->user());
+
+        // existing active user
+        $status = new Status([
+            'kirby'  => $this->app,
+            'email'  => 'homer@simpsons.com',
+            'status' => 'active'
+        ]);
+        $this->assertSame('Homer Simpson', $status->user()->name()->value());
+
+        // existing impersonated user
+        $status = new Status([
+            'kirby'  => $this->app,
+            'email'  => 'homer@simpsons.com',
+            'status' => 'impersonated'
+        ]);
+        $this->assertSame('Homer Simpson', $status->user()->name()->value());
+
+        // invalid active user
+        $status = new Status([
+            'kirby'  => $this->app,
+            'email'  => 'invalid@simpsons.com',
+            'status' => 'active'
+        ]);
+        $this->assertNull($status->user());
+    }
+}
