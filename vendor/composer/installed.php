317f494ee5166f36e32f9acf286f69786de30512
Bastian Allgeier <mail@bastianallgeier.com>
2021-02-11 11:38:27 +0100

    Updated translations and composer.lock

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array (
   'root' => 
   array (
-    'pretty_version' => '3.5.2',
-    'version' => '3.5.2.0',
+    'pretty_version' => '3.5.3',
+    'version' => '3.5.3.0',
     'aliases' => 
     array (
     ),
@@ -31,8 +31,8 @@
     ),
     'getkirby/cms' => 
     array (
-      'pretty_version' => '3.5.2',
-      'version' => '3.5.2.0',
+      'pretty_version' => '3.5.3',
+      'version' => '3.5.3.0',
       'aliases' => 
       array (
       ),
