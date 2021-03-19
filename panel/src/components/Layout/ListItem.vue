c00ebcd5d4e5ed3427ed025976a8a4f8d3b2c137
Lukas Bestle <lukas@getkirby.com>
2021-03-19 19:42:29 +0100

    Slots for the ListItem component
    This allows plugins to use the ListItem and List components with custom content that doesn't fit into the "text and info" model.

--- panel/src/components/Layout/ListItem.vue
+++ panel/src/components/Layout/ListItem.vue
@@ -7,13 +7,17 @@
       class="k-list-item-content"
     >
       <span v-if="image" class="k-list-item-image">
-        <k-image v-if="imageOptions" v-bind="imageOptions" />
-        <k-icon v-else v-bind="icon" />
+        <slot name="image">
+          <k-image v-if="imageOptions" v-bind="imageOptions" />
+          <k-icon v-else v-bind="icon" />
+        </slot>
       </span>
       <span class="k-list-item-text">
-        <em>{{ text }}</em>
-        <!-- eslint-disable-next-line vue/no-v-html -->
-        <small v-if="info" v-html="info" />
+        <slot>
+          <em>{{ text }}</em>
+          <!-- eslint-disable-next-line vue/no-v-html -->
+          <small v-if="info" v-html="info" />
+        </slot>
       </span>
     </k-link>
     <nav class="k-list-item-options">
