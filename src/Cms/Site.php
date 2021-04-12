c26cad70dd48b6184ea5cd89720d9bf61f31fe08
Lukas Bestle <lukas@getkirby.com>
2021-04-12 10:54:03 +0200

    More specific type hints
    Co-authored-by: Nico Hoffmann <on-github@nhoffmann.com>

--- src/Cms/Site.php
+++ src/Cms/Site.php
@@ -572,7 +572,7 @@ class Site extends ModelWithContent
      * @param string|null $url
      * @return $this
      */
-    protected function setUrl($url = null)
+    protected function setUrl(?string $url = null)
     {
         $this->url = $url;
         return $this;
@@ -604,7 +604,7 @@ class Site extends ModelWithContent
      * @param string|null $language
      * @return string
      */
-    public function url($language = null): string
+    public function url(?string $language = null): string
     {
         if ($language !== null || $this->kirby()->multilang() === true) {
             return $this->urlForLanguage($language);
