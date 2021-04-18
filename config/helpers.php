43a08baf9fcda1e74b74d4818a0db8796a23cb9d
Lukas Bestle <lukas@getkirby.com>
2021-04-18 21:00:55 +0200

    Update docblocks for esc() helpers/methods

--- config/helpers.php
+++ config/helpers.php
@@ -171,7 +171,7 @@ if (function_exists('e') === false) {
  * Escape context specific output
  *
  * @param string $string Untrusted data
- * @param string $context Location of output
+ * @param string $context Location of output (`html`, `attr`, `js`, `css`, `url` or `xml`)
  * @return string Escaped data
  */
 function esc($string, $context = 'html')
