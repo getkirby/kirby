facd51a64b3a14c3b4335120cd9c36db0f5b1ab6
Lukas Bestle <lukas@getkirby.com>
2021-04-24 22:23:53 +0200

    Update version number

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -14,8 +14,8 @@ class InstalledVersions
 private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => '3.5.3.1',
-    'version' => '3.5.3.1',
+    'pretty_version' => '3.5.4',
+    'version' => '3.5.4.0',
     'aliases' => 
     array (
     ),
@@ -44,8 +44,8 @@ private static $installed = array (
     ),
     'getkirby/cms' => 
     array (
-      'pretty_version' => '3.5.3.1',
-      'version' => '3.5.3.1',
+      'pretty_version' => '3.5.4',
+      'version' => '3.5.4.0',
       'aliases' => 
       array (
       ),
