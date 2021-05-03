88eb084c639ec24181a6a53b4ec6512fc00d033b
Nico Hoffmann <on-github@nhoffmann.com>
2021-05-03 20:41:56 +0200

    Add eslint back in

---
+++ panel/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:cypress/recommended",
+    "plugin:vue/recommended"
+  ],
+  parserOptions: {
+    "ecmaVersion": 2017,
+    "parser": "babel-eslint"
+  },
+  rules: {
+    "vue/component-definition-name-casing": "off",
+    "vue/require-default-prop": "off",
+    "vue/attributes-order": "error",
+    "vue/require-prop-types": "error",
+    "vue/max-attributes-per-line": [
+      "error",
+      {
+        "singleline": 3,
+        "multiline": {
+          "max": 1,
+          "allowFirstLine": false
+        }
+      }
+    ],
+    "vue/html-closing-bracket-newline": [
+      "error",
+      {
+        "singleline": "never",
+        "multiline": "always"
+      }
+    ]
+  }
+}
