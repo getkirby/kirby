bba24f96e09e9eaff5c9f500514986da14314109
Bastian Allgeier <mail@bastianallgeier.com>
2021-05-11 09:51:33 +0200

    Update version number

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -27,8 +27,8 @@ class InstalledVersions
 private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => '3.5.5-rc.1',
-    'version' => '3.5.5.0-RC1',
+    'pretty_version' => '3.5.5',
+    'version' => '3.5.5.0',
     'aliases' => 
     array (
     ),
@@ -57,8 +57,8 @@ private static $installed = array (
     ),
     'getkirby/cms' => 
     array (
-      'pretty_version' => '3.5.5-rc.1',
-      'version' => '3.5.5.0-RC1',
+      'pretty_version' => '3.5.5',
+      'version' => '3.5.5.0',
       'aliases' => 
       array (
       ),
