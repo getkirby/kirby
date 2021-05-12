3bcec63deea0340828dc0de97f5c7d2a653d67bf
Nico Hoffmann <on-github@nhoffmann.com>
2021-05-12 21:31:31 +0200

    Refactor Vue setup based on Inertia

--- panel/src/index.js
+++ panel/src/index.js
@@ -1,10 +1,13 @@
-import App from "./App.vue";
+import Vue from "vue";
+import { App, plugin } from '@inertiajs/inertia-vue'
 import Api from "./config/api.js";
 import Events from "./config/events.js";
-import I18n from "./config/i18n.js";
-import Vue from "vue";
-import Vuelidate from "vuelidate";
 import Helpers from "./helpers/index.js";
+import Vuelidate from "vuelidate";
+import VuePortal from "@linusborg/vue-simple-portal";
+
+import store from "./store/store.js";
+import str from "./helpers/string.js";
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
@@ -18,49 +21,81 @@ import "./config/plugins.js";
 
 Vue.use(Events);
 Vue.use(Vuelidate);
-
-import VuePortal from "@linusborg/vue-simple-portal";
 Vue.use(VuePortal);
-
-import router from "./config/router.js";
-import store from "./store/store.js";
-
+Vue.use(plugin)
 Vue.use(Api, store);
-Vue.use(I18n, store);
 
-Vue.prototype.$go = (path) => {
+const base = document.querySelector("base");
 
-  // support links with hash
-  path = path.split("#");
-  path = {
-    path: path[0],
-    hash: path[1] || null
-  };
-
-  router.push(path).catch(e => {
-    if (e && e.name && e.name === "NavigationDuplicated") {
-      return true;
-    }
-
-    throw e;
-  });
+Vue.prototype.$go = function (path, options) {
+  this.$inertia.visit(this.$url(path), options);
 };
 
+Vue.prototype.$reload = function(options) {
+  Vue.prototype.$inertia.reload(options);
+};
+
+Vue.prototype.$url = function (path = "") {
+  return base.href + path.replace(/^\//, "");
+};
+
+document.addEventListener("inertia:visit", function () {
+  store.dispatch("isLoading", true);
+});
+
+document.addEventListener("inertia:load", function () {
+  store.dispatch("isLoading", false);
+});
+
 new Vue({
-  router,
   store,
   created() {
-    window.panel.app = this;
+    window.panel.$vue = window.panel.app = this;
 
     // created plugin callbacks
-    window.panel.plugins.created.forEach(plugin => {
-      plugin(this);
-    });
+    window.panel.plugins.created.forEach(plugin => plugin(this));
 
-    // initialize content store
     this.$store.dispatch("content/init");
   },
-  render: h => {
-    return h(App);
-  },
+  render: (h) => {
+    return h(App, {
+      props: {
+        initialPage: window.inertia,
+        resolveComponent: async (name) =>  {
+          const module = await import(`./components/Views/${name}.vue`);
+          return module.default;
+        },
+        transformProps: (props) => {
+
+          /** Set translation */
+          document.querySelector("html").setAttribute("lang", props.$translation.code);
+
+          /** Set globals */
+          Vue.prototype.$config      = window.panel.$config      = props.$config;
+          Vue.prototype.$language    = window.panel.$language    = props.$language;
+          Vue.prototype.$languages   = window.panel.$languages   = props.$languages;
+          Vue.prototype.$permissions = window.panel.$permissions = props.$permissions;
+          Vue.prototype.$system      = window.panel.$system      = props.$system;
+          Vue.prototype.$translation = window.panel.$translation = props.$translation;
+          Vue.prototype.$t            = window.panel.$views      = (key, data) => {
+            if (typeof key !== 'string') {
+              return;
+            }
+          
+            const string = window.panel.$translation.data[key];
+            return str.template(string, data);
+          }
+          Vue.prototype.$urls        = window.panel.$urls        = props.$urls;
+          Vue.prototype.$user        = window.panel.$user        = props.$user;
+          Vue.prototype.$view        = window.panel.$view        = props.$view;
+          Vue.prototype.$views       = window.panel.$views       = {
+            ...props.$views,
+            ...window.panel.plugins.views
+          };
+
+          return props.$props;
+        }
+      },
+    });
+  }
 }).$mount("#app");
