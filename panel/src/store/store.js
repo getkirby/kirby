cbc07b30567fcb08ab53821449b0341107f2f348
Nico Hoffmann <on-github@nhoffmann.com>
2021-05-12 21:30:34 +0200

    Remove unused Vuex store modules

--- panel/src/store/store.js
+++ panel/src/store/store.js
@@ -6,11 +6,7 @@ import blocks from "./modules/blocks.js";
 import content from "./modules/content.js";
 import drawers from "./modules/drawers.js";
 import heartbeat from "./modules/heartbeat.js";
-import languages from "./modules/languages.js";
 import notification from "./modules/notification.js";
-import system from "./modules/system.js";
-import translation from "./modules/translation.js";
-import user from "./modules/user.js";
 
 Vue.use(Vuex);
 
@@ -18,18 +14,12 @@ export default new Vuex.Store({
   // eslint-disable-next-line
   strict: process.env.NODE_ENV !== "production",
   state: {
-    breadcrumb: [],
     dialog: null,
     drag: null,
     fatal: null,
-    isLoading: false,
-    title: null,
-    view: null
+    isLoading: false
   },
   mutations: {
-    SET_BREADCRUMB(state, breadcrumb) {
-      state.breadcrumb = breadcrumb;
-    },
     SET_DIALOG(state, dialog) {
       state.dialog = dialog;
     },
@@ -39,12 +29,6 @@ export default new Vuex.Store({
     SET_FATAL(state, html) {
       state.fatal = html;
     },
-    SET_TITLE(state, title) {
-      state.title = title;
-    },
-    SET_VIEW(state, view) {
-      state.view = view;
-    },
     START_LOADING(state) {
       state.isLoading = true;
     },
@@ -53,9 +37,6 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    breadcrumb(context, breadcrumb) {
-      context.commit("SET_BREADCRUMB", breadcrumb);
-    },
     dialog(context, dialog) {
       context.commit("SET_DIALOG", dialog);
     },
@@ -67,32 +48,6 @@ export default new Vuex.Store({
     },
     isLoading(context, loading) {
       context.commit(loading === true ? "START_LOADING" : "STOP_LOADING");
-    },
-    title(context, title) {
-      let site;
-
-      if (context.state.user.current) {
-        site = Vue.$api.site.get(["title"]);
-      } else {
-        site = new Promise(resolve => {
-          resolve(context.state.system.info);
-        });
-      }
-
-      site.then(site => {
-        context.commit("SET_TITLE", title);
-        context.dispatch("system/title", site.title);
-        document.title = title || "";
-
-        if (title !== null) {
-          document.title += " | " + site.title;
-        } else {
-          document.title += site.title;
-        }
-      });
-    },
-    view(context, view) {
-      context.commit("SET_VIEW", view);
     }
   },
   modules: {
@@ -100,10 +55,6 @@ export default new Vuex.Store({
     content: content,
     drawers: drawers,
     heartbeat: heartbeat,
-    languages: languages,
-    notification: notification,
-    system: system,
-    translation: translation,
-    user: user
+    notification: notification
   }
 });
