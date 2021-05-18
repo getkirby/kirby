50bf5943e8ea500d592f1a5600e721b2d6fa420a
Bastian Allgeier <mail@bastianallgeier.com>
2021-05-18 13:48:46 +0200

    Update composer loader

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -20,6 +20,8 @@ use Composer\Semver\VersionParser;
 
 
 
+
+
 class InstalledVersions
 {
 private static $installed = array (
@@ -183,7 +185,6 @@ $packages = array();
 foreach (self::getInstalled() as $installed) {
 $packages[] = array_keys($installed['versions']);
 }
-
 
 if (1 === \count($packages)) {
 return $packages[0];
@@ -376,6 +377,7 @@ public static function reload($data)
 self::$installed = $data;
 self::$installedByVendor = array();
 }
+
 
 
 
