f14ac46853d0cf2ee67bfabc45ebedb0f2d4c9c7
Nico Hoffmann <on-github@nhoffmann.com>
2021-05-21 18:37:29 +0200

    Bundle lightweight inertia

--- panel/src/index.js
+++ panel/src/index.js
@@ -1,7 +1,7 @@
 import "vite/dynamic-import-polyfill"
 
 import Vue from "vue";
-import { App, plugin } from "@inertiajs/inertia-vue";
+import { default as App, plugin as Inertia } from "./inertia/app.js";
 import Api from "./config/api.js";
 import Events from "./config/events.js";
 import Helpers from "./helpers/index.js";
@@ -25,17 +25,8 @@ Vue.use(Events);
 Vue.use(I18n);
 Vue.use(Vuelidate);
 Vue.use(VuePortal);
-Vue.use(plugin)
+Vue.use(Inertia)
 Vue.use(Api, store);
-
-const base = document.querySelector("base");
-
-Vue.prototype.$go = function (path, options) {
-  this.$inertia.visit(this.$url(path), options);
-};
-
-Vue.prototype.$reload = (options)   =>  Vue.prototype.$inertia.reload(options);
-Vue.prototype.$url    = (path = "") => base.href + path.replace(/^\//, "");
 
 document.addEventListener("inertia:start", () => {
   store.dispatch("isLoading", true);
@@ -51,40 +42,8 @@ new Vue({
   store,
   created() {
     window.panel.$vue = window.panel.app = this;
-
-    // created plugin callbacks
     window.panel.plugins.created.forEach(plugin => plugin(this));
-
     this.$store.dispatch("content/init");
   },
-  render: (h) => {
-    return h(App, {
-      props: {
-        initialPage: window.inertia,
-        resolveComponent: (name) => Vue.component(name),
-        transformProps: (props) => {
-
-          /** Set translation */
-          document.querySelector("html").setAttribute("lang", props.$translation.code);
-
-          /** Set globals */
-          Vue.prototype.$config      = window.panel.$config      = props.$config;
-          Vue.prototype.$language    = window.panel.$language    = props.$language;
-          Vue.prototype.$languages   = window.panel.$languages   = props.$languages;
-          Vue.prototype.$permissions = window.panel.$permissions = props.$permissions;
-          Vue.prototype.$system      = window.panel.$system      = props.$system;
-          Vue.prototype.$translation = window.panel.$translation = props.$translation;
-          Vue.prototype.$urls        = window.panel.$urls        = props.$urls;
-          Vue.prototype.$user        = window.panel.$user        = props.$user;
-          Vue.prototype.$view        = window.panel.$view        = props.$view;
-          Vue.prototype.$views       = window.panel.$views       = {
-            ...props.$views,
-            ...window.panel.plugins.views
-          };
-
-          return props.$props;
-        }
-      },
-    });
-  }
+  render: (h) => h(App)  
 }).$mount("#app");
