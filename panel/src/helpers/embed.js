56bd0aba6a0b2e27a89f695684e1bb28fa365196
Nico Hoffmann <on-github@nhoffmann.com>
2021-05-22 12:14:26 +0200

    JS helpers: export individual functions

--- panel/src/helpers/embed.js
+++ panel/src/helpers/embed.js
@@ -1,137 +1,136 @@
-export default {
-  youtube(url) {
 
-    if (!url.match("youtu")) {
+export function youtube(url) {
+  if (!url.match("youtu")) {
+    return false;
+  }
+
+  let uri = null;
+
+  try {
+    uri = new URL(url);
+  } catch (e) {
+    return false;
+  }
+
+  const path = uri.pathname.split("/").filter(item => item !== "");
+  const first = path[0];
+  const second = path[1];
+  const host = "https://" + uri.host + "/embed";
+
+  const isYoutubeId = (id) => {
+    if (!id) {
       return false;
     }
 
-    let uri = null;
+    return id.match(/^[a-zA-Z0-9_-]+$/) !== null;
+  };
 
-    try {
-      uri = new URL(url);
-    } catch (e) {
-      return false;
-    }
+  let query = uri.searchParams;
 
-    const path = uri.pathname.split("/").filter(item => item !== "");
-    const first = path[0];
-    const second = path[1];
-    const host = "https://" + uri.host + "/embed";
+  // build the correct base URL for the embed,
+  // the query params are appended below
+  let src = null;
+  switch (path.join("/")) {
+    case "embed/videoseries":
+    case "playlist":
+      if (isYoutubeId(query.get("list"))) {
+        src = host + "/videoseries";
+      }
+      break;
+    case "watch":
+      if (isYoutubeId(query.get("v"))) {
+        src = host + "/" + query.get("v");
 
-    const isYoutubeId = (id) => {
-      if (!id) {
-        return false;
+        if (query.has("t")) {
+          query.set("start", query.get("t"));
+        }
+
+        query.delete("v");
+        query.delete("t");
       }
 
-      return id.match(/^[a-zA-Z0-9_-]+$/) !== null;
-    };
+      break;
+    default:
+      // short URLs
+      if (uri.host.includes("youtu.be") && isYoutubeId(first)) {
+        src = 'https://www.youtube.com/embed/' + first;
 
-    let query = uri.searchParams;
-
-    // build the correct base URL for the embed,
-    // the query params are appended below
-    let src = null;
-    switch (path.join("/")) {
-      case "embed/videoseries":
-      case "playlist":
-        if (isYoutubeId(query.get("list"))) {
-          src = host + "/videoseries";
-        }
-        break;
-      case "watch":
-        if (isYoutubeId(query.get("v"))) {
-          src = host + "/" + query.get("v");
-
-          if (query.has("t")) {
-            query.set("start", query.get("t"));
-          }
-
-          query.delete("v");
-          query.delete("t");
+        if (query.has("t")) {
+          query.set("start", query.get("t"));
         }
 
-        break;
-      default:
-        // short URLs
-        if (uri.host.includes("youtu.be") && isYoutubeId(first)) {
-          src = 'https://www.youtube.com/embed/' + first;
-
-          if (query.has("t")) {
-            query.set("start", query.get("t"));
-          }
-
-          query.delete("t");
-        } else if (first === "embed" && isYoutubeId(second)) {
-          src = host + "/" + second;
-        }
-    }
-
-    if (!src) {
-      return false;
-    }
-
-    const queryString = query.toString();
-
-    if (queryString.length) {
-      src += "?" + queryString;
-    }
-
-    return src;
-
-  },
-  video(url) {
-
-    // YouTube video
-    if (url.includes("youtu")) {
-      return this.youtube(url);
-    }
-
-    // Vimeo video
-    if (url.includes("vimeo")) {
-      return this.vimeo(url);
-    }
-
-    return false;
-
-  },
-  vimeo(url) {
-
-    let uri = null;
-
-    try {
-      uri = new URL(url);
-    } catch (e) {
-      return false;
-    }
-
-    const path = uri.pathname.split("/").filter(item => item !== "");
-
-    let query = uri.searchParams;
-    let id = null;
-
-    switch (uri.host) {
-      case "vimeo.com":
-      case "www.vimeo.com":
-        id = path[0];
-        break;
-      case "player.vimeo.com":
-        id = path[1];
-        break;
-    }
-
-    if (!id || !id.match(/^[0-9]*$/)) {
-      return false;
-    }
-
-    let src = "https://player.vimeo.com/video/" + id;
-
-    const queryString = query.toString();
-
-    if (queryString.length) {
-      src += "?" + queryString;
-    }
-
-    return src;
-
+        query.delete("t");
+      } else if (first === "embed" && isYoutubeId(second)) {
+        src = host + "/" + second;
+      }
   }
+
+  if (!src) {
+    return false;
+  }
+
+  const queryString = query.toString();
+
+  if (queryString.length) {
+    src += "?" + queryString;
+  }
+
+  return src;
+}
+
+export function vimeo(url) {
+  let uri = null;
+
+  try {
+    uri = new URL(url);
+  } catch (e) {
+    return false;
+  }
+
+  const path = uri.pathname.split("/").filter(item => item !== "");
+
+  let query = uri.searchParams;
+  let id = null;
+
+  switch (uri.host) {
+    case "vimeo.com":
+    case "www.vimeo.com":
+      id = path[0];
+      break;
+    case "player.vimeo.com":
+      id = path[1];
+      break;
+  }
+
+  if (!id || !id.match(/^[0-9]*$/)) {
+    return false;
+  }
+
+  let src = "https://player.vimeo.com/video/" + id;
+
+  const queryString = query.toString();
+
+  if (queryString.length) {
+    src += "?" + queryString;
+  }
+
+  return src;
+}
+
+export function video(url) {
+  if (url.includes("youtu")) {
+    return youtube(url);
+  }
+
+  if (url.includes("vimeo")) {
+    return vimeo(url);
+  }
+
+  return false;
+}
+
+export default {
+  youtube,
+  vimeo,
+  video
 };
