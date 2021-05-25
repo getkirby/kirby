285313a2bd3c15b8a50d798f007483e82f06bb46
Nico Hoffmann <on-github@nhoffmann.com>
2021-05-25 21:18:31 +0200

    Remove unused GET API routes for lock info

--- config/api/routes/lock.php
+++ config/api/routes/lock.php
@@ -8,33 +8,11 @@ use Kirby\Exception\Exception;
 return [
     [
         'pattern' => '(:all)/lock',
-        'method'  => 'GET',
-        'action'  => function (string $path) {
-            if ($lock = $this->parent($path)->lock()) {
-                return [
-                    'supported' => true,
-                    'locked'    => $lock->get()
-                ];
-            }
-
-            return [
-                'supported' => false,
-                'locked'    => null
-            ];
-        }
-    ],
-    [
-        'pattern' => '(:all)/lock',
         'method'  => 'PATCH',
         'action'  => function (string $path) {
             if ($lock = $this->parent($path)->lock()) {
                 return $lock->create();
             }
-
-            throw new Exception([
-                'key'      => 'lock.notImplemented',
-                'httpCode' => 501
-            ]);
         }
     ],
     [
@@ -44,28 +22,6 @@ return [
             if ($lock = $this->parent($path)->lock()) {
                 return $lock->remove();
             }
-
-            throw new Exception([
-                'key'      => 'lock.notImplemented',
-                'httpCode' => 501
-            ]);
-        }
-    ],
-    [
-        'pattern' => '(:all)/unlock',
-        'method'  => 'GET',
-        'action'  => function (string $path) {
-            if ($lock = $this->parent($path)->lock()) {
-                return [
-                    'supported' => true,
-                    'unlocked'  => $lock->isUnlocked()
-                ];
-            }
-
-            return [
-                'supported' => false,
-                'unlocked'  => null
-            ];
         }
     ],
     [
@@ -75,11 +31,6 @@ return [
             if ($lock = $this->parent($path)->lock()) {
                 return $lock->unlock();
             }
-
-            throw new Exception([
-                'key'      => 'lock.notImplemented',
-                'httpCode' => 501
-            ]);
         }
     ],
     [
@@ -89,11 +40,6 @@ return [
             if ($lock = $this->parent($path)->lock()) {
                 return $lock->resolve();
             }
-
-            throw new Exception([
-                'key'      => 'lock.notImplemented',
-                'httpCode' => 501
-            ]);
         }
     ],
 ];
