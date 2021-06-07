6acd23e66bb560f9299557cb71e7970fc378f2ec
Nico Hoffmann <on-github@nhoffmann.com>
2021-06-07 18:47:38 +0200

    Fix frontend tests

--- panel/src/components/Views/PageView.spec.js
+++ panel/src/components/Views/PageView.spec.js
@@ -1,81 +1,92 @@
 const dialog = () => {
-  return cy.get('.k-dialog');
+  return cy.get(".k-dialog");
 };
 
-describe('PageView', () => {
-
+describe("PageView", () => {
   before(() => {
-    cy.visit('/env/install/starterkit');
-    cy.visit('/env/user/test');
+    cy.visit("/env/install/starterkit");
+    cy.visit("/env/user/test");
   });
 
   beforeEach(() => {
-    cy.visit('/env/auth/test');
+    cy.visit("/env/auth/test");
   });
 
-  describe('Photography', () => {
-
+  describe("Photography", () => {
     beforeEach(() => {
-      cy.visit('/panel/pages/photography');
-      cy.get('.k-section-name-drafts').as('drafts');
-      cy.get('.k-section-name-listed').as('listed');
+      cy.visit("/panel/pages/photography");
+      cy.get(".k-section-name-drafts").as("drafts");
+      cy.get(".k-section-name-listed").as("listed");
     });
 
-    it('should display correctly', () => {
+    it("should display correctly", () => {
       // Title
-      cy.get('.k-headline-editable').should('contain', 'Photography');
-      cy.get('.k-topbar-crumbs a:last-child').should('contain', 'Photography');
+      cy.get(".k-headline-editable").should("contain", "Photography");
+      cy.get(".k-topbar-breadcrumb a:last-child").should(
+        "contain",
+        "Photography"
+      );
 
       // Buttons
-      cy.get('.k-header-buttons .k-button-group:first-child .k-button:first-child').as('button')
-      cy.get('@button').should('have.attr', 'target', '_blank');
-      cy.get('@button').should('have.attr', 'href', Cypress.config().baseUrl + '/photography');
+      cy.get(
+        ".k-header-buttons .k-button-group:first-child .k-button:first-child"
+      ).as("button");
+      cy.get("@button").should("have.attr", "target", "_blank");
+      cy.get("@button").should(
+        "have.attr",
+        "href",
+        Cypress.config().baseUrl + "/photography"
+      );
 
-      cy.get('.k-header-buttons .k-button-group:first-child .k-status-icon').should('contain', 'Public');
+      cy.get(
+        ".k-header-buttons .k-button-group:first-child .k-status-icon"
+      ).should("contain", "Public");
 
       // Drafts
-      cy.get('@drafts').find('.k-headline').should('contain', 'Drafts');
+      cy.get("@drafts").find(".k-headline").should("contain", "Drafts");
 
       // Published Albums
-      cy.get('@listed').find('.k-headline').should('contain', 'Published Albums');
+      cy.get("@listed")
+        .find(".k-headline")
+        .should("contain", "Published Albums");
     });
 
-    it('should create draft', () => {
-      cy.get('@drafts').find('.k-section-header .k-button').click();
+    it("should create draft", () => {
+      cy.get("@drafts").find(".k-section-header .k-button").click();
 
-      dialog().find('input[name=title]').type('Portraits');
-      dialog().find('form').submit();
+      dialog().find("input[name=title]").type("Portraits");
+      dialog().find("form").submit();
 
-      cy.url().should('contain', '/pages/photography+portraits');
-      cy.get('.k-headline-editable').should('contain', 'Portraits');
-      cy.get('.k-status-icon').should('contain', 'Draft');
+      cy.url().should("contain", "/pages/photography+portraits");
+      cy.get(".k-headline-editable").should("contain", "Portraits");
+      cy.get(".k-status-icon").should("contain", "Draft");
     });
 
-    it('should publish draft', () => {
-      cy.get('@drafts').find('.k-card').should('have.length', 2);
-      cy.get('@listed').find('.k-card').should('have.length', 8);
+    it("should publish draft", () => {
+      cy.get("@drafts").find(".k-card").should("have.length", 2);
+      cy.get("@listed").find(".k-card").should("have.length", 8);
 
-      cy.get('@drafts').find('.k-card:first-child .k-status-icon').click();
+      cy.get("@drafts").find(".k-card:first-child .k-status-icon").click();
 
-      dialog().find('.k-radio-input li:last-child label').click();
-      dialog().find('form').submit();
+      dialog().find(".k-radio-input li:last-child label").click();
+      dialog().find("form").submit();
 
-      cy.get('@drafts').find('.k-card').should('have.length', 1);
-      cy.get('@listed').find('.k-card').should('have.length', 9);
+      cy.get("@drafts").find(".k-card").should("have.length", 1);
+      cy.get("@listed").find(".k-card").should("have.length", 9);
     });
 
-    it('should delete draft', () => {
-      cy.get('@drafts').as('draft');
-      cy.get('@drafts').find('.k-card').should('have.length', 1);
+    it("should delete draft", () => {
+      cy.get("@drafts").as("draft");
+      cy.get("@drafts").find(".k-card").should("have.length", 1);
 
-      cy.get('@draft').find('.k-card-options-button:last-child').click();
-      cy.get('@draft').find('.k-card-options-dropdown .k-button:last-child').click();
+      cy.get("@draft").find(".k-card-options-button:last-child").click();
+      cy.get("@draft")
+        .find(".k-card-options-dropdown .k-button:last-child")
+        .click();
 
-      dialog().find('.k-dialog-button-submit').click();
+      dialog().find(".k-dialog-button-submit").click();
 
-      cy.get('@drafts').find('.k-card').should('have.length', 0);
+      cy.get("@drafts").find(".k-card").should("have.length", 0);
     });
-
   });
-
 });
--- panel/src/components/Views/UsersView.spec.js
+++ panel/src/components/Views/UsersView.spec.js
@@ -1,63 +1,67 @@
-describe('UsersView', () => {
-
+describe("UsersView", () => {
   before(() => {
-    cy.visit('/env/install/roles');
-    cy.visit('/env/user/test');
+    cy.visit("/env/install/roles");
+    cy.visit("/env/user/test");
   });
 
   beforeEach(() => {
-    cy.visit('/env/auth/test');
-    cy.visit('/panel/users');
-    cy.get('.k-users-view .k-collection').as('users');
+    cy.visit("/env/auth/test");
+    cy.visit("/panel/users");
+    cy.get(".k-users-view .k-collection").as("users");
   });
 
-  it('should display correctly', () => {
-    cy.get('.k-topbar-menu-button').click();
-    cy.get('.k-topbar-menu li:nth-child(3)').should('have.attr', 'aria-current', 'true');
+  it("should display correctly", () => {
+    cy.get(".k-topbar-menu-button").click();
+    cy.get(".k-topbar-menu a:nth-child(3)").should(
+      "have.attr",
+      "aria-current",
+      "true"
+    );
 
-    cy.get('.k-headline').should('contain', 'Users');
-    cy.get('.k-topbar-view-button').should('contain', 'Users');
+    cy.get(".k-headline").should("contain", "Users");
+    cy.get(".k-topbar-breadcrumb a:first-child").should("contain", "Users");
 
-    cy.get('@users').should('have.attr', 'data-layout', 'list');
-    cy.get('@users').find('li').should('have.length', 1);
-    cy.get('@users').find('li:first-child').should('contain', 'test@getkirby.com');
-    cy.get('@users').find('li:first-child').should('contain', 'Admin');
+    cy.get("@users").should("have.attr", "data-layout", "list");
+    cy.get("@users").find("li").should("have.length", 1);
+    cy.get("@users")
+      .find("li:first-child")
+      .should("contain", "test@getkirby.com");
+    cy.get("@users").find("li:first-child").should("contain", "Admin");
   });
 
-  describe('UserCreateDialog', () => {
-
+  describe("UserCreateDialog", () => {
     beforeEach(() => {
-      cy.get('.k-header-buttons .k-button-group:first-child > .k-button:first-child').click();
-      cy.get('.k-dialog').as('dialog');
+      cy.get(
+        ".k-header-buttons .k-button-group:first-child > .k-button:first-child"
+      ).click();
+      cy.get(".k-dialog").as("dialog");
     });
 
-    it('should create admin', () => {
-      cy.get('@dialog').find('input[name="name"]').type('Ada');
-      cy.get('@dialog').find('input[name="email"]').type('ada@getkirby.com');
-      cy.get('@dialog').find('input[name="password"]').type('top-secret-1234');
-      cy.get('@dialog').find('form').submit();
+    it("should create admin", () => {
+      cy.get("@dialog").find('input[name="name"]').type("Ada");
+      cy.get("@dialog").find('input[name="email"]').type("ada@getkirby.com");
+      cy.get("@dialog").find('input[name="password"]').type("top-secret-1234");
+      cy.get("@dialog").find("form").submit();
 
-      cy.get('@users').find('li').should('have.length', 2);
+      cy.get("@users").find("li").should("have.length", 2);
       // @todo the order of the users in the list is non-deterministic;
       // cannot reliably test the order and if the role is in the same line as the name
-      cy.get('@users').find('li.k-list-item').should('contain', 'Ada');
-      cy.get('@users').find('li.k-list-item').should('contain', 'Admin');
+      cy.get("@users").find("li.k-list-item").should("contain", "Ada");
+      cy.get("@users").find("li.k-list-item").should("contain", "Admin");
     });
 
-    it('should create editor', () => {
-      cy.get('@dialog').find('input[name="name"]').type('Grace');
-      cy.get('@dialog').find('input[name="email"]').type('grace@getkirby.com');
-      cy.get('@dialog').find('input[name="password"]').type('top-secret-1234');
-      cy.get('@dialog').find('.k-radio-input li:last-child label').click();
-      cy.get('@dialog').find('form').submit();
+    it("should create editor", () => {
+      cy.get("@dialog").find('input[name="name"]').type("Grace");
+      cy.get("@dialog").find('input[name="email"]').type("grace@getkirby.com");
+      cy.get("@dialog").find('input[name="password"]').type("top-secret-1234");
+      cy.get("@dialog").find(".k-radio-input li:last-child label").click();
+      cy.get("@dialog").find("form").submit();
 
-      cy.get('@users').find('li').should('have.length', 3);
+      cy.get("@users").find("li").should("have.length", 3);
       // @todo the order of the users in the list is non-deterministic;
       // cannot reliably test the order and if the role is in the same line as the name
-      cy.get('@users').find('li.k-list-item').should('contain', 'Grace');
-      cy.get('@users').find('li.k-list-item').should('contain', 'Editor');
+      cy.get("@users").find("li.k-list-item").should("contain", "Grace");
+      cy.get("@users").find("li.k-list-item").should("contain", "Editor");
     });
-
   });
-
 });
