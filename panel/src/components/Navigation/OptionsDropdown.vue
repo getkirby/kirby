872953bbda4a3fe6f6cf6ae43f2291142fca2e83
Nico Hoffmann <on-github@nhoffmann.com>
2021-06-09 22:17:42 +0200

    Add `k-options-dropdown`

---
+++ panel/src/components/Navigation/OptionsDropdown.vue
@@ -0,0 +1,98 @@
+<template>
+  <!-- Single option = button -->
+  <k-button
+    v-if="single && options[0]"
+    :icon="options[0].icon"
+    :tooltip="options[0].text"
+    :disabled="options[0].disabled"
+    class="k-options-dropdown-toggle"
+    @click="onOption(options[0].option || options[0].click, options[0], 0)"
+  >
+    <template v-if="label">
+      {{ label }}
+    </template>
+  </k-button>
+
+  <!-- Multiple options = dropdown -->
+  <k-dropdown
+    v-else-if="options.length"
+    class="k-options-dropdown"
+  >
+    <k-button
+      :icon="icon || 'dots'"
+      :tooltip="$t('options')"
+      class="k-options-dropdown-toggle"
+      @click="$refs.options.toggle()"
+    >
+      <template v-if="text && text !== true">
+        {{ text }}
+      </template>
+    </k-button>
+    <k-dropdown-content
+      ref="options"
+      :align="align"
+      :options="options"
+      class="k-options-dropdown-content"
+      @option="onOption"
+    />
+  </k-dropdown>
+</template>
+
+<script>
+export default {
+  props: {
+    align: {
+      type: String,
+      default: "right"
+    },
+    icon: {
+      type: String,
+      default: "dots",
+    },
+    options: {
+      type: [Array, Function],
+      default() {
+        return []
+      }
+    },
+    text: {
+      type: [Boolean, String],
+      default: false
+    },
+  },
+  computed: {
+    label() {
+      if (this.text !== true) {
+        return this.text;
+      }
+
+      return this.options[0].text;
+    },
+    single() {
+      return Array.isArray(this.options) && this.options.length === 1;
+    }
+  },
+  methods: {
+    onOption(option, item, itemIndex) {
+      this.$emit("option", option, item, itemIndex);
+    }
+  }
+}
+</script>
+
+<style>
+.k-options-dropdown {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 38px;
+}
+.k-options-dropdown-toggle {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 38px;
+  height: 38px;
+  padding: 0 .75rem;
+}
+</style>
