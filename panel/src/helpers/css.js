f6605e054668dabcc6273c165c7ead7b32c695c8
Nico Hoffmann <on-github@nhoffmann.com>
2021-06-10 10:05:23 +0200

    `css.color` JS helper

---
+++ panel/src/helpers/css.js
@@ -0,0 +1,21 @@
+export function color(string) {
+  if (typeof string !== "string") {
+    return;
+  }
+
+  const isHex = string.substring(0, 1) === "#";
+
+  if (isHex || string.startsWith("var(")) {
+    return string;
+  }
+
+  if (string === "pattern") {
+    return `var(--color-gray-800) var(--bg-pattern)`;
+  }
+
+  return `var(--color-${string})`;
+}
+
+export default {
+  color
+};
