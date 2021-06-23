3e2dec34ff04f08d4e7c72c14469f389715d5aa2
Bastian Allgeier <mail@bastianallgeier.com>
2021-06-23 13:11:06 +0200

    Fix z-index issue in items

--- panel/src/components/Layout/Item.vue
+++ panel/src/components/Layout/Item.vue
@@ -174,10 +174,9 @@ export default {
   justify-content: flex-end;
   flex-shrink: 0;
   flex-grow: 1;
-  z-index: 2;
 }
 .k-item-buttons > .k-button,
-.k-item-buttons > .k-dropdown  {
+.k-item-buttons > .k-dropdown {
   position: relative;
   width: 38px;
   height: 38px;
@@ -185,10 +184,12 @@ export default {
   align-items: center;
   justify-content: center;
   line-height: 1;
+}
+.k-item-buttons > .k-button {
   z-index: 1;
 }
-.k-item-options-dropdown {
-  z-index: 2;
+.k-item-buttons > .k-dropdown {
+  z-index: var(--z-dropdown);
 }
 
 /** List Item **/
