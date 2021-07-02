d2ca67289eb111657df2dcf890b59d5c522d7738
Ahmet Bora <byybora@gmail.com>
2021-07-02 20:40:15 +0300

    Resolves `escapeHTML` tests

--- panel/src/helpers/string.js
+++ panel/src/helpers/string.js
@@ -4,23 +4,23 @@ export function camelToKebab(string) {
 
 /**
   * Source: https://github.com/janl/mustache.js/blob/v4.2.0/mustache.js#L60-L75
-  * 
+  *
   * The MIT License
   *
   * Copyright (c) 2009 Chris Wanstrath (Ruby)
   * Copyright (c) 2010-2014 Jan Lehnardt (JavaScript)
   * Copyright (c) 2010-2015 The mustache.js community
-  * 
+  *
   * Permission is hereby granted, free of charge, to any person obtaining a
   * copy of this software and associated documentation files (the "Software"),
   * to deal in the Software without restriction, including without limitation
   * the rights to use, copy, modify, merge, publish, distribute, sublicense,
   * and/or sell copies of the Software, and to permit persons to whom the
   * Software is furnished to do so, subject to the following conditions:
-  * 
+  *
   * The above copyright notice and this permission notice shall be included
   * in all copies or substantial portions of the Software.
-  * 
+  *
   * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
   * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -113,6 +113,7 @@ export function ucwords(string) {
 
 export default {
   camelToKebab,
+  escapeHTML,
   hasEmoji,
   lcfirst,
   random,
