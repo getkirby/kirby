9e210601d970c961c9a429ccd84806a1ad2f5916
Nico Hoffmann <on-github@nhoffmann.com>
2021-07-05 19:19:44 +0200

    Revert `k-outside` changes

--- panel/src/components/Layout/Outside.vue
+++ panel/src/components/Layout/Outside.vue
@@ -1,19 +1,5 @@
 <template>
-  <div
-    :data-language="language.code"
-    :dir="$translation.direction"
-    class="k-panel k-panel-outside"
-  >
+  <div :dir="$translation.direction" class="k-panel k-panel-outside">
     <slot />
   </div>
 </template>
-
-<script>
-export default {
-  computed: {
-    language() {
-      return this.$language ? this.$language.code : false;
-    }
-  }
-}
-</script>
