a433790c2c5d3500c146c3afe264aeedbe39161a
Ahmet Bora <byybora@gmail.com>
2021-07-07 19:20:41 +0300

    Remove `TranslationString()` helper

--- panel/src/config/i18n.js
+++ panel/src/config/i18n.js
@@ -1,13 +1,5 @@
 import { template } from "../helpers/string.js";
 
-export class TranslationString extends String {
-  toString() {
-    const Vue = window.panel.$vue;
-    const value = this.valueOf();
-    return Vue ? Vue.$t(value) : value;
-  }
-}
-
 export default {
   install(app) {
     app.$t = app.prototype.$t = window.panel.$t = (key, data) => {
