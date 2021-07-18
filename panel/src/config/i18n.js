ebda4200ccea29da9ed506ca50f665dec6e60f61
Nico Hoffmann <on-github@nhoffmann.com>
2021-07-18 20:30:24 +0200

    `i18n.$t` - add fallback parameter

--- panel/src/config/i18n.js
+++ panel/src/config/i18n.js
@@ -2,15 +2,15 @@ import { template } from "../helpers/string.js";
 
 export default {
   install(app) {
-    app.$t = app.prototype.$t = window.panel.$t = (key, data) => {
+    app.$t = app.prototype.$t = window.panel.$t = (key, data, fallback = null) => {
       if (typeof key !== "string") {
         return;
       }
 
-      const string = window.panel.$translation.data[key];
+      const string = window.panel.$translation.data[key] || fallback;
 
-      if (!string) {
-        return null;
+      if (typeof string !== "string") {
+        return string;
       }
 
       return template(string, data);
