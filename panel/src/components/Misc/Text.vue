1ca8538f00da4ddfa582622c98d31c13e2b16b98
Nico Hoffmann <on-github@nhoffmann.com>
2021-07-23 21:31:52 +0200

    CSS utility: align

--- panel/src/components/Misc/Text.vue
+++ panel/src/components/Misc/Text.vue
@@ -60,13 +60,6 @@ export default {
   margin-block-end: 0;
 }
 
-.k-text[data-align="center"] {
-  text-align: center;
-}
-.k-text[data-align="right"] {
-  text-align: end;
-}
-
 .k-text[data-size="tiny"] {
   font-size: var(--text-xs);
 }
