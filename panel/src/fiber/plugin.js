5ae86684ba10f497d9933c07d476de2f6d209499
Nico Hoffmann <on-github@nhoffmann.com>
2021-07-23 14:56:25 +0200

    Fiber: simplify `plugin.js`

--- panel/src/fiber/plugin.js
+++ panel/src/fiber/plugin.js
@@ -5,23 +5,11 @@ import dropdown from "./dropdown"
 
 export default {
   install(app) {
-    app.prototype.$dialog = window.panel.$dialog = dialog;
+    app.prototype.$dialog   = window.panel.$dialog   = dialog;
     app.prototype.$dropdown = window.panel.$dropdown = dropdown;
-
-    app.prototype.$go = window.panel.$go = function (path, options) {
-      return Fiber.go(this.$url(path), options);
-    };
-
-    app.prototype.$reload = window.panel.$reload = function (options) {
-      return Fiber.reload(options);
-    };
-
-    app.prototype.$request = async function (...args) {
-      return await Fiber.request(...args);
-    };
-
-    app.prototype.$url = function (...args) {
-      return Fiber.url(...args);
-    };
+    app.prototype.$go       = window.panel.$go       = (path, options) => Fiber.go(Fiber.url(path), options);
+    app.prototype.$reload   = window.panel.$reload   = Fiber.reload;
+    app.prototype.$request  = window.panel.$request  = Fiber.request;
+    app.prototype.$url      = window.panel.$url      = (...args) => Fiber.url(...args);
   }
 };
