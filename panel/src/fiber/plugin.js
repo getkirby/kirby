4ff561bc70be8cf92ccca38058ddce1a5846071c
Nico Hoffmann <on-github@nhoffmann.com>
2021-07-23 15:31:55 +0200

    Fix Fiber `plugin.js`

--- panel/src/fiber/plugin.js
+++ panel/src/fiber/plugin.js
@@ -8,8 +8,8 @@ export default {
     app.prototype.$dialog   = window.panel.$dialog   = dialog;
     app.prototype.$dropdown = window.panel.$dropdown = dropdown;
     app.prototype.$go       = window.panel.$go       = (path, options) => Fiber.go(Fiber.url(path), options);
-    app.prototype.$reload   = window.panel.$reload   = Fiber.reload;
-    app.prototype.$request  = window.panel.$request  = Fiber.request;
+    app.prototype.$reload   = window.panel.$reload   = (options) => Fiber.reload(options);
+    app.prototype.$request  = window.panel.$request  = (...args) => Fiber.request(...args);
     app.prototype.$url      = window.panel.$url      = (...args) => Fiber.url(...args);
   }
 };
