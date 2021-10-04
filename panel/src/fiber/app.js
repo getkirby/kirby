f521f149dea5c7f04451f76e26098b05e1d40795
Bastian Allgeier <mail@bastianallgeier.com>
2021-10-04 17:51:37 +0200

    Refactor Fiber internals to fix history

--- panel/src/fiber/app.js
+++ panel/src/fiber/app.js
@@ -1,36 +1,100 @@
 
 import Fiber from "./index";
+import Vue from "vue";
 
 export default {
   name: "Fiber",
   data() {
     return {
       component: null,
-      page: window.fiber,
+      state: window.fiber,
       key: null
     };
   },
   created() {
     Fiber.init({
-      page: window.fiber,
+      state: this.state,
       csrf: window.fiber.$system.csrf,
-      swap: async ({ component, page, preserveState }) => {
-        this.component = component;
-        this.page = page;
-        this.key = preserveState ? this.key : Date.now();
+      finish: () => {
+        if (this.$api.requests.length === 0) {
+          this.$store.dispatch("isLoading", false);
+        }
+      },
+      start: ({ silent }) => {
+        if (silent !== true) {
+          this.$store.dispatch("isLoading", true);
+        }
+      },
+      swap: async (state, options) => {
+        options = {
+          navigate: true,
+          replace: false,
+          ...options
+        };
 
-        if (preserveState !== true) {
-          document.documentElement.style.overflow = "visible";
-          this.$store.dispatch("navigate");
+        this.setGlobals(state);
+        this.setTitle(state);
+        this.setTranslation(state);
+
+        this.component = state.$view.component;
+        this.state     = state;
+        this.key       = options.replace === true ? this.key : state.$view.timestamp;
+
+        if (options.navigate === true) {
+          this.navigate();
         }
       }
     });
+  },
+  methods: {
+    navigate() {
+      document.documentElement.style.overflow = "visible";
+      this.$store.dispatch("navigate");
+    },
+    setGlobals(state) {
+      [
+        "$config",
+        "$direction",
+        "$language",
+        "$languages",
+        "$license",
+        "$menu",
+        "$multilang",
+        "$permissions",
+        "$searches",
+        "$system",
+        "$translation",
+        "$urls",
+        "$user",
+        "$view"
+      ].forEach((key) => {
+        if (state[key] !== undefined) {
+          Vue.prototype[key] = window.panel[key] = state[key];
+        } else {
+          Vue.prototype[key] = state[key] = window.panel[key];
+        }
+      });
+    },
+    setTitle(state) {
+      // set the document title according to $view.title
+      if (state.$view.title) {
+        document.title = state.$view.title + " | " + state.$system.title;
+      } else {
+        document.title = state.$system.title;
+      }
+    },
+    setTranslation(state) {
+      // set the lang attribute according to the current translation
+      if (state.$translation) {
+        document.documentElement.lang = state.$translation.code;
+      }
+    }
   },
   render(h) {
     if (this.component) {
       return h(this.component, {
         key: this.key,
-        props: this.page.$view.props
+        props: this.state.$view.props
       });
     }
   }
--- panel/src/fiber/index.js
+++ panel/src/fiber/index.js
@@ -24,408 +24,221 @@
  * SOFTWARE.
  */
 
- import Vue from "vue";
- import clone from "../helpers/clone";
- import debounce from "../helpers/debounce";
- import { merge } from "../helpers/object";
- import { toJson } from "../api/request";
+import clone from "../helpers/clone";
+import { merge } from "../helpers/object";
+import { toJson } from "../api/request";
 
- export default {
-   base: null,
-   csrf: null,
-   page: null,
-   swap: null,
+export default {
+  base: null,
+  finish: null,
+  csrf: null,
+  start: null,
+  state: null,
+  swap: null,
 
-   /**
-    * Setup call to make Fiber ready
-    *
-    * @param {object} options
-    */
-   init({ csrf, page, swap, base }) {
+  /**
+   * Setup call to make Fiber ready
+   *
+   * @param {object} options
+   */
+  init({
+    csrf,
+    base,
+    finish,
+    state,
+    start,
+    swap,
+  }) {
 
-     // set the base URL for all requests
-     this.base = base || document.querySelector("base").href;
+    // set the base URL for all requests
+    this.base = base || document.querySelector("base").href;
+    this.csrf = csrf;
 
-     this.csrf = csrf;
+    // callback functions which handle
+    // swapping components and loading events
+    this.finish = finish;
+    this.start  = start;
+    this.swap   = swap;
 
-     // callback function which handles
-     // swapping components
-     this.swap = swap;
+    // set initial state
+    this.setState(state);
 
-     // set initial page
-     this.setPage(page);
+    // back button event
+    window.addEventListener("popstate", this.reload.bind(this));
+  },
 
-     // back button event
-     window.addEventListener("popstate", this.onPopstateEvent.bind(this));
+  /**
+   * Prepares a set of values to
+   * be included in a comma-separated list
+   * in a URL query (see include & only params)
+   *
+   * @param {string|Array} array
+   * @returns Array
+   */
+  arrayToString(array) {
+    if (Array.isArray(array) === false) {
+      return String(array);
+    }
+    return array.join(",");
+  },
 
-     // remember scroll position
-     document.addEventListener(
-       "scroll",
-       debounce(this.onScrollEvent.bind(this), 100),
-       true
-     );
-   },
+  /**
+   * Creates a proper request body
+   *
+   * @param {string|object}
+   * @returns {string}
+   */
+  body(body) {
+    if (typeof body === "object") {
+      return JSON.stringify(body);
+    }
 
-   /**
-    * Prepares a set of values to
-    * be included in a comma-separated list
-    * in a URL query (see include & only params)
-    *
-    * @param {string|Array} array
-    * @returns Array
-    */
-   arrayToString(array) {
-     if (Array.isArray(array) === false) {
-       return String(array);
-     }
-     return array.join(",");
-   },
+    return body;
+  },
 
-   /**
-    * Creates a proper request body
-    *
-    * @param {string|object}
-    * @returns {string}
-    */
-   body(body) {
-     if (typeof body === "object") {
-       return JSON.stringify(body);
-     }
+  /**
+   * Sends a view request to load and
+   * navigate to a new view
+   *
+   * @param {string} url
+   * @param {object} options
+   * @returns {object}
+   */
+  async go(url, options) {
+    const json = await this.request(url, options);
+    return this.setState(json, options);
+  },
 
-     return body;
-   },
+  /**
+   * Builds a query string for request URLs
+   *
+   * @param {object} data
+   * @param {object} base
+   * @returns {URLSearchParams}
+   */
+  query(query = {}, base = {}) {
+    let params = new URLSearchParams(base);
 
-   /**
-    * Loads the Vue component for a
-    * Fiber view
-    *
-    * @param {string} name
-    * @returns {object}
-    */
-   component(name) {
-     return Vue.component(name);
-   },
+    // make sure that we always work with a data object
+    if (typeof query !== "object") {
+      query = {};
+    }
 
-   /**
-    * After a new view response is loaded
-    * the props are all processed to set the
-    * document title and language. The props are
-    * merged with the globals from window.panel
-    * to get a full state for the view
-    *
-    * @param {object} data
-    * @returns {object}
-    */
-   data(data) {
-     [
-       "$config",
-       "$direction",
-       "$language",
-       "$languages",
-       "$license",
-       "$menu",
-       "$multilang",
-       "$permissions",
-       "$searches",
-       "$system",
-       "$translation",
-       "$urls",
-       "$user",
-       "$view"
-     ].forEach((key) => {
-       if (data[key] !== undefined) {
-         Vue.prototype[key] = window.panel[key] = data[key];
-       } else {
-         Vue.prototype[key] = data[key] = window.panel[key];
-       }
-     });
+    // add all data params unless they are empty/null
+    Object.entries(query).forEach(([key, value]) => {
+      if (value !== null) {
+        params.set(key, value);
+      }
+    });
 
-     // set the lang attribute according to the current translation
-     if (data.$translation) {
-       document.documentElement.lang = data.$translation.code;
-     }
+    return params;
+  },
 
-     // set the document title according to $view.title
-     if (data.$view.title) {
-       document.title = data.$view.title + " | " + data.$system.title;
-     } else {
-       document.title = data.$system.title;
-     }
+  /**
+   * A wrapper around go() which
+   * reloads the current URL
+   *
+   * @param {object} options
+   * @returns {object}
+   */
+  reload(options = {}) {
+    return this.go(window.location.href, {
+      ...options,
+      replace: true
+    });
+  },
 
-     // return the full data object
-     return data;
-   },
+  /**
+   * Sends a generic Fiber request
+   *
+   * @param {string|URL} path
+   * @param {Object} options
+   * @returns {Object}
+   */
+  async request(path, options = {}) {
+    options = {
+      globals: false,
+      method: "GET",
+      only: [],
+      query: {},
+      silent: false,
+      ...options
+    };
 
-   /**
-    * Sends a view request to load and
-    * navigate to a new view
-    *
-    * @param {string} url
-    * @param {object} options
-    * @returns {object}
-    */
-   async go(url, options) {
-     options = {
-       headers: {},
-       only: [],
-       preserveScroll: false,
-       preserveState: false,
-       globals: false,
-       silent: false,
-       ...options || {}
-     };
+    const globals = this.arrayToString(options.globals);
+    const only    = this.arrayToString(options.only);
 
-     // save the current scrolling positions
-     // for all scroll regions
-     this.saveScroll();
+    this.start(options);
 
-     const globals = this.arrayToString(options.globals);
-     const only    = this.arrayToString(options.only);
+    try {
+      const url = this.url(path, options.query);
+      const response = await fetch(url, {
+        method: options.method,
+        body: this.body(options.body),
+        credentials: "same-origin",
+        cache: "no-store",
+        headers: {
+          "X-CSRF": this.csrf,
+          "X-Fiber": true,
+          "X-Fiber-Globals": globals,
+          "X-Fiber-Only": only,
+          "X-Fiber-Referrer": this.state.$view.path,
+          ...options.headers,
+        }
+      });
 
-     let json = await this.request(url, {
-       ...options,
-       headers: {
-         "X-Fiber-Globals": globals,
-         "X-Fiber-Only": only,
-         ...options.headers
-       }
-     });
+      let json = await toJson(response);
 
-     // add exisiting data to partial requests
-     if (only.length) {
-       json = merge(this.page, json);
-     }
+      // add exisiting data to partial requests
+      if (only.length) {
+        json = merge(this.state, json);
+      }
 
-     return this.setPage(json, options);
-   },
+      return json;
+    } finally {
+      this.finish(options);
+    }
 
-   /**
-    * Handles the browser back button event
-    */
-   async onPopstateEvent() {
-     this.reload();
-   },
+  },
 
-   /**
-    * Saves the scroll position of every area
-    * that has the scroll-region attribute
-    *
-    * @param {*} event
-    */
-   onScrollEvent(event) {
-     if (
-       typeof event.target.hasAttribute === "function" &&
-       event.target.hasAttribute("scroll-region")
-     ) {
-       this.saveScroll();
-     }
-   },
+  /**
+   * Stores the state for the current page
+   *
+   * @param {object} state
+   * @param {object} options
+   */
+  async setState(state, options = {}) {
+    // clone existing data
+    this.state = clone(state);
 
-   /**
-    * Builds a query string for request URLs
-    *
-    * @param {object} data
-    * @param {object} base
-    * @returns {URLSearchParams}
-    */
-   query(query = {}, base = {}) {
-     let params = new URLSearchParams(base);
+    // either replacing the whole state
+    // or pushing onto it
+    if (options.replace === true || this.url(this.state.$url).href === window.location.href) {
+      window.history.replaceState(this.state, "", this.state.$url);
+    } else {
+      window.history.pushState(this.state, "", this.state.$url);
+    }
 
-     // make sure that we always work with a data object
-     if (typeof query !== "object") {
-       query = {};
-     }
+    this.swap(state, options);
+  },
 
-     // add all data params unless they are empty/null
-     Object.entries(query).forEach(([key, value]) => {
-       if (value !== null) {
-         params.set(key, value);
-       }
-     });
+  /**
+   * Builds a full URL object based on the
+   * given path or another URL object and query data
+   *
+   * @param {string|URL} url
+   * @param {Object} query
+   * @returns
+   */
+  url(url = "", query = {}) {
+    if (typeof url === "string" && url.match(/^https?:\/\//) === null) {
+      url = new URL(this.base + url.replace(/^\//, ""));
+    } else {
+      url = new URL(url);
+    }
 
-     return params;
-   },
-
-   /**
-    * A wrapper around go() which
-    * reloads the current URL
-    *
-    * @param {object} options
-    * @returns {object}
-    */
-   reload(options = {}) {
-     return this.go(window.location.href, {
-       ...options,
-       preserveScroll: true,
-       preserveState: true
-     });
-   },
-
-   /**
-    * Sends a generic Fiber request
-    *
-    * @param {string|URL} path
-    * @param {Object} options
-    * @returns {Object}
-    */
-   async request(path, options = {}) {
-     options = {
-       method: "GET",
-       query: {},
-       silent: false,
-       ...options
-     };
-
-     document.dispatchEvent(new CustomEvent("fiber.start", { detail: options }));
-
-     try {
-       const url      = this.url(path, options.query);
-       const response = await fetch(url, {
-         method: options.method,
-         body: this.body(options.body),
-         credentials: "same-origin",
-         cache: "no-store",
-         headers: {
-           "X-CSRF": this.csrf,
-           "X-Fiber": true,
-           "X-Fiber-Referrer": this.page.$view.path,
-           ...options.headers,
-         }
-       });
-
-       return toJson(response);
-     } finally {
-       document.dispatchEvent(new Event("fiber.finish"));
-     }
-
-   },
-
-   /**
-    * Moves the scroll position of every
-    * scroll region back to the top
-    */
-   resetScroll() {
-     // update the scroll position of the document
-     document.documentElement.scrollTop = 0;
-     document.documentElement.scrollLeft = 0;
-
-     // update the scroll position of each region
-     this.scrollRegions().forEach((region) => {
-       region.scrollTop = 0;
-       region.scrollLeft = 0;
-     });
-
-     // resave the restored scroll position
-     this.saveScroll();
-   },
-
-   /**
-    * Restores the previously saved scroll
-    * positions for every scroll region
-    */
-   restoreScroll() {
-     if (this.page.scrollRegions) {
-       this.scrollRegions().forEach((region, index) => {
-         region.scrollTop = this.page.scrollRegions[index].top;
-         region.scrollLeft = this.page.scrollRegions[index].left;
-       });
-     }
-   },
-
-   /**
-    * Saves the scroll position for every
-    * scroll region
-    */
-   saveScroll() {
-     const regions = Array.from(this.scrollRegions());
-     this.state({
-       ...this.page,
-       scrollRegions: regions.map((region) => ({
-         top: region.scrollTop,
-         left: region.scrollLeft
-       }))
-     });
-   },
-
-   /**
-    * Fetches all DOM elements with
-    * the scroll region attribute
-    *
-    * @returns NodeList
-    */
-   scrollRegions() {
-     return document.querySelectorAll("[scroll-region]");
-   },
-
-   /**
-    * Stores the state for the current page
-    *
-    * @param {object} page
-    * @param {object} options
-    */
-   async setPage(page, {
-     replace = false,
-     preserveScroll = false,
-     preserveState = false
-   } = {}) {
-     // resolve component
-     const component = await this.component(page.$view.component);
-
-     // get all scroll regions
-     page.scrollRegions = page.scrollRegions || [];
-
-     // either replacing the whole state
-     // or pushing onto it
-     if (replace || this.url(page.$url).href === window.location.href) {
-       this.state(page);
-     } else {
-       this.state(page, "push");
-     }
-
-     // clone existing data
-     let data = clone(page);
-
-     // apply all data
-     data = this.data(data);
-
-     // call and wait for the swap callback
-     await this.swap({ component, page: data, preserveState });
-
-     // reset scrolling if it should not be preserved
-     if (!preserveScroll) {
-       this.resetScroll();
-     }
-   },
-
-   /**
-    * Updates the browser history and the page
-    * object with the current state
-    *
-    * @param {object} page
-    * @param {string} action
-    */
-   state(page, action = "replace") {
-     this.page = page;
-     window.history[action + "State"](page, "", page.$url);
-   },
-
-   /**
-    * Builds a full URL object based on the
-    * given path or another URL object and query data
-    *
-    * @param {string|URL} url
-    * @param {Object} query
-    * @returns
-    */
-   url(url = "", query = {}) {
-     if (typeof url === "string" && url.match(/^https?:\/\//) === null) {
-       url = new URL(this.base + url.replace(/^\//, ""));
-     } else {
-       url = new URL(url);
-     }
-
-     url.search = this.query(query, url.search);
-     return url;
-   },
+    url.search = this.query(query, url.search);
+    return url;
+  },
 
  };
