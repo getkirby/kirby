7ce722e7d23f94c946446b7043f6ef3051a82eac
Bastian Allgeier <mail@bastianallgeier.com>
2021-10-18 16:25:38 +0200

    Slightly different badge design

--- panel/src/components/Layout/Tabs.vue
+++ panel/src/components/Layout/Tabs.vue
@@ -170,6 +170,7 @@ export default {
   overflow: hidden;
   max-width: 10rem;
   text-overflow: ellipsis;
+  opacity: 1;
 }
 
 @media screen and (min-width: 30em) {
@@ -216,10 +217,15 @@ export default {
 }
 .k-tabs-badge {
   position: absolute;
-  top: .25rem;
-  inset-inline-end: .5rem;
+  top: -.5rem;
+  font-variant-numeric: tabular-nums;
+  inset-inline-end: -.5rem;
+  line-height: var(--leading-normal);
+  padding: 0rem .375rem;
+  border-radius: var(--rounded);
 }
 .k-tabs[data-theme="notice"] .k-tabs-badge {
-  color: var(--color-orange-600);
+  background: var(--theme-light);
+  color: var(--color-black);
 }
 </style>
