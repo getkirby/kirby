0c28119f38bb863630aa3da7fb8218d8c58ce3ea
Bastian Allgeier <mail@bastianallgeier.com>
2021-11-03 12:04:36 +0100

    Fix breaking change for the multilang dropdown

--- src/Panel/View.php
+++ src/Panel/View.php
@@ -142,7 +142,7 @@ class View
         $kirby = kirby();
 
         // multilang setup check
-        $multilang = $kirby->option('languages', false) !== false;
+        $multilang = $kirby->multilang();
 
         // get the authenticated user
         $user = $kirby->user();
