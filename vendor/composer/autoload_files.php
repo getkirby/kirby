31e5e3caa8483af83b0b6e060eab7078b3ca7756
Nico Hoffmann <me@nhoffmann.com>
2021-12-09 19:10:34 +0100

    Add PHP extensions to `composer.json`

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -8,6 +8,8 @@ $baseDir = dirname($vendorDir);
 return array(
     'e69f7f6ee287b969198c3c9d6777bd38' => $vendorDir . '/symfony/polyfill-intl-normalizer/bootstrap.php',
     '04c6c5c2f7095ccf6c481d3e53e1776f' => $vendorDir . '/mustangostang/spyc/Spyc.php',
+    '320cde22f66dd4f5d3fd621d3e88b98f' => $vendorDir . '/symfony/polyfill-ctype/bootstrap.php',
+    'def43f6c87e4f8dfd0c9e1b1bab14fe8' => $vendorDir . '/symfony/polyfill-iconv/bootstrap.php',
     'f598d06aa772fa33d905e87be6398fb1' => $vendorDir . '/symfony/polyfill-intl-idn/bootstrap.php',
     '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
     'f864ae44e8154e5ff6f4eec32f46d37f' => $baseDir . '/config/setup.php',
