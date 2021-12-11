a05e1c67b6f52fac1681ef515ff061f25c50b43e
Nico Hoffmann <me@nhoffmann.com>
2021-12-11 17:59:00 +0100

    `dayjs-merge` plugin

--- panel/src/libraries/dayjs.js
+++ panel/src/libraries/dayjs.js
@@ -1,10 +1,12 @@
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
+import merge from "./dayjs-merge.js";
 import round from "./dayjs-round.js";
 import units from "./dayjs-units.js";
 import utc from "dayjs/plugin/utc";
 
 dayjs.extend(customParseFormat);
+dayjs.extend(merge);
 dayjs.extend(round);
 dayjs.extend(units);
 dayjs.extend(utc);
