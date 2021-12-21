f50c63da407c1ee4f0bdf23285f6a5a0296c8683
Bastian Allgeier <mail@bastianallgeier.com>
2021-12-21 10:59:24 +0100

    Additional helper tests

---
+++ panel/src/helpers/object.merge.test.js
@@ -0,0 +1,28 @@
+import { merge } from "./object.js";
+
+describe("$helper.object.merge", () => {
+  it("should merge two objects", () => {
+    const target = {
+      nested: {
+        a: "a"
+      }
+    };
+
+    const source = {
+      nested: {
+        b: "b"
+      }
+    };
+
+    const expected = {
+      nested: {
+        a: "a",
+        b: "b"
+      }
+    };
+
+    const result = merge(target, source);
+
+    expect(result).toStrictEqual(expected);
+  });
+});
