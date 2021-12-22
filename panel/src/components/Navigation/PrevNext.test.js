0ce22d730408188894555485bb4686f32e940495
Nico Hoffmann <me@nhoffmann.com>
2021-12-22 10:42:08 +0100

    `k-prev-next` component tests

---
+++ panel/src/components/Navigation/PrevNext.test.js
@@ -0,0 +1,48 @@
+import { mount } from "@vue/test-utils";
+import PrevNext from "./PrevNext.vue";
+
+const setup = {
+  stubs: ["k-button-group"]
+};
+
+describe("PrevNext.vue", () => {
+  it("has buttons", async () => {
+    const wrapper = mount(PrevNext, {
+      propsData: {
+        prev: {
+          link: "https://getkirby.com"
+        }
+      },
+      ...setup
+    });
+
+    expect(wrapper.vm.buttons).toEqual([
+      { link: "https://getkirby.com", icon: "angle-left" },
+      { disabled: true, link: "#", icon: "angle-right" }
+    ]);
+    expect(wrapper.findComponent("k-button-group-stub").exists()).toBe(true);
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it("has `button()` method", async () => {
+    const wrapper = mount(PrevNext, setup);
+
+    const defaults = {
+      disabled: true,
+      link: "#"
+    };
+
+    const config = {
+      link: "https://getkirby.com"
+    };
+
+    expect(wrapper.vm.button()).toEqual(defaults);
+    expect(wrapper.vm.button()).toEqual(defaults);
+    expect(wrapper.vm.button(config)).toEqual(config);
+  });
+
+  it("has CSS selector", async () => {
+    const wrapper = mount(PrevNext, setup);
+    expect(wrapper.classes()).toContain("k-prev-next");
+  });
+});
--- panel/src/components/Navigation/PrevNext.vue
+++ panel/src/components/Navigation/PrevNext.vue
@@ -1,26 +1,51 @@
 <template>
-  <k-button-group
-    :buttons="[
-      { ...button(prev), icon: 'angle-left' },
-      { ...button(next), icon: 'angle-right' }
-    ]"
-    class="k-prev-next"
-  />
+  <k-button-group :buttons="buttons" class="k-prev-next" />
 </template>
 
 <script>
+/**
+ * Group of buttons with left- and right-pointing arrows
+ * used for navigation between previous and next items
+ * @example <k-prev-next :prev="{ link: 'a' }" :next="false" />
+ * @public
+ */
 export default {
   props: {
+    /**
+     * Button configuraiton for left-pointing arrow
+     */
     prev: {
       type: [Boolean, Object],
       default: false
     },
+    /**
+     * Button configuraiton for right-pointing arrow
+     */
     next: {
       type: [Boolean, Object],
       default: false
     }
   },
+  computed: {
+    /**
+     * Array of button configurations to be passed
+     * to ButtonGroup component
+     * @returns {array}
+     */
+    buttons() {
+      return [
+        { ...this.button(this.prev), icon: "angle-left" },
+        { ...this.button(this.next), icon: "angle-right" }
+      ];
+    }
+  },
   methods: {
+    /**
+     * Returns button configuration with
+     * defaults for disabled button
+     * @param {bool|Object} config
+     * @returns {Object}
+     */
     button(config) {
       if (!config) {
         return {
