44892d21f3f89a01340a4033735028a2f6bdf0f9
Bastian Allgeier <mail@bastianallgeier.com>
2022-01-14 12:38:06 +0100

    Unit tests for the Fiber search

---
+++ panel/src/fiber/search.test.js
@@ -0,0 +1,24 @@
+/**
+ * @vitest-environment node
+ */
+
+import search from "./search.js";
+
+describe.concurrent("$search()", () => {
+  it("should search", async () => {
+    const vue = {
+      $fiber: {
+        request(path, options) {
+          expect(path).toBe("search/pages");
+          expect(options.query.query).toBe("Blog");
+          expect(options.type).toBe("$search");
+          expect(options.headers).toEqual({ "x-test": "foo" });
+        }
+      }
+    };
+
+    await search.call(vue, "pages", "Blog", {
+      headers: { "x-test": "foo" }
+    });
+  });
+});
