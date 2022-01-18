a9bd661e2b14ff5e1c7e451e6a09617da1a0d08e
Bastian Allgeier <mail@bastianallgeier.com>
2022-01-18 11:51:33 +0100

    Update version
    Co-authored-by: Ahmet Bora <byybora@gmail.com>

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,7 +1,7 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '3.6.2',
-        'version' => '3.6.2.0',
+        'pretty_version' => '3.6.2-rc.1',
+        'version' => '3.6.2.0-RC1',
         'type' => 'kirby-cms',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -29,8 +29,8 @@
             'dev_requirement' => false,
         ),
         'getkirby/cms' => array(
-            'pretty_version' => '3.6.2',
-            'version' => '3.6.2.0',
+            'pretty_version' => '3.6.2-rc.1',
+            'version' => '3.6.2.0-RC1',
             'type' => 'kirby-cms',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
