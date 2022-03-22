c50a38778fe2d92ee2a7b438fe5faf58f322c5a4
Ahmet Bora <byybora@gmail.com>
2022-03-22 18:30:40 +0300

    Fix extending some top level props #4014

--- src/Cms/PageBlueprint.php
+++ src/Cms/PageBlueprint.php
@@ -25,7 +25,7 @@ class PageBlueprint extends Blueprint
 
         // normalize all available page options
         $this->props['options'] = $this->normalizeOptions(
-            $props['options'] ?? true,
+            $this->props['options'] ?? true,
             // defaults
             [
                 'changeSlug'     => null,
@@ -50,10 +50,10 @@ class PageBlueprint extends Blueprint
         );
 
         // normalize the ordering number
-        $this->props['num'] = $this->normalizeNum($props['num'] ?? 'default');
+        $this->props['num'] = $this->normalizeNum($this->props['num'] ?? 'default');
 
         // normalize the available status array
-        $this->props['status'] = $this->normalizeStatus($props['status'] ?? null);
+        $this->props['status'] = $this->normalizeStatus($this->props['status'] ?? null);
     }
 
     /**
