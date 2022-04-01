d39f3521e18135b99fe329b6d3669a8be4463963
Nico Hoffmann <me@nhoffmann.com>
2022-04-01 14:48:10 -0300

    New `k-models-section` for pages + files sections

--- panel/src/components/Sections/FilesSection.vue
+++ panel/src/components/Sections/FilesSection.vue
@@ -1,119 +1,30 @@
-<template>
-  <section
-    v-if="isLoading === false"
-    :data-processing="isProcessing"
-    class="k-files-section"
-  >
-    <header class="k-section-header">
-      <k-headline>
-        {{ headline }}
-        <abbr v-if="options.min" :title="$t('section.required')">*</abbr>
-      </k-headline>
-      <k-button-group
-        v-if="add"
-        :buttons="[{ text: $t('add'), icon: 'upload', click: upload }]"
-      />
-    </header>
-
-    <template v-if="error">
-      <k-box theme="negative">
-        <k-text size="small">
-          <strong>{{ $t("error.section.notLoaded", { name: name }) }}:</strong>
-          {{ error }}
-        </k-text>
-      </k-box>
-    </template>
-
-    <template v-else>
-      <k-dropzone :disabled="add === false" @drop="drop">
-        <k-collection
-          v-if="data.length"
-          :help="help"
-          :items="data"
-          :layout="options.layout"
-          :pagination="pagination"
-          :sortable="!isProcessing && options.sortable"
-          :size="options.size"
-          :data-invalid="isInvalid"
-          @sort="sort"
-          @paginate="paginate"
-          @action="action"
-        />
-        <template v-else>
-          <k-empty
-            :layout="options.layout"
-            :data-invalid="isInvalid"
-            icon="image"
-            v-on="add ? { click: upload } : {}"
-          >
-            {{ options.empty || $t("files.empty") }}
-          </k-empty>
-          <footer class="k-collection-footer">
-            <!-- eslint-disable vue/no-v-html -->
-            <k-text
-              v-if="help"
-              theme="help"
-              class="k-collection-help"
-              v-html="help"
-            />
-            <!-- eslint-enable vue/no-v-html -->
-          </footer>
-        </template>
-      </k-dropzone>
-
-      <k-upload ref="upload" @success="uploaded" @error="reload" />
-    </template>
-  </section>
-</template>
-
 <script>
-import CollectionSectionMixin from "@/mixins/section/collection.js";
+import ModelsSection from "@/components/Sections/ModelsSection.vue";
 
 export default {
-  mixins: [CollectionSectionMixin],
+  extends: ModelsSection,
   computed: {
-    add() {
-      if (this.$permissions.files.create && this.options.upload !== false) {
-        return this.options.upload;
-      } else {
-        return false;
-      }
-    }
-  },
-  created() {
-    this.load();
-    this.$events.$on("model.update", this.reload);
-    this.$events.$on("file.sort", this.reload);
-  },
-  destroyed() {
-    this.$events.$off("model.update", this.reload);
-    this.$events.$off("file.sort", this.reload);
-  },
-  methods: {
-    action(action, file) {
-      if (action === "replace") {
-        this.replace(file);
-      }
+    canAdd() {
+      return this.$permissions.files.create && this.options.upload !== false;
     },
-    drop(files) {
-      if (this.add === false) {
-        return false;
-      }
-
-      this.$refs.upload.drop(files, {
-        ...this.add,
-        url: this.$urls.api + "/" + this.add.api
-      });
+    canDrop() {
+      return this.canAdd !== false;
     },
-    items(data) {
-      return data.map((file) => {
+    emptyProps() {
+      return {
+        icon: "image",
+        text: this.options.empty || this.$t("files.empty")
+      };
+    },
+    items() {
+      return this.data.map((file) => {
         file.sortable = this.options.sortable;
         file.column = this.column;
         file.options = this.$dropdown(file.link, {
           query: {
             view: "list",
             update: this.options.sortable,
-            delete: data.length > this.options.min
+            delete: this.data.length > this.options.min
           }
         });
 
@@ -126,27 +37,51 @@ export default {
         return file;
       });
     },
-    replace(file) {
-      this.$refs.upload.open({
-        url: this.$urls.api + "/" + file.link,
-        accept: "." + file.extension + "," + file.mime,
-        multiple: false
-      });
+    type() {
+      return "files";
     },
-    async sort(items) {
+    uploadProps() {
+      return {
+        ...this.options.upload,
+        url: this.$urls.api + "/" + this.options.upload.api
+      };
+    }
+  },
+  created() {
+    this.load();
+    this.$events.$on("model.update", this.reload);
+    this.$events.$on("file.sort", this.reload);
+  },
+  destroyed() {
+    this.$events.$off("model.update", this.reload);
+    this.$events.$off("file.sort", this.reload);
+  },
+  methods: {
+    onAction(action, file) {
+      if (action === "replace") {
+        this.replace(file);
+      }
+    },
+    onAdd() {
+      if (this.canAdd) {
+        this.$refs.upload.open(this.uploadProps);
+      }
+    },
+    onDrop(files) {
+      if (this.canAdd) {
+        this.$refs.upload.drop(files, this.uploadProps);
+      }
+    },
+    async onSort(items) {
       if (this.options.sortable === false) {
         return false;
       }
 
       this.isProcessing = true;
 
-      items = items.map((item) => {
-        return item.id;
-      });
-
       try {
         await this.$api.patch(this.options.apiUrl + "/files/sort", {
-          files: items,
+          files: items.map((item) => item.id),
           index: this.pagination.offset
         });
         this.$store.dispatch("notification/success", ":)");
@@ -158,30 +93,18 @@ export default {
         this.isProcessing = false;
       }
     },
-    update() {
-      this.$events.$emit("model.update");
-    },
-    upload() {
-      if (this.add === false) {
-        return false;
-      }
-
-      this.$refs.upload.open({
-        ...this.add,
-        url: this.$urls.api + "/" + this.add.api
-      });
-    },
-    uploaded() {
+    onUpload() {
       this.$events.$emit("file.create");
       this.$events.$emit("model.update");
       this.$store.dispatch("notification/success", ":)");
+    },
+    replace(file) {
+      this.$refs.upload.open({
+        url: this.$urls.api + "/" + file.link,
+        accept: "." + file.extension + "," + file.mime,
+        multiple: false
+      });
     }
   }
 };
 </script>
-
-<style>
-.k-files-section[data-processing="true"] {
-  pointer-events: none;
-}
-</style>
---
+++ panel/src/components/Sections/ModelsSection.vue
@@ -0,0 +1,189 @@
+<template>
+  <section
+    v-if="isLoading === false"
+    :data-processing="isProcessing"
+    :class="`k-models-section k-${type}-section`"
+  >
+    <header class="k-section-header">
+      <k-headline :link="options.link">
+        {{ options.headline || " " }}
+        <abbr v-if="options.min" :title="$t('section.required')">*</abbr>
+      </k-headline>
+
+      <k-button-group
+        v-if="canAdd"
+        :buttons="[{ text: $t('add'), icon: 'add', click: onAdd }]"
+      />
+    </header>
+
+    <k-box v-if="error" theme="negative">
+      <k-text size="small">
+        <strong> {{ $t("error.section.notLoaded", { name: name }) }}: </strong>
+        {{ error }}
+      </k-text>
+    </k-box>
+
+    <template v-else>
+      <k-dropzone :disabled="!canDrop" @drop="onDrop">
+        <k-collection
+          v-bind="collection"
+          :data-invalid="isInvalid"
+          @action="onAction"
+          @change="onChange"
+          @sort="onSort"
+          @empty="add ? onAdd : null"
+          @paginate="onPaginate"
+        />
+      </k-dropzone>
+
+      <k-upload ref="upload" @success="onUpload" @error="reload" />
+    </template>
+  </section>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    blueprint: String,
+    column: String,
+    parent: String,
+    name: String,
+    timestamp: Number
+  },
+  data() {
+    return {
+      data: [],
+      error: null,
+      isLoading: false,
+      isProcessing: false,
+      options: {
+        empty: null,
+        headline: null,
+        help: null,
+        layout: "list",
+        link: null,
+        max: null,
+        min: null,
+        size: null,
+        sortable: null
+      },
+      pagination: {
+        page: null
+      }
+    };
+  },
+  computed: {
+    canAdd() {
+      return true;
+    },
+    canDrop() {
+      return false;
+    },
+    collection() {
+      return {
+        empty: this.emptyProps,
+        layout: this.options.layout,
+        help: this.options.help,
+        items: this.items,
+        pagination: this.pagination,
+        sortable: !this.isProcessing && this.options.sortable,
+        size: this.options.size
+      };
+    },
+    emptyProps() {
+      return {
+        icon: "page",
+        text: this.options.empty || this.$t("pages.empty")
+      };
+    },
+    items() {
+      return this.data;
+    },
+    isInvalid() {
+      if (this.options.min && this.data.length < this.options.min) {
+        return true;
+      }
+
+      if (this.options.max && this.data.length > this.options.max) {
+        return true;
+      }
+
+      return false;
+    },
+    paginationId() {
+      return "kirby$pagination$" + this.parent + "/" + this.name;
+    },
+    type() {
+      return "models";
+    }
+  },
+  watch: {
+    // Reload the section when
+    // the view has changed in the backend
+    timestamp() {
+      this.reload();
+    }
+  },
+  created() {
+    this.load();
+  },
+  methods: {
+    add() {},
+
+    async load(reload) {
+      if (!reload) {
+        this.isLoading = true;
+      }
+
+      this.isProcessing = true;
+
+      if (this.pagination.page === null) {
+        this.pagination.page = localStorage.getItem(this.paginationId) || 1;
+      }
+
+      try {
+        const response = await this.$api.get(
+          this.parent + "/sections/" + this.name,
+          { page: this.pagination.page }
+        );
+
+        this.options = response.options;
+        this.pagination = response.pagination;
+        this.data = response.data;
+      } catch (error) {
+        this.error = error.message;
+      } finally {
+        this.isProcessing = false;
+        this.isLoading = false;
+      }
+    },
+
+    onAction(action, model) {},
+    onAdd() {},
+    onChange() {},
+    onDrop(files) {},
+    onSort() {},
+    onPaginate(pagination) {
+      localStorage.setItem(this.paginationId, pagination.page);
+      this.pagination = pagination;
+      this.reload();
+    },
+    onUpload() {},
+
+    async reload() {
+      await this.load(true);
+    },
+    update() {
+      this.reload();
+      this.$events.$emit("model.update");
+    }
+  }
+};
+</script>
+
+<style>
+.k-models-section[data-processing="true"] {
+  pointer-events: none;
+}
+</style>
