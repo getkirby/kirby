1ebe54c68b4af2a733c2b93e67516bb54bf4d002
Bastian Allgeier <mail@bastianallgeier.com>
2022-06-01 14:53:04 +0200

    Fix rounded corners in date inputs

--- panel/src/components/Forms/Field/DateField.vue
+++ panel/src/components/Forms/Field/DateField.vue
@@ -286,7 +286,7 @@ export default {
 .k-date-field-body > .k-input[data-theme="field"] {
   border: 0;
   box-shadow: none;
-  border-radius: var(--rounded-sm);
+  border-radius: var(--rounded);
 }
 .k-date-field-body > .k-input[data-invalid="true"],
 .k-date-field-body > .k-input[data-invalid="true"]:focus-within {
