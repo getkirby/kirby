a08885bd195e91bca1e125a87a0cf9c35473a148
Nico Hoffmann <me@nhoffmann.com>
2022-07-02 07:56:53 +0200

    Add typescript

--- panel/.eslintrc.js
+++ panel/.eslintrc.js
@@ -1,9 +1,34 @@
 module.exports = {
+	root: true,
+	env: {
+		es2022: true,
+		browser: true,
+		node: true
+	},
 	extends: [
 		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
 		"plugin:cypress/recommended",
 		"plugin:vue/recommended",
 		"prettier"
+	],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint"],
+	parserOptions: {
+		ecmaVersion: 13,
+		sourceType: "module"
+	},
+	overrides: [
+		{
+			files: ["*.vue"],
+			parser: "vue-eslint-parser",
+			parserOptions: {
+				parser: "@typescript-eslint/parser"
+			},
+			rules: {
+				"no-undef": "off"
+			}
+		}
 	],
 	rules: {
 		"vue/attributes-order": "error",
