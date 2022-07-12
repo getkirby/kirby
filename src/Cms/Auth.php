b39a9f93058dfbcdda1f155d4379719177bf208b
Nico Hoffmann <me@nhoffmann.com>
2022-07-12 10:24:14 +0200

    Fix phpmd

--- src/Cms/Auth.php
+++ src/Cms/Auth.php
@@ -320,7 +320,7 @@ class Auth
 				'id'    => 'nobody',
 				'role'  => 'nobody',
 			]),
-			default  => $this->kirby->users()->find($who) ?? throw new NotFoundException('The user "' . $who . '" cannot be found')
+			default  => ($this->kirby->users()->find($who) ?? throw new NotFoundException('The user "' . $who . '" cannot be found'))
 		};
 	}
 
