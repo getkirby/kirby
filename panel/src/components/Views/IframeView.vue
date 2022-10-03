2d5ffce71527d9615bad96526981e4909d495b1d
Nico Hoffmann <me@nhoffmann.com>
2022-10-03 20:32:32 +0200

    New `k-iframe-view` component

---
+++ panel/src/components/Views/IframeView.vue
@@ -0,0 +1,28 @@
+<template>
+	<k-inside :class="`k-iframe-view k-view-name-${$view.id}`">
+		<k-view v-if="url">
+			<iframe :src="url" scrolling="no" frameborder="0" loading="lazy" />
+		</k-view>
+		<k-error-view v-else :error="error" />
+	</k-inside>
+</template>
+
+<script>
+export default {
+	props: {
+		error: String,
+		url: String
+	}
+};
+</script>
+
+<style>
+.k-iframe-view .k-panel-view {
+	padding-bottom: 0;
+}
+.k-iframe-view iframe {
+	display: block;
+	width: 100%;
+	height: calc(100vh - 2.5rem);
+}
+</style>
