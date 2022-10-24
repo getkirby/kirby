54d2e2f55952ec89480de28631aaf6d27d28c437
Nico Hoffmann <me@nhoffmann.com>
2022-10-24 21:44:45 +0700

    Improve `ContentTranslation::content()` syntax

--- src/Cms/ContentTranslation.php
+++ src/Cms/ContentTranslation.php
@@ -84,26 +84,18 @@ class ContentTranslation
 	 */
 	public function content(): array
 	{
-		$parent = $this->parent();
-
-		if ($this->content === null) {
-			$this->content = $parent->readContent($this->code());
-		}
-
-		$content = $this->content;
+		$parent 		 = $this->parent();
+		$this->content ??= $parent->readContent($this->code());
+		$content 		 = $this->content;
 
 		// merge with the default content
 		if (
 			$this->isDefault() === false &&
 			$defaultLanguage = $parent->kirby()->defaultLanguage()
 		) {
-			$default = [];
-
-			if ($defaultTranslation = $parent->translation($defaultLanguage->code())) {
-				$default = $defaultTranslation->content();
+			if ($default = $parent->translation($defaultLanguage->code())?->content()) {
+				$content = array_merge($default, $content);
 			}
-
-			$content = array_merge($default, $content);
 		}
 
 		return $content;
