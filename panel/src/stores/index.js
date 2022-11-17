fbfd8102d1447bfc88df3f908dbb40913170cf22
Nico Hoffmann <me@nhoffmann.com>
2022-11-17 16:33:14 +0100

    Refactoring stores more

--- panel/src/stores/index.js
+++ panel/src/stores/index.js
@@ -1,4 +1,67 @@
-import store from "./store.js";
+import { reactive } from "vue";
+
+// modules
+import content from "./content.js";
+import drawers from "./drawers.js";
+import notification from "./notification.js";
+
+const store = reactive({
+	/**
+	 * Modules
+	 */
+	content,
+	drawers,
+	notification,
+
+	/**
+	 * Root state
+	 */
+	state: {
+		dialog: null,
+		drag: null,
+		fatal: false,
+		isLoading: false
+	},
+
+	/**
+	 * Root actions
+	 */
+	dialog(dialog) {
+		this.state.dialog = dialog;
+	},
+	drag(drag) {
+		this.state.drag = drag;
+	},
+	fatal(options) {
+		// close the fatal window if false
+		// is passed as options
+		if (options === false) {
+			this.state.fatal = false;
+			return;
+		}
+
+		console.error("The JSON response could not be parsed");
+
+		// show the full response in the console
+		// if debug mode is enabled
+		if (window.panel.$config.debug) {
+			console.info(options.html);
+		}
+
+		// only show the fatal dialog if the silent
+		// option is not set to true
+		if (!options.silent) {
+			this.state.fatal = options.html;
+		}
+	},
+	isLoading(loading) {
+		this.state.isLoading = loading === true;
+	},
+	navigate() {
+		this.dialog(null);
+		this.drawers.close();
+	}
+});
 
 export default {
 	install(app) {
@@ -7,24 +70,30 @@ export default {
 		store.state.drawers = new Proxy(store.drawers.state, {});
 		store.state.notification = new Proxy(store.notification.state, {});
 
-		// polyfill: getters and dispatch actions
-		const base = (name) => {
-			name = name.split("/");
-			let base = store;
+		// polyfill:dispatch actions
+		store.dispatch = function (event, args) {
+			const name = event.split("/");
+			const base = store[name[0]];
 
-			for (const part of name) {
-				base = base[part];
+			if (!name[1]) {
+				return store[name[0]](args);
 			}
 
-			return base;
+			return base[name[1]].apply(base, args);
 		};
 
-		store.dispatch = (event, args) => base(event)(args);
+		// polyfill: getters
 		store.getters = new Proxy(
 			{},
 			{
 				get(target, property) {
-					return base(property);
+					const name = property.split("/");
+
+					if (!name[1]) {
+						return store[name[0]];
+					}
+
+					return store[name[0]][name[1]].bind(store[name[0]]);
 				}
 			}
 		);
