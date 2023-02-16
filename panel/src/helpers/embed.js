72485e8d8dd3f237756c67df3aad6d1160981e50
Ahmet Bora <byybora@gmail.com>
2023-02-16 19:50:46 +0300

    Fix doNotTrack argument in youtube embed helper #5058

--- panel/src/helpers/embed.js
+++ panel/src/helpers/embed.js
@@ -55,7 +55,11 @@ export function youtube(url, doNotTrack = false) {
 		default:
 			// short URLs
 			if (uri.host.includes("youtu.be") && isYoutubeId(first)) {
-				src = "https://www.youtube.com/embed/" + first;
+				if (doNotTrack === true) {
+					src = "https://www.youtube-nocookie.com/embed/" + first;
+				} else {
+					src = "https://www.youtube.com/embed/" + first;
+				}
 
 				if (query.has("t")) {
 					query.set("start", query.get("t"));
