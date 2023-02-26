45cdd9fc404090c0159f5f389a7071d8ad269784
Nico Hoffmann <me@nhoffmann.com>
2023-02-26 19:14:39 +0100

    Clean up `Toolkit` package

--- src/Toolkit/Facade.php
+++ src/Toolkit/Facade.php
@@ -17,17 +17,11 @@ abstract class Facade
 	/**
 	 * Returns the instance that should be
 	 * available statically
-	 *
-	 * @return mixed
 	 */
 	abstract public static function instance();
 
 	/**
 	 * Proxy for all public instance calls
-	 *
-	 * @param string $method
-	 * @param array $args
-	 * @return mixed
 	 */
 	public static function __callStatic(string $method, array $args = null)
 	{
