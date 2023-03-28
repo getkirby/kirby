12eba3e3cba17e4b79930a525bf323c353ec90cd
Nico Hoffmann <me@nhoffmann.com>
2023-03-28 14:10:40 +0200

    `<k-writer-input>` with validations

--- panel/src/components/Forms/Field/WriterField.vue
+++ panel/src/components/Forms/Field/WriterField.vue
@@ -7,41 +7,25 @@
 	>
 		<k-input
 			v-bind="$props"
+			ref="input"
 			:after="after"
 			:before="before"
 			:icon="icon"
 			theme="field"
-		>
-			<k-writer
-				ref="input"
-				v-bind="$props"
-				:value="value"
-				class="k-writer-field-input"
-				@input="$emit('input', $event)"
-			/>
-		</k-input>
+			@input="$emit('input', $event)"
+		/>
 	</k-field>
 </template>
 
 <script>
 import { props as Field } from "../Field.vue";
 import { props as Input } from "../Input.vue";
-import { props as Writer } from "@/components/Forms/Writer/Writer.vue";
+import { props as Writer } from "@/components/Forms/Input/WriterInput.vue";
 import counter from "@/mixins/forms/counter.js";
 
 export default {
 	mixins: [Field, Input, Writer, counter],
 	inheritAttrs: false,
-	props: {
-		maxlength: Number,
-		minlength: Number
-	},
-	computed: {
-		counterValue() {
-			const plain = this.$helper.string.stripHTML(this.value);
-			return this.$helper.string.unescapeHTML(plain);
-		}
-	},
 	methods: {
 		focus() {
 			this.$refs.input.focus();
@@ -49,12 +33,3 @@ export default {
 	}
 };
 </script>
-
-<style>
-.k-writer-field-input .ProseMirror,
-/* ::before is used for the placeholder */
-.k-writer-field-input::before {
-	line-height: 1.5em;
-	padding: 0.375rem 0.5rem;
-}
-</style>
