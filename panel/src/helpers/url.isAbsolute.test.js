4c25495cd4625c588d899a2f14a149f6628f5fe6
Bastian Allgeier <mail@bastianallgeier.com>
2023-03-31 09:45:58 +0200

    Test for http in absolute URL check

--- panel/src/helpers/url.isAbsolute.test.js
+++ panel/src/helpers/url.isAbsolute.test.js
@@ -4,6 +4,7 @@ import url from "./url.js";
 describe("$helper.url.isAbsolute", () => {
 	it("should work", () => {
 		expect(url.isAbsolute("https://getkirby.com")).toStrictEqual(true);
+		expect(url.isAbsolute("http://getkirby.com")).toStrictEqual(true);
 		expect(url.isAbsolute("/foo")).toStrictEqual(false);
 	});
 });
