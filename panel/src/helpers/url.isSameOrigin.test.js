a801228965cdba5a61cc6507c2136f59877bbe21
Bastian Allgeier <mail@bastianallgeier.com>
2023-03-31 09:49:43 +0200

    Better check for origin

--- panel/src/helpers/url.isSameOrigin.test.js
+++ panel/src/helpers/url.isSameOrigin.test.js
@@ -2,8 +2,10 @@ import { describe, expect, it } from "vitest";
 import url from "./url.js";
 
 describe("$helper.url.isSameOrigin", () => {
+	window.location = new URL("https://test.com");
+
 	it("should detect same origin", () => {
-		const input = "http://localhost:3000";
+		const input = "https://test.com";
 		const result = url.isSameOrigin(input);
 
 		expect(result).toStrictEqual(true);
