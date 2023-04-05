6de7042782b59644688b595a90e7dc99ec4e2d89
Bastian Allgeier <mail@bastianallgeier.com>
2023-04-05 21:46:19 +0200

    Add language module

---
+++ panel/src/panel/language.js
@@ -0,0 +1,22 @@
+import Module from "./module.js";
+
+export const defaults = () => {
+	return {
+		code: null,
+		default: false,
+		direction: "ltr",
+		name: null,
+		rules: []
+	};
+};
+
+export default () => {
+	const parent = Module("$language", defaults());
+
+	return {
+		...parent,
+		get isDefault() {
+			return this.default;
+		}
+	};
+};
