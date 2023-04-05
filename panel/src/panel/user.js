40c7bae705ef2d445146eff783df17191caa056b
Bastian Allgeier <mail@bastianallgeier.com>
2023-04-05 21:49:03 +0200

    Add user module

---
+++ panel/src/panel/user.js
@@ -0,0 +1,15 @@
+import Module from "./module.js";
+
+export const defaults = () => {
+	return {
+		email: null,
+		id: null,
+		language: null,
+		role: null,
+		username: null
+	};
+};
+
+export default () => {
+	return Module("$user", defaults());
+};
