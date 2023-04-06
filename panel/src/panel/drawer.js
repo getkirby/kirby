2b29860023717f71176eaf0a7eff85277bf728f9
Bastian Allgeier <mail@bastianallgeier.com>
2023-04-06 17:08:10 +0200

    Simplify drawer

--- panel/src/panel/drawer.js
+++ panel/src/panel/drawer.js
@@ -1,9 +1,5 @@
 import Island, { defaults } from "./island.js";
 
 export default (panel) => {
-	const parent = Island(panel, "drawer", defaults());
-
-	return {
-		...parent
-	};
+	return Island(panel, "drawer", defaults());
 };
