ae9459ab8c71d69c25bc83b2d5eb81e67723280e
Bastian Allgeier <mail@bastianallgeier.com>
2023-04-06 16:19:09 +0200

    Simplify dialog

--- panel/src/panel/dialog.js
+++ panel/src/panel/dialog.js
@@ -1,9 +1,5 @@
 import Island, { defaults } from "./island.js";
 
 export default (panel) => {
-	const parent = Island(panel, "dialog", defaults());
-
-	return {
-		...parent
-	};
+	return Island(panel, "dialog", defaults());
 };
