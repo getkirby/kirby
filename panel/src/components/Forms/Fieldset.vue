a2e57e7b24758cd6ca4f7ce92a3d905c4538db64
Nico Hoffmann <me@nhoffmann.com>
2023-04-11 14:07:45 +0200

    Keep local data state for values in Fieldset

--- panel/src/components/Forms/Fieldset.vue
+++ panel/src/components/Forms/Fieldset.vue
@@ -21,7 +21,7 @@
 							:form-data="value"
 							:name="fieldName"
 							:novalidate="novalidate"
-							:value="value[fieldName]"
+							:value="values[fieldName]"
 							@input="onInput($event, field, fieldName)"
 							@focus="$emit('focus', $event, field, fieldName)"
 							@invalid="
@@ -73,8 +73,14 @@ export default {
 	emits: ["focus", "input", "invalid", "submit"],
 	data() {
 		return {
+			values: this.value,
 			errors: {}
 		};
+	},
+	watch: {
+		value(value) {
+			this.values = value;
+		}
 	},
 	methods: {
 		/**
@@ -117,8 +123,8 @@ export default {
 			this.$emit("invalid", this.errors);
 		},
 		onInput(value, field, name) {
-			const values = { ...this.value, [name]: value };
-			this.$emit("input", values, field, name);
+			this.values = { ...this.value, [name]: value };
+			this.$emit("input", this.values, field, name);
 		},
 		hasErrors() {
 			return this.$helper.object.length(this.errors) > 0;
