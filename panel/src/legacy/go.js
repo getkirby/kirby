97012566a438786d2a67f687532b43280f062b8d
Bastian Allgeier <mail@bastianallgeier.com>
2023-04-12 12:44:14 +0200

    Don’t deprecate go

--- panel/src/legacy/go.js
+++ panel/src/legacy/go.js
@@ -1,7 +1,6 @@
 /**
  * Opens a view at the given URL
  *
- * @deprecated since 4.0 Use panel.view.open instead
  * @param  {String} url
  * @param  {Object} options
  * @returns {Object} Returns the new view state
