0df9d9960579df02285050e47145311fd45e1bf3
Nico Hoffmann <me@nhoffmann.com>
2023-04-15 15:49:07 +0200

    Fix #151

--- panel/src/api/patch.js
+++ panel/src/api/patch.js
@@ -1,5 +1,5 @@
 export default (api) => {
-	async (path, data, options, silent = false) => {
+	return async (path, data, options, silent = false) => {
 		return api.post(path, data, options, "PATCH", silent);
 	};
 };
