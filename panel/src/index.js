27efdcffa02222f7236d7a1d9a0bb6116c83537f
Bastian Allgeier <mail@bastianallgeier.com>
2023-04-20 20:19:08 +0200

    Refactor index.js

--- panel/src/index.js
+++ panel/src/index.js
@@ -15,50 +15,23 @@ import Vuelidate from "vuelidate";
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
-const app = new Vue({
+/**
+ * Create the Panel instance
+ */
+window.panel = Panel.create(Vue, window.panel.plugins);
+
+/**
+ * This is the single source of truth
+ * for all Vue components.
+ */
+Vue.prototype.$panel = window.panel;
+
+/**
+ * Create the Vue application
+ */
+window.panel.vue = new Vue({
 	store,
 	created() {
-		/**
-		 * Shortcut to the panel for all components
-		 */
-		Vue.prototype.$panel = window.panel;
-
-		/**
-		 * Make the new panel temporarily available in the console
-		 * to test features manually
-		 */
-		window.p = Panel.create(Vue, window.panel.plugins);
-
-		/**
-		 * This is temporary panel setup
-		 * code until the entire panel.js class is there
-		 */
-		this.$panel.api = window.p.api;
-		this.$panel.config = window.p.config;
-		this.$panel.debug = window.p.debug;
-		this.$panel.events = window.p.events;
-		this.$panel.isLoading = window.p.isLoading;
-		this.$panel.language = window.p.language;
-		this.$panel.languages = window.p.languages;
-		this.$panel.license = window.p.license;
-		this.$panel.menu = window.p.menu;
-		this.$panel.multilang = window.p.multilang;
-		this.$panel.notification = window.p.notification;
-		this.$panel.permissions = window.p.permissions;
-		this.$panel.plugins = window.p.plugins;
-		this.$panel.searches = window.p.searches;
-		this.$panel.system = window.p.system;
-		this.$panel.t = window.p.t;
-		this.$panel.translation = window.p.translation;
-		this.$panel.urls = window.p.urls;
-		this.$panel.user = window.p.user;
-
-		/**
-		 * Make notification reactive. This will be done in
-		 * the Panel object later
-		 */
-		reactive(this.$panel);
-
 		/**
 		 * Delegate all required window events to the
 		 * event emitter
@@ -75,12 +48,17 @@ const app = new Vue({
 	render: () => h(App)
 });
 
-// Global styles
+/**
+ * Global styles need to be loaded before
+ * components
+ */
 import "./styles/variables.css";
 import "./styles/reset.css";
 import "./styles/animations.css";
 
-// Load functionalities
+/**
+ * Additional functionalities and app configuration
+ */
 Vue.use(ErrorHandling, window.panel);
 Vue.use(Legacy);
 Vue.use(Helpers);
@@ -90,8 +68,13 @@ Vue.use(Fiber);
 Vue.use(Vuelidate);
 Vue.use(Components);
 
-// Load CSS utilities after components
-// to increase specificity
+/**
+ * Load CSS utilities after components
+ * to increase specificity
+ */
 import "./styles/utilities.css";
 
-app.$mount("#app");
+/**
+ * Mount the Vue application
+ */
+window.panel.vue.$mount("#app");
