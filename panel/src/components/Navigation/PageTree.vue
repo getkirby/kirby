dead39fb106024c917625e0a3156d0a633d3fe0c
Bastian Allgeier <mail@bastianallgeier.com>
2023-05-12 12:01:48 +0200

    New Tree and PageTree components

---
+++ panel/src/components/Navigation/PageTree.vue
@@ -0,0 +1,77 @@
+<template>
+	<k-tree
+		:current="current"
+		:items="pages"
+		:level="level"
+		class="k-page-tree"
+		element="k-page-tree"
+		@select="select"
+		@toggle="toggle"
+	/>
+</template>
+
+<script>
+import Tree from "./Tree.vue";
+
+export default {
+	inheritAttrs: false,
+	mixins: [Tree],
+	props: {
+		current: {
+			default: "/site",
+			type: String
+		},
+		items: {
+			type: String
+		}
+	},
+	async created() {
+		if (this.items) {
+			this.pages = await this.load();
+		} else {
+			this.pages = [
+				{
+					icon: "home",
+					id: "/site",
+					label: this.$t("view.site"),
+					hasChildren: true,
+					children: "/site",
+					open: true
+				}
+			];
+		}
+	},
+	data() {
+		return {
+			pages: []
+		};
+	},
+	methods: {
+		async load() {
+			const { data } = await this.$api.get(this.items + "/children", {
+				select: "hasChildren,id,title",
+				status: "all"
+			});
+
+			const pages = {};
+
+			data.forEach((page) => {
+				const id = "/pages/" + this.$api.pages.id(page.id);
+
+				pages[id] = {
+					id: id,
+					label: page.title,
+					hasChildren: page.hasChildren,
+					children: id,
+					open: this.current?.includes(id) && this.current !== id
+				};
+			});
+
+			return pages;
+		},
+		async toggle(page) {
+			page.open = !page.open;
+		}
+	}
+};
+</script>
