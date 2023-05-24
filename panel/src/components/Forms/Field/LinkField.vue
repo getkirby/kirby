b6c7d0c79717fde0cd52c36f17b043468f8dc426
Nico Hoffmann <me@nhoffmann.com>
2023-05-24 13:19:26 -0700

    Link field: preview image for selected file

--- panel/src/components/Forms/Field/LinkField.vue
+++ panel/src/components/Forms/Field/LinkField.vue
@@ -35,6 +35,12 @@
 						class="k-link-input-model-preview"
 						@remove="clear"
 					>
+						<k-item-image
+							v-if="model.image"
+							:image="{ ...model.image, cover: true, back: 'gray-200' }"
+							class="k-link-input-model-preview-image"
+						/>
+
 						{{ model.label }}
 					</k-tag>
 					<k-button v-else class="k-link-input-model-placeholder">
@@ -255,11 +261,12 @@ export default {
 		async previewForFile(id) {
 			try {
 				const file = await this.$api.files.get(null, id, {
-					select: "filename"
+					select: "filename, panelImage"
 				});
 
 				return {
-					label: file.filename
+					label: file.filename,
+					image: file.panelImage
 				};
 			} catch (e) {
 				return null;
@@ -358,8 +365,19 @@ export default {
 	white-space: nowrap;
 }
 .k-link-input-model-preview .k-tag-text {
+	display: flex;
+	gap: 0.5rem;
+	align-items: center;
 	overflow: hidden;
 	text-overflow: ellipsis;
+}
+.k-link-input-model-preview-image {
+	height: calc(var(--height-sm) - 0.5rem);
+	aspect-ratio: 1/1;
+	border-radius: var(--rounded);
+}
+.k-link-input-model-preview .k-tag-text:has(.k-link-input-model-preview-image) {
+	padding-inline-start: 0.25rem;
 }
 .k-link-input-model-placeholder.k-button {
 	display: flex;
