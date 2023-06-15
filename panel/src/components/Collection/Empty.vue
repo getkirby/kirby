d9d8d0e8bd73d25cc2529290d716069bee6474eb
Bastian Allgeier <mail@bastianallgeier.com>
2023-06-15 17:56:29 +0200

    Fix click on empty box

--- panel/src/components/Collection/Empty.vue
+++ panel/src/components/Collection/Empty.vue
@@ -1,5 +1,5 @@
 <template>
-	<k-box v-bind="attrs" class="k-empty" v-on="$listeners">
+	<k-box v-bind="attrs" class="k-empty" @click.native="$emit('click', $event)">
 		<slot>
 			{{ text }}
 		</slot>
