3332328c56362b6521a58bcc54d0f92d2c1809e5
Bastian Allgeier <mail@bastianallgeier.com>
2023-06-19 16:55:07 +0200

    Fix dialog body padding

--- panel/src/components/Dialogs/Elements/Body.vue
+++ panel/src/components/Dialogs/Elements/Body.vue
@@ -6,6 +6,9 @@
 
 <style>
 .k-dialog-body {
-	padding: var(--dialog-padding) var(--dialog-padding) 0;
+	padding: var(--dialog-padding) var(--dialog-padding);
+}
+.k-dialog:has(.k-dialog-footer) .k-dialog-body {
+	padding-bottom: 0;
 }
 </style>
