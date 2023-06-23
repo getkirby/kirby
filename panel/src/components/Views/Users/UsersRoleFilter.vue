b52009afd7148c04a2375d181ebdd2dd1c658c10
Bastian Allgeier <mail@bastianallgeier.com>
2023-06-23 11:30:29 +0200

    Simplify User Role Filter

--- panel/src/components/Views/Users/UsersRoleFilter.vue
+++ panel/src/components/Views/Users/UsersRoleFilter.vue
@@ -1,36 +1,34 @@
 <template>
-	<k-button-group>
-		<k-dropdown>
-			<k-button
-				:dropdown="true"
-				:responsive="true"
-				:text="role ? role.title : $t('role.all')"
-				icon="funnel"
-				size="sm"
-				variant="filled"
-				@click="$refs.roles.toggle()"
-			/>
-			<k-dropdown-content
-				ref="roles"
-				:options="[
-					{
-						current: !role,
-						icon: 'bolt',
-						link: '/users',
-						text: $t('role.all')
-					},
-					'-',
-					...roles.map((roleFilter) => ({
-						current: role && role.id === roleFilter.id,
-						icon: 'bolt',
-						link: '/users?role=' + roleFilter.id,
-						text: roleFilter.title
-					}))
-				]"
-				align="right"
-			/>
-		</k-dropdown>
-	</k-button-group>
+	<k-dropdown>
+		<k-button
+			:dropdown="true"
+			:responsive="true"
+			:text="role ? role.title : $t('role.all')"
+			icon="funnel"
+			size="sm"
+			variant="filled"
+			@click="$refs.roles.toggle()"
+		/>
+		<k-dropdown-content
+			ref="roles"
+			:options="[
+				{
+					current: !role,
+					icon: 'bolt',
+					link: '/users',
+					text: $t('role.all')
+				},
+				'-',
+				...roles.map((roleFilter) => ({
+					current: role && role.id === roleFilter.id,
+					icon: 'bolt',
+					link: '/users?role=' + roleFilter.id,
+					text: roleFilter.title
+				}))
+			]"
+			align="right"
+		/>
+	</k-dropdown>
 </template>
 
 <script>
