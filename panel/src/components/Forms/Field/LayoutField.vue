8a923949bcf6a390d2c894848afcba09d9458b3c
Bastian Allgeier <mail@bastianallgeier.com>
2023-07-06 15:28:15 +0200

    Simplify collapsed button group and fix pagination

--- panel/src/components/Forms/Field/LayoutField.vue
+++ panel/src/components/Forms/Field/LayoutField.vue
@@ -9,39 +9,32 @@
 					size="xs"
 					@click="$refs.layouts.select(0)"
 				/>
-
-				<k-dropdown>
-					<k-button
-						icon="dots"
-						variant="filled"
-						size="xs"
-						@click="$refs.options.toggle()"
-					/>
-
-					<k-dropdown-content ref="options" align="right">
-						<k-dropdown-item
-							:disabled="isEmpty"
-							icon="template"
-							@click="$refs.layouts.copy()"
-						>
-							{{ $t("copy.all") }}
-						</k-dropdown-item>
-						<k-dropdown-item
-							icon="download"
-							@click="$refs.layouts.pasteboard()"
-						>
-							{{ $t("paste") }}
-						</k-dropdown-item>
-						<hr />
-						<k-dropdown-item
-							:disabled="isEmpty"
-							icon="trash"
-							@click="$refs.layouts.removeAll()"
-						>
-							{{ $t("delete.all") }}
-						</k-dropdown-item>
-					</k-dropdown-content>
-				</k-dropdown>
+				<k-button
+					icon="dots"
+					variant="filled"
+					size="xs"
+					@click="$refs.options.toggle()"
+				/>
+				<k-dropdown-content ref="options" align="right">
+					<k-dropdown-item
+						:disabled="isEmpty"
+						icon="template"
+						@click="$refs.layouts.copy()"
+					>
+						{{ $t("copy.all") }}
+					</k-dropdown-item>
+					<k-dropdown-item icon="download" @click="$refs.layouts.pasteboard()">
+						{{ $t("paste") }}
+					</k-dropdown-item>
+					<hr />
+					<k-dropdown-item
+						:disabled="isEmpty"
+						icon="trash"
+						@click="$refs.layouts.removeAll()"
+					>
+						{{ $t("delete.all") }}
+					</k-dropdown-item>
+				</k-dropdown-content>
 			</k-button-group>
 		</template>
 
