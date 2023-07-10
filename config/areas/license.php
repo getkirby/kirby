d72b5fd761ce93d358ffac7e8d48777653415725
Bastian Allgeier <mail@bastianallgeier.com>
2023-07-10 11:09:11 +0200

    Only show registration button if there’s no license

--- config/areas/license.php
+++ config/areas/license.php
@@ -4,12 +4,18 @@ use Kirby\Toolkit\I18n;
 
 return function ($kirby) {
 	return [
-		'icon'    => 'key',
-		'label'   => I18n::translate('license.register'),
-		'menu'    => [
-			'link'	 => null,
-			'dialog' => 'registration'
-		],
+		'icon'  => 'key',
+		'label' => I18n::translate('license.register'),
+		'menu'  => function () use ($kirby) {
+			if ($kirby->system()->license() !== false) {
+				return false;
+			}
+
+			return [
+				'link'	 => null,
+				'dialog' => 'registration'
+			];
+		},
 		'dialogs' => require __DIR__ . '/license/dialogs.php'
 	];
 };
