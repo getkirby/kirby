771d25eca9335c5034a09a34bf623779f5b22db2
Lukas Bestle <lukas@getkirby.com>
2023-07-26 21:17:20 +0200

    Fix password length vulnerability
    https://github.com/getkirby/kirby/security/advisories/GHSA-3v6j-v3qc-cxff

--- tests/Cms/Users/UserTest.php
+++ tests/Cms/Users/UserTest.php
@@ -228,6 +228,7 @@ class UserTest extends TestCase
             [null, false],
             ['', false],
             ['short', false],
+            [str_repeat('long', 300), false],
             ['invalid-password', false],
             ['correct-horse-battery-staple', true],
         ];
@@ -249,6 +250,48 @@ class UserTest extends TestCase
         } else {
             $this->assertTrue($user->validatePassword($input));
         }
+    }
+
+    public function testValidatePasswordHttpCode()
+    {
+        $user = new User([
+            'email'    => 'test@getkirby.com',
+            'password' => User::hashPassword('correct-horse-battery-staple')
+        ]);
+
+        $caught = 0;
+
+        try {
+            $user->validatePassword('short');
+        } catch (\Kirby\Exception\InvalidArgumentException $e) {
+            $this->assertSame(
+                'Please enter a valid password. Passwords must be at least 8 characters long.',
+                $e->getMessage()
+            );
+            $this->assertSame(400, $e->getHttpCode());
+            $caught++;
+        }
+
+        try {
+            $user->validatePassword(str_repeat('long', 300));
+        } catch (\Kirby\Exception\InvalidArgumentException $e) {
+            $this->assertSame(
+                'Please enter a valid password. Passwords must not be longer than 1000 characters.',
+                $e->getMessage()
+            );
+            $this->assertSame(400, $e->getHttpCode());
+            $caught++;
+        }
+
+        try {
+            $user->validatePassword('longbutinvalid');
+        } catch (\Kirby\Exception\InvalidArgumentException $e) {
+            $this->assertSame('Wrong password', $e->getMessage());
+            $this->assertSame(400, $e->getHttpCode());
+            $caught++;
+        }
+
+        $this->assertSame(3, $caught);
     }
 
     public function testValidateUndefinedPassword()
