6928085ec8adb3ef6b752bd4edf21863ab168dcf
Nico Hoffmann <me@nhoffmann.com>
2023-08-05 22:55:45 +0200

    Add more JS unit tests

--- panel/src/helpers/array.js
+++ panel/src/helpers/array.js
@@ -1,4 +1,5 @@
 import sort from "./sort";
+import "./regex";
 
 /**
  * Array.fromObject()
@@ -8,12 +9,46 @@ Array.fromObject = function (object) {
 };
 
 /**
+ * Search through an array by query
+ *
+ * @param {Array} array
+ * @param {String} query
+ * @param {Object} options
+ * @returns {Array}
+ */
+export const search = (array, query, options = {}) => {
+	if ((query ?? "").length <= (options.min ?? 0)) {
+		return array;
+	}
+
+	// Filter options by query to retrieve items (no more than this.limit)
+	const regex = new RegExp(RegExp.escape(query), "ig");
+	const field = options.field ?? "text";
+
+	const items = array.filter((item) => {
+		// skip all items without the searched field
+		if (!item[field]) {
+			return false;
+		}
+
+		// match the search with the text
+		return item[field].match(regex) !== null;
+	});
+
+	if (options.limit) {
+		return items.slice(0, options.limit);
+	}
+
+	return items;
+};
+
+/**
  * Array.sortBy()
  */
 Array.prototype.sortBy = function (sortBy) {
 	const options = sortBy.split(" ");
 	const field = options[0];
-	const direction = options[1] || "asc";
+	const direction = options[1] ?? "asc";
 
 	return this.sort((a, b) => {
 		const valueA = String(a[field]).toLowerCase();
@@ -53,40 +88,6 @@ Array.prototype.split = function (delimiter) {
  */
 Array.wrap = function (array) {
 	return Array.isArray(array) ? array : [array];
-};
-
-/**
- * Search through an array by query
- *
- * @param {Array} array
- * @param {String} query
- * @param {Object} options
- * @returns {Array}
- */
-export const search = (array, query, options = {}) => {
-	if ((query ?? "").length <= (options.min ?? 0)) {
-		return array;
-	}
-
-	// Filter options by query to retrieve items (no more than this.limit)
-	const regex = new RegExp(RegExp.escape(query), "ig");
-	const field = options.field ?? "text";
-
-	const items = array.filter((item) => {
-		// skip all items without the searched field
-		if (!item[field]) {
-			return false;
-		}
-
-		// match the search with the text
-		return item[field].match(regex) !== null;
-	});
-
-	if (options.limit) {
-		return items.slice(0, options.limit);
-	}
-
-	return items;
 };
 
 export default {
