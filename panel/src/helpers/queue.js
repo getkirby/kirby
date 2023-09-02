e24f8b4634fc91532cdc6450875f9385e9bb2d67
Nico Hoffmann <me@nhoffmann.com>
2023-09-02 10:21:39 +0200

    `$helper.queue()`: add docblock

--- panel/src/helpers/queue.js
+++ panel/src/helpers/queue.js
@@ -1,3 +1,11 @@
+/**
+ * Async function that runs a list of async functions
+ * with a max number of concurrent tasks
+ *
+ * @param {Array} tasks list of async functions
+ * @param {Number} concurrent max number of concurrent tasks
+ * @returns {Promise<Array>}
+ */
 export default async function (tasks, concurrent = 20) {
 	let active = 0;
 	let index = 0;
@@ -14,8 +22,10 @@ export default async function (tasks, concurrent = 20) {
 				tasks[index]().then(resolve(index)).catch(resolve(index));
 				index++;
 				active++;
-				next();
-			} else if (active === 0 && index === tasks.length) {
+				return next();
+			}
+
+			if (active === 0 && index === tasks.length) {
 				done(tasks);
 			}
 		};
