0a5ccd299d6252ed13802b07022cd3eb3f72e899
Bastian Allgeier <mail@bastianallgeier.com>
2023-09-06 10:24:50 +0200

    Remove redundant files

--- panel/src/components/Text/Link.vue
+++
@@ -1,9 +0,0 @@
-<style>
-.k-text :where(.k-link, a) {
-	color: var(--link-color);
-	text-decoration: underline;
-	text-underline-offset: var(--link-underline-offset);
-	border-radius: var(--rounded-xs);
-	outline-offset: 2px;
-}
-</style>
