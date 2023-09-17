8b7603fae22e2a77a19eb67954af69348769a0ae
Nico Hoffmann <me@nhoffmann.com>
2023-09-17 19:10:19 +0200

    Clean up

--- src/Cms/Auth/TotpChallenge.php
+++ src/Cms/Auth/TotpChallenge.php
@@ -7,30 +7,30 @@ use chillerlan\Authenticator\Authenticator as TOTP;
 
 class TotpChallenge extends Challenge
 {
-    /**
-     * Checks whether TOTP is activated for the user
-     */
-    public static function isAvailable(User $user, string $mode): bool
-    {
-        return $user->totp() !== null;
-    }
+	/**
+	 * Checks whether TOTP is activated for the user
+	 */
+	public static function isAvailable(User $user, string $mode): bool
+	{
+		 return $user->totp() !== null;
+	}
 
-    /**
-     * Since TOTP codes are generated automatically,
+	/**
+	 * Since TOTP codes are generated automatically,
 	 * don't return a code here
-     */
-    public static function create(User $user, array $options): string|null
-    {
-        return null;
-    }
+	 */
+	public static function create(User $user, array $options): string|null
+	{
+		return null;
+	}
 
-    /**
-     * Check TOTP code
-     */
-    public static function verify(User $user, string $code): bool
-    {
+	/**
+	 * Check TOTP code
+	 */
+	public static function verify(User $user, string $code): bool
+	{
 		$otp = new TOTP();
 		$otp->setSecret($user->totp());
-        return $otp->verify($code);
-    }
+		return $otp->verify($code);
+	}
 }
