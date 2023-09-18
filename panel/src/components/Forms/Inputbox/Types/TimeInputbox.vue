8b4d33f7be35c8480be223a2a8aeb3713bf6b9c1
Bastian Allgeier <mail@bastianallgeier.com>
2023-09-18 21:58:01 +0200

    New times prop for the time inputbox

--- panel/src/components/Forms/Inputbox/Types/TimeInputbox.vue
+++ panel/src/components/Forms/Inputbox/Types/TimeInputbox.vue
@@ -1,7 +1,7 @@
 <template>
 	<k-inputbox v-bind="$props" type="time">
 		<k-time-input v-bind="$props" @input="$emit('input', $event)" />
-		<k-inputbox-icon slot="icon">
+		<k-inputbox-icon v-if="times" slot="icon">
 			<k-button
 				:disabled="disabled"
 				:icon="icon"
@@ -23,9 +23,19 @@ import { props as InputProps } from "@/components/Forms/Input/TimeInput.vue";
 export const props = {
 	mixins: [InputboxProps, InputProps],
 	props: {
+		/**
+		 * Icon used for the times dropdown
+		 */
 		icon: {
 			default: "clock",
 			type: String
+		},
+		/**
+		 * Deactivate the times dropdown or not
+		 */
+		times: {
+			type: Boolean,
+			default: true
 		}
 	}
 };
