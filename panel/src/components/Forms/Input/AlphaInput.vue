abf938da0a45a14715220207fca6a1aeee64739b
Bastian Allgeier <mail@bastianallgeier.com>
2023-09-18 11:49:42 +0200

    New k-alpha-input

---
+++ panel/src/components/Forms/Input/AlphaInput.vue
@@ -0,0 +1,39 @@
+<template>
+	<input
+		ref="range"
+		v-bind="$props"
+		:min="0"
+		:max="1"
+		:step="0.01"
+		class="k-alpha-input"
+		type="range"
+		@input="$emit('input', Number($event.target.value))"
+	/>
+</template>
+
+<script>
+import { autofocus, disabled, id, name, required } from "@/mixins/props.js";
+
+export const props = {
+	mixins: [autofocus, disabled, id, name, required],
+	props: {
+		value: Number
+	}
+};
+
+/**
+ * @example <k-alpha-input :value="alpha" @input="alpha = $event" />
+ */
+export default {
+	mixins: [props],
+	inheritAttrs: false
+};
+</script>
+
+<style>
+.k-alpha-input {
+	--range-track-back: linear-gradient(to right, transparent, currentColor);
+	color: black;
+	background: var(--color-white) var(--pattern-light);
+}
+</style>
