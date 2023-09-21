0eeadb6a2ac9011135c3da38a349247673be4512
Bastian Allgeier <mail@bastianallgeier.com>
2023-09-21 18:29:34 +0200

    Get rid of outdated elements

--- panel/src/components/Forms/Element/index.js
+++
@@ -1,9 +0,0 @@
-import Choice from "./Choice.vue";
-import Range from "./Range.vue";
-
-export default {
-	install(app) {
-		app.component("k-choice", Choice);
-		app.component("k-range", Range);
-	}
-};
