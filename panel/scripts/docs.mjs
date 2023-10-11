a3696d6a2b2983edb792d0f9f4f6499afe8a4dee
Nico Hoffmann <me@nhoffmann.com>
2023-10-11 23:14:07 +0200

    `build:docs` for single file via arg

--- panel/scripts/docs.mjs
+++ panel/scripts/docs.mjs
@@ -7,23 +7,41 @@ import docgen from "vue-docgen-api";
 console.log("\n");
 console.log("Generating UI documentation...");
 
-const root = path.dirname(fileURLToPath(import.meta.url));
-const dist = path.resolve(root, "../dist/ui");
-const alias = { "@": path.resolve(root, "../src/") };
+const script = path.dirname(fileURLToPath(import.meta.url));
+const root = path.resolve(script, "../");
+const dist = path.resolve(root, "./dist/ui");
+const alias = { "@": path.resolve(root, "./src/") };
 
 if (fs.existsSync(dist) === false) {
 	fs.mkdirSync(dist);
 }
 
-// Get all Vue SFC files
-const files = await glob("src/components/**/*.vue");
+let files = [];
 
+// Checks for -- file argument
+if (process.argv[2]) {
+	files = [path.resolve(root, "./" + process.argv[2])];
+}
+
+// If no --file argument is given, get all Vue SFC files
+if (files.length === 0) {
+	files = await glob("src/components/**/*.vue");
+}
+
+// Parse each Vue SFC file and write earch result to a separate JSON file
 for (const file of files) {
+	// parse with Vue docgen API
 	const data = await docgen.parse(file, { alias });
+
+	// clean up data
+	delete data.sourceFiles;
+	data.sourceFile = path.relative(root, file);
+
+	// write file
 	fs.writeFileSync(
 		path.resolve(dist, data.displayName + ".json"),
 		JSON.stringify(data)
 	);
 }
 
-console.log("-> " + files.length + " components compiled");
+console.log("--> " + files.length + " components compiled");
