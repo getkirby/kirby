357e9d23f147f3d2c705862db4f7a08eaa4e981d
Nico Hoffmann <me@nhoffmann.com>
2023-10-14 22:01:37 +0200

    Tweak lab example for `k-dropzone`

--- panel/lab/components/dropzones/index.vue
+++ panel/lab/components/dropzones/index.vue
@@ -1,9 +1,25 @@
 <template>
 	<k-lab-examples>
 		<k-lab-example label="dropzone">
-			<k-dropzone>
-				<k-box theme="empty" text="Drag files here …" icon="upload" height="5rem" align="center" />
+			<k-dropzone @drop="onDrop">
+				<k-box
+					theme="empty"
+					text="Drag files here …"
+					icon="upload"
+					height="5rem"
+					align="center"
+				/>
 			</k-dropzone>
 		</k-lab-example>
 	</k-lab-examples>
 </template>
+
+<script>
+export default {
+	methods: {
+		onDrop(files) {
+			alert(files.length + " file(s) dropped");
+		}
+	}
+};
+</script>
