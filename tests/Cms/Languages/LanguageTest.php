45c3e8c9e07f42017fc0cc7f89029e3198b2ba8b
Nico Hoffmann  ෴ <me@nhoffmann.com>
2023-10-21 22:05:20 +0200

    Update tests/Cms/Languages/LanguageTest.php
    Co-authored-by: Lukas Bestle <lukas@getkirby.com>

--- tests/Cms/Languages/LanguageTest.php
+++ tests/Cms/Languages/LanguageTest.php
@@ -284,7 +284,7 @@ class LanguageTest extends TestCase
 	/**
 	 * @covers ::label
 	 */
-	public function tesLabel()
+	public function testLabel()
 	{
 		$language = new Language([
 			'code' => 'en',
