4795fa48ad2cb24d3afdd13788f1d0c83091b0a5
Nico Hoffmann  ෴ <me@nhoffmann.com>
2023-10-29 13:55:30 +0100

    Apply suggestions from code review
    Co-authored-by: Bastian Allgeier <mail@bastianallgeier.com>

--- src/Http/Remote.php
+++ src/Http/Remote.php
@@ -61,7 +61,7 @@ class Remote
 	/**
 	 * Constructor
 	 *
-	 * @throws Exception when the curl request failed
+	 * @throws \Exception when the curl request failed
 	 */
 	public function __construct(string $url, array $options = [])
 	{
@@ -122,7 +122,7 @@ class Remote
 	 * Sets up all curl options and sends the request
 	 *
 	 * @return $this
-	 * @throws Exception when the curl request failed
+	 * @throws \Exception when the curl request failed
 	 */
 	public function fetch(): static
 	{
@@ -262,7 +262,7 @@ class Remote
 	/**
 	 * Static method to send a GET request
 	 *
-	 * @throws Exception when the curl request failed
+	 * @throws \Exception when the curl request failed
 	 */
 	public static function get(string $url, array $params = []): static
 	{
@@ -345,7 +345,7 @@ class Remote
 	/**
 	 * Static method to init this class and send a request
 	 *
-	 * @throws Exception when the curl request failed
+	 * @throws \Exception when the curl request failed
 	 */
 	public static function request(string $url, array $params = []): static
 	{
