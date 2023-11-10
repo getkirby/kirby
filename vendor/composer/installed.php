bda8378882d9ef788baca2a6c3382676d3785a36
Bastian Allgeier <mail@bastianallgeier.com>
2023-11-10 10:40:09 +0100

    Update composer dependencies

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'getkirby/cms',
-        'pretty_version' => '4.0.0-beta.3',
-        'version' => '4.0.0.0-beta3',
+        'pretty_version' => '4.0.0-rc.1',
+        'version' => '4.0.0.0-RC1',
         'reference' => NULL,
         'type' => 'kirby-cms',
         'install_path' => __DIR__ . '/../../',
@@ -47,8 +47,8 @@
             'dev_requirement' => false,
         ),
         'getkirby/cms' => array(
-            'pretty_version' => '4.0.0-beta.3',
-            'version' => '4.0.0.0-beta3',
+            'pretty_version' => '4.0.0-rc.1',
+            'version' => '4.0.0.0-RC1',
             'reference' => NULL,
             'type' => 'kirby-cms',
             'install_path' => __DIR__ . '/../../',
