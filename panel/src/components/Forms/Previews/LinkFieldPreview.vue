5dbf3ceecef9a787f939427ecc17b54228f4d119
Bastian Allgeier <mail@bastianallgeier.com>
2024-01-18 12:56:08 +0100

    Improve link field preview

--- panel/src/components/Forms/Previews/LinkFieldPreview.vue
+++ panel/src/components/Forms/Previews/LinkFieldPreview.vue
@@ -4,20 +4,27 @@
 			<template v-if="model">
 				<k-pages-field-preview
 					v-if="currentType === 'page'"
-					:value="[{ text: model.label, value: value }]"
+					:html="false"
+					:value="[{ text: model.label, value: value, image: model.image }]"
 				/>
 				<k-files-field-preview
 					v-else
+					:html="false"
 					:value="[{ filename: model.label, image: model.image }]"
 				/>
 			</template>
 
 			<slot v-else name="placeholder" />
 		</template>
-
-		<k-url-field-preview v-else-if="currentType === 'url'" :value="value" />
-		<k-email-field-preview v-else-if="currentType === 'email'" :value="value" />
-		<k-text-field-preview v-else :value="value" />
+		<k-url-field-preview
+			v-else-if="currentType === 'url'"
+			:value="detected.link"
+		/>
+		<k-email-field-preview
+			v-else-if="currentType === 'email'"
+			:value="detected.link"
+		/>
+		<k-text-field-preview v-else :value="detected.link" />
 	</div>
 </template>
 
