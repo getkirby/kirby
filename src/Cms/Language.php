a291869803a85121706a53230f043523ff96bfd8
Ahmet Bora <byybora@gmail.com>
2024-01-27 20:17:24 +0300

    New `translations` root

--- src/Cms/Language.php
+++ src/Cms/Language.php
@@ -62,7 +62,6 @@ class Language
 		$this->name         = trim($props['name'] ?? $this->code);
 		$this->slugs        = $props['slugs'] ?? [];
 		$this->smartypants  = $props['smartypants'] ?? [];
-		$this->translations = $props['translations'] ?? [];
 		$this->url          = $props['url'] ?? null;
 
 		if ($locale = $props['locale'] ?? null) {
@@ -70,6 +69,8 @@ class Language
 		} else {
 			$this->locale = [LC_ALL => $this->code];
 		}
+
+		$this->setTranslations($props['translations'] ?? []);
 	}
 
 	/**
@@ -409,6 +410,13 @@ class Language
 	{
 		try {
 			$existingData = Data::read($this->root());
+
+			// inject translations from custom root
+			// returns existing translations
+			// if custom root is not defined as fallback
+			$data['translations'] = $this
+				->translationsObject()
+				->load($existingData['translations'] ?? []);
 		} catch (Throwable) {
 			$existingData = [];
 		}
@@ -427,7 +435,27 @@ class Language
 
 		ksort($data);
 
+		// save translations to the custom root and remove translations
+		// to prevent duplication write into the language file
+		if ($this->translationsObject()->root() !== null) {
+			$this->translationsObject()
+				->save($data['translations'] ?? []);
+			$data['translations'] = [];
+		}
+
 		Data::write($this->root(), $data);
+
+		return $this;
+	}
+
+	/**
+	 * Sets the translations data
+	 *
+	 * @return $this
+	 */
+	protected function setTranslations(array $translations = []): static
+	{
+		$this->translations = (new LanguageTranslations($this))->load($translations);
 
 		return $this;
 	}
@@ -475,10 +503,23 @@ class Language
 
 	/**
 	 * Returns the translation strings for this language
+	 * @todo In v5, remove this method and
+	 * 		rename `->translationsObject()` method with the `translations`.
+	 * 		This will be a breaking change.
 	 */
 	public function translations(): array
 	{
 		return $this->translations;
+	}
+
+	/**
+	 * Returns the language translations object for this language
+	 * @todo In v5, rename this method name as `translations`
+	 * @internal
+	 */
+	public function translationsObject(): LanguageTranslations
+	{
+		return new LanguageTranslations($this, $this->translations);
 	}
 
 	/**
---
+++ src/Cms/LanguageTranslations.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Kirby\Cms;
+
+use Exception;
+use Kirby\Data\Data;
+use Kirby\Filesystem\F;
+
+/**
+ * With helper methods provides get language translations or
+ * loads from custom `translations` root
+ * @since 4.2.0
+ *
+ * @package   Kirby Cms
+ * @author    Ahmet Bora <ahmet@getkirby.com>
+ * @link      https://getkirby.com
+ * @copyright Bastian Allgeier
+ * @license   https://getkirby.com/license
+ */
+class LanguageTranslations
+{
+	protected array $data;
+
+	public function __construct(
+		protected Language $language,
+		self|array $translations = []
+	) {
+		$this->setTranslations($translations);
+	}
+
+	/**
+	 * Returns a single translation string by key
+	 */
+	public function get(string $key, string $default = null): string|null
+	{
+		return $this->data[$key] ?? $default;
+	}
+
+	/**
+	 * Loads the language translations based on custom roots for provided language code
+	 */
+	public function load(array $default = []): array
+	{
+		if ($file = static::root()) {
+			try {
+				return Data::read($file);
+			} catch (Exception) {
+				return $default;
+			}
+		}
+
+		return $default;
+	}
+
+	/**
+	 * Saves the language translations in the custom root
+	 * @internal
+	 *
+	 * @return $this
+	 */
+	public function save(array $translations = []): static
+	{
+		$this->setTranslations($translations);
+
+		if ($root = $this->root()) {
+			Data::write($root, $translations);
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Returns custom translations root path if defined
+	 */
+	public function root(): string|null
+	{
+		$kirby = App::instance();
+		$root  = $kirby->root('translations');
+		$file  = ($root ?? '') . '/' . $this->language->code() . '.php';
+
+		if (
+			$root !== null &&
+			F::exists($file) === true
+		) {
+			return $file;
+		}
+
+		return null;
+	}
+
+	/**
+	 * Removes a translation key
+	 *
+	 * @return $this
+	 */
+	public function remove(string $key): static
+	{
+		unset($this->data[$key]);
+		return $this;
+	}
+
+	/**
+	 * Sets the translation key
+	 *
+	 * @return $this
+	 */
+	public function set(string $key, string|null $value = null): static
+	{
+		$this->data[$key] = $value;
+		return $this;
+	}
+
+	/**
+	 * Set translations
+	 *
+	 * @return $this
+	 */
+	public function setTranslations(self|array $translations = []): static
+	{
+		if (empty($translations) === false) {
+			if ($translations instanceof self) {
+				$this->data = $translations->toArray();
+			} else {
+				$this->data = $translations;
+			}
+		} else {
+			$this->data = static::load();
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Returns translations
+	 */
+	public function toArray(): array
+	{
+		return $this->data;
+	}
+}
