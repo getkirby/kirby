08ad1a7f674d36b57b01904012d0fea44bc51b30
Bastian Allgeier <mail@bastianallgeier.com>
2024-02-05 11:55:20 +0100

    Check for valid date and fix with value as fallback

--- panel/src/components/Forms/Previews/DateFieldPreview.vue
+++ panel/src/components/Forms/Previews/DateFieldPreview.vue
@@ -24,10 +24,11 @@ export default {
 			return this.$library.dayjs(this.value);
 		},
 		text() {
-			// don't parse and format if custom date value defined
-			return this.column.value?.length > 0 ?
-				this.value :
-				this.parsed?.format(this.format);
+			if (this.parsed.isValid() === false) {
+				return this.value;
+			}
+
+			return this.parsed?.format(this.format);
 		},
 		time() {
 			return this.column.time ?? this.field.time;
