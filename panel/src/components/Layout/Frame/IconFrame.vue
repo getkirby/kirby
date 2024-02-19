81d7d7a7419f00cc8659d70a0aacebbf6e241949
Ahmet Bora <byybora@gmail.com>
2024-02-19 16:29:51 +0300

    Remove emoji in icon frame component

--- panel/src/components/Layout/Frame/IconFrame.vue
+++ panel/src/components/Layout/Frame/IconFrame.vue
@@ -1,7 +1,6 @@
 <template>
 	<k-frame v-bind="$props" element="figure" class="k-icon-frame">
-		<span v-if="isEmoji" data-type="emoji">{{ icon }}</span>
-		<k-icon v-else v-bind="{ color, type: icon, alt }" />
+		<k-icon v-bind="{ color, type: icon, alt }" />
 	</k-frame>
 </template>
 
@@ -33,25 +32,6 @@ export const props = {
  */
 export default {
 	mixins: [props],
-	inheritAttrs: false,
-	computed: {
-		isEmoji() {
-			return this.$helper.string.hasEmoji(this.icon);
-		}
-	}
+	inheritAttrs: false
 };
 </script>
-
-<style>
-/* fix emoji alignment on high-res screens */
-@media only screen and (-webkit-min-device-pixel-ratio: 2),
-	not all,
-	not all,
-	not all,
-	only screen and (min-resolution: 192dpi),
-	only screen and (min-resolution: 2dppx) {
-	.k-icon-frame [data-type="emoji"] {
-		font-size: 1.25em;
-	}
-}
-</style>
