ae19b026daa771992f18404c585ed68c25499de0
Lukas Bestle <lukas@getkirby.com>
2024-02-25 18:16:54 +0100

    Update version number

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'getkirby/cms',
-        'pretty_version' => '3.7.5.3',
-        'version' => '3.7.5.3',
-        'reference' => NULL,
+        'pretty_version' => '3.7.5.4',
+        'version' => '3.7.5.4',
+        'reference' => null,
         'type' => 'kirby-cms',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -29,9 +29,9 @@
             'dev_requirement' => false,
         ),
         'getkirby/cms' => array(
-            'pretty_version' => '3.7.5.3',
-            'version' => '3.7.5.3',
-            'reference' => NULL,
+            'pretty_version' => '3.7.5.4',
+            'version' => '3.7.5.4',
+            'reference' => null,
             'type' => 'kirby-cms',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
