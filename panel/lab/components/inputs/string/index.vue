551cb308d71264702ef0b7907c23055004a7cd9a
Nico Hoffmann <me@nhoffmann.com>
2024-03-02 14:16:42 +0100

    Add string input lab examples

---
+++ panel/lab/components/inputs/string/index.vue
@@ -0,0 +1,72 @@
+<template>
+	<k-lab-form>
+		<k-lab-examples class="k-lab-input-examples">
+			<k-lab-example label="Default">
+				<k-string-input name="string" :value="value" @input="value = $event" />
+			</k-lab-example>
+
+			<k-lab-example label="Autofocus">
+				<k-string-input
+					:autofocus="true"
+					:value="value"
+					@input="value = $event"
+				/>
+			</k-lab-example>
+
+			<k-lab-example label="Required">
+				<k-string-input
+					:required="true"
+					:value="value"
+					@input="value = $event"
+				/>
+			</k-lab-example>
+
+			<k-lab-example label="Placeholder">
+				<k-string-input
+					placeholder="Placeholder text …"
+					:value="value"
+					@input="value = $event"
+				/>
+			</k-lab-example>
+
+			<k-lab-example label="Focus" class="k-lab-input-examples-focus">
+				<k-string-input ref="input" :value="value" @input="value = $event" />
+				<k-button text="Focus" variant="filled" @click="$refs.input.focus()" />
+			</k-lab-example>
+
+			<k-lab-example label="Disabled">
+				<k-string-input
+					:disabled="true"
+					:value="value"
+					@input="value = $event"
+				/>
+			</k-lab-example>
+
+			<k-lab-example label="Minlegth: 5">
+				<k-string-input :minlength="5" :value="value" @input="value = $event" />
+			</k-lab-example>
+
+			<k-lab-example label="Maxlength: 5">
+				<k-string-input :maxlength="5" :value="value" @input="value = $event" />
+			</k-lab-example>
+
+			<k-lab-example label="Pattern: a-f">
+				<k-string-input
+					pattern="[a-f]*"
+					:value="value"
+					@input="value = $event"
+				/>
+			</k-lab-example>
+		</k-lab-examples>
+	</k-lab-form>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			value: null
+		};
+	}
+};
+</script>
