6580413498c2511104360fa6a44e0950cbe552b6
Bastian Allgeier <mail@bastianallgeier.com>
2024-03-25 11:16:25 +0100

    Make directive reactive

--- panel/src/config/i18n.js
+++ panel/src/config/i18n.js
@@ -1,18 +1,21 @@
 export default {
 	install(app) {
+		const dir = (el, binding, vnode) => {
+			if (vnode.context.disabled !== true) {
+				el.dir = window.panel.language.direction;
+			} else {
+				el.dir = null;
+			}
+		};
+
 		/**
 		 * v-direction directive
 		 * only applies `:dir="$direction"` if the
 		 * component isn't disabled
 		 */
 		app.directive("direction", {
-			inserted(el, binding, vnode) {
-				if (vnode.context.disabled !== true) {
-					el.dir = window.panel.language.direction;
-				} else {
-					el.dir = null;
-				}
-			}
+			bind: dir,
+			update: dir
 		});
 	}
 };
