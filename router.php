1f21b195303ccc184ffd7ba1e4b9e20846027b5f
Lukas Bestle <lukas@getkirby.com>
2024-03-27 20:51:27 +0100

    Add comment

--- router.php
+++ router.php
@@ -1,5 +1,7 @@
 <?php
 
+// prepend a fake host to ensure that PHP can parse the path even if it contains weird stuff;
+// afterwards just take the plain path back out from the parsed result
 $uri = parse_url('https://getkirby.com/' . ltrim($_SERVER['REQUEST_URI'], '/'), PHP_URL_PATH) ?? '/';
 $uri = urldecode($uri);
 
