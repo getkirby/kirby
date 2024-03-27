5d29f1e1a9828fe37145d6048b66126c292220b9
Bastian Allgeier <mail@bastianallgeier.com>
2024-03-27 11:38:03 +0100

    Another try

--- router.php
+++ router.php
@@ -1,11 +1,10 @@
 <?php
 
-$uri = urldecode(
-	parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?? '/'
-);
+$uri = parse_url('https://getkirby.com/' . ltrim($_SERVER['REQUEST_URI'], '/'), PHP_URL_PATH) ?? '/';
+$uri = urldecode($uri);
 
 // Emulate Apache's `mod_rewrite` functionality
-if ($uri !== '/' && file_exists($_SERVER['DOCUMENT_ROOT'] . '/' . $uri)) {
+if ($uri !== '/' && file_exists($_SERVER['DOCUMENT_ROOT'] . '/' . ltrim($uri, '/'))) {
 	return false;
 }
 
