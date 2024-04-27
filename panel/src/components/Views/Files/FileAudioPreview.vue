85987ed9a03335fe44ebdb772ac42a4558a00300
Nico Hoffmann <me@nhoffmann.com>
2024-04-27 18:26:04 +0200

    Audio file preview

---
+++ panel/src/components/Views/Files/FileAudioPreview.vue
@@ -0,0 +1,35 @@
+<template>
+	<k-file-preview :details="details" :options="options">
+		<template #thumb>
+			<audio controls preload="metadata" :src="url" />
+		</template>
+	</k-file-preview>
+</template>
+
+<script>
+export default {
+	props: {
+		details: {
+			default: () => [],
+			type: Array
+		},
+		image: {
+			default: () => ({}),
+			type: Object
+		},
+		url: String
+	},
+	computed: {
+		options() {
+			return [
+				{
+					icon: "download",
+					text: this.$t("download"),
+					link: this.url,
+					download: true
+				}
+			];
+		}
+	}
+};
+</script>
--- panel/src/components/Views/Files/index.js
+++ panel/src/components/Views/Files/index.js
@@ -1,15 +1,21 @@
 import FileView from "./FileView.vue";
+
 import FilePreview from "./FilePreview.vue";
+import FileAudioPreview from "./FileAudioPreview.vue";
 import FileDefaultPreview from "./FileDefaultPreview.vue";
 import FileImagePreview from "./FileImagePreview.vue";
+
 import FileFocusButton from "./FileFocusButton.vue";
 
 export default {
 	install(app) {
 		app.component("k-file-view", FileView);
+
 		app.component("k-file-preview", FilePreview);
+		app.component("k-file-audio-preview", FileAudioPreview);
 		app.component("k-file-default-preview", FileDefaultPreview);
 		app.component("k-file-image-preview", FileImagePreview);
+
 		app.component("k-file-focus-button", FileFocusButton);
 	}
 };
