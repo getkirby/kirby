af58c7871e65effc06b3a4bfc14c71ab4ff1d79a
Bastian Allgeier <mail@bastianallgeier.com>
2024-05-02 21:25:25 +0200

    New lab examples

---
+++ panel/lab/components/uploaditem/index.vue
@@ -0,0 +1,64 @@
+<template>
+	<k-lab-examples>
+		<k-lab-example label="Upload Item">
+			<k-upload-item
+				extension="jpeg"
+				type="image/jpeg"
+				name="test"
+				niceSize="128 kB"
+				url="https://picsum.photos/200/200"
+				@remove="remove"
+				@rename="rename"
+			/>
+		</k-lab-example>
+		<k-lab-example label="Progress">
+			<k-upload-item
+				:progress="80"
+				extension="jpeg"
+				type="image/jpeg"
+				name="test"
+				niceSize="128 kB"
+				url="https://picsum.photos/200/200"
+				@remove="remove"
+				@rename="rename"
+			/>
+		</k-lab-example>
+		<k-lab-example label="Completed">
+			<k-upload-item
+				:completed="true"
+				extension="jpeg"
+				type="image/jpeg"
+				name="test"
+				niceSize="128 kB"
+				url="https://picsum.photos/200/200"
+				@remove="remove"
+				@rename="rename"
+			/>
+		</k-lab-example>
+		<k-lab-example label="Error">
+			<k-upload-item
+				extension="jpeg"
+				type="image/jpeg"
+				name="test"
+				niceSize="128 kB"
+				error="The file could not be uploaded"
+				url="https://picsum.photos/200/200"
+				@remove="remove"
+				@rename="rename"
+			/>
+		</k-lab-example>
+	</k-lab-examples>
+</template>
+
+<script>
+export default {
+	methods: {
+		remove() {
+			console.log("Remove");
+		},
+		rename(name) {
+			console.log("Rename to", name);
+		}
+	}
+};
+</script>
---
+++ panel/lab/components/uploaditempreview/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<k-lab-examples>
+		<k-lab-example label="Image with preview">
+			<k-upload-item-preview
+				type="image/jpeg"
+				url="https://picsum.photos/200/200"
+			/>
+		</k-lab-example>
+		<k-lab-example label="Image without preview">
+			<k-upload-item-preview
+				type="image/bitmap"
+				url="https://picsum.photos/200/200"
+			/>
+		</k-lab-example>
+		<k-lab-example label="Audio">
+			<k-upload-item-preview type="audio/mp3" url="/some.mp3" />
+		</k-lab-example>
+		<k-lab-example label="Video">
+			<k-upload-item-preview type="video/mp4" url="/some.mp4" />
+		</k-lab-example>
+	</k-lab-examples>
+</template>
+
+<style>
+.k-lab-example .k-upload-item-preview {
+	width: 6rem;
+}
+</style>
