8a8c99172979aa7d4778fb8aeb6ad461b0efdfce
Nico Hoffmann <me@nhoffmann.com>
2024-06-01 12:28:34 +0200

    Resort view components

--- panel/src/components/Views/Login/index.js
+++ panel/src/components/Views/Login/index.js
@@ -1,25 +1,14 @@
-import LoginView from "./LoginView.vue";
-
-import LoginForm from "./LoginForm.vue";
 import LoginAlert from "./LoginAlert.vue";
 import LoginCodeForm from "./LoginCodeForm.vue";
-
-import InstallationView from "./InstallationView.vue";
-import ResetPasswordView from "./ResetPasswordView.vue";
-
-import UserInfo from "./UserInfo.vue";
+import LoginForm from "./LoginForm.vue";
+import LoginView from "./LoginView.vue";
 
 export default {
 	install(app) {
-		app.component("k-login-view", LoginView);
-		app.component("k-login-form", LoginForm);
-		app.component("k-login-code-form", LoginCodeForm);
 		app.component("k-login-alert", LoginAlert);
-
-		app.component("k-installation-view", InstallationView);
-		app.component("k-reset-password-view", ResetPasswordView);
-
-		app.component("k-user-info", UserInfo);
+		app.component("k-login-code-form", LoginCodeForm);
+		app.component("k-login-form", LoginForm);
+		app.component("k-login-view", LoginView);
 
 		/** deprecated */
 		app.component("k-login", LoginForm);
--- panel/src/components/Views/index.js
+++ panel/src/components/Views/index.js
@@ -1,26 +1,13 @@
 import ErrorView from "./ErrorView.vue";
 import SearchView from "./SearchView.vue";
 
-import FileView from "./Files/FileView.vue";
-import FilePreview from "./Files/FilePreview.vue";
-import FileFocusButton from "./Files/FileFocusButton.vue";
-
-import LanguagesView from "./Languages/LanguagesView.vue";
-import LanguageView from "./Languages/LanguageView.vue";
-
+import Files from "./Files/index.js";
+import Installation from "./Installation/index.js";
+import Languages from "./Languages/index.js";
 import Login from "./Login/index.js";
-
-import PageView from "./Pages/PageView.vue";
-import SiteView from "./Pages/SiteView.vue";
-
-import SystemView from "./System/SystemView.vue";
-import TableUpdateStatusCell from "./System/TableUpdateStatusCell.vue";
-
-import AccountView from "./Users/AccountView.vue";
-import UserAvatar from "./Users/UserAvatar.vue";
-import UserProfile from "./Users/UserProfile.vue";
-import UserView from "./Users/UserView.vue";
-import UsersView from "./Users/UsersView.vue";
+import Pages from "./Pages/index.js";
+import Users from "./Users/index.js";
+import System from "./System/index.js";
 
 import LegacyPluginView from "./LegacyPluginView.vue";
 
@@ -29,27 +16,15 @@ export default {
 		app.component("k-error-view", ErrorView);
 		app.component("k-search-view", SearchView);
 
-		app.component("k-file-view", FileView);
-		app.component("k-file-preview", FilePreview);
-		app.component("k-file-focus-button", FileFocusButton);
-
-		app.component("k-languages-view", LanguagesView);
-		app.component("k-language-view", LanguageView);
-
+		app.use(Files);
+		app.use(Installation);
+		app.use(Languages);
 		app.use(Login);
+		app.use(Pages);
+		app.use(System);
+		app.use(Users);
 
-		app.component("k-page-view", PageView);
-		app.component("k-site-view", SiteView);
-
-		app.component("k-system-view", SystemView);
-		app.component("k-table-update-status-cell", TableUpdateStatusCell);
-
-		app.component("k-account-view", AccountView);
-		app.component("k-user-avatar", UserAvatar);
-		app.component("k-user-profile", UserProfile);
-		app.component("k-user-view", UserView);
-		app.component("k-users-view", UsersView);
-
+		/* Deprecated */
 		app.component("k-plugin-view", LegacyPluginView);
 	}
 };
