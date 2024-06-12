df2bb10f43c8194e0bd94a64fdb06bd479adfba8
Bastian Allgeier <mail@bastianallgeier.com>
2024-06-12 15:30:13 +0200

    New LabPage class and unit tests

---
+++ src/Content/LabPage.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Kirby\Content;
+
+use Kirby\Cms\Collection;
+use Kirby\Cms\Language;
+use Kirby\Cms\Page;
+
+/**
+ * Test Model to prototype the content and translation
+ * mechanics
+ */
+class LabPage extends Page
+{
+	/**
+	 * Creates a new instance with the same
+	 * initial properties
+	 *
+	 * @todo eventually refactor without need of propertyData
+	 */
+	public function clone(array $props = []): static
+	{
+		$this->storage = MemoryContentStorageHandler::from($this->storage);
+		return parent::clone($props);
+	}
+
+	/**
+	 * Returns the content for the default version and given language code
+	 */
+	public function content(string|null $languageCode = null): Content
+	{
+		return $this->version()->content($languageCode ?? 'current');
+	}
+
+	/**
+	 * @deprecated since 5.0.0 Use `::version()->read()` instead
+	 */
+	public function readContent(string|null $languageCode = null): array
+	{
+		return $this->version()->read($languageCode ?? 'current');
+	}
+
+	/**
+	 * @deprecated since 5.0.0 Use `::version()->save()` instead
+	 */
+	public function save(
+		array|null $data = null,
+		string|null $languageCode = null,
+		bool $overwrite = false
+	): static {
+		$clone = $this->clone();
+		$clone->version()->save($data ?? [], $languageCode ?? 'current', $overwrite);
+		return $clone;
+	}
+
+	/**
+	 * @deprecated since 5.0.0 Use `::version()->save()` instead
+	 */
+	protected function saveContent(
+		array|null $data = null,
+		bool $overwrite = false
+	): static {
+		$clone = $this->clone();
+		$clone->version()->save($data ?? [], 'current', $overwrite);
+		return $clone;
+	}
+
+	/**
+	 * @deprecated since 5.0.0 Use `::version()->save()` instead
+	 */
+	protected function saveTranslation(
+		array|null $data = null,
+		string|null $languageCode = null,
+		bool $overwrite = false
+	): static {
+		$clone = $this->clone();
+		$clone->version()->save($data ?? [], $languageCode ?? 'current', $overwrite);
+		return $clone;
+	}
+
+	/**
+	 * Sets the content when initializing a model manually.
+	 * This will switch to the in memory storage to keep the
+	 * content there instead of on disk or in a database.
+	 * The content will be created for the default version and language
+	 */
+	protected function setContent(array|null $content = null): static
+	{
+		// don't set anything if there's no content
+		if ($content === null) {
+			return $this;
+		}
+
+		$this->storage = new MemoryContentStorageHandler(
+			model: $this
+		);
+
+		Translation::create(
+			model: $this,
+			version: $this->version(),
+			language: Language::ensure('default'),
+			fields: $content
+		);
+
+		return $this;
+	}
+
+	/**
+	 * Stores in-memory translations for the model if they
+	 * are passed to the constructor with the translations prop.
+	 */
+	protected function setTranslations(array|null $translations = null): static
+	{
+		// don't set anything if there's no content
+		if ($translations === null) {
+			return $this;
+		}
+
+		// switch to in-memory content storage
+		$this->storage = new MemoryContentStorageHandler(
+			model: $this
+		);
+
+		Translations::create(
+			model: $this,
+			version: $this->version(),
+			translations: $translations
+		);
+
+		return $this;
+	}
+
+	/**
+	 * Returns a single translation by language code
+	 * If no code is specified the current translation is returned
+	 */
+	public function translation(
+		string|null $languageCode = null
+	): Translation|null {
+		return $this->translations()->find($languageCode ?? 'current');
+	}
+
+	/**
+	 * Returns the translations collection
+	 */
+	public function translations(): Translations
+	{
+		return Translations::load(
+			model: $this,
+			version: $this->version()
+		);
+	}
+
+	/**
+	 * @deprecated since 5.0.0 Use `::version()->save()` instead
+	 */
+	public function writeContent(array $data, string|null $languageCode = null): bool
+	{
+		$this->clone()->version()->save($data, $languageCode ?? 'default', true);
+		return true;
+	}
+}
---
+++ tests/Content/LabPageTest.php
@@ -0,0 +1,307 @@
+<?php
+
+namespace Kirby\Content;
+
+use Kirby\Data\Data;
+
+class LabPageTest extends TestCase
+{
+	public const TMP = KIRBY_TMP_DIR . '/Content.LabeModel';
+
+	public function testContentMultiLanguage()
+	{
+		$this->setUpMultiLanguage();
+
+		$page = new LabPage([
+			'slug'     => 'test',
+			'template' => 'article'
+		]);
+
+		// write something to the content file to make sure it
+		// can be read from disk for the test.
+		Data::write($page->root() . '/article.en.txt', $content = [
+			'title' => 'Test'
+		]);
+
+		$this->assertSame($content, $page->content('en')->toArray());
+
+		$this->markTestIncomplete('TODO: The following assertion is only correct as long as we don’t merge translated content. It’s a breaking change so far compared to the previous behavior and needs to be fixed.');
+		$this->assertSame([], $page->content('de')->toArray());
+	}
+
+	public function testContentSingleLanguage()
+	{
+		$this->setUpSingleLanguage();
+
+		$page = new LabPage([
+			'slug'     => 'test',
+			'template' => 'article'
+		]);
+
+		// write something to the content file to make sure it
+		// can be read from disk for the test.
+		Data::write($page->root() . '/article.txt', $content = [
+			'title' => 'Test'
+		]);
+
+		$this->assertSame($content, $page->content()->toArray());
+	}
+
+	public function testContentNonExistingMultiLanguage()
+	{
+		$this->setUpMultiLanguage();
+
+		$page = new LabPage([
+			'slug' => 'test'
+		]);
+
+		$this->assertSame([], $page->content('en')->toArray());
+		$this->assertSame([], $page->content('de')->toArray());
+	}
+
+	public function testContentNonExistingSingleLanguage()
+	{
+		$this->setUpSingleLanguage();
+
+		$page = new LabPage([
+			'slug' => 'test'
+		]);
+
+		$this->assertSame([], $page->content()->toArray());
+	}
+
+	public function testSetContentMultiLanguage()
+	{
+		$this->setUpMultiLanguage();
+
+		$page = new LabPage([
+			'slug'    => 'test',
+			'content' => $content = [
+				'title' => 'Test'
+			]
+		]);
+
+		$this->assertSame($content, $page->content()->toArray());
+		$this->assertSame($content, $page->content('en')->toArray());
+
+		$this->markTestIncomplete('TODO: The following assertion is only correct as long as we don’t merge translated content. It’s a breaking change so far compared to the previous behavior and needs to be fixed.');
+		$this->assertSame([], $page->content('de')->toArray());
+	}
+
+	public function testSetContentSingleLanguage()
+	{
+		$this->setUpSingleLanguage();
+
+		$page = new LabPage([
+			'slug'    => 'test',
+			'content' => $content = [
+				'title' => 'Test'
+			]
+		]);
+
+		$this->assertSame($content, $page->content()->toArray());
+	}
+
+	public function testSetTranslationMultiLanguage()
+	{
+		$this->setUpMultiLanguage();
+
+		$page = new LabPage([
+			'slug'         => 'test',
+			'translations' => [
+				[
+					'code'    => 'en',
+					'content' => $en = [
+						'title' => 'Test English'
+					]
+				],
+				[
+					'code'    => 'de',
+					'content' => $de = [
+						'title' => 'Test Deutsch'
+					]
+				]
+			]
+		]);
+
+		$this->assertSame($en, $page->content()->toArray());
+		$this->assertSame($en, $page->content('en')->toArray());
+		$this->assertSame($en, $page->translation('en')->content());
+
+		$this->assertSame($de, $page->content('de')->toArray());
+		$this->assertSame($de, $page->translation('de')->content());
+	}
+
+	public function testSetTranslationSingleLanguage()
+	{
+		$this->setUpSingleLanguage();
+
+		$page = new LabPage([
+			'slug'         => 'test',
+			'translations' => [
+				[
+					'code'    => 'en',
+					'content' => $content = [
+						'title' => 'Test'
+					]
+				]
+			]
+		]);
+
+		$this->assertSame($content, $page->content()->toArray());
+		$this->assertSame($content, $page->translation('en')->content());
+	}
+
+	public function testTranslationMultiLanguage()
+	{
+		$this->setUpMultiLanguage();
+
+		$page = new LabPage([
+			'slug'     => 'test',
+			'template' => 'article'
+		]);
+
+		Data::write($page->root() . '/article.en.txt', $en = [
+			'title' => 'Test English'
+		]);
+
+		Data::write($page->root() . '/article.de.txt', $de = [
+			'title' => 'Test Deutsch'
+		]);
+
+		$this->assertSame($en, $page->translation('en')->content());
+		$this->assertSame($de, $page->translation('de')->content());
+	}
+
+	public function testTranslationSingleLanguage()
+	{
+		$this->setUpSingleLanguage();
+
+		$page = new LabPage([
+			'slug'     => 'test',
+			'template' => 'article'
+		]);
+
+		Data::write($page->root() . '/article.txt', $content = [
+			'title' => 'Test'
+		]);
+
+		$this->assertSame($content, $page->translation()->content());
+		$this->assertSame($content, $page->translation('en')->content());
+	}
+
+	public function testTranslationsMultiLanguage()
+	{
+		$this->setUpMultiLanguage();
+
+		$page = new LabPage([
+			'slug'     => 'test',
+			'template' => 'article'
+		]);
+
+		Data::write($page->root() . '/article.en.txt', $en = [
+			'title' => 'Test English'
+		]);
+
+		Data::write($page->root() . '/article.de.txt', $de = [
+			'title' => 'Test Deutsch'
+		]);
+
+		$translations = $page->translations();
+
+		$this->assertCount(2, $translations);
+		$this->assertSame($en, $translations->find('en')->content());
+		$this->assertSame($de, $translations->find('de')->content());
+	}
+
+	public function testTranslationsSingleLanguage()
+	{
+		$this->setUpSingleLanguage();
+
+		$page = new LabPage([
+			'slug'     => 'test',
+			'template' => 'article'
+		]);
+
+		// write something to the content file to make sure it
+		// can be read from disk for the test.
+		Data::write($page->root() . '/article.txt', $content = [
+			'title' => 'Test'
+		]);
+
+		$translations = $page->translations();
+
+		$this->assertCount(1, $translations);
+		$this->assertSame($content, $translations->first()->content());
+	}
+
+	public function testUpdateMultiLanguage()
+	{
+		$this->setUpMultiLanguage();
+
+		$page = new LabPage([
+			'slug'     => 'test',
+			'template' => 'article'
+		]);
+
+		// make sure to be authenticated
+		$this->app->impersonate('kirby');
+
+		// write something to the content file to make sure it
+		// can be read from disk for the test.
+		Data::write($page->root() . '/article.en.txt', $en = [
+			'title' => 'Test English'
+		]);
+
+		Data::write($page->root() . '/article.de.txt', $de = [
+			'title' => 'Test Deutsch'
+		]);
+
+		$updatedPage = $page->update([
+			'title' => 'Updated Test English'
+		], 'en');
+
+		$updatedPage = $page->update([
+			'title' => 'Updated Test Deutsch'
+		], 'de');
+
+		// check if the old version is still the same
+		$this->assertSame('Test English', $page->title()->value());
+
+		$this->assertSame('Updated Test English', $updatedPage->title()->value());
+		$this->assertSame('Updated Test English', $updatedPage->content('en')->title()->value());
+		$this->assertSame('Updated Test English', Data::read($updatedPage->root() . '/article.en.txt')['title']);
+
+		$this->assertSame('Updated Test Deutsch', $updatedPage->content('de')->title()->value());
+		$this->assertSame('Updated Test Deutsch', Data::read($updatedPage->root() . '/article.de.txt')['title']);
+	}
+
+	public function testUpdateSingleLanguage()
+	{
+		$this->setUpSingleLanguage();
+
+		$page = new LabPage([
+			'slug'     => 'test',
+			'template' => 'article'
+		]);
+
+		// make sure to be authenticated
+		$this->app->impersonate('kirby');
+
+		// write something to the content file to make sure it
+		// can be read from disk for the test.
+		Data::write($page->root() . '/article.txt', $content = [
+			'title' => 'Test'
+		]);
+
+		$updatedPage = $page->update([
+			'title' => 'Updated title'
+		]);
+
+		// check if the old version is still the same
+		$this->assertSame('Test', $page->title()->value());
+
+		$this->assertSame('Updated title', $updatedPage->title()->value());
+		$this->assertSame('Updated title', Data::read($updatedPage->root() . '/article.txt')['title']);
+	}
+}
