022f1deed1eb6b15a8081d02e514b6220b6d652a
Bastian Allgeier <mail@bastianallgeier.com>
2024-06-12 15:12:27 +0200

    More unit tests and fixes

--- tests/Content/TranslationsTest.php
+++ tests/Content/TranslationsTest.php
@@ -32,17 +32,35 @@ class TranslationsTest extends TestCase
 		);
 
 		$this->assertCount(2, $translations);
+		$this->assertSame('en', $translations->first()->code());
+		$this->assertSame('de', $translations->last()->code());
 	}
 
 	public function testFindByKeyMultiLanguage(): void
 	{
 		$this->setUpMultiLanguage();
 
+		$translations = Translations::load(
+			model: $this->model,
+			version: $this->model->version()
+		);
+
+		$this->assertSame('en', $translations->findByKey('en')->code());
+		$this->assertSame('de', $translations->findByKey('de')->code());
+		$this->assertNull($translations->findByKey('fr'));
 	}
 
 	public function testLoadMultiLanguage(): void
 	{
 		$this->setUpMultiLanguage();
 
+		$translations = Translations::load(
+			model: $this->model,
+			version: $this->model->version()
+		);
+
+		$this->assertCount(2, $translations);
+		$this->assertSame('en', $translations->first()->code());
+		$this->assertSame('de', $translations->last()->code());
 	}
 }
