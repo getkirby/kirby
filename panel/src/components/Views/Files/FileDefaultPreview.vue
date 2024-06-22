5c10d1dfe99602801bf010109ae5c53c7402d6c1
Nico Hoffmann <me@nhoffmann.com>
2024-06-22 21:40:49 +0200

    Separate file preview details & thumb components

--- panel/src/components/Views/Files/FileDefaultPreview.vue
+++ panel/src/components/Views/Files/FileDefaultPreview.vue
@@ -1,11 +1,15 @@
 <template>
-	<k-file-preview :details="details">
-		<k-icon
-			:color="$helper.color(image.color)"
-			:type="image.icon"
-			class="k-item-icon"
-		/>
-	</k-file-preview>
+	<div class="k-file-default-preview">
+		<k-file-preview-thumb>
+			<k-icon
+				:color="$helper.color(image.color)"
+				:type="image.icon"
+				class="k-item-icon"
+			/>
+		</k-file-preview-thumb>
+
+		<k-file-preview-details :details="details" />
+	</div>
 </template>
 
 <script>
@@ -28,7 +32,34 @@ export default {
 </script>
 
 <style>
-.k-file-preview-thumb > .k-icon {
+.k-file-default-preview {
+	display: grid;
+	align-items: stretch;
+}
+
+.k-file-default-preview .k-file-preview-thumb-column {
+	aspect-ratio: 1/1;
+}
+
+.k-file-default-preview .k-file-preview-thumb > .k-icon {
 	--icon-size: 3rem;
+}
+
+@container (min-width: 36rem) {
+	.k-file-default-preview {
+		grid-template-columns: 50% auto;
+	}
+	.k-file-default-preview-thumb-column {
+		aspect-ratio: auto;
+	}
+}
+
+@container (min-width: 65rem) {
+	.k-file-default-preview {
+		grid-template-columns: 33.333% auto;
+	}
+	.k-file-default-preview-thumb-column {
+		aspect-ratio: 1/1;
+	}
 }
 </style>
