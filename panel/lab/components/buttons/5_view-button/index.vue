9af90baede256f755af87c32ff46e7a0f2e94553
Nico Hoffmann <me@nhoffmann.com>
2024-07-22 10:26:27 +0200

    Fix Lab examples

--- panel/lab/components/buttons/5_view-button/index.vue
+++ panel/lab/components/buttons/5_view-button/index.vue
@@ -1,38 +1,21 @@
 <template>
 	<k-lab-examples>
 		<k-lab-example label="Simple button">
-			<k-view-button icon="info" text="A simple button" />
+			<k-view-button
+				icon="info"
+				text="A simple button"
+				link="https://getkirby.com"
+				target="_blank"
+			/>
 		</k-lab-example>
 		<k-lab-example label="Dropdown: Fiber route">
-			<k-view-button text="A dropdown" dropdown="a/route" />
+			<k-view-button text="A dropdown" options="pages/sections+files" />
 		</k-lab-example>
 		<k-lab-example label="Dropdown: Options array">
 			<k-view-button
 				icon="cog"
-				:dropdown="[{ text: 'Option A' }, { text: 'Option B' }]"
+				:options="[{ text: 'Option A' }, { text: 'Option B' }]"
 			/>
-		</k-lab-example>
-		<k-lab-example label="Languages button">
-			<k-view-languages-button
-				:options="[{ text: 'English' }, '-', { text: 'Deutsch' }]"
-				text="EN"
-			/>
-		</k-lab-example>
-		<k-lab-example label="Preview button">
-			<k-view-preview-button link="https://getkirby.com" />
-		</k-lab-example>
-		<k-lab-example label="Settings button">
-			<k-view-settings-button dropdown="pages/sections+info" />
-		</k-lab-example>
-		<k-lab-example label="Status button">
-			<k-view-status-button
-				text="Draft"
-				icon="status-draft"
-				theme="negative-icon"
-			/>
-		</k-lab-example>
-		<k-lab-example label="Theme button">
-			<k-view-theme-button />
 		</k-lab-example>
 	</k-lab-examples>
 </template>
