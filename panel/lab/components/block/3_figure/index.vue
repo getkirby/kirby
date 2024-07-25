6a94f92c222626a3e25df14a9666d586f1c48062
Nico Hoffmann <me@nhoffmann.com>
2024-07-25 15:30:50 +0200

    Background selector for Image & Gallery blocks

--- panel/lab/components/block/3_figure/index.vue
+++ panel/lab/components/block/3_figure/index.vue
@@ -33,6 +33,17 @@
 				/>
 			</k-block-figure>
 		</k-lab-example>
+		<k-lab-example label="Back">
+			<k-block-figure
+				caption="This is a nice caption"
+				back="var(--pattern-light)"
+			>
+				<k-image-frame
+					ratio="16/9"
+					src="https://fakeimg.pl/1200x600/ff0000,0/333333,255/?text=Pattern"
+				/>
+			</k-block-figure>
+		</k-lab-example>
 		<k-lab-example label="Video">
 			<k-block-figure caption="This is a nice caption">
 				<k-frame :cover="true" ratio="16/9">
