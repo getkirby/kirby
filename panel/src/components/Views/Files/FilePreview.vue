d24fc0056f7e09d6191f61ba86c6024ddea845cc
Nico Hoffmann <me@nhoffmann.com>
2024-07-25 19:23:55 +0200

    Refactor Vue components for file preview

--- panel/src/components/Views/Files/FilePreview.vue
+++ panel/src/components/Views/Files/FilePreview.vue
@@ -1,134 +1,24 @@
 <template>
-	<div class="k-file-preview" :data-has-focus="Boolean(focus)">
-		<!-- Thumb -->
-		<div class="k-file-preview-thumb-column">
-			<div class="k-file-preview-thumb">
-				<!-- Image with focus picker -->
-				<template v-if="image.src">
-					<k-coords-input
-						:disabled="!focusable"
-						:value="focus"
-						@input="setFocus($event)"
-					>
-						<img v-bind="image" @dragstart.prevent />
-					</k-coords-input>
-
-					<k-button
-						icon="dots"
-						size="xs"
-						style="color: var(--color-gray-500)"
-						@click="$refs.dropdown.toggle()"
-					/>
-					<k-dropdown-content ref="dropdown" :options="options" theme="light" />
-				</template>
-
-				<!-- Icon -->
-				<k-icon
-					v-else
-					:color="$helper.color(image.color)"
-					:type="image.icon"
-					class="k-item-icon"
-				/>
-			</div>
-		</div>
-
-		<!-- Details -->
-		<div class="k-file-preview-details">
-			<dl>
-				<div v-for="detail in details" :key="detail.title">
-					<dt>{{ detail.title }}</dt>
-					<dd>
-						<k-link
-							v-if="detail.link"
-							:to="detail.link"
-							tabindex="-1"
-							target="_blank"
-						>
-							/{{ detail.text }}
-						</k-link>
-						<template v-else>
-							{{ detail.text }}
-						</template>
-					</dd>
-				</div>
-
-				<div v-if="image.src" class="k-file-preview-focus-info">
-					<dt>{{ $t("file.focus.title") }}</dt>
-					<dd>
-						<k-file-focus-button
-							v-if="focusable"
-							ref="focus"
-							:focus="focus"
-							@set="setFocus"
-						/>
-						<template v-else-if="focus">
-							{{ focus.x }}% {{ focus.y }}%
-						</template>
-						<template v-else>–</template>
-					</dd>
-				</div>
-			</dl>
-		</div>
-	</div>
+	<component :is="preview" v-bind="props" class="k-file-preview" />
 </template>
 
 <script>
+/**
+ * Wrapper for file view previews
+ * @since 5.0.0
+ */
 export default {
 	props: {
-		details: {
-			default: () => [],
-			type: Array
-		},
-		focusable: Boolean,
-		image: {
-			default: () => ({}),
-			type: Object
-		},
-		url: String
+		component: String,
+		props: Object
 	},
-	emits: ["focus"],
 	computed: {
-		focus() {
-			const focus = this.$panel.content.values["focus"];
-
-			if (!focus) {
-				return;
+		preview() {
+			if (this.$helper.isComponent(this.component)) {
+				return this.component;
 			}
 
-			const [x, y] = focus.replaceAll("%", "").split(" ");
-
-			return { x: parseFloat(x), y: parseFloat(y) };
-		},
-		options() {
-			return [
-				{
-					icon: "open",
-					text: this.$t("open"),
-					link: this.url,
-					target: "_blank"
-				},
-				{
-					icon: "cancel",
-					text: this.$t("file.focus.reset"),
-					click: () => this.$refs.focus.reset(),
-					when: this.focusable && this.focus
-				},
-				{
-					icon: "preview",
-					text: this.$t("file.focus.placeholder"),
-					click: () => this.$refs.focus.set(),
-					when: this.focusable && !this.focus
-				}
-			];
-		}
-	},
-	methods: {
-		setFocus(focus) {
-			if (this.$helper.object.isObject(focus) === true) {
-				focus = `${focus.x.toFixed(1)}% ${focus.y.toFixed(1)}%`;
-			}
-
-			this.$panel.content.set({ focus });
+			return "k-file-default-preview";
 		}
 	}
 };
@@ -151,123 +41,5 @@ export default {
 	border-radius: var(--rounded-lg);
 	margin-bottom: var(--spacing-12);
 	overflow: hidden;
-}
-
-/* Thumb */
-.k-file-preview-thumb-column {
-	background: var(--pattern);
-	aspect-ratio: 1/1;
-}
-.k-file-preview-thumb {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	height: 100%;
-	padding: var(--spacing-12);
-	container-type: size;
-}
-
-.k-file-preview-thumb img {
-	width: auto;
-	max-width: 100cqw;
-	max-height: 100cqh;
-}
-.k-file-preview-thumb > .k-icon {
-	--icon-size: 3rem;
-}
-.k-file-preview-thumb > .k-button {
-	position: absolute;
-	top: var(--spacing-2);
-	inset-inline-start: var(--spacing-2);
-}
-.k-file-preview .k-coords-input {
-	--opacity-disabled: 1;
-	--range-thumb-color: hsl(216 60% 60% / 0.75);
-	--range-thumb-size: 1.25rem;
-	--range-thumb-shadow: none;
-	cursor: crosshair;
-}
-.k-file-preview .k-coords-input-thumb::after {
-	--size: 0.4rem;
-	--pos: calc(50% - (var(--size) / 2));
-
-	position: absolute;
-	top: var(--pos);
-	inset-inline-start: var(--pos);
-	width: var(--size);
-	height: var(--size);
-	content: "";
-	background: white;
-	border-radius: 50%;
-}
-.k-file-preview:not([data-has-focus="true"]) .k-coords-input-thumb {
-	display: none;
-}
-
-/* Details */
-.k-file-preview-details {
-	display: grid;
-}
-.k-file-preview-details dl {
-	display: grid;
-	grid-template-columns: repeat(auto-fill, minmax(14rem, 1fr));
-	grid-gap: var(--spacing-6) var(--spacing-12);
-	align-self: center;
-	padding: var(--spacing-6);
-	line-height: 1.5em;
-	padding: var(--spacing-6);
-}
-.k-file-preview-details dt {
-	font-size: var(--text-sm);
-	font-weight: 500;
-	font-weight: var(--font-semi);
-	color: var(--color-gray-500);
-	margin-bottom: var(--spacing-1);
-}
-.k-file-preview-details :where(dd, a) {
-	font-size: var(--text-xs);
-	white-space: nowrap;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	color: var(--file-preview-text);
-	font-size: var(--text-sm);
-}
-.k-file-preview-focus-info dd {
-	display: flex;
-	align-items: center;
-}
-
-.k-file-preview-focus-info .k-button {
-	--button-color-back: var(--color-gray-800);
-	--button-padding: var(--spacing-2);
-}
-.k-file-preview[data-has-focus="true"] .k-file-preview-focus-info .k-button {
-	flex-direction: row-reverse;
-}
-.k-panel[data-theme="dark"] .k-file-preview-focus-info .k-button {
-	--button-color-back: var(--color-gray-400);
-}
-
-@container (min-width: 36rem) {
-	.k-file-preview {
-		grid-template-columns: 50% auto;
-	}
-	.k-file-preview-thumb-column {
-		aspect-ratio: auto;
-	}
-}
-
-@container (min-width: 65rem) {
-	.k-file-preview {
-		grid-template-columns: 33.333% auto;
-	}
-	.k-file-preview-thumb-column {
-		aspect-ratio: 1/1;
-	}
-}
-@container (min-width: 90rem) {
-	.k-file-preview-layout {
-		grid-template-columns: 25% auto;
-	}
 }
 </style>
---
+++ panel/src/components/Views/Files/FilePreviewThumb.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="k-file-preview-thumb-column">
+		<div class="k-file-preview-thumb">
+			<slot />
+
+			<template v-if="options.length">
+				<k-button
+					icon="dots"
+					size="xs"
+					style="color: var(--color-gray-500)"
+					@click="$refs.dropdown.toggle()"
+				/>
+				<k-dropdown-content
+					ref="dropdown"
+					:options="options"
+					theme="light"
+					@action="$emit('action', $event)"
+				/>
+			</template>
+		</div>
+	</div>
+</template>
+
+<script>
+/**
+ * @since 5.0.0
+ */
+export default {
+	props: {
+		options: {
+			default: () => [],
+			type: Array
+		}
+	}
+};
+</script>
+
+<style>
+.k-file-preview-thumb-column {
+	aspect-ratio: 1/1;
+	background: var(--pattern);
+}
+.k-file-preview-thumb {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100%;
+	padding: var(--spacing-10);
+	container-type: size;
+}
+.k-file-preview-thumb :where(img, audio, video) {
+	width: auto;
+	max-width: 100cqw;
+	max-height: 100cqh;
+}
+.k-file-preview-thumb > .k-button {
+	position: absolute;
+	top: var(--spacing-2);
+	inset-inline-start: var(--spacing-2);
+}
+</style>
