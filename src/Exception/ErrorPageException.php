6d74997928dd74341e5d5782eed92cc0301711ab
Nico Hoffmann <me@nhoffmann.com>
2024-08-15 16:09:41 +0200

    Exceptions: support named arguments

--- src/Exception/ErrorPageException.php
+++ src/Exception/ErrorPageException.php
@@ -3,15 +3,14 @@
 namespace Kirby\Exception;
 
 /**
- * ErrorPageException
  * Thrown to trigger the CMS error page
- * @since 3.3.0
  *
  * @package   Kirby Exception
  * @author    Lukas Bestle <lukas@getkirby.com>
  * @link      https://getkirby.com
  * @copyright Bastian Allgeier
  * @license   https://opensource.org/licenses/MIT
+ * @since 3.3.0
  */
 class ErrorPageException extends Exception
 {
