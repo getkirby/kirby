2b73a54dfd40d9c897428c755ccd9a75b0561399
Nico Hoffmann <me@nhoffmann.com>
2024-08-18 19:22:16 +0200

    Modernize `Email\Body` class

--- src/Email/Email.php
+++ src/Email/Email.php
@@ -62,7 +62,7 @@ class Email
 		$this->attachments = Attachment::factory($props['attachments'] ?? []);
 		$this->bcc         = Address::factory($props['bcc'] ?? [], multiple: true);
 		$this->beforeSend  = $props['beforeSend'] ?? null;
-		$this->body        = new Body($props['body']);
+		$this->body        = Body::factory($props['body']);
 		$this->cc          = Address::factory($props['cc'] ?? [], multiple: true);
 		$this->from        = Address::factory([$props['from'] => $props['fromName'] ?? null]);
 		$this->subject     = $props['subject'];
@@ -224,7 +224,10 @@ class Email
 		return [
 			'attachments'   => $this->attachments(),
 			'bcc'			=> $this->bcc(),
-			'body'			=> $this->body()->toArray(),
+			'body'			=> [
+				'html' => $this->body()->html(),
+				'text' => $this->body()->text()
+			],
 			'cc'   			=> $this->cc(),
 			'from'			=> $this->from(),
 			'fromName'   	=> $this->fromName(),
--- tests/Email/BodyTest.php
+++ tests/Email/BodyTest.php
@@ -2,40 +2,161 @@
 
 namespace Kirby\Email;
 
+use Kirby\Cms\App;
+use Kirby\Cms\User;
+use Kirby\Exception\InvalidArgumentException;
+use Kirby\Exception\NotFoundException;
+use Kirby\TestCase;
+
 /**
  * @coversDefaultClass \Kirby\Email\Body
+ * @covers ::__construct
  */
 class BodyTest extends TestCase
 {
-	public function testConstruct()
-	{
-		$body = new Body();
+	public const FIXTURES = __DIR__ . '/fixtures';
 
+	/**
+	 * @covers ::factory
+	 */
+	public function testFactory(): void
+	{
+		$body = Body::factory($text = 'test');
+		$this->assertSame($text, $body->text());
 		$this->assertSame('', $body->html());
-		$this->assertSame('', $body->text());
+
+		$body = Body::factory([
+			'text' => $text,
+			'html' => $html = '<b>test</b>'
+		]);
+		$this->assertSame($text, $body->text());
+		$this->assertSame($html, $body->html());
 	}
 
-	public function testConstructParams()
+	/**
+	 * @covers ::factory
+	 */
+	public function testFactoryTemplate(): void
 	{
-		$data = [
-			'html' => '<strong>We will never reply</strong>',
-			'text' => 'We will never reply'
-		];
-
-		$body = new Body($data);
-
-		$this->assertSame($data['html'], $body->html());
-		$this->assertSame($data['text'], $body->text());
-	}
-
-	public function testConstructNullParams()
-	{
-		$body = new Body([
-			'html' => null,
-			'text' => null
+		new App([
+			'templates' => [
+				'emails/contact' => static::FIXTURES . '/contact.php'
+			]
 		]);
 
-		$this->assertSame('', $body->html());
+		$body = Body::factory(null, 'contact', ['name' => 'Alex']);
+		$this->assertSame('Cheers, Alex!', $body->text());
+	}
+
+	/**
+	 * @covers ::factory
+	 */
+	public function testFactoryTemplateHtml(): void
+	{
+		new App([
+			'templates' => [
+				'emails/media.html' => static::FIXTURES . '/media.html.php'
+			]
+		]);
+
+		$body = Body::factory(null, 'media');
+		$this->assertSame('<b>Image:</b> <img src=""/>', $body->html());
 		$this->assertSame('', $body->text());
+	}
+
+	/**
+	 * @covers ::factory
+	 */
+	public function testFactoryTemplateHtmlText(): void
+	{
+		new App([
+			'templates' => [
+				'emails/media.html' => static::FIXTURES . '/media.html.php',
+				'emails/media.text' => static::FIXTURES . '/media.text.php',
+			]
+		]);
+
+		$body = Body::factory(null, 'media');
+		$this->assertSame('<b>Image:</b> <img src=""/>', $body->html());
+		$this->assertSame('Image: Description', $body->text());
+	}
+
+	/**
+	 * @covers ::factory
+	 */
+	public function testFactoryTemplateData(): void
+	{
+		new App([
+			'roots' => [
+				'index' => '/dev/null'
+			],
+			'templates' => [
+				'emails/user-info' => static::FIXTURES . '/user-info.php'
+			]
+		]);
+
+		$user = new User([
+			'email' => 'ceo@company.com',
+			'name'  => 'Mario'
+		]);
+
+		$body = Body::factory(null, 'user-info', ['user' => $user]);
+		$this->assertSame('Welcome, Mario!', trim($body->text()));
+	}
+
+	/**
+	 * @covers ::factory
+	 */
+	public function testFactoryTemplateInvalid(): void
+	{
+		$this->expectException(NotFoundException::class);
+		$this->expectExceptionMessage('The email template "subscription" cannot be found');
+		Body::factory(null, 'subscription');
+	}
+
+	/**
+	 * @covers ::factory
+	 */
+	public function testFactoryInvalid(): void
+	{
+		$this->expectException(InvalidArgumentException::class);
+		$this->expectExceptionMessage('Email requires either body or template');
+		Body::factory();
+	}
+
+	/**
+	 * @covers ::html
+	 */
+	public function testHtml(): void
+	{
+		$body = new Body();
+		$this->assertSame('', $body->html());
+
+		$body = new Body(html: $html = '<strong>Foo</strong>');
+		$this->assertSame($html, $body->html());
+	}
+
+	/**
+	 * @covers ::isHtml
+	 */
+	public function testIsHtml(): void
+	{
+		$body = new Body();
+		$this->assertFalse($body->isHtml());
+
+		$body = new Body(html: '<strong>Foo</strong>');
+		$this->assertTrue($body->isHtml());
+	}
+
+	/**
+	 * @covers ::text
+	 */
+	public function testText(): void
+	{
+		$body = new Body();
+		$this->assertSame('', $body->text());
+
+		$body = new Body(text: $text = 'Foo');
+		$this->assertSame($text, $body->text());
 	}
 }
