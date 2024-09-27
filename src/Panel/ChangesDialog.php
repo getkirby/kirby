24381a73c4c5a93678c0dc0ed3b9e538bd6fac42
Bastian Allgeier <mail@bastianallgeier.com>
2024-09-27 11:08:23 +0200

    Add unit tests and code comments for the changes dialog

--- src/Panel/ChangesDialog.php
+++ src/Panel/ChangesDialog.php
@@ -5,6 +5,17 @@ namespace Kirby\Panel;
 use Kirby\Cms\Collection;
 use Kirby\Content\Changes;
 
+/**
+ * Manages the Panel dialog for content changes in
+ * pages, users and files
+ * @since 5.0.0
+ *
+ * @package   Kirby Panel
+ * @author    Bastian Allgeier <bastian@getkirby.com>
+ * @link      https://getkirby.com
+ * @copyright Bastian Allgeier
+ * @license   https://getkirby.com/license
+ */
 class ChangesDialog
 {
 	protected Changes $changes;
@@ -14,11 +25,17 @@ class ChangesDialog
 		$this->changes = new Changes();
 	}
 
+	/**
+	 * Returns the item props for all changed files
+	 */
 	public function files(): array
 	{
 		return $this->items($this->changes->files());
 	}
 
+	/**
+	 * Helper method to return item props for the given models
+	 */
 	public function items(Collection $models): array
 	{
 		return $models->values(
@@ -26,6 +43,9 @@ class ChangesDialog
 		);
 	}
 
+	/**
+	 * Returns the backend full definition for dialog
+	 */
 	public function load(): array
 	{
 		return [
@@ -38,11 +58,17 @@ class ChangesDialog
 		];
 	}
 
+	/**
+	 * Returns the item props for all changed pages
+	 */
 	public function pages(): array
 	{
 		return $this->items($this->changes->pages());
 	}
 
+	/**
+	 * Returns the item props for all changed users
+	 */
 	public function users(): array
 	{
 		return $this->items($this->changes->users());
---
+++ tests/Panel/ChangesDialogTest.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace Kirby\Panel;
+
+use Kirby\Cms\Page;
+use Kirby\Cms\Pages;
+use Kirby\Content\Changes;
+use Kirby\Panel\Areas\AreaTestCase;
+use Kirby\Uuid\Uuids;
+
+/**
+ * @coversDefaultClass \Kirby\Panel\ChangesDialog
+ */
+class ChangesDialogTest extends AreaTestCase
+{
+	protected Changes $changes;
+
+	public function setUp(): void
+	{
+		parent::setUp();
+		$this->install();
+		$this->login();
+
+		$this->changes = new Changes();
+	}
+
+	public function setUpModels(): void
+	{
+		$this->app = $this->app->clone([
+			'roots' => [
+				'index' => static::TMP
+			],
+			'site' => [
+				'children' => [
+					[
+						'slug' => 'test',
+						'content' => [
+							'uuid' => 'test'
+						],
+						'files' => [
+							[
+								'filename' => 'test.jpg',
+								'content'  => [
+									'uuid' => 'test'
+								],
+							]
+						]
+					]
+				]
+			],
+			'users' => [
+				[
+					'id' => 'test',
+				]
+			]
+		]);
+
+		$this->app->impersonate('kirby');
+
+		Uuids::populate();
+	}
+
+	/**
+	 * @covers ::files
+	 */
+	public function testFiles(): void
+	{
+		$this->setUpModels();
+
+		$this->changes->track(
+			$this->app->file('file://test')
+		);
+
+		$dialog = new ChangesDialog();
+		$files  = $dialog->files();
+
+		$this->assertCount(1, $files);
+		$this->assertSame('test.jpg', $files[0]['text']);
+		$this->assertSame('/pages/test/files/test.jpg', $files[0]['link']);
+	}
+
+	/**
+	 * @covers ::files
+	 */
+	public function testFilesWithoutChanges(): void
+	{
+		$dialog = new ChangesDialog();
+		$this->assertSame([], $dialog->files());
+	}
+
+	/**
+	 * @covers ::items
+	 */
+	public function testItems(): void
+	{
+		$dialog = new ChangesDialog();
+		$pages  = new Pages([
+			new Page(['slug' => 'test-a']),
+			new Page(['slug' => 'test-b'])
+		]);
+
+		$items = $dialog->items($pages);
+
+		$this->assertCount(2, $items);
+
+		$this->assertSame('test-a', $items[0]['text']);
+		$this->assertSame('/pages/test-a', $items[0]['link']);
+
+		$this->assertSame('test-b', $items[1]['text']);
+		$this->assertSame('/pages/test-b', $items[1]['link']);
+	}
+
+	/**
+	 * @covers ::load
+	 */
+	public function testLoad(): void
+	{
+		$dialog = new ChangesDialog();
+
+		$expected = [
+			'component' => 'k-changes-dialog',
+			'props' => [
+				'files' => [],
+				'pages' => [],
+				'users' => [],
+			]
+		];
+
+		$this->assertSame($expected, $dialog->load());
+	}
+
+	/**
+	 * @covers ::pages
+	 */
+	public function testPages(): void
+	{
+		$this->setUpModels();
+
+		$this->changes->track(
+			$this->app->page('page://test')
+		);
+
+		$dialog = new ChangesDialog();
+		$pages  = $dialog->pages();
+
+		$this->assertCount(1, $pages);
+		$this->assertSame('test', $pages[0]['text']);
+		$this->assertSame('/pages/test', $pages[0]['link']);
+	}
+
+	/**
+	 * @covers ::pages
+	 */
+	public function testPagesWithoutChanges(): void
+	{
+		$dialog = new ChangesDialog();
+		$this->assertSame([], $dialog->pages());
+	}
+
+	/**
+	 * @covers ::users
+	 */
+	public function testUsers(): void
+	{
+		$this->setUpModels();
+
+		$this->changes->track(
+			$this->app->user('user://test')
+		);
+
+		$dialog = new ChangesDialog();
+		$users  = $dialog->users();
+
+		$this->assertCount(1, $users);
+		$this->assertSame('test@getkirby.com', $users[0]['text']);
+		$this->assertSame('/users/test', $users[0]['link']);
+	}
+
+	/**
+	 * @covers ::users
+	 */
+	public function testUsersWithoutChanges(): void
+	{
+		$dialog = new ChangesDialog();
+		$this->assertSame([], $dialog->users());
+	}
+}
