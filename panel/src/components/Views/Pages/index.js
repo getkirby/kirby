15a6b3c845be567fb918ecda7d80b54821ab78c6
Bastian Allgeier <mail@bastianallgeier.com>
2024-10-09 16:32:07 +0200

    First draft for a new comparison view

--- panel/src/components/Views/Pages/index.js
+++ panel/src/components/Views/Pages/index.js
@@ -1,9 +1,11 @@
 import PageView from "./PageView.vue";
+import PageComparisonView from "./PageComparisonView.vue";
 import SiteView from "./SiteView.vue";
 
 export default {
 	install(app) {
 		app.component("k-page-view", PageView);
+		app.component("k-page-comparison-view", PageComparisonView);
 		app.component("k-site-view", SiteView);
 	}
 };
