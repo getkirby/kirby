3a79ce4f2e341849532e97916aa75eb8453f0a08
Bastian Allgeier <mail@bastianallgeier.com>
2024-10-22 12:31:19 +0200

    Rename view to k-page-changes-view

---
+++ panel/src/components/Views/Pages/PageChangesView.vue
@@ -0,0 +1,165 @@
+<template>
+	<k-panel class="k-panel-inside k-page-changes-view">
+		<header class="k-page-changes-header">
+			<k-button-group>
+				<k-button :link="link" icon="angle-left" size="sm" variant="filled" />
+				<k-button
+					:icon="modes[mode].icon"
+					size="sm"
+					variant="filled"
+					@click="$refs.layout.toggle()"
+				/>
+				<k-dropdown-content ref="layout" :options="dropdown" />
+			</k-button-group>
+
+			<k-form-controls
+				:editor="editor"
+				:is-locked="isLocked"
+				:is-unsaved="isUnsaved"
+				:modified="modified"
+				@discard="onDiscard"
+				@submit="onSubmit"
+			/>
+		</header>
+		<main class="k-page-changes-grid" :data-mode="mode">
+			<section v-if="mode === 'latest' || mode === 'compare'">
+				<k-headline>
+					{{ modes.latest.label }}
+					<k-button
+						:link="src.latest"
+						icon="open"
+						size="xs"
+						target="_blank"
+						variant="filled"
+					/>
+				</k-headline>
+				<iframe :src="src.latest"></iframe>
+			</section>
+			<section v-if="mode === 'changes' || mode === 'compare'">
+				<k-headline>
+					{{ modes.changes.label }}
+					<k-button
+						v-if="isUnsaved"
+						:link="src.changes"
+						icon="open"
+						size="xs"
+						target="_blank"
+						variant="filled"
+					/>
+				</k-headline>
+				<iframe v-if="isUnsaved" :src="src.changes"></iframe>
+				<k-empty v-else>There are no unsaved changes</k-empty>
+			</section>
+		</main>
+	</k-panel>
+</template>
+
+<script>
+import PageView from "@/components/Views/Pages/PageView.vue";
+
+export default {
+	extends: PageView,
+	props: {
+		src: Object
+	},
+	data() {
+		return {
+			mode: "compare"
+		};
+	},
+	computed: {
+		modes() {
+			return {
+				latest: {
+					label: "Latest version",
+					icon: "layout-right",
+					current: this.mode === "latest",
+					click: () => (this.mode = "latest")
+				},
+				changes: {
+					label: "Changed version",
+					icon: "layout-left",
+					current: this.mode === "changes",
+					click: () => (this.mode = "changes")
+				},
+				compare: {
+					label: "Side by side",
+					icon: "layout-columns",
+					current: this.mode === "compare",
+					click: () => (this.mode = "compare")
+				}
+			};
+		},
+		dropdown() {
+			return [this.modes.latest, this.modes.changes, "-", this.modes.compare];
+		}
+	},
+	methods: {
+		async onDiscard() {
+			if (this.isLocked === true) {
+				return false;
+			}
+
+			await this.$panel.content.discard();
+			await this.$panel.view.open(this.link);
+		},
+		async onSubmit() {
+			if (this.isLocked === true) {
+				return false;
+			}
+
+			await this.$panel.content.publish();
+			await this.$panel.view.open(this.link);
+		}
+	}
+};
+</script>
+
+<style>
+.k-page-changes-view {
+	position: fixed;
+	inset: 0;
+	height: 100%;
+	display: grid;
+	grid-template-rows: auto 1fr;
+}
+.k-page-changes-header {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: var(--spacing-2);
+	border-bottom: 1px solid var(--color-border);
+}
+.k-page-changes-grid {
+	display: grid;
+	grid-template-columns: 100%;
+}
+.k-page-changes-grid[data-mode="compare"] {
+	grid-template-columns: 50% 50%;
+}
+
+.k-page-changes-grid > section {
+	display: flex;
+	flex-direction: column;
+	padding: var(--spacing-6);
+}
+.k-page-changes-grid > section:first-child {
+	border-right: 1px solid var(--color-border);
+}
+.k-page-changes-grid > section .k-headline {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	margin-bottom: var(--spacing-3);
+}
+.k-page-changes-grid iframe {
+	width: 100%;
+	flex-grow: 1;
+	border-radius: var(--rounded-lg);
+	box-shadow: var(--shadow-xl);
+}
+.k-page-changes-grid .k-empty {
+	flex-grow: 1;
+	justify-content: center;
+}
+</style>
--- src/Panel/Page.php
+++ src/Panel/Page.php
@@ -365,7 +365,7 @@ class Page extends Model
 			...$props,
 			...$this->prevNext(),
 			'blueprint'  => $this->model->intendedTemplate()->name(),
-			'changesUrl' => $this->url(true) . '/changes/compare',
+			'changesUrl' => $this->url(true) . '/changes',
 			'model'      => $model,
 			'title'      => $model['title'],
 		];
