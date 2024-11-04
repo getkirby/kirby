99662e30e2c8b72b9eec4ae22521835d06f625d3
Bastian Allgeier <mail@bastianallgeier.com>
2024-11-04 16:52:46 +0100

    Various UX changes

--- panel/src/components/Views/Pages/PageChangesView.vue
+++ panel/src/components/Views/Pages/PageChangesView.vue
@@ -5,59 +5,92 @@
 				<k-button
 					:link="link"
 					:responsive="true"
-					:text="$t('back')"
+					:title="$t('back')"
 					icon="angle-left"
 					size="sm"
 					variant="filled"
-				/>
+					@click="$refs.tree.toggle()"
+				>
+				</k-button>
+				<k-button icon="page" element="span">
+					{{ title }}
+				</k-button>
+			</k-button-group>
+			<k-button-group>
 				<k-button
 					:icon="modes[mode].icon"
 					:dropdown="true"
 					:responsive="true"
-					:text="$t('view')"
+					:title="modes[mode].label"
 					size="sm"
 					variant="filled"
-					@click="$refs.layout.toggle()"
+					@click="$refs.view.toggle()"
 				/>
-				<k-dropdown-content ref="layout" :options="dropdown" />
+				<k-dropdown-content ref="view" :options="dropdown" align-x="end" />
 			</k-button-group>
-
-			<k-form-controls
-				:editor="editor"
-				:is-locked="isLocked"
-				:is-unsaved="isUnsaved"
-				:modified="modified"
-				@discard="onDiscard"
-				@submit="onSubmit"
-			/>
 		</header>
 		<main class="k-page-changes-grid" :data-mode="mode">
-			<section v-if="mode === 'latest' || mode === 'compare'">
-				<k-headline>
-					{{ modes.latest.label }}
-					<k-button
-						:link="src.latest"
-						icon="open"
-						size="xs"
-						target="_blank"
-						variant="filled"
-					/>
-				</k-headline>
-				<iframe :src="src.latest"></iframe>
+			<section
+				v-if="mode === 'latest' || mode === 'compare'"
+				class="k-page-changes-panel"
+			>
+				<header>
+					<k-headline>{{ modes.latest.label }}</k-headline>
+					<k-button-group>
+						<k-button
+							size="sm"
+							variant="filled"
+							:icon="
+								mode === 'compare' ? 'expand-horizontal' : 'collapse-horizontal'
+							"
+							@click="changeMode(mode === 'compare' ? 'latest' : 'compare')"
+						/>
+						<k-button
+							:link="src.latest"
+							icon="open"
+							size="sm"
+							target="_blank"
+							variant="filled"
+						/>
+					</k-button-group>
+				</header>
+				<iframe ref="latestFrame" :src="src.latest"></iframe>
 			</section>
-			<section v-if="mode === 'changes' || mode === 'compare'">
-				<k-headline>
-					{{ modes.changes.label }}
-					<k-button
-						v-if="isUnsaved"
-						:link="src.changes"
-						icon="open"
-						size="xs"
-						target="_blank"
-						variant="filled"
-					/>
-				</k-headline>
-				<iframe v-if="isUnsaved" :src="src.changes"></iframe>
+
+			<section
+				v-if="mode === 'changes' || mode === 'compare'"
+				class="k-page-changes-panel"
+			>
+				<header>
+					<k-headline>{{ modes.changes.label }}</k-headline>
+					<k-button-group>
+						<k-button
+							size="sm"
+							variant="filled"
+							:icon="
+								mode === 'compare' ? 'expand-horizontal' : 'collapse-horizontal'
+							"
+							@click="changeMode(mode === 'compare' ? 'changes' : 'compare')"
+						/>
+						<k-button
+							:link="src.changes"
+							icon="open"
+							size="sm"
+							target="_blank"
+							variant="filled"
+						/>
+						<k-form-controls
+							:editor="editor"
+							:is-locked="isLocked"
+							:is-unsaved="isUnsaved"
+							:modified="modified"
+							size="sm"
+							@discard="onDiscard"
+							@submit="onSubmit"
+						/>
+					</k-button-group>
+				</header>
+				<iframe v-if="isUnsaved" ref="changesFrame" :src="src.changes"></iframe>
 				<k-empty v-else>{{ $t("lock.unsaved.empty") }}</k-empty>
 			</section>
 		</main>
@@ -80,23 +113,23 @@ export default {
 	computed: {
 		modes() {
 			return {
-				latest: {
-					label: this.$t("version.latest"),
-					icon: "layout-right",
-					current: this.mode === "latest",
-					click: () => (this.mode = "latest")
-				},
 				changes: {
 					label: this.$t("version.changes"),
 					icon: "layout-left",
 					current: this.mode === "changes",
-					click: () => (this.mode = "changes")
+					click: () => this.changeMode("changes")
 				},
 				compare: {
 					label: this.$t("version.compare"),
 					icon: "layout-columns",
 					current: this.mode === "compare",
-					click: () => (this.mode = "compare")
+					click: () => this.changeMode("compare")
+				},
+				latest: {
+					label: this.$t("version.latest"),
+					icon: "layout-right",
+					current: this.mode === "latest",
+					click: () => this.changeMode("latest")
 				}
 			};
 		},
@@ -104,14 +137,30 @@ export default {
 			return [this.modes.compare, "-", this.modes.latest, this.modes.changes];
 		}
 	},
+	watch: {
+		id: {
+			handler() {
+				this.changeMode(localStorage.getItem("kirby$preview$mode"));
+			},
+			immediate: true
+		}
+	},
 	methods: {
+		changeMode(mode) {
+			if (!mode || !this.modes[mode]) {
+				return;
+			}
+
+			this.mode = mode;
+			localStorage.setItem("kirby$preview$mode", mode);
+		},
 		async onDiscard() {
 			if (this.isLocked === true) {
 				return false;
 			}
 
 			await this.$panel.content.discard();
-			await this.$panel.view.open(this.link);
+			await this.$panel.view.reload();
 		},
 		async onSubmit() {
 			if (this.isLocked === true) {
@@ -119,7 +168,7 @@ export default {
 			}
 
 			await this.$panel.content.publish();
-			await this.$panel.view.open(this.link);
+			await this.$panel.reload();
 		}
 	}
 };
@@ -136,31 +185,39 @@ export default {
 .k-page-changes-header {
 	container-type: inline-size;
 	display: flex;
-	align-items: center;
+	gap: var(--spacing-2);
 	justify-content: space-between;
+	align-items: center;
 	padding: var(--spacing-2);
 	border-bottom: 1px solid var(--color-border);
 }
-.k-page-changes-grid {
-	display: grid;
-	grid-template-columns: 100%;
-}
 
-@media screen and (min-width: 50rem) {
-	.k-page-changes-grid[data-mode="compare"] {
-		grid-template-columns: 50% 50%;
+@media screen and (max-width: 40rem) {
+	.k-page-changes-header-controls > .k-button {
+		--button-text-display: none;
 	}
 }
 
-.k-page-changes-grid > section {
+.k-page-changes-grid {
+	display: flex;
+}
+@media screen and (max-width: 60rem) {
+	.k-page-changes-grid {
+		flex-direction: column;
+	}
+}
+.k-page-changes-grid .k-page-changes-panel + .k-page-changes-panel {
+	border-left: 1px solid var(--color-border);
+}
+.k-page-changes-panel {
+	flex-grow: 1;
 	display: flex;
 	flex-direction: column;
 	padding: var(--spacing-6);
+	background: var(--color-gray-200);
 }
-.k-page-changes-grid > section:first-child {
-	border-right: 1px solid var(--color-border);
-}
-.k-page-changes-grid > section .k-headline {
+.k-page-changes-panel header {
+	container-type: inline-size;
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
