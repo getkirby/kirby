ae94934e8271adb28f949c3bbd9ac811554fc3ea
Roman Steiner <roman@toastlab.ch>
2024-12-03 16:02:05 +0100

    code style

--- tests/Toolkit/Query/ParserTest.php
+++ tests/Toolkit/Query/ParserTest.php
@@ -1,34 +1,39 @@
 <?php
 
 namespace Kirby\Toolkit\Query;
+
 use Kirby\TestCase;
 use Kirby\Toolkit\Query\AST\ArgumentListNode;
 use Kirby\Toolkit\Query\AST\LiteralNode;
 use Kirby\Toolkit\Query\AST\MemberAccessNode;
 use Kirby\Toolkit\Query\AST\VariableNode;
-use Kirby\Toolkit\Query\Parser;
-use Kirby\Toolkit\Query\Tokenizer;
 
-class ParserTest extends TestCase {
-	function testMemberAccess() {
+class ParserTest extends TestCase
+{
+	public function testMemberAccess()
+	{
 		$tokenizer = new Tokenizer('user.name');
 		$ast = (new Parser($tokenizer))->parse();
-		$this->assertEquals($ast,
+		$this->assertEquals(
+			$ast,
 			new MemberAccessNode(
-				new VariableNode('user'), 'name'
+				new VariableNode('user'),
+				'name'
 			)
 		);
 	}
 
-	function testSquentialMemberAccess() {
+	public function testSquentialMemberAccess()
+	{
 		$tokenizer = new Tokenizer('user.name("arg").age');
 		$ast = (new Parser($tokenizer))->parse();
-		$this->assertEquals($ast,
+		$this->assertEquals(
+			$ast,
 			new MemberAccessNode(
 				new MemberAccessNode(
 					new VariableNode('user'),
 					'name',
-					new ArgumentListNode([new LiteralNode("arg")])
+					new ArgumentListNode([new LiteralNode('arg')])
 				),
 				'age'
 			)
