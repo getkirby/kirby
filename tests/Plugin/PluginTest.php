7e57f5690606f6fac9eab83ce26c35e74fb7d54c
Bastian Allgeier <mail@bastianallgeier.com>
2024-12-04 13:30:40 +0100

    Add unit tests

--- tests/Plugin/PluginTest.php
+++ tests/Plugin/PluginTest.php
@@ -253,7 +253,16 @@ class PluginTest extends TestCase
 	public function testLicense(): void
 	{
 		$plugin = new Plugin(name: 'getkirby/test-plugin');
+		$this->assertInstanceOf(License::class, $license = $plugin->license());
+		// check for cached instance
+		$this->assertSame($license, $plugin->license());
+	}
+
+	public function testLicenseFromString(): void
+	{
+		$plugin = new Plugin(name: 'getkirby/test-plugin', license: 'mit');
 		$this->assertInstanceOf(License::class, $plugin->license());
+		$this->assertSame('mit', $plugin->license()->name());
 	}
 
 	/**
