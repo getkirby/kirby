d5003c8e798fb4e65a1f8081e66897792812c7a6
Bastian Allgeier <mail@bastianallgeier.com>
2024-12-10 13:28:47 +0100

    Simplify code
    Co-authored-by: Nico Hoffmann  ෴. <me@nhoffmann.com>

--- panel/src/components/Sections/ModelsSection.vue
+++ panel/src/components/Sections/ModelsSection.vue
@@ -313,11 +313,11 @@ export default {
 			}
 		},
 		onSelectToggle() {
+			this.isSelecting = !this.isSelecting;
+			
+			// start new selection with a fresh list
 			if (this.isSelecting) {
 				this.selected = [];
-				this.isSelecting = false;
-			} else {
-				this.isSelecting = true;
 			}
 		},
 		onSort() {},
