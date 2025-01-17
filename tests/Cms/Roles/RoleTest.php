be3be83c2c0b0862ce4db85fcd9186c8d32a3c14
Nico Hoffmann <me@nhoffmann.com>
2025-01-17 23:02:50 +0100

    Roles: fix extending blueprint
    Fixes #6167

--- tests/Cms/Roles/RoleTest.php
+++ tests/Cms/Roles/RoleTest.php
@@ -35,6 +35,7 @@ class RoleTest extends TestCase
 
 		$this->assertSame('editor', $role->name());
 		$this->assertSame('Editor', $role->title());
+		$this->assertSame('This should be inherited', $role->description());
 	}
 
 	public function testMissingRole()
