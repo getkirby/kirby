84d5a555de698c26185da7cf37c42d301376f40d
Bastian Allgeier <mail@bastianallgeier.com>
2025-01-27 12:43:20 +0100

    Lab example with lots of text

--- panel/lab/components/drawers/form/index.vue
+++ panel/lab/components/drawers/form/index.vue
@@ -13,14 +13,14 @@
 							fields: {
 								name: {
 									label: 'Name',
-									type: 'text',
+									type: 'text'
 								},
 								email: {
 									label: 'Email',
-									type: 'email',
-								},
-							},
-						},
+									type: 'email'
+								}
+							}
+						}
 					})
 				"
 			>
@@ -45,9 +45,9 @@
 										text: {
 											label: 'Text',
 											type: 'textarea',
-											size: 'large',
-										},
-									},
+											size: 'large'
+										}
+									}
 								},
 								settings: {
 									name: 'settings',
@@ -55,16 +55,70 @@
 									fields: {
 										date: {
 											label: 'Date',
-											type: 'date',
+											type: 'date'
 										},
 										public: {
 											label: 'Public',
-											type: 'toggle',
-										},
-									},
+											type: 'toggle'
+										}
+									}
+								}
+							}
+						}
+					})
+				"
+			>
+				Open
+			</k-button>
+		</k-lab-example>
+		<k-lab-example label="lots of tabs">
+			<k-button
+				icon="open"
+				variant="filled"
+				@click="
+					$panel.drawer.open({
+						component: 'k-form-drawer',
+						props: {
+							icon: 'box',
+							title: 'Form',
+							tabs: {
+								content: {
+									name: 'content',
+									label: 'Content',
+									fields: {}
 								},
-							},
-						},
+								settings: {
+									name: 'settings',
+									label: 'Settings',
+									fields: {}
+								},
+								seo: {
+									name: 'seo',
+									label: 'SEO',
+									fields: {}
+								},
+								validation: {
+									name: 'validation',
+									label: 'Validation',
+									fields: {}
+								},
+								help: {
+									name: 'help',
+									label: 'Help',
+									fields: {}
+								},
+								theme: {
+									name: 'theme',
+									label: 'Theme',
+									fields: {}
+								},
+								misc: {
+									name: 'misc',
+									label: 'Misc',
+									fields: {}
+								}
+							}
+						}
 					})
 				"
 			>
