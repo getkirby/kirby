afd58bf0be4121188b033f20322e100b20cb5ad9
Bastian Allgeier <mail@bastianallgeier.com>
2025-02-03 14:02:23 +0100

    Add unit tests

---
+++ tests/Panel/Ui/MenuItemTest.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace Kirby\Panel\Ui;
+
+use Kirby\Exception\Exception;
+use Kirby\TestCase;
+use Kirby\Toolkit\I18n;
+
+/**
+ * @coversDefaultClass \Kirby\Panel\Ui\MenuItem
+ */
+class MenuItemTest extends TestCase
+{
+	/**
+	 * @covers ::__construct
+	 */
+	public function testConstruct()
+	{
+		$menuItem = new MenuItem(
+			icon: 'edit',
+			link: 'site',
+			text: 'Test',
+		);
+
+		$this->assertSame('edit', $menuItem->icon());
+		$this->assertSame('Test', $menuItem->text());
+		$this->assertFalse($menuItem->current());
+		$this->assertNull($menuItem->dialog());
+		$this->assertFalse($menuItem->disabled());
+		$this->assertNull($menuItem->drawer());
+		$this->assertSame('site', $menuItem->link());
+	}
+
+	public function testConstructWithMissingLink()
+	{
+		$this->expectException(Exception::class);
+		$this->expectExceptionMessage('You must define a dialog, drawer or link for the menu item');
+
+		new MenuItem(
+			icon: 'edit',
+			text: 'Test',
+		);
+	}
+
+	/**
+	 * @covers ::__call
+	 */
+	public function testCall()
+	{
+		$menuItem = new MenuItem(
+			icon: 'edit',
+			link: 'test',
+			text: 'Test',
+		);
+
+		$this->assertSame('edit', $menuItem->icon());
+		$this->assertSame('test', $menuItem->link());
+		$this->assertSame('Test', $menuItem->text());
+	}
+
+	/**
+	 * @covers ::link
+	 */
+	public function testLink()
+	{
+		$menuItem = new MenuItem(
+			icon: 'edit',
+			link: 'test',
+			text: 'Test',
+		);
+
+		$this->assertSame('test', $menuItem->link());
+	}
+
+	/**
+	 * @covers ::link
+	 */
+	public function testLinkWithDialog()
+	{
+		$menuItem = new MenuItem(
+			dialog: 'test',
+			icon: 'edit',
+			link: 'test',
+			text: 'Test',
+		);
+
+		$this->assertSame('test', $menuItem->dialog());
+		$this->assertNull($menuItem->link());
+	}
+
+	/**
+	 * @covers ::link
+	 */
+	public function testLinkWithDrawer()
+	{
+		$menuItem = new MenuItem(
+			drawer: 'test',
+			icon: 'edit',
+			link: 'test',
+			text: 'Test',
+		);
+
+		$this->assertSame('test', $menuItem->drawer());
+		$this->assertNull($menuItem->link());
+	}
+
+	/**
+	 * @covers ::merge
+	 */
+	public function testMerge()
+	{
+		$menuItem = new MenuItem(
+			icon: 'edit',
+			link: 'test',
+			text: 'Test',
+		);
+
+		$this->assertFalse($menuItem->disabled());
+
+		$menuItem->merge([
+			'disabled' => true
+		]);
+
+		$this->assertTrue($menuItem->disabled());
+	}
+
+	/**
+	 * @covers ::text
+	 */
+	public function testText()
+	{
+		$menuItem = new MenuItem(
+			icon: 'edit',
+			link: 'test',
+			text: 'Test',
+		);
+
+		$this->assertSame('Test', $menuItem->text());
+	}
+
+	/**
+	 * @covers ::text
+	 */
+	public function testTextWithTranslationKey()
+	{
+		I18n::$translations = [
+			'en' => [
+				'logout' => 'Logout'
+			]
+		];
+
+		$menuItem = new MenuItem(
+			icon: 'edit',
+			link: 'test',
+			text: 'logout',
+		);
+
+		$this->assertSame('Logout', $menuItem->text());
+	}
+
+	/**
+	 * @covers ::text
+	 */
+	public function testTextWithTranslationArray()
+	{
+		$menuItem = new MenuItem(
+			icon: 'edit',
+			link: 'test',
+			text: [
+				'en' => 'Logout',
+				'de' => 'Abmelden'
+			],
+		);
+
+		$this->assertSame('Logout', $menuItem->text());
+	}
+
+	/**
+	 * @covers ::toArray
+	 */
+	public function testToArray()
+	{
+		$menuItem = new MenuItem(
+			icon: 'edit',
+			link: 'site',
+			text: 'Test',
+		);
+
+		$expected = [
+			'icon' => 'edit',
+			'link' => 'site',
+			'text' => 'Test'
+		];
+
+		$this->assertSame($expected, $menuItem->toArray());
+	}
+}
