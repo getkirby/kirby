702220645a422998d62a17ada25c7ccddbf4942f
Bastian Allgeier <mail@bastianallgeier.com>
2025-02-18 09:35:21 +0100

    New slug tests

---
+++ tests/Cms/NewPage/NewPageSlugTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Kirby\Cms;
+
+use Kirby\Cms\NewPage as Page;
+use Kirby\Content\MemoryStorage;
+use Kirby\Data\Data;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(NewPage::class)]
+class NewPageSlugTest extends NewPageTestCase
+{
+	public const TMP = KIRBY_TMP_DIR . '/Cms.NewPageSlugTest';
+
+	public function testSlugInSingleLanguageMode()
+	{
+		$page = new Page(['slug' => 'test']);
+		$this->assertSame('test', $page->slug());
+	}
+
+	public function testSlugInMultiLanguageMode()
+	{
+		$this->setUpMultiLanguage();
+
+		$page = new Page([
+			'slug' => 'test', 
+			'translations' => [
+				[
+					'code' => 'de',
+					'slug' => 'test-de'
+				]
+			]
+		]);
+
+		$this->assertSame('test', $page->slug());
+		$this->assertSame('test', $page->slug('en'));
+		$this->assertSame('test-de', $page->slug('de'));
+	}
+
+	public function testSlugInMultiLanguageModeWithSlugFieldInDefaultTranslation()
+	{
+		$this->setUpMultiLanguage();
+
+		$page = new Page([
+			'slug' => 'test', 
+			'translations' => [
+				[
+					'code' => 'en',
+					'slug' => 'test-en'
+				]
+			]
+		]);
+
+		// In our current logic, a slug field in the default translation is not respected.
+		// We might want to change this in the future.
+		$this->assertSame('test', $page->slug());
+		$this->assertSame('test', $page->slug('en'));
+	}
+}
