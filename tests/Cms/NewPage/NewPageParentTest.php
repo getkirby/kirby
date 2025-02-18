65b9b3fdbed76a93d97e7db35d92fb0e87481f9e
Bastian Allgeier <mail@bastianallgeier.com>
2025-02-18 11:09:58 +0100

    New page parent tests

---
+++ tests/Cms/NewPage/NewPageParentTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Kirby\Cms;
+
+use Kirby\Cms\NewPage as Page;
+use PHPUnit\Framework\Attributes\CoversClass;
+use TypeError;
+
+#[CoversClass(Page::class)]
+class NewPageParentTest extends NewPageTestCase
+{
+	public const TMP = KIRBY_TMP_DIR . '/Cms.NewPageParentTest';
+
+	public function testParent()
+	{
+		$mother = new Page([
+			'slug' => 'mother',
+		]);
+
+		$child = new Page([
+			'slug'   => 'child',
+			'parent' => $mother
+		]);
+
+		$this->assertNull($mother->parent());
+		$this->assertSame($mother, $child->parent());
+	}
+
+	public function testParentId()
+	{
+		$mother = new Page([
+			'slug' => 'mother',
+		]);
+
+		$child = new Page([
+			'slug'   => 'child',
+			'parent' => $mother
+		]);
+
+		$this->assertNull($mother->parentId());
+		$this->assertSame('mother', $child->parentId());
+	}
+
+	public function testParentPrevNext()
+	{
+		$root = new Page([
+			'slug' => 'root',
+			'children' => [
+				[
+					'slug' => 'projects',
+					'children' => [
+						[
+							'slug' => 'project-a',
+						],
+						[
+							'slug' => 'project-b',
+						]
+					]
+				],
+				[
+					'slug' => 'blog'
+				]
+			]
+		]);
+
+		$child = $root->find('projects/project-a');
+		$blog  = $root->find('blog');
+
+		$this->assertSame($blog, $child->parent()->next());
+		$this->assertNull($child->parent()->prev());
+	}
+
+	public function testParentWithInvalidValue()
+	{
+		$this->expectException(TypeError::class);
+
+		new Page([
+			'slug'   => 'test',
+			'parent' => 'some parent'
+		]);
+	}
+}
