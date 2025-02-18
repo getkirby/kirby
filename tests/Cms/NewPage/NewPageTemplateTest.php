c74e95dbe85485b2bc4e67dd2ad227adffe1bf1a
Bastian Allgeier <mail@bastianallgeier.com>
2025-02-18 14:11:46 +0100

    Improve template tests

--- tests/Cms/NewPage/NewPageTemplateTest.php
+++ tests/Cms/NewPage/NewPageTemplateTest.php
@@ -3,6 +3,7 @@
 namespace Kirby\Cms;
 
 use Kirby\Cms\NewPage as Page;
+use Kirby\Template\Template;
 use PHPUnit\Framework\Attributes\CoversClass;
 use TypeError;
 
@@ -18,10 +19,21 @@ class NewPageTemplateTest extends NewPageTestCase
 			'template' => 'test'
 		]);
 
+		$this->assertInstanceOf(Template::class, $page->intendedTemplate());
 		$this->assertSame('test', $page->intendedTemplate()->name());
 	}
 
-	public function testInvalidTemplate()
+	public function testIntendedTemplateWithoutValue()
+	{
+		$page = new Page([
+			'slug' => 'test',
+		]);
+
+		$this->assertInstanceOf(Template::class, $page->intendedTemplate());
+		$this->assertSame('default', $page->intendedTemplate()->name());
+	}
+
+	public function testInvalidTemplateValue()
 	{
 		$this->expectException(TypeError::class);
 
@@ -31,7 +43,36 @@ class NewPageTemplateTest extends NewPageTestCase
 		]);
 	}
 
-	public function testTemplate()
+	public function testTemplateWithExistingTemplate()
+	{
+		$this->app = $this->app->clone([
+			'templates' => [
+				// the file only needs to exist, it doesn't need to be a valid template
+				'test' => __FILE__
+			]
+		]);
+
+		$page = new Page([
+			'slug'     => 'test',
+			'template' => 'test'
+		]);
+
+		$this->assertInstanceOf(Template::class, $page->template());
+		$this->assertSame('test', $page->template()->name());
+	}
+
+	public function testTemplateWithNonExistingTemplate()
+	{
+		$page = new Page([
+			'slug'     => 'test',
+			'template' => 'test'
+		]);
+
+		$this->assertInstanceOf(Template::class, $page->template());
+		$this->assertSame('default', $page->template()->name());
+	}
+
+	public function testTemplateWithoutValue()
 	{
 		$page = new Page([
 			'slug' => 'test',
