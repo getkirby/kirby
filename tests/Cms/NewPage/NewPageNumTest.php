929c5d902e3b6774be23a33338d7bcb04bb49310
Bastian Allgeier <mail@bastianallgeier.com>
2025-02-18 11:05:16 +0100

    New page num tests

---
+++ tests/Cms/NewPage/NewPageNumTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Kirby\Cms;
+
+use Kirby\Cms\NewPage as Page;
+use PHPUnit\Framework\Attributes\CoversClass;
+use TypeError;
+
+#[CoversClass(Page::class)]
+class NewPageNumTest extends NewPageTestCase
+{
+	public const TMP = KIRBY_TMP_DIR . '/Cms.NewPageNumTest';
+
+	public function testNum()
+	{
+		$page = new Page([
+			'slug' => 'test',
+			'num'  => 1,
+		]);
+
+		$this->assertSame(1, $page->num());
+	}
+
+	public function testNumWithInvalidValue()
+	{
+		$this->expectException(TypeError::class);
+
+		new Page([
+			'slug' => 'test',
+			'num'  => []
+		]);
+	}
+
+	public function testNumWithEmptyValue()
+	{
+		$page = new Page([
+			'slug' => 'test',
+		]);
+
+		$this->assertNull($page->num());
+	}
+}
