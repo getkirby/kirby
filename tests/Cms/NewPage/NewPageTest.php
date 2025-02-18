af0434c8265882173fda507da853946b2e9ab6c2
Bastian Allgeier <mail@bastianallgeier.com>
2025-02-18 10:36:11 +0100

    Remove redundant test

--- tests/Cms/NewPage/NewPageTest.php
+++ tests/Cms/NewPage/NewPageTest.php
@@ -11,41 +11,4 @@ use PHPUnit\Framework\Attributes\CoversClass;
 class NewPageTest extends NewPageTestCase
 {
 	public const TMP = KIRBY_TMP_DIR . '/Cms.NewPageTest';
-
-	public function testMoveToStorage(): void
-	{
-		$pageA = new Page([
-			'slug' => 'test',
-		]);
-
-		$contentAOnDisk = null;
-
-		$pageB = $pageA->update($contentB = [
-			'title'    => 'Title 1',
-			'subtitle' => 'Subtitle 1'
-		]);
-
-		$contentBOnDisk = Data::read(static::TMP . '/content/test/default.txt');
-
-		$this->assertSame($contentAOnDisk, $pageA->version()->read());
-		$this->assertSame($contentBOnDisk, $pageB->version()->read());
-
-		$pageB->moveToStorage(new MemoryStorage($pageB));
-
-		$this->assertSame($contentBOnDisk, $pageB->version()->read());
-		$this->assertInstanceOf(MemoryStorage::class, $pageB->storage());
-		$this->assertInstanceOf(MemoryStorage::class, $pageB->version()->model()->storage());
-
-		$pageC = $pageB->update($contentC = [
-			'title'    => 'Title 2',
-			'subtitle' => 'Subtitle 2'
-		]);
-
-		$this->assertSame($contentB, $pageB->content()->toArray());
-		$this->assertSame($contentC, $pageC->content()->toArray());
-
-		$contentCOnDisk = Data::read(static::TMP . '/content/test/default.txt');
-
-		$this->assertSame($contentBOnDisk, $contentCOnDisk);
-	}
 }
