0bb2624ea836771ebdb3771815884980eb7e96d3
Bastian Allgeier <mail@bastianallgeier.com>
2025-02-18 20:38:59 +0100

    New unpublish tests

---
+++ tests/Cms/NewPage/NewPageUnpublishTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Kirby\Cms;
+
+use Kirby\Cms\NewPage as Page;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(Page::class)]
+class NewPageUnpublishTest extends NewPageTestCase
+{
+	public const TMP = KIRBY_TMP_DIR . '/Cms.NewPageUnpublishTest';
+
+	public function testUnpublish()
+	{
+		$page = Page::create([
+			'slug' => 'test',
+			'draft' => false
+		]);
+
+		Page::create([
+			'slug' => 'child-a',
+			'draft' => false,
+			'num' => 1,
+			'parent' => $page
+		]);
+
+		Page::create([
+			'slug' => 'child-b',
+			'draft' => false,
+			'num' => 2,
+			'parent' => $page
+		]);
+
+		Page::create([
+			'slug' => 'child-c',
+			'draft' => false,
+			'parent' => $page
+		]);
+
+		Page::create([
+			'slug' => 'child-d',
+			'draft' => true,
+			'parent' => $page
+		]);
+
+		$listed = $page->children()->listed();
+		$unlisted = $page->children()->unlisted();
+		$drafts = $page->drafts();
+
+		$this->assertCount(2, $listed);
+		foreach ($listed as $child) {
+			$this->assertSame('listed', $child->status());
+		}
+
+		$this->assertCount(1, $unlisted);
+		foreach ($unlisted as $child) {
+			$this->assertSame('unlisted', $child->status());
+		}
+
+		$this->assertCount(1, $drafts);
+		foreach ($drafts as $child) {
+			$this->assertSame('draft', $child->status());
+		}
+
+		// unpublish all
+		foreach ($page->children() as $child) {
+			$child->unpublish();
+		}
+
+		// make sure that not cached children
+		$clone = $page->clone();
+
+		$this->assertCount(0, $clone->children()->listed());
+		$this->assertCount(0, $clone->children()->unlisted());
+		$this->assertCount(4, $clone->drafts());
+	}
+
+}
