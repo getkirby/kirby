22bb45285ebf627b9636c65d8bb9f23b4d2eb15b
Bastian Allgeier <mail@bastianallgeier.com>
2025-02-18 14:22:49 +0100

    New page representation tests

---
+++ tests/Cms/NewPage/NewPageRepresentationTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Kirby\Cms;
+
+use Kirby\Cms\NewPage as Page;
+use Kirby\Exception\NotFoundException;
+use Kirby\Template\Template;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(Page::class)]
+class NewPageRepresentationTest extends NewPageTestCase
+{
+	public const TMP = KIRBY_TMP_DIR . '/Cms.NewPageRepresentationTest';
+
+	public function testRepresentationWithExistingTemplate()
+	{
+		$this->app = $this->app->clone([
+			'templates' => [
+				'test'      => __FILE__,
+				'test.json' => __FILE__
+			]
+		]);
+
+		$page = new Page([
+			'slug'     => 'test',
+			'template' => 'test'
+		]);
+
+		$representation = $page->representation('json');
+		$this->assertInstanceOf(Template::class, $representation);
+		$this->assertSame('test', $representation->name());
+		$this->assertSame('json', $representation->type());
+	}
+
+	public function testRepresentationWithFallbackToDefaultRepresentation()
+	{
+		$this->app = $this->app->clone([
+			'templates' => [
+				'default'      => __FILE__,
+				'default.json' => __FILE__
+			]
+		]);
+
+		$page = new Page([
+			'slug'     => 'test',
+			'template' => 'test'
+		]);
+
+		$representation = $page->representation('json');
+		$this->assertInstanceOf(Template::class, $representation);
+		$this->assertSame('default', $representation->name());
+		$this->assertSame('json', $representation->type());
+	}
+
+	public function testRepresentationWithMultipleTypes()
+	{
+		$this->app = $this->app->clone([
+			'templates' => [
+				'test'      => __FILE__,
+				'test.xml'  => __FILE__,
+				'test.json' => __FILE__
+			]
+		]);
+
+		$page = new Page([
+			'slug'     => 'test',
+			'template' => 'test'
+		]);
+
+		$representation = $page->representation('xml');
+		$this->assertInstanceOf(Template::class, $representation);
+		$this->assertSame('test', $representation->name());
+		$this->assertSame('xml', $representation->type());
+
+		$representation = $page->representation('json');
+		$this->assertInstanceOf(Template::class, $representation);
+		$this->assertSame('test', $representation->name());
+		$this->assertSame('json', $representation->type());
+	}
+
+	public function testRepresentationWithMissingRepresentation()
+	{
+		$page = new Page([
+			'slug' => 'test',
+		]);
+
+		$this->expectException(NotFoundException::class);
+		$this->expectExceptionMessage('The content representation cannot be found');
+
+		$page->representation('json');
+	}
+}
