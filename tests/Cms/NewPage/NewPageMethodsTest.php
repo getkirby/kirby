e7ff6a823cbda019e014ab10e66b2d24b319aeac
Bastian Allgeier <mail@bastianallgeier.com>
2025-02-18 12:35:00 +0100

    New page methods test

---
+++ tests/Cms/NewPage/NewPageMethodsTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Kirby\Cms;
+
+use Kirby\Cms\NewPage as Page;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(Page::class)]
+class NewPageMethodsTest extends NewPageTestCase
+{
+	public const TMP = KIRBY_TMP_DIR . '/Cms.NewPageMethodsTest';
+
+	public function setUp(): void
+	{
+		parent::setUp();
+		Page::$methods = [];
+	}
+
+	public function tearDown(): void
+	{
+		parent::tearDown();
+		Page::$methods = [];
+	}
+
+	public function testPageMethod()
+	{
+		Page::$methods = [
+			'test' => fn () => 'page method for: ' . $this->slug()
+		];
+
+		$page = new Page([
+			'slug' => 'test'
+		]);
+
+		$this->assertSame('page method for: test', $page->test());
+	}
+}
