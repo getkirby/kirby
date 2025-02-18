6ff983e43d54ad2acb61621c292c8c4833a1ae14
Bastian Allgeier <mail@bastianallgeier.com>
2025-02-18 20:44:14 +0100

    Add move tests

---
+++ tests/Cms/NewPage/NewPageMoveTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Kirby\Cms;
+
+use Kirby\Cms\NewPage as Page;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(Page::class)]
+class NewPageMoveTest extends NewPageTestCase
+{
+	public const TMP = KIRBY_TMP_DIR . '/Cms.NewPageMoveTest';
+
+	public function testMove()
+	{
+		$this->app = $this->app->clone([
+			'blueprints' => [
+				'pages/parent' => [
+					'sections' => [
+						'subpages' => [
+							'type'     => 'pages',
+							'template' => 'child'
+						]
+					]
+				]
+			]
+		]);
+
+		$this->app->impersonate('kirby');
+
+		$parentA = Page::create([
+			'slug'     => 'parent-a',
+			'template' => 'parent'
+		]);
+
+		$parentB = Page::create([
+			'slug'     => 'parent-b',
+			'template' => 'parent'
+		]);
+
+		$child = Page::create([
+			'parent'   => $parentB,
+			'slug'     => 'child',
+			'template' => 'child'
+		]);
+
+		$moved = $child->move($parentB);
+
+		$this->assertTrue($moved->parent()->is($parentB));
+	}
+}
