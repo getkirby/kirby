c2649a32641f4bf012263e9df62d26b097de9c15
Bastian Allgeier <mail@bastianallgeier.com>
2025-02-18 11:05:05 +0100

    New depth tests

--- tests/Cms/NewPage/NewPageTest.php
+++ tests/Cms/NewPage/NewPageTest.php
@@ -3,12 +3,31 @@
 namespace Kirby\Cms;
 
 use Kirby\Cms\NewPage as Page;
-use Kirby\Content\MemoryStorage;
-use Kirby\Data\Data;
 use PHPUnit\Framework\Attributes\CoversClass;
 
 #[CoversClass(Page::class)]
 class NewPageTest extends NewPageTestCase
 {
 	public const TMP = KIRBY_TMP_DIR . '/Cms.NewPageTest';
+
+	public function testDepth()
+	{
+		$grandma = new Page([
+			'slug' => 'grandma',
+		]);
+
+		$mother = new Page([
+			'slug'   => 'mother',
+			'parent' => $grandma,
+		]);
+
+		$child = new Page([
+			'slug'   => 'test',
+			'parent' => $mother
+		]);
+
+		$this->assertSame(1, $grandma->depth());
+		$this->assertSame(2, $mother->depth());
+		$this->assertSame(3, $child->depth());
+	}
 }
