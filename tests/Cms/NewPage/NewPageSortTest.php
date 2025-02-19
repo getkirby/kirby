3b5bb374880c7127e1a13e5f39c1b7aaeb1b6274
Bastian Allgeier <mail@bastianallgeier.com>
2025-02-19 12:33:55 +0100

    NewPagePublishTest class

--- tests/Cms/NewPage/NewPageSortTest.php
+++ tests/Cms/NewPage/NewPageSortTest.php
@@ -448,53 +448,6 @@ class NewPageSortTest extends NewPageTestCase
 		$this->assertSame(2016, $app->page('test')->createNum());
 	}
 
-	public function testPublish()
-	{
-		// main page
-		$page = Page::create([
-			'slug' => 'test',
-		]);
-
-		$site      = $this->app->site();
-		$published = $page->publish();
-
-		$this->assertSame('unlisted', $published->status());
-
-		$this->assertFalse($page->parentModel()->drafts()->has($published));
-		$this->assertTrue($page->parentModel()->children()->has($published));
-
-		$this->assertFalse($site->drafts()->has($published));
-		$this->assertTrue($site->children()->has($published));
-
-		// child
-		$child = Page::create([
-			'parent' => $page,
-			'slug'   => 'child'
-		]);
-
-		$published = $child->publish();
-
-		$this->assertSame('unlisted', $published->status());
-
-		$this->assertFalse($child->parentModel()->drafts()->has($published->id()));
-		$this->assertTrue($child->parentModel()->children()->has($published->id()));
-
-		$this->assertFalse($page->drafts()->has($published->id()));
-		$this->assertTrue($page->children()->has($published->id()));
-	}
-
-	public function testPublishAlreadyPublished()
-	{
-		$page = Page::create([
-			'slug' => 'test'
-		]);
-
-		$page = $page->publish();
-
-		$this->assertSame('unlisted', $page->status());
-		$this->assertSame('unlisted', $page->publish()->status());
-	}
-
 	public static function sortProvider(): array
 	{
 		return [
