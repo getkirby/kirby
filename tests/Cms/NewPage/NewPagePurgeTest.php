d674b5dcf781af0fb6d02e3787714432a280b81a
Bastian Allgeier <mail@bastianallgeier.com>
2025-02-19 12:31:05 +0100

    Add purge tests

---
+++ tests/Cms/NewPage/NewPagePurgeTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Kirby\Cms;
+
+use Kirby\Cms\NewPage as Page;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(Page::class)]
+class NewPagePurgeTest extends NewPageTestCase
+{
+	public const TMP = KIRBY_TMP_DIR . '/Cms.NewPagePurgeTest';
+
+	public function testPurge()
+	{
+		$page = new Page([
+			'slug' => 'test'
+		]);
+
+		$page->children();
+		$page->drafts();
+		$page->childrenAndDrafts();
+
+		$this->assertNotNull($page->children);
+		$this->assertNotNull($page->drafts);
+		$this->assertNotNull($page->childrenAndDrafts);
+
+		$this->assertIsPage($page, $page->purge());
+
+		$this->assertNull($page->children);
+		$this->assertNull($page->drafts);
+		$this->assertNull($page->childrenAndDrafts);
+	}
+}
