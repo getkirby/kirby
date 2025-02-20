277b10d8c8333b3b3a383605bea5ea05812d0ee9
Nico Hoffmann <me@nhoffmann.com>
2025-02-20 13:30:48 +0100

    Add failing `NewSiteSaveTest`

---
+++ tests/Cms/NewSite/NewSiteSaveTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kirby\Cms;
+
+use Kirby\Cms\NewSite as Site;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(Site::class)]
+class NewSiteSaveTest extends NewModelTestCase
+{
+	public const TMP = KIRBY_TMP_DIR . '/Cms.NewSiteSaveTest';
+
+	public function testSave(): void
+	{
+		$site = new Site();
+		$site = $site->clone(['content' => ['copyright' => 2012]])->save();
+		$this->assertSame(2012, $site->copyright()->value());
+	}
+}
