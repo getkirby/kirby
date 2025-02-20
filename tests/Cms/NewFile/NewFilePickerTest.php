1c74d9fd9d4b0760a38b750242ea90a0af0c92d6
Bastian Allgeier <mail@bastianallgeier.com>
2025-02-20 17:21:15 +0100

    NewFilePickerTest

---
+++ tests/Cms/NewFile/NewFilePickerTest.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Kirby\Cms;
+
+use Kirby\Exception\InvalidArgumentException;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(FilePicker::class)]
+class NewFilePickerTest extends NewModelTestCase
+{
+	public const TMP = KIRBY_TMP_DIR . '/Cms.NewFilePicker';
+
+	public function setUp(): void
+	{
+		parent::setUp();
+
+		$this->app = $this->app->clone([
+			'site' => [
+				'files' => [
+					['filename' => 'a.jpg'],
+					['filename' => 'b.jpg'],
+					['filename' => 'c.jpg']
+				],
+				'children' => [
+					[
+						'slug'  => 'test',
+						'files' => [
+							['filename' => 'd.jpg'],
+							['filename' => 'e.jpg']
+						]
+					]
+				],
+			],
+			'users' => [
+				[
+					'id'    => 'test',
+					'email' => 'test@getkirby.com',
+					'files' => [
+						['filename' => 'f.jpg']
+					]
+				]
+			]
+		]);
+
+		$this->app->impersonate('kirby');
+	}
+
+	public function testDefaults()
+	{
+		$picker = new FilePicker();
+
+		$this->assertCount(3, $picker->items());
+	}
+
+	public function testQuery()
+	{
+		$picker = new FilePicker([
+			'query' => 'site.files.offset(1)'
+		]);
+
+		$this->assertCount(2, $picker->items());
+	}
+
+	public function testQuerySite()
+	{
+		$picker = new FilePicker([
+			'query' => 'site'
+		]);
+
+		$this->assertCount(3, $picker->items());
+	}
+
+	public function testQueryPage()
+	{
+		$picker = new FilePicker([
+			'query' => 'kirby.page("test")'
+		]);
+
+		$this->assertCount(2, $picker->items());
+	}
+
+	public function testQueryUser()
+	{
+		$picker = new FilePicker([
+			'query' => 'kirby.user("test")'
+		]);
+
+		$this->assertCount(1, $picker->items());
+	}
+
+	public function testQueryFile()
+	{
+		$picker = new FilePicker([
+			'model' => $this->app->site()->files()->first()
+		]);
+
+		$this->assertCount(3, $picker->items());
+	}
+
+	public function testQueryInvalid()
+	{
+		$picker = new FilePicker([
+			'query' => 'site.pages'
+		]);
+
+		$this->expectException(InvalidArgumentException::class);
+		$this->expectExceptionMessage('Your query must return a set of files');
+
+		$picker->items();
+	}
+}
