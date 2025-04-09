4241b13f2b5710db39d71ef88cc4583a18c71445
Bastian Allgeier <mail@bastianallgeier.com>
2025-04-09 15:08:32 +0200

    Refactor the fields section with the new toProps method

--- config/sections/fields.php
+++ config/sections/fields.php
@@ -1,7 +1,5 @@
 <?php
 
-use Kirby\Cms\Page;
-use Kirby\Cms\Site;
 use Kirby\Form\Form;
 
 return [
@@ -12,41 +10,16 @@ return [
 	],
 	'computed' => [
 		'form' => function () {
-			$fields   = $this->fields;
-			$disabled = $this->model->permissions()->cannot('update');
-			$lang     = $this->model->kirby()->languageCode();
-			$content  = $this->model->content($lang)->toArray();
-
-			if ($disabled === true) {
-				foreach ($fields as $key => $props) {
-					$fields[$key]['disabled'] = true;
-				}
-			}
-
 			return new Form([
-				'fields' => $fields,
-				'values' => $content,
-				'model'  => $this->model,
-				'strict' => true
+				'fields'   => $this->fields,
+				'values'   => $this->model->content('current')->toArray(),
+				'model'    => $this->model,
+				'language' => 'current',
+				'strict'   => true
 			]);
 		},
 		'fields' => function () {
-			$fields = $this->form->fields()->toArray();
-
-			if (
-				$this->model instanceof Page ||
-				$this->model instanceof Site
-			) {
-				// the title should never be updated directly via
-				// fields section to avoid conflicts with the rename dialog
-				unset($fields['title']);
-			}
-
-			foreach ($fields as $index => $props) {
-				unset($fields[$index]['value']);
-			}
-
-			return $fields;
+			return $this->form->fields()->toProps();
 		}
 	],
 	'methods' => [
