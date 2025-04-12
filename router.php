a01397ec709c06f4a6ab55d75979893855239a80
Lukas Bestle <lukas@getkirby.com>
2025-04-12 18:51:04 +0200

    Fix router path traversal
    https://github.com/getkirby/kirby/security/advisories/GHSA-9p3p-w5jf-8xxg

--- router.php
+++ router.php
@@ -4,8 +4,14 @@ $uri = urldecode(
 	parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-// Emulate Apache's `mod_rewrite` functionality
-if ($uri !== '/' && file_exists($_SERVER['DOCUMENT_ROOT'] . '/' . $uri)) {
+// emulate Apache's `mod_rewrite` functionality, but prevent
+// disclosure of the existence of files outside the document root
+$path = $_SERVER['DOCUMENT_ROOT'] . '/' . ltrim($uri, '/');
+if (
+	$uri !== '/' &&
+	file_exists($path) === true &&
+	substr(realpath($path), 0, strlen($_SERVER['DOCUMENT_ROOT'])) === $_SERVER['DOCUMENT_ROOT']
+) {
 	return false;
 }
 
