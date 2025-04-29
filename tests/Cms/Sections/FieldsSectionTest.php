82d9151f498154e9dab2c13ac9425836a34c3632
Bastian Allgeier <mail@bastianallgeier.com>
2025-04-29 13:09:13 +0200

    Clean up and extend the unit tests

--- tests/Cms/Sections/FieldsSectionTest.php
+++ tests/Cms/Sections/FieldsSectionTest.php
@@ -2,77 +2,51 @@
 
 namespace Kirby\Cms;
 
-use Kirby\TestCase;
-
 class FieldsSectionTest extends TestCase
 {
-	public function setUp(): void
+	public function testErrors(): void
 	{
-		App::destroy();
-
-		$this->app = new App([
-			'roots' => [
-				'index' => '/dev/null'
-			]
-		]);
-	}
-
-	public static function modelProvider(): array
-	{
-		return [
-			[
-				$page = new Page(['slug' => 'test']),
-				true
-			],
-			[
-				new Site(),
-				true
-			],
-			[
-				new File(['filename' => 'test.jpg', 'parent' => $page]),
-				false
-			],
-			[
-				new User(['email' => 'mail@getkirby.com']),
-				false
-			],
-		];
-	}
-
-	/**
-	 * @dataProvider modelProvider
-	 */
-	public function testSkipTitle($model, $skip)
-	{
-		$fields = [
-			'text' => [
-				'type' => 'textarea'
-			]
-		];
-
-		if ($skip === false) {
-			// add a custom title field to those models
-			// which don't skip the title. Files and Users
-			// should still be able to have title fields if needed
-			$fields['title'] = [
-				'type' => 'text'
-			];
-		}
-
 		$section = new Section('fields', [
 			'name'   => 'test',
-			'model'  => $model,
-			'fields' => $fields,
+			'model'  => new Page(['slug' => 'test']),
+			'fields' => [
+				'text' => [
+					'label' => 'Text',
+					'type' => 'textarea',
+					'required' => true
+				]
+			]
 		]);
 
-		if ($skip === true) {
-			$this->assertCount(1, $section->fields());
-			$this->assertArrayHasKey('text', $section->fields());
-			$this->assertArrayNotHasKey('title', $section->fields());
-		} else {
-			$this->assertCount(2, $section->fields());
-			$this->assertArrayHasKey('text', $section->fields());
-			$this->assertArrayHasKey('title', $section->fields());
-		}
+		$this->assertSame([
+			'text' => [
+				'label'   => 'Text',
+				'message' => [
+					'required' => 'Please enter something'
+				]
+			]
+		], $section->errors());
+	}
+
+	public function testErrorsWithContent(): void
+	{
+		$section = new Section('fields', [
+			'name'   => 'test',
+			'model'  => new Page([
+				'slug' => 'test',
+				'content' => [
+					'text' => 'test'
+				]
+			]),
+			'fields' => [
+				'text' => [
+					'label' => 'Text',
+					'type' => 'textarea',
+					'required' => true
+				]
+			]
+		]);
+
+		$this->assertSame([], $section->errors());
 	}
 }
