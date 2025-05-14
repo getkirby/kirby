f0d6dcb892b06189a26db99774a9619651661c94
Bastian Allgeier <mail@bastianallgeier.com>
2025-05-14 17:07:22 +0200

    feat: New VersionsButton class

---
+++ src/Panel/Ui/Buttons/VersionsButton.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Kirby\Panel\Ui\Buttons;
+
+use Kirby\Cms\ModelWithContent;
+use Kirby\Content\VersionId;
+use Kirby\Toolkit\I18n;
+
+/**
+ * Versions view button for models
+ *
+ * @package   Kirby Panel
+ * @author    Nico Hoffmann <nico@getkirby.com>
+ * @link      https://getkirby.com
+ * @copyright Bastian Allgeier
+ * @license   https://getkirby.com/license
+ * @since     5.0.0
+ * @internal
+ */
+class VersionsButton extends ViewButton
+{
+	public function __construct(
+		ModelWithContent $model,
+		VersionId|string $versionId = 'latest'
+	) {
+		$versionId = VersionId::from($versionId);
+		$viewUrl   = $model->panel()->url(true) . '/preview';
+
+		parent::__construct(
+			class: 'k-versions-view-button',
+			icon: 'git-branch',
+			options: [
+				[
+					'label'   => I18n::translate('version.latest'),
+					'icon'    => 'git-branch',
+					'link'    => $viewUrl . '/latest',
+					'current' => $versionId->is('latest')
+				],
+				'-',
+				[
+					'label'   => I18n::translate('version.changes'),
+					'icon'    => 'git-branch',
+					'link'    => $viewUrl . '/changes',
+					'current' => $versionId->is('changes')
+				],
+
+			],
+			text: I18n::translate('version.' . $versionId),
+		);
+	}
+}
