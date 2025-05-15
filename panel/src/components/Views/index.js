7675913c47045b4750d512ee77399b058344c37d
Bastian Allgeier <mail@bastianallgeier.com>
2025-05-15 11:50:26 +0200

    refact: Move PreviewView into Preview subfolder

--- panel/src/components/Views/index.js
+++ panel/src/components/Views/index.js
@@ -1,5 +1,4 @@
 import ErrorView from "./ErrorView.vue";
-import PreviewView from "./PreviewView.vue";
 import SearchView from "./SearchView.vue";
 
 import Files from "./Files/index.js";
@@ -7,13 +6,13 @@ import Installation from "./Installation/index.js";
 import Languages from "./Languages/index.js";
 import Login from "./Login/index.js";
 import Pages from "./Pages/index.js";
+import Preview from "./Preview/index.js";
 import Users from "./Users/index.js";
 import System from "./System/index.js";
 
 export default {
 	install(app) {
 		app.component("k-error-view", ErrorView);
-		app.component("k-preview-view", PreviewView);
 		app.component("k-search-view", SearchView);
 
 		app.use(Files);
@@ -21,6 +20,7 @@ export default {
 		app.use(Languages);
 		app.use(Login);
 		app.use(Pages);
+		app.use(Preview);
 		app.use(System);
 		app.use(Users);
 	}
