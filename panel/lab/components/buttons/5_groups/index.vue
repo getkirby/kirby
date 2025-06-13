cbeae1264ac44eb947c2383c5e725819e0e57991
Bastian Allgeier <mail@bastianallgeier.com>
2025-06-13 10:22:11 +0200

    test: Add lab example for coloured and collapsed button groups

--- panel/lab/components/buttons/5_groups/index.vue
+++ panel/lab/components/buttons/5_groups/index.vue
@@ -47,6 +47,18 @@
 				</k-dropdown-content>
 			</k-button-group>
 		</k-lab-example>
+		<k-lab-example :flex="true" label="layout: collapsed and themed">
+			<k-button-group layout="collapsed">
+				<k-button variant="filled" icon="bolt" theme="green">A</k-button>
+				<k-button variant="filled" icon="bolt" theme="orange">B</k-button>
+				<k-button variant="filled" icon="bolt" theme="red">C</k-button>
+			</k-button-group>
+			<k-button-group layout="collapsed">
+				<k-button variant="filled" icon="bolt" theme="green">A</k-button>
+				<k-button variant="filled" icon="bolt" theme="green">B</k-button>
+				<k-button variant="filled" icon="bolt" theme="green">C</k-button>
+			</k-button-group>
+		</k-lab-example>
 		<k-lab-example label="layout: collapsed wrap test">
 			<k-bar style="max-width: 23rem">
 				<k-label>Alonglabelshouldnotbreakthebuttongroup</k-label>
