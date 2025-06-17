fd29d352c88ea703b4cf041066d019f9e3ecafb8
Bastian Allgeier <mail@bastianallgeier.com>
2025-06-17 14:35:11 +0200

    build: Preflight for 5.0.0-rc.5

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'getkirby/cms',
-        'pretty_version' => '5.0.0-rc.4',
-        'version' => '5.0.0.0-RC4',
+        'pretty_version' => '5.0.0-rc.5',
+        'version' => '5.0.0.0-RC5',
         'reference' => null,
         'type' => 'kirby-cms',
         'install_path' => __DIR__ . '/../../',
@@ -47,8 +47,8 @@
             'dev_requirement' => false,
         ),
         'getkirby/cms' => array(
-            'pretty_version' => '5.0.0-rc.4',
-            'version' => '5.0.0.0-RC4',
+            'pretty_version' => '5.0.0-rc.5',
+            'version' => '5.0.0.0-RC5',
             'reference' => null,
             'type' => 'kirby-cms',
             'install_path' => __DIR__ . '/../../',
