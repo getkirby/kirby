f0d6b34f931e79beb1260604a1a81d12e8e59949
Bastian Allgeier <mail@bastianallgeier.com>
2025-07-12 19:46:00 +0200

    feat: New Stat class to represent the k-stat component on the backend

---
+++ src/Panel/Ui/Stat.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Kirby\Panel\Ui;
+
+use Kirby\Cms\ModelWithContent;
+use Kirby\Exception\InvalidArgumentException;
+use Kirby\Toolkit\I18n;
+
+/**
+ * @package   Kirby Panel
+ * @author    Bastian Allgeier <bastian@getkirby.com>
+ * @link      https://getkirby.com
+ * @copyright Bastian Allgeier
+ * @license   https://getkirby.com/license
+ * @since     5.1.0
+ */
+class Stat extends Component
+{
+	public function __construct(
+		public ModelWithContent $model,
+		public string $component = 'k-stat',
+		public string|null $icon = null,
+		public array|string|null $info = null,
+		public array|string|null $label = null,
+		public array|string|null $link = null,
+		public string|null $theme = null,
+		public array|string|null $value = null,
+	) {
+	}
+
+	public static function from(
+		ModelWithContent $model,
+		array|string $input
+	): static {
+		if (is_string($input) === true) {
+			return static::fromQuery(
+				model: $model,
+				query: $input
+			);
+		}
+
+		return new static(...[
+			...$input,
+			'model' => $model
+		]);
+	}
+
+	public static function fromQuery(
+		ModelWithContent $model,
+		string $query
+	): static {
+		$stat = $model->query($query);
+
+		if (is_array($stat) === false) {
+			throw new InvalidArgumentException(
+				message: 'Invalid data from stat query. The query must return an array.'
+			);
+		}
+
+		return new static(...[
+			...$stat,
+			'model' => $model
+		]);
+	}
+
+	public function icon(): string|null
+	{
+		return $this->query($this->icon);
+	}
+
+	public function info(): string|null
+	{
+		return $this->query(
+			$this->translate($this->info)
+		);
+	}
+
+	public function label(): string|null
+	{
+		return $this->query(
+			$this->translate($this->label)
+		);
+	}
+
+	public function link(): string|null
+	{
+		return $this->query(
+			$this->translate($this->link)
+		);
+	}
+
+	public function props(): array
+	{
+		return [
+			'icon'  => $this->icon(),
+			'info'  => $this->info(),
+			'label' => $this->label(),
+			'link'  => $this->link(),
+			'theme' => $this->theme(),
+			'value' => $this->value(),
+		];
+	}
+
+	protected function query(string|null $query): string|null
+	{
+		return $query === null ? null : $this->model->toString($query);
+	}
+
+	public function theme(): string|null
+	{
+		return $this->query($this->theme);
+	}
+
+	protected function translate(array|string|null $prop): string|null
+	{
+		if ($prop === null) {
+			return $prop;
+		}
+
+		return I18n::translate($prop, $prop);
+	}
+
+	public function value(): string|null
+	{
+		return $this->query(
+			$this->translate($this->value)
+		);
+	}
+}
