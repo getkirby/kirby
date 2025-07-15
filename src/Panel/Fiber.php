56538390c682cfa1150d54338721ba07b3cdc92a
Nico Hoffmann <me@nhoffmann.com>
2025-07-15 14:37:21 +0900

    refact: New `Kirby\Panel\Fiber` class

---
+++ src/Panel/Fiber.php
@@ -0,0 +1,374 @@
+<?php
+
+namespace Kirby\Panel;
+
+use Kirby\Api\Upload;
+use Kirby\Cms\App;
+use Kirby\Cms\Language;
+use Kirby\Cms\User;
+use Kirby\Toolkit\A;
+use Kirby\Toolkit\Date;
+use Kirby\Toolkit\Str;
+
+/**
+ * Bundles all the Fiber data to be sent to the Panel
+ * in as much of a lazy manner than possible
+ *
+ * @package   Kirby Panel
+ * @author    Nico Hoffmann <nico@getkirby.com>
+ * @link      https://getkirby.com
+ * @copyright Bastian Allgeier
+ * @license   https://getkirby.com/license
+ * @since     6.0.0
+ */
+class Fiber
+{
+	protected App $kirby;
+	protected bool $multilang;
+	protected Language|null $language;
+	protected array $permissions;
+	protected User|null $user;
+
+	public function __construct(
+		protected array $view = [],
+		protected array|null $area = null,
+		protected array $areas = [],
+	) {
+		$this->kirby       = App::instance();
+		$this->multilang   = Panel::multilang();
+		$this->language    = $this->kirby->language();
+		$this->user        = $this->kirby->user();
+		$this->permissions = $this->user?->role()->permissions()->toArray() ?? [];
+	}
+
+	public function config(): array
+	{
+		return [
+			'api'         => [
+				'methodOverride' => $this->kirby->option('api.methodOverride', true)
+			],
+			'debug'       => $this->kirby->option('debug', false),
+			'kirbytext'   => $this->kirby->option('panel.kirbytext', true),
+			'translation' => $this->kirby->option('panel.language', 'en'),
+			'upload'      => Upload::chunkSize(),
+		];
+	}
+
+	/**
+	 * Creates the shared data array for the individual views
+	 * The full shared data is always sent on every JSON and
+	 * full document request unless the `X-Fiber-Only` header or
+	 * the `_only` query parameter is set.
+	 */
+	public function data(): array
+	{
+		// shared data for all requests
+		return [
+			'direction'   => $this->direction(...),
+			'dialog'      => null,
+			'drawer'      => null,
+			'language'    => $this->language(...),
+			'languages'   => $this->languages(...),
+			'menu'        => $this->menu(...),
+			'permissions' => $this->permissions(...),
+			'license'     => $this->license(...),
+			'multilang'   => $this->multilang(...),
+			'searches'    => $this->searches(...),
+			'url'         => $this->url(...),
+			'user'        => $this->user(...),
+			'view'        => $this->view(...)
+		];
+	}
+
+	public function direction(): string|null
+	{
+		if ($this->multilang === true && $this->language && $this->user) {
+			$default = $this->kirby->defaultLanguage();
+
+			if (
+				$this->language->direction() !== $default->direction() &&
+				$this->language->code() !== $this->user->language()
+			) {
+				return $this->language->direction();
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Filters the data array based on headers or  query parameters.
+	 *
+	 * This way, JSON requests can tailor the returned data to
+	 * only include certain data fields.
+	 *
+	 * This can be activated with the `X-Fiber-Only` header or
+	 * the `_only` query parameter in a request.
+	 *
+	 * Globals are normally only loaded with the full document request.
+	 * In addition, they can be requested via the `X-Fiber-Globals` header
+	 * or the `_globals` query parameter.
+	 */
+	public function filter(array $data): array
+	{
+		$result = [];
+
+		// requested data ids
+		$request  = $this->kirby->request();
+		$filter   = $request->header('X-Fiber-Only');
+		$filter ??= $request->get('_only');
+
+		if (empty($filter) === false) {
+			// split include string into an array of fields
+			$keys = Str::split($filter, ',');
+
+			// take care of potentially requested globals
+			$globals     = $this->globals();
+			$keysEntries = A::map($keys, fn ($key) => Str::split($key, '.')[0]);
+
+			// check if the keys from `_only` include any global id as entry
+			if (array_intersect($keysEntries, array_keys($globals)) !== []) {
+				$data = array_merge_recursive($globals, $data);
+			}
+
+			// make sure the data is already resolved to make
+			// nested data fetching work
+			$data = A::apply($data);
+
+			// build a new array with all requested data
+			foreach ($keys as $key) {
+				$result[$key] = A::get($data, $key);
+			}
+
+			// Nest dotted keys in array but ignore $translation
+			return A::nest($result, ['translation']);
+		}
+
+		$filterGlobals   = $request->header('X-Fiber-Globals');
+		$filterGlobals ??= $request->get('_globals');
+
+		if (empty($filterGlobals) === false) {
+			// split globals string into an array of fields
+			$keys = Str::split($filterGlobals, ',');
+
+			// add requested globals
+			$globals = $this->globals();
+
+			foreach ($keys as $key) {
+				if (isset($globals[$key]) === true) {
+					$data[$key] = $globals[$key];
+				}
+			}
+
+			return A::apply($data);
+		}
+
+		return A::apply($data);
+	}
+
+	/**
+	 * Creates global data for the Panel.
+	 * This will be injected in the full Panel
+	 * view via the script tag. Global data
+	 * is only requested once on the first page load.
+	 * It can be loaded partially later if needed,
+	 * but is otherwise not included in Fiber calls.
+	 */
+	public function globals(): array
+	{
+		return [
+			'config'      => $this->config(...),
+			'system'      => $this->system(...),
+			'translation' => $this->translation(...),
+			'urls'        => $this->urls(...)
+		];
+	}
+
+	public function language(): array|null
+	{
+		if ($this->multilang === false) {
+			return null;
+		}
+
+		return $this->language?->toArray();
+	}
+
+	public function languages(): array
+	{
+		if ($this->multilang === false) {
+			return [];
+		}
+
+		return $this->kirby->languages()->values(
+			fn ($language) => $language->toArray()
+		);
+	}
+
+	public function license(): string
+	{
+		return $this->kirby->system()->license()->status()->value();
+	}
+
+	public function menu(): array
+	{
+		$menu = new Menu(
+			$this->areas,
+			$this->permissions,
+			$this->area['id'] ?? null
+		);
+		return $menu->entries();
+	}
+
+	public function multilang(): bool
+	{
+		return $this->multilang;
+	}
+
+	/**
+	 * Returns the permissions array for the current user
+	 */
+	public function permissions(): array
+	{
+		return $this->permissions;
+	}
+
+	public function searches(): array
+	{
+		$searches = [];
+
+		foreach ($this->areas as $area) {
+			// by default, all areas are accessible unless
+			// the permissions are explicitly set to false
+			if (($this->permissions['access'][$area['id']] ?? true) !== false) {
+				foreach (($area['searches'] ?? []) as $id => $params) {
+					$searches[$id] = [
+						'icon'  => $params['icon'] ?? 'search',
+						'label' => $params['label'] ?? Str::ucfirst($id),
+						'id'    => $id
+					];
+				}
+			}
+		}
+		return $searches;
+	}
+
+	public function system(): array
+	{
+		$locales = [];
+
+		foreach ($this->kirby->translations() as $translation) {
+			$locales[$translation->code()] = $translation->locale();
+		}
+
+		return [
+			'ascii'   => Str::$ascii,
+			'csrf'    => $this->kirby->auth()->csrfFromSession(),
+			'isLocal' => $this->kirby->system()->isLocal(),
+			'locales' => $locales,
+			'slugs'   => Str::$language,
+			'title'   => $this->kirby->site()->title()->or('Kirby Panel')->toString()
+		];
+	}
+
+	public function toArray(bool $globals = true): array
+	{
+		// get all data for the request
+		$data = $this->data();
+
+		// if requested, send only non-global data
+		if ($globals === false) {
+			// filter data, if only globals headers or
+			// query parameters are requested
+			return $this->filter($data);
+		}
+
+		// load globals for the full document response
+		$globals = $this->globals();
+
+		// resolve and merge globals and shared data
+		return array_merge_recursive(
+			A::apply($globals),
+			A::apply($data)
+		);
+	}
+
+	public function translation(): array
+	{
+		$language = match ($user = $this->kirby->user()) {
+			null    => $this->kirby->panelLanguage(),
+			default => $user->language()
+		};
+
+		$translation = $this->kirby->translation($language);
+
+		return [
+			'code'      => $translation->code(),
+			'data'      => $translation->dataWithFallback(),
+			'direction' => $translation->direction(),
+			'name'      => $translation->name(),
+			'weekday'   => Date::firstWeekday($translation->locale())
+		];
+	}
+
+	public function url(): string
+	{
+		return $this->kirby->request()->url()->toString();
+	}
+
+	public function urls(): array
+	{
+		return [
+			'api'  => $this->kirby->url('api'),
+			'site' => $this->kirby->url('index')
+		];
+	}
+
+	public function user(): array|null
+	{
+		if ($this->user) {
+			return [
+				'email'    => $this->user->email(),
+				'id'       => $this->user->id(),
+				'language' => $this->user->language(),
+				'role'     => $this->user->role()->id(),
+				'username' => $this->user->username(),
+			];
+		}
+
+		return null;
+	}
+
+	public function view(): array
+	{
+		$defaults = [
+			'breadcrumb' => [],
+			'code'       => 200,
+			'path'       => Str::after($this->kirby->path(), '/'),
+			'props'      => [],
+			'query'      => $this->kirby->request()->query()->toArray(),
+			'referrer'   => Panel::referrer(),
+			'search'     => $this->kirby->option('panel.search.type', 'pages'),
+			'timestamp'  => (int)(microtime(true) * 1000),
+		];
+
+		$view = array_replace_recursive(
+			$defaults,
+			$this->area ?? [],
+			$this->view
+		);
+
+		// make sure that views and dialogs are gone
+		unset(
+			$view['buttons'],
+			$view['dialogs'],
+			$view['drawers'],
+			$view['dropdowns'],
+			$view['requests'],
+			$view['searches'],
+			$view['views']
+		);
+
+		// resolve all callbacks in the view array
+		return A::apply($view);
+	}
+}
---
+++ tests/Panel/FiberTest.php
@@ -0,0 +1,580 @@
+<?php
+
+namespace Kirby\Panel;
+
+use Kirby\Cms\App;
+use Kirby\Cms\Language;
+use Kirby\Filesystem\Dir;
+use Kirby\TestCase;
+use Kirby\Toolkit\A;
+use Kirby\Toolkit\Str;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(Fiber::class)]
+class FiberTest extends TestCase
+{
+	public const TMP = KIRBY_TMP_DIR . '/Panel.Fiber';
+
+	public function setUp(): void
+	{
+		$this->app = new App([
+			'roots' => [
+				'index' => static::TMP,
+			]
+		]);
+
+		Dir::make(static::TMP);
+	}
+
+	public function tearDown(): void
+	{
+		// clear session file first
+		$this->app->session()->destroy();
+
+		Dir::remove(static::TMP);
+	}
+
+	public function testConfig(): void
+	{
+		// without custom data
+		$fiber = new Fiber();
+		$config  = $fiber->config();
+		$config = A::apply($config);
+
+		$this->assertArrayHasKey('api', $config);
+		$this->assertArrayHasKey('debug', $config);
+		$this->assertArrayHasKey('kirbytext', $config);
+		$this->assertArrayHasKey('translation', $config);
+		$this->assertArrayHasKey('upload', $config);
+	}
+
+	public function testData(): void
+	{
+		// without custom data
+		$fiber = new Fiber();
+		$data  = $fiber->data();
+
+		$this->assertArrayHasKey('direction', $data);
+		$this->assertArrayHasKey('language', $data);
+		$this->assertArrayHasKey('languages', $data);
+		$this->assertArrayHasKey('license', $data);
+		$this->assertArrayHasKey('menu', $data);
+		$this->assertArrayHasKey('multilang', $data);
+		$this->assertArrayHasKey('permissions', $data);
+		$this->assertArrayHasKey('url', $data);
+		$this->assertArrayHasKey('user', $data);
+		$this->assertArrayHasKey('view', $data);
+
+		// default view settings
+		$view = A::apply($data)['view'];
+
+		$this->assertSame([], $view['breadcrumb']);
+		$this->assertSame(200, $view['code']);
+		$this->assertSame('', $view['path']);
+		$this->assertIsInt($view['timestamp']);
+		$this->assertSame([], $view['props']);
+		$this->assertSame('pages', $view['search']);
+
+		$this->assertArrayNotHasKey('views', $view);
+		$this->assertArrayNotHasKey('dialogs', $view);
+	}
+
+	public function testDataWithCustomViewData(): void
+	{
+		$fiber = new Fiber([
+			'props' => $props = [
+				'foo' => 'bar'
+			]
+		]);
+		$data = $fiber->data();
+		$data = A::apply($data);
+
+		$this->assertSame($props, $data['view']['props']);
+	}
+
+	public function testDirection(): void
+	{
+		$this->app = $this->app->clone([
+			'languages' => [
+				['code' => 'en', 'name' => 'English', 'default' => true],
+				['code' => 'de', 'name' => 'Deutsch'],
+				['code' => 'ar', 'name' => 'Arabic', 'direction' => 'rtl'],
+			],
+			'options' => [
+				'languages' => true
+			]
+		]);
+
+		// set non-default, non-user language
+		$this->app->setCurrentLanguage('ar');
+
+		// authenticate pseudo user
+		$this->app->impersonate('kirby');
+
+		// without custom data
+		$fiber = new Fiber();
+		$this->assertSame('rtl', $fiber->direction());
+	}
+
+	public function testFilter()
+	{
+		$data = [
+			'a' => 'A',
+			'b' => 'B'
+		];
+
+		// default (no special request)
+		$result = (new Fiber())->filter($data);
+
+		$this->assertSame($data, $result);
+	}
+
+	public function testFilterOnlyRequest(): void
+	{
+		// empty only
+		$data   = ['foo' => 'bar'];
+		$result = (new Fiber())->filter($data);
+		$this->assertSame($data, $result);
+
+		// via get
+		$this->app = $this->app->clone([
+			'request' => [
+				'query' => [
+					'_only' => 'a',
+				]
+			]
+		]);
+
+		$data = [
+			'a' => 'A',
+			'b' => 'B'
+		];
+
+		$result = (new Fiber())->filter($data);
+		$this->assertSame(['a' => 'A'], $result);
+
+		// via headers
+		$this->app = $this->app->clone([
+			'request' => [
+				'headers' => [
+					'X-Fiber-Only' => 'a',
+				]
+			]
+		]);
+
+		$data = [
+			'a' => 'A',
+			'b' => 'B'
+		];
+
+		$result = (new Fiber())->filter($data);
+		$this->assertSame(['a' => 'A'], $result);
+	}
+
+	public function testFilterOnlyRequestWithGlobal(): void
+	{
+		// simulate a simple partial request
+		$this->app = $this->app->clone([
+			'request' => [
+				'query' => [
+					'_only' => 'a,urls',
+				]
+			]
+		]);
+
+		$data = [
+			'a' => 'A',
+			'b' => 'B'
+		];
+
+		$result = (new Fiber())->filter($data);
+
+		$expected = [
+			'a' => 'A',
+			'urls' => [
+				'api' => '/api',
+				'site' => '/'
+			]
+		];
+
+		$this->assertSame($expected, $result);
+	}
+
+	public function testFilterOnlyRequestWithNestedData(): void
+	{
+		// simulate a simple partial request
+		$this->app = $this->app->clone([
+			'request' => [
+				'query' => [
+					'_only' => 'b.c',
+				]
+			]
+		]);
+
+		$data = [
+			'a' => 'A',
+			'b' => [
+				'c' => 'C'
+			]
+		];
+
+		$result = (new Fiber())->filter($data);
+
+		$expected = [
+			'b' => [
+				'c' => 'C'
+			]
+		];
+
+		$this->assertSame($expected, $result);
+	}
+
+	public function testFilterOnlyRequestWithNestedGlobal(): void
+	{
+		// simulate a simple partial request
+		$this->app = $this->app->clone([
+			'request' => [
+				'query' => [
+					'_only' => 'a,urls.site',
+				]
+			]
+		]);
+
+		$data = [
+			'a' => 'A',
+			'b' => 'B'
+		];
+
+		$result = (new Fiber())->filter($data);
+
+		$expected = [
+			'a' => 'A',
+			'urls' => [
+				'site' => '/'
+			]
+		];
+
+		$this->assertSame($expected, $result);
+	}
+
+	public function testFilterGlobalsRequest(): void
+	{
+		// not included
+		$data = (new Fiber())->filter([]);
+		$this->assertArrayNotHasKey('translation', $data);
+
+		// empty globals
+		$data = ['foo' => 'bar'];
+		$result = (new Fiber())->filter($data);
+		$this->assertSame($data, $result);
+
+		// via query
+		$this->app = $this->app->clone([
+			'request' => [
+				'query' => [
+					'_globals' => 'translation'
+				]
+			]
+		]);
+
+		$data = (new Fiber())->filter([]);
+		$this->assertArrayHasKey('translation', $data);
+
+		// via header
+		$this->app = $this->app->clone([
+			'request' => [
+				'headers' => [
+					'X-Fiber-Globals' => 'translation'
+				]
+			]
+		]);
+
+		$data = (new Fiber())->filter([]);
+		$this->assertArrayHasKey('translation', $data);
+	}
+
+	public function testGlobals(): void
+	{
+		// defaults
+		$fiber   = new Fiber();
+		$globals = $fiber->globals();
+
+		$this->assertInstanceOf('Closure', $globals['config']);
+		$this->assertInstanceOf('Closure', $globals['system']);
+		$this->assertInstanceOf('Closure', $globals['system']);
+		$this->assertInstanceOf('Closure', $globals['translation']);
+		$this->assertInstanceOf('Closure', $globals['urls']);
+
+		// defaults after apply
+		$globals     = A::apply($globals);
+		$config      = $globals['config'];
+		$system      = $globals['system'];
+		$translation = $globals['translation'];
+		$urls        = $globals['urls'];
+
+		// $config
+		$this->assertFalse($config['debug']);
+		$this->assertTrue($config['kirbytext']);
+		$this->assertSame('en', $config['translation']);
+
+		// $system
+		$this->assertSame(Str::$ascii, $system['ascii']);
+		$this->assertSame(csrf(), $system['csrf']);
+		$this->assertFalse($system['isLocal']);
+		$this->assertArrayHasKey('de', $system['locales']);
+		$this->assertArrayHasKey('en', $system['locales']);
+		$this->assertSame('en_US', $system['locales']['en']);
+		$this->assertSame('de_DE', $system['locales']['de']);
+
+		// $translation
+		$this->assertSame('en', $translation['code']);
+		$this->assertSame($this->app->translation('en')->dataWithFallback(), $translation['data']);
+		$this->assertSame('ltr', $translation['direction']);
+		$this->assertSame('English', $translation['name']);
+
+		// $urls
+		$this->assertSame('/api', $urls['api']);
+		$this->assertSame('/', $urls['site']);
+	}
+
+	public function testGlobalsWithUser(): void
+	{
+		$this->app = $this->app->clone([
+			'users' => [
+				[
+					'email'    => 'test@getkirby.com',
+					'language' => 'de',
+					'role'     => 'admin'
+				]
+			]
+		]);
+
+		$this->app->impersonate('test@getkirby.com');
+		$fiber   = new Fiber();
+		$globals = $fiber->globals();
+		$globals = A::apply($globals);
+		$this->assertSame('de', $globals['translation']['code']);
+	}
+
+	public function testLanguages(): void
+	{
+		$this->app = $this->app->clone([
+			'languages' => [
+				['code' => 'en', 'name' => 'English', 'default' => true],
+				['code' => 'de', 'name' => 'Deutsch']
+			],
+			'options' => [
+				'languages' => true
+			]
+		]);
+
+		$fiber = new Fiber();
+
+		// multilang
+		$multilang = $fiber->multilang();
+		$this->assertTrue($multilang);
+
+		// languages
+		$languages = $fiber->languages();
+		$language  = $fiber->language();
+		$direction = $fiber->direction();
+		$expected = [
+			[
+				'code'      => 'en',
+				'default'   => true,
+				'direction' => 'ltr',
+				'locale'    => [LC_ALL => 'en'],
+				'name'      => 'English',
+				'rules'     => Language::loadRules('en'),
+				'url'       => '/en'
+			],
+			[
+				'code'      => 'de',
+				'default'   => false,
+				'direction' => 'ltr',
+				'locale'    => [LC_ALL => 'de'],
+				'name'      => 'Deutsch',
+				'rules'     => Language::loadRules('de'),
+				'url'       => '/de'
+			]
+		];
+
+		$this->assertSame($expected, $languages);
+		$this->assertSame($expected[0], $language);
+		$this->assertNull($direction);
+	}
+
+	public function testSearches()
+	{
+		$this->app = $this->app->clone([
+			'blueprints' => [
+				'users/editor' => [
+					'name' => 'editor',
+					'permissions' => [
+						'access' => [
+							'a' => true,
+							'b' => false
+						]
+					]
+				],
+			],
+			'users' => [
+				['email' => 'test@getkirby.com', 'role' => 'editor']
+			]
+		]);
+
+		$this->app->impersonate('test@getkirby.com');
+
+		$areas  = [
+			[
+				'id' => 'a',
+				'searches' => [
+					'foo' => [],
+				]
+			],
+			[
+				'id' => 'b',
+				'searches' => [
+					'bar' => [],
+				]
+			],
+			[
+				'id' => 'c',
+				'searches' => [
+					'test' => [],
+				]
+			]
+		];
+
+		$fiber    = new Fiber(areas: $areas);
+		$searches = $fiber->searches();
+
+		$this->assertArrayHasKey('foo', $searches);
+		$this->assertArrayNotHasKey('bar', $searches);
+		$this->assertArrayHasKey('test', $searches);
+	}
+
+	public function testSystem(): void
+	{
+		// without custom data
+		$fiber   = new Fiber();
+		$system  = $fiber->system();
+		$system = A::apply($system);
+
+		$this->assertArrayHasKey('ascii', $system);
+		$this->assertArrayHasKey('csrf', $system);
+		$this->assertArrayHasKey('isLocal', $system);
+		$this->assertArrayHasKey('locales', $system);
+		$this->assertArrayHasKey('slugs', $system);
+		$this->assertArrayHasKey('title', $system);
+	}
+
+	public function testToArray(): void
+	{
+		$fiber = new Fiber();
+		$array = $fiber->toArray(globals: false);
+		$this->assertArrayHasKey('user', $array);
+		$this->assertArrayNotHasKey('config', $array);
+
+		$array = $fiber->toArray(globals: true);
+		$this->assertArrayHasKey('user', $array);
+		$this->assertArrayHasKey('config', $array);
+	}
+
+	public function testTranslation(): void
+	{
+		$fiber       = new Fiber();
+		$translation = $fiber->translation();
+		$translation = A::apply($translation);
+
+		$this->assertSame('en', $translation['code']);
+		$this->assertArrayHasKey('data', $translation);
+		$this->assertSame('ltr', $translation['direction']);
+		$this->assertSame('English', $translation['name']);
+		$this->assertSame(0, $translation['weekday']);
+	}
+
+	public function testTranslationWithUserLanguage(): void
+	{
+		$this->app = $this->app->clone([
+			'users' => [
+				[
+					'email'    => 'test@getkirby.com',
+					'language' => 'de',
+					'role'     => 'admin'
+				]
+			]
+		]);
+
+		$this->app->impersonate('test@getkirby.com');
+
+		$fiber       = new Fiber();
+		$translation = $fiber->translation();
+		$translation = A::apply($translation);
+
+		$this->assertSame('de', $translation['code']);
+		$this->assertArrayHasKey('data', $translation);
+		$this->assertSame('ltr', $translation['direction']);
+		$this->assertSame('Deutsch', $translation['name']);
+		$this->assertSame(1, $translation['weekday']);
+	}
+
+	public function testUrl(): void
+	{
+		// custom request url
+		$this->app = $this->app->clone([
+			'cli' => false,
+			'options' => [
+				'url' => 'https://localhost.com:8888'
+			],
+			'server' => [
+				'REQUEST_URI' => '/foo/bar/?foo=bar'
+			]
+		]);
+
+		// without custom data
+		$fiber = new Fiber();
+		$url   = $fiber->url();
+		$this->assertSame('https://localhost.com:8888/foo/bar?foo=bar', $url);
+	}
+
+	public function testUrls(): void
+	{
+		$fiber = new Fiber();
+		$urls  = $fiber->urls();
+		$this->assertArrayHasKey('api', $urls);
+		$this->assertArrayHasKey('site', $urls);
+	}
+
+	public function testUserAuthenticated(): void
+	{
+		// authenticate pseudo user
+		$this->app->impersonate('kirby');
+
+		$fiber = new Fiber();
+
+		// user
+		$user = $fiber->user();
+		$user = A::apply($user);
+
+		$expected = [
+			'email'    => 'kirby@getkirby.com',
+			'id'       => 'kirby',
+			'language' => 'en',
+			'role'     => 'admin',
+			'username' => 'kirby@getkirby.com'
+		];
+
+		$this->assertSame($expected, $user);
+
+
+		// permissions;
+		$permissions = $fiber->permissions();
+		$permissions = A::apply($permissions);
+
+		$this->assertSame(
+			$this->app->user()->role()->permissions()->toArray(),
+			$permissions
+		);
+	}
+}
