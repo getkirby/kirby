bf4d7add1132462de41ba2a30eda8c3cc4512dbd
Bastian Allgeier <mail@bastianallgeier.com>
2025-07-15 13:11:41 +0200

    feat: `Kirby\Panel\Ui\Stat` backend component

--- config/sections/stats.php
+++ config/sections/stats.php
@@ -1,6 +1,7 @@
 <?php
 
-use Kirby\Toolkit\I18n;
+use Kirby\Exception\InvalidArgumentException;
+use Kirby\Panel\Ui\Stat;
 
 return [
 	'mixins' => [
@@ -34,32 +35,20 @@ return [
 	],
 	'computed' => [
 		'reports' => function () {
-			$reports  = [];
-			$model    = $this->model();
-			$toString = fn ($value) => $value === null ? null : $model->toString($value);
+			$reports = [];
+			$model   = $this->model();
 
 			foreach ($this->reports as $report) {
-				if (is_string($report) === true) {
-					$report = $model->query($report);
-				}
-
-				if (is_array($report) === false) {
+				try {
+					$stat = Stat::from(
+						model: $model,
+						input: $report
+					);
+				} catch (InvalidArgumentException) {
 					continue;
 				}
 
-				$info  = $report['info'] ?? null;
-				$label = $report['label'] ?? null;
-				$link  = $report['link'] ?? null;
-				$value = $report['value'] ?? null;
-
-				$reports[] = [
-					'icon'  => $toString($report['icon'] ?? null),
-					'info'  => $toString(I18n::translate($info, $info)),
-					'label' => $toString(I18n::translate($label, $label)),
-					'link'  => $toString(I18n::translate($link, $link)),
-					'theme' => $toString($report['theme'] ?? null),
-					'value' => $toString(I18n::translate($value, $value))
-				];
+				$reports[] = $stat->props();
 			}
 
 			return $reports;
---
+++ tests/Panel/Ui/StatTest.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Kirby\Panel\Ui;
+
+use Kirby\Cms\ModelWithContent;
+use Kirby\Cms\Page;
+use Kirby\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(Stat::class)]
+class StatTest extends TestCase
+{
+	protected ModelWithContent $model;
+
+	public function setUp(): void
+	{
+		$this->model = new Page(['slug' => 'test']);
+	}
+
+	public function assertProp(
+		string $prop,
+		bool $translatable = true,
+		bool $queryable = true,
+		bool $nullable = true
+	): void {
+		$stat = new Stat(
+			...[
+				'label' => 'Test Label',
+				'value' => 'Test Value',
+				'model' => $this->model,
+				$prop => 'Test'
+			]
+		);
+
+		$this->assertSame('Test', $stat->$prop());
+
+		if ($nullable === true) {
+			$stat = new Stat(
+				model: $this->model,
+				label: 'Test Label',
+				value: 'Test Value',
+			);
+			$this->assertNull($stat->$prop());
+		}
+
+		if ($translatable === true) {
+			$stat = new Stat(
+				...[
+					'model' => $this->model,
+					'label' => 'Test Label',
+					'value' => 'Test Value',
+					$prop => [
+						'en' => 'Test'
+					]
+				]
+			);
+
+			$this->assertSame('Test', $stat->$prop());
+		}
+
+		if ($queryable === true) {
+			$stat = new Stat(
+				...[
+					'model' => $this->model,
+					'label' => 'Test Label',
+					'value' => 'Test Value',
+					$prop => '{{ page.slug }}'
+				]
+			);
+
+			$this->assertSame('test', $stat->$prop());
+		}
+	}
+
+	public function testComponent(): void
+	{
+		$stat = new Stat(
+			model: $this->model,
+			label: 'Test Label',
+			value: 'Test Value'
+		);
+		$this->assertSame('k-stat', $stat->component());
+
+		$stat = new Stat(
+			model: $this->model,
+			label: 'Test Label',
+			value: 'Test Value',
+			component: 'k-stat-test'
+		);
+
+		$this->assertSame('k-stat-test', $stat->component());
+	}
+
+	public function testIcon(): void
+	{
+		$this->assertProp(
+			prop: 'icon',
+			nullable: true,
+			translatable: false,
+			queryable: true
+		);
+	}
+
+	public function testInfo(): void
+	{
+		$this->assertProp(
+			prop: 'info',
+			nullable: true,
+			translatable: true,
+			queryable: true
+		);
+	}
+
+	public function testLabel(): void
+	{
+		$this->assertProp(
+			prop: 'label',
+			nullable: false,
+			translatable: true,
+			queryable: true
+		);
+	}
+
+	public function testLink(): void
+	{
+		$this->assertProp(
+			prop: 'link',
+			nullable: true,
+			translatable: true,
+			queryable: true
+		);
+	}
+
+	public function testTheme(): void
+	{
+		$this->assertProp(
+			prop: 'theme',
+			nullable: true,
+			translatable: false,
+			queryable: true
+		);
+	}
+
+	public function testValue(): void
+	{
+		$this->assertProp(
+			prop: 'value',
+			nullable: false,
+			translatable: true,
+			queryable: true,
+		);
+	}
+}
