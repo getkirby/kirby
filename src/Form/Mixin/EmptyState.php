bb84d474ea8b82ef561244f93370a1fef3f561e3
Bastian Allgeier <mail@bastianallgeier.com>
2025-07-16 17:56:37 +0200

    docs: Improve the inline docs for mixins

--- src/Form/Mixin/EmptyState.php
+++ src/Form/Mixin/EmptyState.php
@@ -4,6 +4,9 @@ namespace Kirby\Form\Mixin;
 
 trait EmptyState
 {
+	/**
+	 * Sets the text for the empty state box
+	 */
 	protected string|null $empty;
 
 	protected function setEmpty(string|array|null $empty = null): void
