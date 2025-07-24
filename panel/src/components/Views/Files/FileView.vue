d1a18cab8817384573f77a3acbcdf606ed6fe3ba
Bastian Allgeier <mail@bastianallgeier.com>
2025-07-24 13:22:07 +0200

    feat: Hook up new unlock feature in model views

--- panel/src/components/Views/Files/FileView.vue
+++ panel/src/components/Views/Files/FileView.vue
@@ -23,9 +23,11 @@
 					:has-diff="hasDiff"
 					:is-locked="isLocked"
 					:is-processing="isSaving"
+					:is-unlockable="permissions.unlock"
 					:modified="modified"
 					@discard="onDiscard"
 					@submit="onSubmit"
+					@unlock="onUnlock"
 				/>
 			</template>
 		</k-header>
