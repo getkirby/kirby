45b056d6928d479b39d5a7d197cd13315949d7bf
Ahmet Bora <byybora@gmail.com>
2025-07-25 22:07:40 +0300

    refactor: Move disabled prop into mixin class

---
+++ src/Form/Mixin/Disabled.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Kirby\Form\Mixin;
+
+trait Disabled
+{
+	/**
+	 * If `true`, the field is no longer editable and will not be saved
+	 */
+	protected bool $disabled;
+
+	public function disabled(): bool
+	{
+		return $this->disabled;
+	}
+
+	public function isDisabled(): bool
+	{
+		return $this->disabled;
+	}
+
+	protected function setDisabled(bool $disabled = false): void
+	{
+		$this->disabled = $disabled;
+	}
+}
