ee254135e0f4b06069072b8f493ccbb27d8731ae
Ahmet Bora <byybora@gmail.com>
2025-07-30 23:45:14 +0300

    refactor!: StatsField constructor to use named arguments
    Updated the StatsField class to use explicit named parameters in the constructor instead of a single params array. Added setReports and setSize methods for better property assignment and maintainability.

--- src/Form/Field/StatsField.php
+++ src/Form/Field/StatsField.php
@@ -2,7 +2,9 @@
 
 namespace Kirby\Form\Field;
 
+use Kirby\Cms\ModelWithContent;
 use Kirby\Form\FieldClass;
+use Kirby\Form\Fields;
 use Kirby\Panel\Ui\Stats;
 
 /**
@@ -32,12 +34,54 @@ class StatsField extends FieldClass
 	 */
 	protected Stats $stats;
 
-	public function __construct(array $params)
-	{
-		parent::__construct($params);
+	public function __construct(
+		array|string|null $after = null,
+		bool $autofocus = false,
+		array|string|null $before = null,
+		mixed $default = null,
+		bool $disabled = false,
+		array|string|null $help = null,
+		bool $hidden = false,
+		string|null $icon = null,
+		string|null $label = null,
+		ModelWithContent|null $model = null,
+		string|null $name = null,
+		array|string|null $placeholder = null,
+		array|string $reports = [],
+		bool $required = false,
+		Fields|null $siblings = null,
+		string $size = 'large',
+		bool $translate = true,
+		array|null $when = null,
+		string|null $width = null,
+		mixed $value = null,
+		// additional parameters can be passed to the field
+		...$params
+	) {
+		parent::__construct(
+			...$params,
+			after: $after,
+			autofocus: $autofocus,
+			before: $before,
+			default: $default,
+			disabled: $disabled,
+			help: $help,
+			hidden: $hidden,
+			icon: $icon,
+			label: $label,
+			model: $model,
+			name: $name,
+			placeholder: $placeholder,
+			required: $required,
+			siblings: $siblings,
+			translate: $translate,
+			when: $when,
+			width: $width,
+			value: $value
+		);
 
-		$this->reports = $params['reports'] ?? [];
-		$this->size    = $params['size']    ?? 'large';
+		$this->setReports($reports);
+		$this->setSize($size);
 	}
 
 	public function hasValue(): bool
@@ -48,6 +92,16 @@ class StatsField extends FieldClass
 	public function reports(): array
 	{
 		return $this->stats()->reports();
+	}
+
+	protected function setReports(array|string $reports): void
+	{
+		$this->reports = $reports;
+	}
+
+	protected function setSize(string $size = 'large'): void
+	{
+		$this->size = $size;
 	}
 
 	public function size(): string
