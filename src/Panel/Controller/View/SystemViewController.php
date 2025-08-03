c3e09074c039be89c7399a60b667229e11df4554
Nico Hoffmann <me@nhoffmann.com>
2025-08-03 13:11:59 +0200

    refact: System view as controller

---
+++ src/Panel/Controller/View/SystemViewController.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace Kirby\Panel\Controller\View;
+
+use Kirby\Cms\License;
+use Kirby\Cms\System;
+use Kirby\Cms\System\UpdateStatus;
+use Kirby\Panel\Controller\ViewController;
+use Kirby\Panel\Ui\Button\ViewButtons;
+use Kirby\Panel\Ui\Stat;
+use Kirby\Panel\Ui\Stats;
+use Kirby\Panel\Ui\View;
+use Kirby\Plugin\Plugin;
+
+/**
+ * Controls the system view
+ *
+ * @package   Kirby Panel
+ * @author    Nico Hoffmann <nico@getkirby.com>
+ * @link      https://getkirby.com
+ * @copyright Bastian Allgeier
+ * @license   https://getkirby.com/license
+ * @since     6.0.0
+ */
+class SystemViewController extends ViewController
+{
+	protected array $exceptions;
+	protected License $license;
+	protected array $plugins;
+	protected System $system;
+	protected UpdateStatus|null $update;
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->system     = $this->kirby->system();
+		$this->license    = $this->system->license();
+		$this->update     = $this->system->updateStatus();
+		$this->exceptions = $this->update?->exceptionMessages() ?? [];
+	}
+
+	public function buttons(): ViewButtons
+	{
+		return ViewButtons::view('system');
+	}
+
+	public function exceptions(): array
+	{
+		if ($this->isDebug() === false) {
+			return [];
+		}
+
+		// Call `::plugins()` to ensure they inject their exceptions
+		$this->plugins();
+
+		return $this->exceptions;
+	}
+
+	protected function isDebug(): bool
+	{
+		return $this->kirby->option('debug', false) === true;
+	}
+
+	protected function isLocal(): bool
+	{
+		return $this->system->isLocal();
+	}
+
+	public function load(): View
+	{
+		return new View(
+			component: 'k-system-view',
+			buttons: $this->buttons(),
+			environment: $this->stats()->reports(),
+			exceptions: $this->exceptions(),
+			info: $this->system->info(),
+			plugins: $this->plugins(),
+			security: $this->security(),
+			urls: $this->urls()
+		);
+	}
+
+	public function plugins(): array
+	{
+		return $this->plugins ??= $this->system->plugins()->values(function (Plugin $plugin) {
+			$authors   = $plugin->authorsNames();
+			$update    = $plugin->updateStatus();
+			$version   = $update?->toArray() ?? $plugin->version() ?? '–';
+
+			// Inject exceptions from plugin to global exceptions
+			$this->exceptions = [
+				...$this->exceptions,
+				...$update?->exceptionMessages() ?? []
+			];
+
+			return [
+				'author'  => $authors === '' ? '–' : $authors,
+				'license' => $plugin->license()->toArray(),
+				'name'    => [
+					'text' => $plugin->name() ?? '–',
+					'href' => $plugin->link(),
+				],
+				'status'  => $plugin->license()->status()->toArray(),
+				'version' => $version,
+			];
+		});
+	}
+
+	public function security(): array
+	{
+		$security = $this->update?->messages() ?? [];
+
+		if ($this->isLocal() === true) {
+			$security[] = [
+				'id'    => 'local',
+				'icon'  => 'info',
+				'theme' => 'info',
+				'text'  => $this->i18n('system.issues.local')
+			];
+		}
+
+		if ($this->isDebug() === true) {
+			$security[] = [
+				'id'    => 'debug',
+				'icon'  => $this->isLocal() ? 'info' : 'alert',
+				'theme' => $this->isLocal() ? 'info' : 'negative',
+				'text'  => $this->i18n('system.issues.debug'),
+				'link'  => 'https://getkirby.com/security/debug'
+			];
+		}
+
+		if (
+			$this->isLocal() === false &&
+			$this->kirby->environment()->https() !== true
+		) {
+			$security[] = [
+				'id'   => 'https',
+				'text' => $this->i18n('system.issues.https'),
+				'link' => 'https://getkirby.com/security/https'
+			];
+		}
+
+		if ($this->kirby->option('panel.vue.compiler', null) === null) {
+			$security[] = [
+				'id'    => 'vue-compiler',
+				'link'  => 'https://getkirby.com/security/vue-compiler',
+				'text'  => $this->i18n('system.issues.vue.compiler'),
+				'theme' => 'notice'
+			];
+		}
+
+		return $security;
+	}
+
+	public function stats(): Stats
+	{
+		return new Stats(reports: [
+			new Stat(
+				label: $this->license->status()->label(),
+				value: $this->license->label(),
+				theme: $this->license->status()->theme(),
+				icon: $this->license->status()->icon(),
+				dialog: $this->license->status()->dialog()
+			),
+			new Stat(
+				label: $this->update?->label() ?? $this->i18n('version'),
+				value: $this->kirby->version(),
+				link: $this->update?->url() ??
+					'https://github.com/getkirby/kirby/releases/tag/' . $this->kirby->version(),
+				theme: $this->update?->theme(),
+				icon: $this->update?->icon() ?? 'info'
+			),
+			new Stat(
+				label: 'PHP',
+				value: phpversion(),
+				icon: 'code'
+			),
+			new Stat(
+				label: $this->i18n('server'),
+				value: $this->system->serverSoftwareShort() ?? '?',
+				icon: 'server'
+			)
+		]);
+	}
+
+	public function urls(): array
+	{
+		if ($this->isLocal() === true) {
+			return [];
+		}
+
+		// sensitive URLs
+		return [
+			'content' => $this->system->exposedFileUrl('content'),
+			'git'     => $this->system->exposedFileUrl('git'),
+			'kirby'   => $this->system->exposedFileUrl('kirby'),
+			'site'    => $this->system->exposedFileUrl('site')
+		];
+	}
+}
---
+++ tests/Panel/Controller/View/SystemViewControllerTest.php
@@ -0,0 +1,410 @@
+<?php
+
+namespace Kirby\Panel\Controller\View;
+
+use Kirby\Cms\App;
+use Kirby\Cms\System\UpdateStatus;
+use Kirby\Panel\TestCase;
+use Kirby\Panel\Ui\Button\ViewButtons;
+use Kirby\Panel\Ui\Stats;
+use Kirby\Panel\Ui\View;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+/**
+ * Helper mock class to not repeat the same code in the tests
+ */
+class SystemMock
+{
+	public static function compilerWarning(): array
+	{
+		return [
+			'id'    => 'vue-compiler',
+			'link'  => 'https://getkirby.com/security/vue-compiler',
+			'text'  => 'The Vue template compiler is enabled',
+			'theme' => 'notice'
+		];
+	}
+
+	public static function customWarning(): array
+	{
+		return [
+			'text'  => 'This is a very important announcement!',
+			'kirby' => '*',
+			'php'   => '*'
+		];
+	}
+
+	public static function unknownLicense(): array
+	{
+		return [
+			'link'   => null,
+			'name'   => '-',
+			'status' => static::unknownLicenseStatus()
+		];
+	}
+
+	public static function unknownLicenseStatus(): array
+	{
+		return [
+			'dialog' => null,
+			'drawer' => null,
+			'icon'   => 'question',
+			'label'  => 'Unknown',
+			'link'	 => null,
+			'theme'  => 'passive',
+			'value'  => 'unknown',
+		];
+	}
+}
+
+#[CoversClass(SystemViewController::class)]
+class SystemViewControllerTest extends TestCase
+{
+	public const FIXTURES = __DIR__ . '/fixtures/system';
+	public const TMP = KIRBY_TMP_DIR . '/Panel.Controller.View.SystemViewController';
+
+	protected static string $host;
+
+	public static function setUpBeforeClass(): void
+	{
+		static::$host = UpdateStatus::$host;
+		UpdateStatus::$host = 'file://' . static::FIXTURES;
+	}
+
+	public static function tearDownAfterClass(): void
+	{
+		UpdateStatus::$host = static::$host;
+	}
+
+	public function setUp(): void
+	{
+		parent::setUp();
+
+		$this->app = $this->app->clone([
+			'options' => [
+				'url' => 'https://example.com'
+			]
+		]);
+	}
+
+	protected function installPlugins(): void
+	{
+		App::plugin('getkirby/private', [
+			'info' => []
+		]);
+
+		App::plugin('getkirby/public', [
+			'info' => [
+				'authors' => [
+					[
+						'name' => 'A'
+					],
+					[
+						'name' => 'B'
+					]
+				],
+				'homepage' => 'https://getkirby.com',
+				'version'  => '1.0.0',
+			]
+		]);
+
+		App::plugin('getkirby/unknown', [
+			'info' => [
+				'version' => '1.0.0'
+			]
+		]);
+	}
+
+	public function testButtons(): void
+	{
+		$controller = new SystemViewController();
+		$buttons    = $controller->buttons();
+		$this->assertInstanceOf(ViewButtons::class, $buttons);
+	}
+
+	public function testExceptions(): void
+	{
+		$controller = new SystemViewController();
+		$exceptions = $controller->exceptions();
+		$this->assertIsArray($exceptions);
+		$this->assertCount(0, $exceptions);
+	}
+
+	public function testExceptionsPlugins(): void
+	{
+		$this->installPlugins();
+		$controller = new SystemViewController();
+		$exceptions = $controller->exceptions();
+		$this->assertCount(0, $exceptions);
+	}
+
+	public function testExceptionsPluginsDebug(): void
+	{
+		$this->installPlugins();
+		$this->app = $this->app->clone([
+			'options' => [
+				'debug' => true
+			]
+		]);
+
+		$controller = new SystemViewController();
+		$exceptions = $controller->exceptions();
+		$this->assertSame([
+			'Could not load update data for plugin getkirby/private: Couldn\'t open file ' .
+			static::FIXTURES . '/plugins/getkirby/private.json',
+			'Could not load update data for plugin getkirby/unknown: Couldn\'t open file ' .
+			static::FIXTURES . '/plugins/getkirby/unknown.json',
+		], $exceptions);
+	}
+
+	public function testExceptionsLocal(): void
+	{
+		$this->app = $this->app->clone([
+			'server' => [
+				'REMOTE_ADDR' => '127.0.0.1',
+			]
+		]);
+
+		$controller = new SystemViewController();
+		$this->assertSame([], $controller->exceptions());
+	}
+
+	public function testLoad(): void
+	{
+		$controller = new SystemViewController();
+		$view       = $controller->load();
+		$this->assertInstanceOf(View::class, $view);
+		$this->assertSame('k-system-view', $view->component);
+
+		$props = $view->props();
+		$this->assertIsArray($props['buttons']);
+		$this->assertIsArray($props['environment']);
+		$this->assertArrayHasKey('plugins', $props);
+		$this->assertArrayHasKey('exceptions', $props);
+		$this->assertSame($this->app->system()->info(), $props['info']);
+		$this->assertArrayHasKey('security', $props);
+		$this->assertArrayHasKey('urls', $props);
+	}
+
+	public function testPlugins(): void
+	{
+		$controller = new SystemViewController();
+		$plugins    = $controller->plugins();
+		$this->assertIsArray($plugins);
+		$this->assertCount(0, $plugins);
+
+		$this->installPlugins();
+
+		$controller = new SystemViewController();
+		$plugins    = $controller->plugins();
+		$this->assertCount(3, $plugins);
+
+		$expected = [
+			[
+				'author'  => '–',
+				'license' => SystemMock::unknownLicense(),
+				'name'    => [
+					'text' => 'getkirby/private',
+					'href' => null
+				],
+				'status'  => SystemMock::unknownLicenseStatus(),
+				'version' => [
+					'currentVersion' => '?',
+					'icon'           => 'question',
+					'label'          => 'Could not check for updates',
+					'latestVersion'  => '?',
+					'pluginName'     => 'getkirby/private',
+					'theme'          => 'passive',
+					'url'            => null
+				]
+			],
+			[
+				'author'  => 'A, B',
+				'license' => SystemMock::unknownLicense(),
+				'name'    => [
+					'text' => 'getkirby/public',
+					'href' => 'https://getkirby.com'
+				],
+				'status'  => SystemMock::unknownLicenseStatus(),
+				'version' => [
+					'currentVersion' => '1.0.0',
+					'icon'           => 'info',
+					'label'          => 'Free update 88888.8.8 available',
+					'latestVersion'  => '99999.9.9',
+					'pluginName'     => 'getkirby/public',
+					'theme'          => 'info',
+					'url'            => 'https://github.com/getkirby/public-plugin/releases/tag/88888.8.8'
+				]
+			],
+			[
+				'author'  => '–',
+				'license' => SystemMock::unknownLicense(),
+				'name'    => [
+					'text' => 'getkirby/unknown',
+					'href' => null
+				],
+				'status'  => SystemMock::unknownLicenseStatus(),
+				'version' => [
+					'currentVersion' => '1.0.0',
+					'icon'           => 'question',
+					'label'          => 'Could not check for updates',
+					'latestVersion'  => '?',
+					'pluginName'     => 'getkirby/unknown',
+					'theme'          => 'passive',
+					'url'            => null
+				]
+			]
+		];
+
+		$this->assertSame($expected, $plugins);
+	}
+
+	public function testPluginsWithoutUpdateCheck(): void
+	{
+		$this->app = $this->app->clone([
+			'options' => [
+				'updates' => false
+			]
+		]);
+
+		$this->installPlugins();
+
+		$controller = new SystemViewController();
+		$plugins    = $controller->plugins();
+		$this->assertSame('1.0.0', $plugins[1]['version']);
+	}
+
+	public function testSecurity(): void
+	{
+		$controller = new SystemViewController();
+		$security   = $controller->security();
+		$this->assertSame([
+			SystemMock::customWarning(),
+			SystemMock::compilerWarning()
+		], $security);
+	}
+
+	public function testSecurityDebug(): void
+	{
+		$this->app = $this->app->clone([
+			'options' => [
+				'debug' => true
+			]
+		]);
+
+		$controller = new SystemViewController();
+		$security   = $controller->security();
+		$this->assertSame([
+			SystemMock::customWarning(),
+			[
+				'id'    => 'debug',
+				'icon'  => 'alert',
+				'theme' => 'negative',
+				'text'  => 'Debugging must be turned off in production',
+				'link'  => 'https://getkirby.com/security/debug'
+			],
+			SystemMock::compilerWarning()
+		], $security);
+	}
+
+	public function testSecurityLocal(): void
+	{
+		$this->app = $this->app->clone([
+			'server' => [
+				'REMOTE_ADDR' => '127.0.0.1',
+			]
+		]);
+
+		$controller = new SystemViewController();
+		$security   = $controller->security();
+		$this->assertSame([
+			SystemMock::customWarning(),
+			[
+				'id'    => 'local',
+				'icon'  => 'info',
+				'theme' => 'info',
+				'text'  => 'The site is running locally with relaxed security checks'
+			],
+			SystemMock::compilerWarning()
+		], $security);
+	}
+
+	public function testSecurityWithoutCompilerWarning(): void
+	{
+		$this->app = $this->app->clone([
+			'options' => [
+				'panel.vue.compiler' => true
+			]
+		]);
+
+		$controller = new SystemViewController();
+		$security   = $controller->security();
+		$this->assertSame([
+			SystemMock::customWarning()
+		], $security);
+	}
+
+	public function testSecurityWithoutUpdate(): void
+	{
+		$this->app = $this->app->clone([
+			'options' => [
+				'updates' => false
+			]
+		]);
+
+		$controller = new SystemViewController();
+		$security   = $controller->security();
+		$this->assertSame([
+			SystemMock::compilerWarning()
+		], $security);
+	}
+
+	public function testSecurityMissingHttps(): void
+	{
+		$this->app = $this->app->clone([
+			'options' => [
+				'url' => 'http://example.com'
+			]
+		]);
+
+		$controller = new SystemViewController();
+		$security   = $controller->security();
+		$this->assertSame([
+			SystemMock::customWarning(),
+			[
+				'id'   => 'https',
+				'text' => 'We recommend HTTPS for all your sites',
+				'link' => 'https://getkirby.com/security/https'
+			],
+			SystemMock::compilerWarning()
+		], $security);
+	}
+
+	public function testStats(): void
+	{
+		$controller = new SystemViewController();
+		$stats      = $controller->stats();
+		$this->assertInstanceOf(Stats::class, $stats);
+		$this->assertCount(4, $stats->reports());
+	}
+
+	public function testUrls(): void
+	{
+		$controller = new SystemViewController();
+		$urls       = $controller->urls();
+		$this->assertCount(4, $urls);
+	}
+
+	public function testUrlsLocal(): void
+	{
+		$this->app = $this->app->clone([
+			'server' => [
+				'REMOTE_ADDR' => '127.0.0.1',
+			]
+		]);
+
+		$controller = new SystemViewController();
+		$urls       = $controller->urls();
+		$this->assertCount(0, $urls);
+	}
+}
