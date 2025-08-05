acbb523aab895ff603ad257147cf6db976b0eea3
Nico Hoffmann  ෴. <me@nhoffmann.com>
2025-08-05 16:16:18 +0200

    refact: Users view as controller (#7476)

--- config/areas/users/views.php
+++ config/areas/users/views.php
@@ -1,54 +1,12 @@
 <?php
 
-use Kirby\Cms\App;
 use Kirby\Cms\Find;
-use Kirby\Panel\Collector\UsersCollector;
-use Kirby\Panel\Ui\Button\ViewButtons;
-use Kirby\Panel\Ui\Item\UserItem;
+use Kirby\Panel\Controller\View\UsersViewController;
 
 return [
 	'users' => [
 		'pattern' => 'users',
-		'action'  => function () {
-			$kirby = App::instance();
-			$role  = $kirby->request()->get('role');
-			$roles = $kirby->roles()->toArray(fn ($role) => [
-				'id'    => $role->id(),
-				'title' => $role->title(),
-			]);
-
-			return [
-				'component' => 'k-users-view',
-				'props'     => [
-					'buttons' => fn () =>
-						ViewButtons::view('users')
-							->defaults('create')
-							->bind(['role' => $role])
-							->render(),
-					'role' => function () use ($roles, $role) {
-						if ($role) {
-							return $roles[$role] ?? null;
-						}
-					},
-					'roles' => array_values($roles),
-					'users' => function () use ($kirby, $role) {
-						$collector = new UsersCollector(
-							limit: 20,
-							page: $kirby->request()->get('page', 1),
-							role: $role,
-							sortBy: 'username asc',
-						);
-
-						$users = $collector->models(paginated: true);
-
-						return [
-							'data'       => $users->values(fn ($user) => (new UserItem(user: $user))->props()),
-							'pagination' => $users->pagination()->toArray()
-						];
-					},
-				]
-			];
-		}
+		'action'  => UsersViewController::class
 	],
 	'user' => [
 		'pattern' => 'users/(:any)',
