a7c94e891061993567a69b17da25088ae5f272d6
Nico Hoffmann <me@nhoffmann.com>
2025-10-25 11:58:05 +0200

    refact!: Clean up `VersionsButton`
    BREAKING CHANGE:  `$versionId` parameter for `Kirby\Panel\Ui\Button\VersionsButton::__construct()` has been renamed to `$mode`

--- src/Panel/Ui/Button/VersionsButton.php
+++ src/Panel/Ui/Button/VersionsButton.php
@@ -21,37 +21,94 @@ class VersionsButton extends ViewButton
 {
 	public function __construct(
 		ModelWithContent $model,
-		VersionId|string $versionId = 'latest'
+		public string $mode = 'latest'
 	) {
-		$versionId = $versionId === 'compare' ? 'compare' : VersionId::from($versionId)->value();
-		$viewUrl   = $model->panel()->url(true) . '/preview';
-
 		parent::__construct(
+			model: $model,
 			class: 'k-versions-view-button',
-			icon: $versionId === 'compare' ? 'layout-columns' : 'git-branch',
-			options: [
-				[
-					'label'   => I18n::translate('version.latest'),
-					'icon'    => 'git-branch',
-					'link'    => $viewUrl . '/latest',
-					'current' => $versionId === 'latest'
-				],
-				[
-					'label'   => I18n::translate('version.changes'),
-					'icon'    => 'git-branch',
-					'link'    => $viewUrl . '/changes',
-					'current' => $versionId === 'changes'
-				],
-				'-',
-				[
-					'label'   => I18n::translate('version.compare'),
-					'icon'    => 'layout-columns',
-					'link'    => $viewUrl . '/compare',
-					'current' => $versionId === 'compare'
-				],
-
-			],
-			text: I18n::translate('version.' . $versionId),
+			icon: $this->icon(),
+			text: $this->i18n('version.' . $this->mode()),
 		);
+	}
+
+	/**
+	 * Returns the button icon based on the view's mode
+	 * @since 5.1.0
+	 */
+	public function icon(): string
+	{
+		return match ($this->mode) {
+			'compare' => 'layout-columns',
+			default   => 'git-branch',
+		};
+	}
+
+	/**
+	 * Whether the given mode is the current mode
+	 * @since 5.1.0
+	 */
+	public function isCurrent(string $mode): bool
+	{
+		return $this->mode() === $mode;
+	}
+
+	/**
+	 * Returns the view's mode using the proper
+	 * values for version IDs
+	 * @since 5.1.0
+	 */
+	public function mode(): string
+	{
+		return match ($this->mode) {
+			'compare' => 'compare',
+			default   => VersionId::from($this->mode)->value()
+		};
+	}
+
+	/**
+	 * Returns the options for the dropdown
+	 * @since 5.1.0
+	 */
+	public function options(): array
+	{
+		return $this->options ??= [
+			[
+				'label'   => I18n::translate('version.latest'),
+				'icon'    => 'git-branch',
+				'link'    => $this->url('latest'),
+				'current' => $this->isCurrent('latest')
+			],
+			[
+				'label'   => I18n::translate('version.changes'),
+				'icon'    => 'git-branch',
+				'link'    => $this->url('changes'),
+				'current' => $this->isCurrent('changes')
+			],
+			'-',
+			[
+				'label'   => I18n::translate('version.compare'),
+				'icon'    => 'layout-columns',
+				'link'    => $this->url('compare'),
+				'current' => $this->isCurrent('compare')
+			],
+
+		];
+	}
+
+	public function props(): array
+	{
+		return [
+			...parent::props(),
+			'options' => $this->options()
+		];
+	}
+
+	/**
+	 * Returns the preview view URL for the given version ID
+	 * @since 5.1.0
+	 */
+	public function url(string $versionId): string
+	{
+		return $this->model->panel()->url(true) . '/preview/' . $versionId;
 	}
 }
