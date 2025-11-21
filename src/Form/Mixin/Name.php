38082f270017df0c0aa7f224c733648fd7566e85
Bastian Allgeier <mail@bastianallgeier.com>
2025-11-21 12:13:30 +0100

    refact!: Move lower-casing of field names into the getter

--- src/Form/Mixin/Name.php
+++ src/Form/Mixin/Name.php
@@ -4,15 +4,15 @@ namespace Kirby\Form\Mixin;
 
 trait Name
 {
-	protected string|null $name;
+	protected string|null $name = null;
 
 	public function name(): string
 	{
-		return $this->name ?? $this->type();
+		return strtolower($this->name ?? $this->type());
 	}
 
-	protected function setName(string|null $name = null): void
+	protected function setName(string|null $name): void
 	{
-		$this->name = strtolower($name ?? $this->type());
+		$this->name = $name;
 	}
 }
