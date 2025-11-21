076ecb19c7dc55db257dce303b4918c5ba990cad
Bastian Allgeier <mail@bastianallgeier.com>
2025-11-21 12:09:37 +0100

    refact! Remove default value for the setMax method

--- src/Form/Mixin/Max.php
+++ src/Form/Mixin/Max.php
@@ -7,14 +7,14 @@ trait Max
 	/**
 	 * Sets the maximum number of allowed items in the field
 	 */
-	protected int|null $max;
+	protected int|null $max = null;
 
 	public function max(): int|null
 	{
 		return $this->max;
 	}
 
-	protected function setMax(int|null $max = null)
+	protected function setMax(int|null $max): void
 	{
 		$this->max = $max;
 	}
