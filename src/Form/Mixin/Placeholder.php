02524d033d002d882b0450f23659b810e252b051
Bastian Allgeier <mail@bastianallgeier.com>
2025-11-24 13:15:18 +0100

    refact!: Remove default value for placeholder

--- src/Form/Mixin/Placeholder.php
+++ src/Form/Mixin/Placeholder.php
@@ -7,7 +7,7 @@ trait Placeholder
 	/**
 	 * Optional placeholder value that will be shown when the field is empty
 	 */
-	protected array|string|null $placeholder = null;
+	protected array|string|null $placeholder;
 
 	public function placeholder(): string|null
 	{
