a325c70f3bc662e056a8bc5c04cf349ed305791f
Bastian Allgeier <mail@bastianallgeier.com>
2025-11-25 14:42:11 +0100

    feat: New Text mixin

---
+++ src/Form/Mixin/Text.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Kirby\Form\Mixin;
+
+trait Text
+{
+	/**
+	 * Text to be displayed
+	 */
+	protected array|string|null $text;
+
+	public function text(): string|null
+	{
+		if ($this->text !== null && $this->text !== [] && $this->text !== '') {
+			$text = $this->i18n($this->text);
+			$text = $this->stringTemplate($text);
+			$text = $this->kirby()->kirbytext($text);
+			return $text;
+		}
+
+		return null;
+	}
+
+	protected function setText(array|string|null $text): void
+	{
+		$this->text = $text;
+	}
+}
