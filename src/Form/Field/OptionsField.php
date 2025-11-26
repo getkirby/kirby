3dae4f500e9edd4051618ffdb3b3e8d38b8b28b6
Bastian Allgeier <mail@bastianallgeier.com>
2025-11-26 13:10:08 +0100

    refact: New OptionsField class

---
+++ src/Form/Field/OptionsField.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Kirby\Form\Field;
+
+use Kirby\Form\Mixin;
+
+/**
+ * Options Field
+ *
+ * @package   Kirby Field
+ * @author    Bastian Allgeier <bastian@getkirby.com>
+ * @link      https://getkirby.com
+ * @copyright Bastian Allgeier
+ * @license   https://getkirby.com/license
+ * @since     6.0.0
+ */
+abstract class OptionsField extends InputField
+{
+	use Mixin\Max;
+	use Mixin\Min;
+	use Mixin\Options;
+
+	protected mixed $value = [];
+
+	public function __construct(
+		bool|null $autofocus = null,
+		mixed $default = null,
+		bool|null $disabled = null,
+		array|string|null $help = null,
+		array|string|null $label = null,
+		int|null $max = null,
+		int|null $min = null,
+		string|null $name = null,
+		array|string|null $options = null,
+		bool|null $required = null,
+		bool|null $translate = null,
+		array|null $when = null,
+		string|null $width = null
+	) {
+		parent::__construct(
+			autofocus: $autofocus,
+			default: $default,
+			disabled: $disabled,
+			help: $help,
+			label: $label,
+			name: $name,
+			required: $required,
+			translate: $translate,
+			when: $when,
+			width: $width
+		);
+
+		$this->max     = $max;
+		$this->min     = $min;
+		$this->options = $options;
+	}
+
+	public function props(): array
+	{
+		return [
+			...parent::props(),
+			'options' => $this->options(),
+		];
+	}
+
+	protected function validations(): array
+	{
+		return [
+			'options',
+			'max',
+			'min'
+		];
+	}
+}
