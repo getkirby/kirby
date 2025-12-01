fc59b7184143be9d1f7cc19f2391a97b01174806
Bastian Allgeier <mail@bastianallgeier.com>
2025-12-01 13:45:06 +0100

    fix: Set correct percentages for gravities

--- src/Image/Gravity.php
+++ src/Image/Gravity.php
@@ -27,14 +27,14 @@ enum Gravity: string
 	public function toPercentageString(): string
 	{
 		return match ($this) {
-			static::TOP          => '0% 50%',
+			static::TOP          => '50% 0%',
 			static::TOP_LEFT     => '0% 0%',
-			static::TOP_RIGHT    => '0% 100%',
-			static::LEFT         => '50% 0%',
+			static::TOP_RIGHT    => '100% 0%',
+			static::LEFT         => '0% 50%',
 			static::CENTER       => '50% 50%',
-			static::RIGHT        => '50% 100%',
-			static::BOTTOM       => '100% 50%',
-			static::BOTTOM_LEFT  => '100% 0%',
+			static::RIGHT        => '100% 50%',
+			static::BOTTOM       => '50% 100%',
+			static::BOTTOM_LEFT  => '0% 100%',
 			static::BOTTOM_RIGHT => '100% 100%',
 		};
 	}
