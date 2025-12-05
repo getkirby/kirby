c0b35e27d49ad22d2ba403209ad1f82292f52686
Bastian Allgeier <mail@bastianallgeier.com>
2025-12-05 12:04:14 +0100

    feat: New stack component

---
+++ panel/src/components/Layout/Stack.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="k-stack" :style="{ flexDirection: direction, gap }">
+		<slot />
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		direction: String,
+		gap: String
+	}
+};
+</script>
+
+<style>
+:where(.k-stack) {
+	display: flex;
+	flex-direction: column;
+	container-type: inline-size;
+	gap: var(--spacing-6);
+}
+</style>
