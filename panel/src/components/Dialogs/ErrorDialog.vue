cfa8d5464a874676c528a64de5c0ea9745e55b2c
Bastian Allgeier <mail@bastianallgeier.com>
2025-12-05 12:05:41 +0100

    refact: Use new error components

--- panel/src/components/Dialogs/ErrorDialog.vue
+++ panel/src/components/Dialogs/ErrorDialog.vue
@@ -9,25 +9,7 @@
 		@cancel="$emit('cancel')"
 	>
 		<k-text>{{ message }}</k-text>
-		<dl v-if="detailsList.length" class="k-error-details">
-			<template v-for="(detail, index) in detailsList" :key="'detail-' + index">
-				<dt>
-					{{ detail.label }}
-				</dt>
-				<dd>
-					<template v-if="typeof detail.message === 'object'">
-						<ul>
-							<li v-for="(msg, msgIndex) in detail.message" :key="msgIndex">
-								{{ msg }}
-							</li>
-						</ul>
-					</template>
-					<template v-else>
-						{{ detail.message }}
-					</template>
-				</dd>
-			</template>
-		</dl>
+		<k-error-details :details="detailsList" />
 	</k-dialog>
 </template>
 
@@ -52,36 +34,3 @@ export default {
 	}
 };
 </script>
-
-<style>
-.k-error-details {
-	background: var(--input-color-back);
-	display: block;
-	overflow: auto;
-	padding: 1rem;
-	border-radius: var(--rounded);
-	font-size: var(--text-sm);
-	line-height: 1.25em;
-	margin-top: 0.75rem;
-}
-.k-error-details dt {
-	color: var(--color-red-500);
-	margin-bottom: 0.25rem;
-}
-.k-error-details dd {
-	overflow: hidden;
-	overflow-wrap: break-word;
-	text-overflow: ellipsis;
-}
-.k-error-details dd:not(:last-of-type) {
-	margin-bottom: 1.5em;
-}
-.k-error-details li {
-	white-space: pre-line;
-}
-.k-error-details li:not(:last-child) {
-	border-bottom: 1px solid var(--color-border);
-	padding-bottom: 0.25rem;
-	margin-bottom: 0.25rem;
-}
-</style>
