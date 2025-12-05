1ff5fe8ff9b09f97538363eb7ac738a19c8d33fa
Bastian Allgeier <mail@bastianallgeier.com>
2025-12-05 07:52:56 +0100

    feat: New k-code-token component
    fix: New default for the token component

--- panel/src/components/Lab/DocsTypes.vue
+++ panel/src/components/Lab/DocsTypes.vue
@@ -23,40 +23,4 @@ export default {
 	flex-wrap: wrap;
 	gap: var(--spacing-1);
 }
-
-.k-lab-docs-types.k-text code {
-	color: light-dark(var(--color-gray-800), var(--color-gray-900));
-	outline-color: light-dark(var(--color-gray-400), var(--color-gray-900));
-	background: var(--color-gray-300);
-}
-.k-lab-docs-types code:is([data-type="boolean"], [data-type="Boolean"]) {
-	color: light-dark(var(--color-purple-800), var(--color-purple-900));
-	outline-color: light-dark(var(--color-purple-400), var(--color-purple-900));
-	background: var(--color-purple-300);
-}
-.k-lab-docs-types code:is([data-type="string"], [data-type="String"]) {
-	color: light-dark(var(--color-green-800), var(--color-green-900));
-	outline-color: light-dark(var(--color-green-500), var(--color-green-900));
-	background: var(--color-green-300);
-}
-.k-lab-docs-types code:is([data-type="number"], [data-type="Number"]) {
-	color: light-dark(var(--color-orange-800), var(--color-orange-900));
-	outline-color: light-dark(var(--color-orange-500), var(--color-orange-900));
-	background: var(--color-orange-300);
-}
-.k-lab-docs-types code:is([data-type="array"], [data-type="Array"]) {
-	color: light-dark(var(--color-aqua-800), var(--color-aqua-900));
-	outline-color: light-dark(var(--color-aqua-500), var(--color-aqua-900));
-	background: var(--color-aqua-300);
-}
-.k-lab-docs-types code:is([data-type="object"], [data-type="Object"]) {
-	color: light-dark(var(--color-yellow-800), var(--color-yellow-900));
-	outline-color: light-dark(var(--color-yellow-500), var(--color-yellow-900));;
-	background: var(--color-yellow-300);
-}
-.k-lab-docs-types code[data-type="func"] {
-	color: light-dark(var(--color-pink-800), var(--color-pink-900));
-	outline-color: var(--color-pink-400);
-	background: var(--color-pink-300);
-}
 </style>
--- panel/src/components/Text/index.js
+++ panel/src/components/Text/index.js
@@ -1,4 +1,5 @@
 import Code from "./Code.vue";
+import CodeToken from "./CodeToken.vue";
 import Headline from "./Headline.vue";
 import Label from "./Label.vue";
 import Text from "./Text.vue";
@@ -6,6 +7,7 @@ import Text from "./Text.vue";
 export default {
 	install(app) {
 		app.component("k-code", Code);
+		app.component("k-code-token", CodeToken);
 		app.component("k-headline", Headline);
 		app.component("k-label", Label);
 		app.component("k-text", Text);
