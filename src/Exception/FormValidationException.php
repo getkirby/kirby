b544a1421f81030c908acbdd5a4467fa21aa194d
Bastian Allgeier <mail@bastianallgeier.com>
2025-12-05 14:48:41 +0100

    feat: New FormValidationException

---
+++ src/Exception/FormValidationException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Kirby\Exception;
+
+/**
+ * Thrown when a form or value validation fails
+ *
+ * @package   Kirby Exception
+ * @author    Bastian Allgeier <bastian@getkirby.com>
+ * @link      https://getkirby.com
+ * @copyright Bastian Allgeier
+ * @license   https://opensource.org/licenses/MIT
+ * @since     6.0.0
+ */
+class FormValidationException extends InvalidArgumentException
+{
+	protected static string $defaultKey = 'form.validation';
+	protected static string $defaultFallback = 'Form validation failed';
+	protected static int $defaultHttpCode = 400;
+	protected static array $defaultData = [];
+}
