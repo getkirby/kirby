c613ccc428115272d2bcf922beea0d1b42da462e
Bastian Allgeier <mail@bastianallgeier.com>
2025-12-11 14:20:19 +0100

    feat: New Definitions components (#7767)

---
+++ panel/lab/components/definitions/1_definitions/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<k-lab-examples>
+		<k-lab-example label="Definitions">
+			<k-definitions>
+				<k-definition term="Term" description="Description" />
+				<k-definition term="Term" description="Description" />
+				<k-definition term="Term" description="Description" />
+			</k-definitions>
+		</k-lab-example>
+	</k-lab-examples>
+</template>
---
+++ panel/src/components/Layout/Definitions.vue
@@ -0,0 +1,16 @@
+<template>
+	<dl class="k-definitions">
+		<slot />
+	</dl>
+</template>
+
+<style>
+.k-definitions {
+	background: var(--table-color-back);
+	box-shadow: var(--shadow);
+	border-radius: var(--rounded);
+	overflow: hidden;
+	line-height: 1.25;
+	container-type: inline-size;
+}
+</style>
