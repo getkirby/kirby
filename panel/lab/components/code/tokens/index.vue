81cc34c6f8bb2d126e3e1b4f66abdedde3621bc9
Bastian Allgeier <mail@bastianallgeier.com>
2025-12-11 10:38:31 +0100

    feat: New k-code-token component (#7765)
    fix: New default for the token component

---
+++ panel/lab/components/code/tokens/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<k-lab-examples>
+		<k-lab-example label="Colors" :flex="true">
+			<k-code-token type="black">black</k-code-token>
+			<k-code-token type="white">white</k-code-token>
+			<k-code-token type="gray">gray</k-code-token>
+			<k-code-token type="red">red</k-code-token>
+			<k-code-token type="orange">orange</k-code-token>
+			<k-code-token type="yellow">yellow</k-code-token>
+			<k-code-token type="green">green</k-code-token>
+			<k-code-token type="aqua">aqua</k-code-token>
+			<k-code-token type="blue">blue</k-code-token>
+			<k-code-token type="purple">purple</k-code-token>
+			<k-code-token type="pink">pink</k-code-token>
+		</k-lab-example>
+		<k-lab-example label="Types" :flex="true">
+			<k-code-token>Default</k-code-token>
+			<k-code-token type="array">Array</k-code-token>
+			<k-code-token type="number">Number</k-code-token>
+			<k-code-token type="string">String</k-code-token>
+			<k-code-token type="boolean">Bool</k-code-token>
+			<k-code-token type="func">Func</k-code-token>
+			<k-code-token type="object">Object</k-code-token>
+			<k-code-token type="undefined">Undefined</k-code-token>
+		</k-lab-example>
+	</k-lab-examples>
+</template>
