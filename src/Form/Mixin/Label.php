cdbfb0a39ba1d636e059a0ad79fce7dfd3c5e622
Nico Hoffmann <me@nhoffmann.com>
2025-12-13 14:11:02 +0100

    feat: Models sections as fields

--- src/Form/Mixin/Label.php
+++ src/Form/Mixin/Label.php
@@ -7,14 +7,15 @@ use Kirby\Toolkit\Str;
 trait Label
 {
 	/**
-	 * The field label can be set as string or associative array with translations
+	 * The field label can be set as string or
+	 * associative array with translations
 	 */
 	protected array|string|null $label;
 
 	public function label(): string|null
 	{
 		if ($this->label === null || $this->label === []) {
-			return Str::ucfirst($this->name());
+			return Str::label($this->name());
 		}
 
 		return $this->stringTemplateI18n($this->label);
