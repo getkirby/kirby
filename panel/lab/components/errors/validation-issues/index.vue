b993d0686d5dcb45a752a3db865c71c05b7c20e3
Bastian Allgeier <mail@bastianallgeier.com>
2025-12-13 10:07:55 +0100

    feat: New k-validation-issues component

---
+++ panel/lab/components/errors/validation-issues/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<k-lab-examples>
+		<k-lab-example label="Validation Issues">
+			<k-validation-issues
+				:fields="{
+					title: {
+						label: 'Title',
+						issues: ['The field is rquired']
+					},
+					text: {
+						label: 'Text',
+						issues: [
+							'The text must not be shorter than 10 characters',
+							'The text must not be longer than 140 characters'
+						]
+					},
+					email: {
+						label: 'Email',
+						issues: ['Enter a valid email address']
+					}
+				}"
+			/>
+		</k-lab-example>
+	</k-lab-examples>
+</template>
