2db7fcdeefbf86e2877d5fa824169325fae08c22
Nico Hoffmann <me@nhoffmann.com>
2025-12-19 23:04:03 +0100

    More stuff for FileDrawer

--- panel/src/components/Drawers/FileDrawer.vue
+++ panel/src/components/Drawers/FileDrawer.vue
@@ -8,7 +8,16 @@
 		@tab="$emit('tab', $event)"
 	>
 		<template #header>
-			<k-form-controls :has-diff="true" size="xs" />
+			<k-form-controls
+				:editor="editor"
+				:has-diff="hasDiff"
+				:is-locked="isLocked"
+				:is-processing="isSaving"
+				:modified="modified"
+				size="xs"
+				@discard="onDiscard"
+				@submit="onSubmit"
+			/>
 		</template>
 
 		<k-file-preview
@@ -41,6 +50,7 @@ import FileView from "@/components/Views/Files/FileView.vue";
 export default {
 	extends: FileView,
 	mixins: [Drawer],
+	feature: "drawer",
 	props: {
 		tab: {
 			type: Object,
@@ -54,3 +64,17 @@ export default {
 	emits: ["cancel", "input", "submit", "tab"]
 };
 </script>
+
+<style>
+.k-file-drawer .k-drawer-header {
+	position: sticky;
+	top: 0;
+	z-index: var(--z-toolbar);
+}
+
+.k-file-drawer .k-form-controls-button {
+	font-size: var(--text-xs);
+	--button-rounded: 3px;
+	--icon-size: 1rem;
+}
+</style>
