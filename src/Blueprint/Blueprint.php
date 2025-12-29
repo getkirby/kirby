d4b7a46e6f5c486f310214a31874ae798e250221
Bastian Allgeier <mail@bastianallgeier.com>
2025-12-29 14:13:41 +0100

    refact!: New AcceptRules class to outsource code from the blueprint class

--- src/Blueprint/Blueprint.php
+++ src/Blueprint/Blueprint.php
@@ -31,13 +31,12 @@ class Blueprint
 	public static array $presets = [];
 	public static array $loaded = [];
 
+	protected AcceptRules $acceptRules;
 	protected array $fields = [];
 	protected ModelWithContent $model;
 	protected array $props;
 	protected array $sections = [];
 	protected array $tabs = [];
-
-	protected array|null $fileTemplates = null;
 
 	/**
 	 * Creates a new blueprint object with the given props
@@ -107,114 +106,21 @@ class Blueprint
 
 	/**
 	 * Gathers what file templates are allowed in
+	 * a model based on the blueprint
+	 * @since 6.0.0
+	 */
+	public function acceptRules(): AcceptRules
+	{
+		return $this->acceptRules ??= new AcceptRules($this);
+	}
+
+	/**
+	 * Gathers what file templates are allowed in
 	 * this model based on the blueprint
 	 */
 	public function acceptedFileTemplates(string|null $inSection = null): array
 	{
-		// get cached results for the current file model
-		// (except when collecting for a specific section)
-		if ($inSection === null && $this->fileTemplates !== null) {
-			return $this->fileTemplates; // @codeCoverageIgnore
-		}
-
-		$templates = [];
-
-		// collect all allowed file templates from blueprint…
-		foreach ($this->sections() as $section) {
-			// if collecting for a specific section, skip all others
-			if ($inSection !== null && $section->name() !== $inSection) {
-				continue;
-			}
-
-			$templates = match ($section->type()) {
-				'files'  => [...$templates, $section->template() ?? 'default'],
-				'fields' => [
-					...$templates,
-					...$this->acceptedFileTemplatesFromFields($section->fields())
-				],
-				default  => $templates
-			};
-		}
-
-		// no caching for when collecting for specific section
-		if ($inSection !== null) {
-			return $templates; // @codeCoverageIgnore
-		}
-
-		return $this->fileTemplates = $templates;
-	}
-
-	/**
-	 * Gathers the allowed file templates from model's fields
-	 */
-	protected function acceptedFileTemplatesFromFields(array $fields): array
-	{
-		$templates = [];
-
-		foreach ($fields as $field) {
-			// fields with uploads settings
-			if (isset($field['uploads']) === true && is_array($field['uploads']) === true) {
-				$templates = [
-					...$templates,
-					...$this->acceptedFileTemplatesFromFieldUploads($field['uploads'])
-				];
-				continue;
-			}
-
-			// structure and object fields
-			if (isset($field['fields']) === true && is_array($field['fields']) === true) {
-				$templates = [
-					...$templates,
-					...$this->acceptedFileTemplatesFromFields($field['fields']),
-				];
-				continue;
-			}
-
-			// layout and blocks fields
-			if (isset($field['fieldsets']) === true && is_array($field['fieldsets']) === true) {
-				$templates = [
-					...$templates,
-					...$this->acceptedFileTemplatesFromFieldsets($field['fieldsets'])
-				];
-				continue;
-			}
-		}
-
-		return $templates;
-	}
-
-	/**
-	 * Gathers the allowed file templates from fieldsets
-	 */
-	protected function acceptedFileTemplatesFromFieldsets(array $fieldsets): array
-	{
-		$templates = [];
-
-		foreach ($fieldsets as $fieldset) {
-			foreach (($fieldset['tabs'] ?? []) as $tab) {
-				$templates = [
-					...$templates,
-					...$this->acceptedFileTemplatesFromFields($tab['fields'] ?? [])
-				];
-			}
-		}
-
-		return $templates;
-	}
-
-	/**
-	 * Extracts templates from field uploads settings
-	 */
-	protected function acceptedFileTemplatesFromFieldUploads(array $uploads): array
-	{
-		// only if the `uploads` parent is this model
-		if ($target = $uploads['parent'] ?? null) {
-			if ($this->model->id() !== $target) {
-				return [];
-			}
-		}
-
-		return [($uploads['template'] ?? 'default')];
+		return $this->acceptRules()->acceptedFileTemplates($inSection);
 	}
 
 	/**
