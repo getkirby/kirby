177bfac4eaf116e3d086c4b3e88cece727e8bd4d
Nico Hoffmann <me@nhoffmann.com>
2026-01-02 22:40:41 +0100

    test: Use auth test case

--- tests/Auth/Exception/ChallengeTimeoutExceptionTest.php
+++ tests/Auth/Exception/ChallengeTimeoutExceptionTest.php
@@ -2,8 +2,8 @@
 
 namespace Kirby\Auth\Exception;
 
-use Kirby\TestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
 
 #[CoversClass(ChallengeTimeoutException::class)]
 class ChallengeTimeoutExceptionTest extends TestCase
