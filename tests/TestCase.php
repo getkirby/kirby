0558b5877efd52c89f9d977df3e27fcb5ce9c877
Nico Hoffmann  ෴. <me@nhoffmann.com>
2026-01-13 18:17:25 +0100

    Merge pull request #7803 from getkirby/v6/test/paratest
    test: Use ParaTest to run PHP unit tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,17 +2,73 @@
 
 namespace Kirby;
 
+use Closure;
 use Kirby\Cms\App;
 use Kirby\Cms\File;
 use Kirby\Cms\Page;
 use Kirby\Cms\Site;
 use Kirby\Cms\User;
+use Kirby\Data\Data;
 use Kirby\Filesystem\Dir;
+use Kirby\Toolkit\I18n;
+use Kirby\Toolkit\Locale;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
 	protected App $app;
+
+	protected string|Closure|null $i18nLocale;
+	protected string|array|Closure|null $i18nFallback;
+	protected Closure|null $i18nLoad;
+	protected array $i18nTranslations;
+	protected array|string $localeBackup;
+
+	public function setUp(): void
+	{
+		App::destroy();
+
+		$translationRoot = dirname(__DIR__) . '/i18n/translations';
+
+		$this->localeBackup = Locale::get();
+		Locale::set('C');
+
+		$this->i18nLocale = I18n::$locale;
+		$this->i18nFallback = I18n::$fallback;
+		$this->i18nLoad = I18n::$load;
+		$this->i18nTranslations = I18n::$translations;
+
+		I18n::$locale = 'en';
+		I18n::$fallback = 'en';
+		I18n::$load = function (string $locale) use ($translationRoot): array {
+			$file = $translationRoot . '/' . basename($locale) . '.json';
+			return Data::read($file, fail: false);
+		};
+		I18n::$translations = [];
+	}
+
+	public function tearDown(): void
+	{
+		if (isset($this->localeBackup) === true) {
+			Locale::set($this->localeBackup);
+		}
+
+		if (isset($this->i18nLocale) === true) {
+			I18n::$locale = $this->i18nLocale;
+		}
+
+		if (isset($this->i18nFallback) === true) {
+			I18n::$fallback = $this->i18nFallback;
+		}
+
+		if (isset($this->i18nLoad) === true) {
+			I18n::$load = $this->i18nLoad;
+		}
+
+		if (isset($this->i18nTranslations) === true) {
+			I18n::$translations = $this->i18nTranslations;
+		}
+	}
 
 	/**
 	 * Whether $actual is a File object
