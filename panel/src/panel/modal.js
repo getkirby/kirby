9246894157ea0942a0bcefe4fc9117e1ba1d60ba
Nico Hoffmann  ෴. <me@nhoffmann.com>
2026-02-02 15:04:01 +0100

    Merge pull request #7903 from getkirby/fix/nested-modal-submit
    fix: Do not reload view on dialog/drawer submit

--- panel/src/panel/modal.js
+++ panel/src/panel/modal.js
@@ -295,8 +295,16 @@ export default (panel, key, defaults) => {
 				// handle any redirects
 				this.successRedirect(success);
 			} else {
-				// reload the parent view to show changes
-				panel.view.reload(success.reload);
+				if (panel.dialog.isOpen === true) {
+					panel.dialog.refresh();
+				}
+
+				if (panel.drawer.isOpen === true) {
+					panel.drawer.refresh();
+				}
+
+				// refresh the parent view to show changes
+				panel.view.refresh(success.reload);
 			}
 
 			return success;
