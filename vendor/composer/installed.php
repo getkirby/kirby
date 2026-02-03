4226919c9df1192afcf08db183f4f0f3073a8ef6
Bastian Allgeier <mail@bastianallgeier.com>
2026-02-03 12:41:19 +0100

    chore: Update composer dependencies

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -98,9 +98,9 @@
             'dev_requirement' => false,
         ),
         'phpmailer/phpmailer' => array(
-            'pretty_version' => 'v7.0.1',
-            'version' => '7.0.1.0',
-            'reference' => '360ae911ce62e25e11249f6140fa58939f556ebe',
+            'pretty_version' => 'v7.0.2',
+            'version' => '7.0.2.0',
+            'reference' => 'ebf1655bd5b99b3f97e1a3ec0a69e5f4cd7ea088',
             'type' => 'library',
             'install_path' => __DIR__ . '/../phpmailer/phpmailer',
             'aliases' => array(),
