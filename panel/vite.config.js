2b181df4a26021fb31eb8b70a3fbeff2b03ed522
Nico Hoffmann <me@nhoffmann.com>
2026-02-23 18:38:40 +0100

    test: Tweak vitest config

--- panel/vite.config.js
+++ panel/vite.config.js
@@ -96,8 +96,10 @@ function createPlugins(mode) {
  */
 function createTest() {
 	return {
+		css: false,
 		environment: "happy-dom",
 		include: ["**/*.test.js"],
+		reporter: "dot",
 		setupFiles: ["vitest.setup.js"]
 	};
 }
