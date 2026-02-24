43f274750e3ed51d53a03812f3247fac84e0ce61
Nico Hoffmann <me@nhoffmann.com>
2026-02-24 13:35:44 +0100

    test: Remove dead resize observer mock

--- panel/vitest.setup.js
+++ panel/vitest.setup.js
@@ -1,10 +1,3 @@
 import { createApp } from "vue";
 
 globalThis.app ??= createApp();
-
-// mock for ResizeObserver
-globalThis.ResizeObserver = class ResizeObserver {
-	observe() {}
-	unobserve() {}
-	disconnect() {}
-};
