6ac901dbc4bdce316c5b79207d81bcd3a48704e1
Nico Hoffmann <me@nhoffmann.com>
2026-02-26 22:33:06 +0100

    test: Fix TS unit test names

--- panel/src/helpers/array.split.test.ts
+++ panel/src/helpers/array.split.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { split } from "./array";
 
-describe.concurrent("Array.split()", () => {
+describe.concurrent("$helper.array.split()", () => {
 	it("should split array into groups", () => {
 		const array = ["a", "b", "|", "c", "d", "|", "e", "f"];
 		const expected = [
