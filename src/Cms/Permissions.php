3ab61baff14ca42340c335981e53c5ea99f7a59f
Lukas Kleinschmidt <github@kleinschmidt.mozmail.com>
2026-03-17 11:54:48 +0100

    chore: Cleanup

--- src/Cms/Permissions.php
+++ src/Cms/Permissions.php
@@ -103,11 +103,6 @@ class Permissions
 	{
 		$defaults = $this->defaults;
 
-		$update = static fn($value) => [
-			'edit' => $value,
-			'save' => $value,
-		];
-
 		// dynamically register the extended actions
 		foreach (static::$extendedActions as $key => $actions) {
 			if (isset($defaults[$key]) === true) {
@@ -118,6 +113,11 @@ class Permissions
 
 			$defaults[$key] = $actions;
 		}
+
+		$update = static fn ($value) => [
+			'edit' => $value,
+			'save' => $value,
+		];
 
 		$this->actions = $this->normalize(
 			settings: $settings,
