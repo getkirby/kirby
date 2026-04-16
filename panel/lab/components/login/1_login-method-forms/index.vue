18248ec2b0a3c38ddba0b9738169a6530204fed5
Nico Hoffmann <me@nhoffmann.com>
2026-04-16 16:47:29 +0200

    feat: New login Vue components

---
+++ panel/lab/components/login/1_login-method-forms/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<k-lab-examples>
+		<k-lab-example label="Password method">
+			<k-login-password-method-form
+				:submit="{ label: 'Login', icon: 'check' }"
+			/>
+		</k-lab-example>
+		<k-lab-example label="Password method (prefilled)">
+			<k-login-password-method-form
+				:submit="{ label: 'Login', icon: 'check' }"
+				:value="{
+					email: 'editor@example.com',
+					password: '12345678',
+					long: true
+				}"
+			/>
+		</k-lab-example>
+		<k-lab-example label="Code method">
+			<k-login-code-method-form
+				:submit="{ label: 'Send code', icon: 'email' }"
+			/>
+		</k-lab-example>
+		<k-lab-example label="Code method (prefilled)">
+			<k-login-code-method-form
+				:submit="{ label: 'Send code', icon: 'email' }"
+				:value="{ email: 'editor@example.com', long: true }"
+			/>
+		</k-lab-example>
+		<k-lab-example label="Password reset method">
+			<k-login-password-reset-method-form
+				:submit="{ label: 'Send reset link', icon: 'email' }"
+			/>
+		</k-lab-example>
+	</k-lab-examples>
+</template>
---
+++ panel/src/components/Views/Login/LoginChallenges.vue
@@ -0,0 +1,36 @@
+<template>
+	<template v-if="alternativeChallenges.length > 0">
+		<p class="k-login-or">
+			<span>{{ $t("login.or") }}</span>
+		</p>
+
+		<k-stack>
+			<k-button
+				v-for="challenge in alternativeChallenges"
+				:key="challenge.type"
+				:text="challenge.label"
+				:icon="challenge.icon"
+				variant="filled"
+				size="lg"
+				@click="$emit('change', challenge)"
+			/>
+		</k-stack>
+	</template>
+</template>
+
+<script>
+export default {
+	props: {
+		challenges: {
+			type: Array,
+			required: true
+		}
+	},
+	emits: ["change"],
+	computed: {
+		alternativeChallenges() {
+			return this.challenges.filter((challenge) => !challenge.active);
+		}
+	}
+};
+</script>
