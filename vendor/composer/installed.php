56996390c4bac5ac3bfd946ec7519172aa00f7a9
Bastian Allgeier <mail@bastianallgeier.com>
2026-05-19 12:02:50 +0200

    fix: Update composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -98,9 +98,9 @@
             'dev_requirement' => false,
         ),
         'phpmailer/phpmailer' => array(
-            'pretty_version' => 'v7.0.2',
-            'version' => '7.0.2.0',
-            'reference' => 'ebf1655bd5b99b3f97e1a3ec0a69e5f4cd7ea088',
+            'pretty_version' => 'v7.1.1',
+            'version' => '7.1.1.0',
+            'reference' => '1bc1716a507a65e039d4ac9d9adebbbd0d346e15',
             'type' => 'library',
             'install_path' => __DIR__ . '/../phpmailer/phpmailer',
             'aliases' => array(),
