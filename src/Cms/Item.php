bf714950fbd44f834ff150f906f59d34b9b3f3c1
Nico Hoffmann  ෴. <me@nhoffmann.com>
2026-06-15 15:49:08 +0200

    [v6] fix: Use proper generic for `Item::$siblings` (#8194)
    fix: Use proper generic for `Item::$siblings`

--- src/Cms/Item.php
+++ src/Cms/Item.php
@@ -32,6 +32,10 @@ class Item
 	protected string $id;
 	protected array $params;
 	protected ModelWithContent $parent;
+
+	/**
+	 * @var TCollection
+	 */
 	protected Items $siblings;
 
 	/**
@@ -99,7 +103,7 @@ class Item
 	 * Returns the sibling collection
 	 * This is required by the HasSiblings trait
 	 *
-	 * @psalm-return self::ITEMS_CLASS
+	 * @return TCollection
 	 */
 	protected function siblingsCollection(): Items
 	{
