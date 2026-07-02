1789f39476bbcbbaeb2775a81b779f36fa589e82
Nico Hoffmann <me@nhoffmann.com>
2026-07-02 20:00:38 +0200

    fix: Writer field preview
    Fixes #8242

---
+++ panel/src/components/Forms/Previews/WriterFieldPreview.vue
@@ -0,0 +1,21 @@
+<script>
+import HtmlFieldPreview from "./HtmlFieldPreview.vue";
+
+export default {
+	extends: HtmlFieldPreview,
+	class: "k-writer-field-preview",
+	computed: {
+		html() {
+			return this.$helper.string.sanitizeHTML(this.value);
+		}
+	}
+};
+</script>
+
+<style>
+.k-writer-field-preview .k-text {
+	overflow-x: hidden;
+	text-overflow: ellipsis;
+	white-space: nowrap;
+}
+</style>
