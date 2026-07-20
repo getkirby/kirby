dc858218849bddfee55e511efa4ae64bdbc0a019
Nico Hoffmann  ෴. <me@nhoffmann.com>
2026-07-20 12:36:19 +0200

    fix: Escape attr/tags in block snippets (#120)

--- config/blocks/heading/heading.php
+++ config/blocks/heading/heading.php
@@ -1,2 +1,10 @@
-<?php /** @var \Kirby\Cms\Block $block */ ?>
-<<?= $level = $block->level()->or('h2') ?>><?= $block->text() ?></<?= $level ?>>
+<?php
+/** @var \Kirby\Cms\Block $block */
+$level = $block->level()->value();
+
+// ensure a valid heading tag
+if (in_array($level, ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'], true) === false) {
+	$level = 'h2';
+}
+?>
+<<?= $level ?>><?= $block->text() ?></<?= $level ?>>
