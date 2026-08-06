4e0b41c0cb521ad926869f1c7c5a2915521b6585
Bastian Allgeier <mail@bastianallgeier.com>
2026-08-06 18:14:29 +0200

    refact: Turn Cms\ModelPermissions into a boolean facade

--- src/Cms/UserPermissions.php
+++ src/Cms/UserPermissions.php
@@ -9,6 +9,7 @@ namespace Kirby\Cms;
  * @license   https://getkirby.com/license
  *
  * @extends ModelPermissions<User>
+ * @deprecated 6.0.0 Use `$user->guards()` instead
  */
 class UserPermissions extends ModelPermissions
 {
@@ -22,54 +23,5 @@ class UserPermissions extends ModelPermissions
 		ModelWithContent|Language $model
 	): string {
 		return $model->role()->id();
-	}
-
-	protected function canChangeRole(): bool
-	{
-		// protect admin from role changes by non-admin
-		if (
-			$this->model->isAdmin() === true &&
-			static::user()->isAdmin() !== true
-		) {
-			return false;
-		}
-
-		// prevent demoting the last admin
-		if ($this->model->isLastAdmin() === true) {
-			return false;
-		}
-
-		return true;
-	}
-
-	protected function canCreate(): bool
-	{
-		// the admin can always create new users
-		if (static::user()->isAdmin() === true) {
-			return true;
-		}
-
-		// users who are not admins cannot create admins
-		if ($this->model->isAdmin() === true) {
-			return false;
-		}
-
-		return true;
-	}
-
-	protected function canDelete(): bool
-	{
-		return $this->model->isLastAdmin() !== true;
-	}
-
-	/**
-	 * @param User $model
-	 * @psalm-suppress MoreSpecificImplementedParamType
-	 */
-	protected static function category(ModelWithContent|Language $model): string
-	{
-		// change the scope of the permissions,
-		// when the current user is this user
-		return static::user()->is($model) ? 'user' : 'users';
 	}
 }
