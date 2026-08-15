0dea03a3b7b22a437069409e4f5fb0974c8af069
Nico Hoffmann  ෴. <me@nhoffmann.com>
2026-08-15 17:15:24 +0200

    feat: Rename files, pages and users fields to filepicker, pagepicker and userpicker (#8382)

--- panel/src/components/Forms/Field/LegacyPagesField.vue
+++ panel/src/components/Forms/Field/LegacyPagesField.vue
@@ -6,7 +6,7 @@ import ModelsField from "./LegacyModelsField.vue";
  * @copyright Bastian Allgeier
  * @license   https://getkirby.com/license
  *
- * @deprecated 6.0.0 Use `k-pages-field` instead
+ * @deprecated 6.0.0 Use `k-pagepicker-field` instead
  */
 export default {
 	extends: ModelsField,
