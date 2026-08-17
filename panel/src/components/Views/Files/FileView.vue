06eb1667ae5b47ac5e7daaf47e89c939df037d1d
Bastian Allgeier <mail@bastianallgeier.com>
2026-08-17 15:38:56 +0200

    feat: Render model views with the model form

--- panel/src/components/Views/Files/FileView.vue
+++ panel/src/components/Views/Files/FileView.vue
@@ -43,17 +43,17 @@
 
 		<k-model-tabs :diff="diff" :tab="tab.name" :tabs="tabs" />
 
-		<k-sections
-			:blueprint="blueprint"
+		<k-model-form
+			:api="api"
+			:columns="tab.columns"
 			:content="content"
+			:diff="diff"
 			:empty="
 				$panel.config.debug
 					? $panel.html($t('file.blueprint', { blueprint: $esc(blueprint) }))
 					: null
 			"
 			:lock="lock"
-			:parent="api"
-			:tab="tab"
 			@input="onInput"
 			@submit="onSubmit"
 		/>
