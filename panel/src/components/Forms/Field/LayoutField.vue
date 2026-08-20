d0f4bfab2705645c209c1208a8c9d142fcc12dce
Nico Hoffmann <me@nhoffmann.com>
2026-08-20 19:37:39 +0200

    perf: `k-validator` for blocks and layout field

--- panel/src/components/Forms/Field/LayoutField.vue
+++ panel/src/components/Forms/Field/LayoutField.vue
@@ -26,16 +26,13 @@
 			</k-button-group>
 		</template>
 
-		<k-input-validator
-			v-bind="{ min, max, required }"
-			:value="JSON.stringify(value)"
-		>
+		<k-validator v-bind="{ min, max, required }" :count="value.length">
 			<k-layouts
 				ref="layouts"
 				v-bind="$props"
 				@input="$emit('input', $event)"
 			/>
-		</k-input-validator>
+		</k-validator>
 
 		<footer v-if="!disabled && hasFieldsets">
 			<k-button
