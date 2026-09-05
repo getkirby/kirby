b96de2ec20a582a91f1d3c6b0e8d44b7763c0b43
Nico Hoffmann <me@nhoffmann.com>
2026-09-05 14:30:22 +0200

    fix: Combine consecutive unsets in `Page::create()`

--- src/Cms/PageActions.php
+++ src/Cms/PageActions.php
@@ -507,15 +507,18 @@ trait PageActions
 	#[BlockCollectionAccess]
 	public static function create(array $props): Page
 	{
-		// Prevent injecting blueprint as this always must be derived from
-		// the template/model name and blueprint object in the app,
-		// never directly be supplied by the caller
-		unset($props['blueprint']);
+		unset(
+			// Prevent injecting blueprint as this always must be derived from
+			// the template/model name and blueprint object in the app,
+			// never directly be supplied by the caller
+			$props['blueprint'],
 
-		// Prevent injecting the root and dirname as those must always be
-		// derived from the parent and slug. Otherwise the caller could
-		// place the page's content at an arbitrary path on disk.
-		unset($props['dirname'], $props['root']);
+			// Prevent injecting the root and dirname as those must always be
+			// derived from the parent and slug. Otherwise the caller could
+			// place the page's content at an arbitrary path on disk.
+			$props['dirname'],
+			$props['root']
+		);
 
 		// clean up the slug
 		$props['slug']      = Url::slug($props['slug'] ?? $props['content']['title'] ?? null);
