aff07f3a71d1ef46efdd9b460affd77590137a4b
Nico Hoffmann  ෴. <me@nhoffmann.com>
2026-09-08 15:43:16 +0200

    fix: Hide protected models in picker fields (#138)

--- config/fields/users.php
+++ config/fields/users.php
@@ -69,7 +69,12 @@ return [
 				}
 
 				if ($id !== null && ($user = $kirby->user($id))) {
-					$users[] = $this->userResponse($user);
+					// never disclose a user the current user must not list,
+					// but keep its ID to not drop it from the value on save
+					$users[] = match ($user->isListable()) {
+						true  => $this->userResponse($user),
+						false => $this->protectedResponse($id)
+					};
 				}
 			}
 
