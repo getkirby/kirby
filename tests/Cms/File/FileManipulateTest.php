aced8c2b752495b792df56767115853377a8ce68
Ahmet Bora <ahmet@getkirby.com>
2026-09-16 12:10:56 +0300

    fix: Keep file format on upload resize #8489

--- tests/Cms/File/FileManipulateTest.php
+++ tests/Cms/File/FileManipulateTest.php
@@ -73,6 +73,63 @@ class FileManipulateTest extends ModelTestCase
 		$this->assertSame(100, $replacedFile->height());
 	}
 
+	public function testManipulateGlobalFormat(): void
+	{
+		$this->app = $this->app->clone([
+			'options' => [
+				'thumbs' => [
+					'format' => 'webp'
+				]
+			]
+		]);
+		$this->app->impersonate('kirby');
+
+		$parent       = new Page(['slug' => 'test']);
+		$originalFile = File::create([
+			'filename' => 'test.jpg',
+			'source'   => static::FIXTURES . '/test.jpg',
+			'parent'   => $parent
+		]);
+
+		$replacedFile = $originalFile->manipulate([
+			'width'  => 100,
+			'height' => 100
+		]);
+
+		$this->assertSame('jpg', $replacedFile->extension());
+		$this->assertSame('image/jpeg', $replacedFile->mime());
+		$this->assertSame(100, $replacedFile->width());
+	}
+
+	public function testManipulateGlobalFormatOverride(): void
+	{
+		$this->app = $this->app->clone([
+			'options' => [
+				'thumbs' => [
+					'format' => 'webp'
+				]
+			]
+		]);
+		$this->app->impersonate('kirby');
+
+		$parent       = new Page(['slug' => 'test']);
+		$originalFile = File::create([
+			'filename' => 'test.jpg',
+			'source'   => static::FIXTURES . '/test.jpg',
+			'parent'   => $parent
+		]);
+
+		$replacedFile = $originalFile->manipulate([
+			'width'  => 100,
+			'height' => 100,
+			'format' => 'webp'
+		]);
+
+		$this->assertSame('webp', $replacedFile->extension());
+		$this->assertSame('image/webp', $replacedFile->mime());
+		$this->assertSame(100, $replacedFile->width());
+	}
+
 	public function testManipulateInvalidValidFormat(): void
 	{
 		$parent       = new Page(['slug' => 'test']);
